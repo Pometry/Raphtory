21235f85ac2ba8285ea630de988f37e243502858
Miratepuffin <b.a.steer@qmul.ac.uk>
2017-10-19 16:17:46 +0100

    typo in variable

--- cluster/src/main/scala/com.gwz.dockerexp/caseclass/clustercase/RouterNode.scala
+++ cluster/src/main/scala/com.gwz.dockerexp/caseclass/clustercase/RouterNode.scala
@@ -7,7 +7,7 @@ import com.gwz.dockerexp.Actors.ClusterActors.{DocSvr}
 import akka.actor.Props
 import com.gwz.dockerexp.Actors.RaphtoryActors._
 
-case class RouterNode(,partitionNumber:String) extends DocSvr {
+case class RouterNode(seedLoc:String,partitionNumber:String) extends DocSvr {
   implicit val system = init(List(seedLoc))
   system.actorOf(Props(new RaphtoryRouter(partitionNumber.toInt)),"router")
 }
