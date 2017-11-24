1630728e8e938c7d318cc749dd01d11db71a66f3
Miratepuffin <b.a.steer@qmul.ac.uk>
2017-11-24 03:29:34 +0000

    Changed the internal IP address as singularity was binding to localhost which stops containers communicating

--- mainproject/cluster/src/main/scala/com.gwz.dockerexp/IpAndPort.scala
+++ mainproject/cluster/src/main/scala/com.gwz.dockerexp/IpAndPort.scala
@@ -4,25 +4,8 @@ import java.net._
 import java.io._
 import scala.util.Try
 
-/**
-Figure out IP and port mappings for this running Docker depending on the
-	deployment scenarios supported in the chart below:
-
-		Run Method			Run Env		Host IP 		Port Mappings
-		-------------------------------------------------------------------
-		docker run 			non-EC2		passed-in		passed-in
-		docker run 			non-EC2		passed-in		inspect agent
-
-		docker run 			EC2			passed-in		passed-in
-		docker run			EC2			passed-in		inspect agent
-
-		ECS ("ocean")		EC2			metadata svc	inspect agent
-  */
 
 object IpPort {
-  val AWS_LOCAL_HOST_IP  = "http://169.254.169.254/latest/meta-data/local-ipv4"
-  val AWS_PUBLIC_HOST_IP = "http://169.254.169.254/latest/meta-data/public-ipv4"
-  val INTERNAL_AKKA_PORT = 2552
 
   val ENV_HOST_IP        = "HOST_IP"
   val ENV_HOST_PORT      = "HOST_PORT"
