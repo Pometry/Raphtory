b8b476a0ac2f0f10d3367cff28443e2e5d19511d
miratepuffin <piratemuffin@hotmail.co.uk>
2018-04-15 15:11:15 +0100

    Created new branch for test and Gen of graph Janitor

---
+++ mainproject/cluster/src/main/scala/com/raphtory/tests/JanitorTest.scala
@@ -0,0 +1,8 @@
+package com.raphtory.tests
+
+object JanitorTest extends App{
+
+  val temporalMode = true //flag denoting if storage should focus on keeping more entities in memory or more history
+  
+
+}
