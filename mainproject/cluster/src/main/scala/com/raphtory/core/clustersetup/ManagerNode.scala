5235b3b479b8e604ebc5b3eed12f8cdd27feae5f
miratepuffin <piratemuffin@hotmail.co.uk>
2018-10-01 17:59:23 +0100

    Going home

--- mainproject/cluster/src/main/scala/com/raphtory/core/clustersetup/ManagerNode.scala
+++ mainproject/cluster/src/main/scala/com/raphtory/core/clustersetup/ManagerNode.scala
@@ -2,6 +2,7 @@ package com.raphtory.core.clustersetup
 
 import akka.actor.Props
 import com.raphtory.core.actors.RaphtoryReplicator
+import com.raphtory.core.storage.RaphtoryDBWrite
 
 import scala.language.postfixOps
 import scala.sys.process._
@@ -13,7 +14,11 @@ case class ManagerNode(seedLoc: String,partitionCount:Int)
 
   system.actorOf(Props(RaphtoryReplicator("Partition Manager",partitionCount)), s"PartitionManager")
 
-  "cassandra &"! //run cassandara in background on manager
+  "cassandra &" ! //run cassandara in background on manager
+
+  Thread.sleep(20000)
+  RaphtoryDBWrite.createDB()
+
   //"redis-server --daemonize yes" ! //start redis running on manager partition
 
 }
--- mainproject/cluster/src/main/scala/com/raphtory/core/clustersetup/SeedNode.scala
+++ mainproject/cluster/src/main/scala/com/raphtory/core/clustersetup/SeedNode.scala
@@ -7,5 +7,6 @@ import com.typesafe.config.{Config, ConfigFactory}
 case class SeedNode(seedLoc:String) extends DocSvr {
   val conf : Config = ConfigFactory.load()
   implicit val system = init(List(seedLoc))
+  println("Bleep Bloop I am a seed node")
   system.actorOf(Props(new SeedActor(this)), "cluster")
 }
