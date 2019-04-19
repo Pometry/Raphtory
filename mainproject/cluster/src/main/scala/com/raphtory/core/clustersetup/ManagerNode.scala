86042f9ed831bb6a0d3e3849a5ff99b1c6b7a222
miratepuffin <piratemuffin@hotmail.co.uk>
2019-04-20 00:53:17 +0100

    turned off cassandra for non saving test

--- mainproject/cluster/src/main/scala/com/raphtory/core/clustersetup/ManagerNode.scala
+++ mainproject/cluster/src/main/scala/com/raphtory/core/clustersetup/ManagerNode.scala
@@ -14,11 +14,12 @@ case class ManagerNode(seedLoc: String,partitionCount:Int)
 
   system.actorOf(Props(RaphtoryReplicator("Partition Manager",partitionCount)), s"PartitionManager")
   val saving: Boolean = System.getenv().getOrDefault("SAVING", "true").trim.toBoolean
+  if(saving){
+    Process("cassandra").lineStream //run cassandara in background on manager
+    Thread.sleep(5000)
+    RaphtoryDBWrite.createDB()
+  }
 
-  Process("cassandra").lineStream //run cassandara in background on manager
-
-  Thread.sleep(5000)
-  RaphtoryDBWrite.createDB()
 
   //"redis-server --daemonize yes" ! //start redis running on manager partition
 
