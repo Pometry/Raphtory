ac3420a9cb4951df0a330100fef0e1eb5007e271
miratepuffin <piratemuffin@hotmail.co.uk>
2019-09-04 15:49:21 +0100

    Spelling

--- mainproject/cluster/src/main/scala/com/raphtory/core/analysis/Managers/LiveManagers/BWindowedLiveAnalysisManager.scala
+++ mainproject/cluster/src/main/scala/com/raphtory/core/analysis/Managers/LiveManagers/BWindowedLiveAnalysisManager.scala
@@ -3,6 +3,6 @@ package com.raphtory.core.analysis.Managers.LiveManagers
 import com.raphtory.core.analysis.API.Analyser
 import com.raphtory.core.analysis.Managers.AnalysisManager
 
-class WindowedLiveAnalaysisManager(jobID:String,analyser:Analyser) extends AnalysisManager(jobID,analyser) {
+class BWindowedLiveAnalysisManager(jobID:String, analyser:Analyser) extends AnalysisManager(jobID,analyser) {
 
 }
--- mainproject/cluster/src/main/scala/com/raphtory/core/analysis/Managers/LiveManagers/WindowedLiveAnalysisManager.scala
+++ mainproject/cluster/src/main/scala/com/raphtory/core/analysis/Managers/LiveManagers/WindowedLiveAnalysisManager.scala
@@ -3,6 +3,6 @@ package com.raphtory.core.analysis.Managers.LiveManagers
 import com.raphtory.core.analysis.API.Analyser
 import com.raphtory.core.analysis.Managers.AnalysisManager
 
-class BWindowedLiveAnalaysisManager(jobID:String,analyser:Analyser) extends AnalysisManager(jobID,analyser) {
+class WindowedLiveAnalysisManager(jobID:String, analyser:Analyser) extends AnalysisManager(jobID,analyser) {
 
 }
