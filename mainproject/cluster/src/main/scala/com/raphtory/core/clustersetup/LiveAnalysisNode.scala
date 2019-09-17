a6b1fc5ee34952f8d44beffb67d307cdbe858268
miratepuffin <piratemuffin@hotmail.co.uk>
2019-09-17 17:47:44 +0100

    Refactor Analysis Actor for new API on cluster

--- mainproject/cluster/src/main/scala/com/raphtory/core/clustersetup/LiveAnalysisNode.scala
+++ mainproject/cluster/src/main/scala/com/raphtory/core/clustersetup/LiveAnalysisNode.scala
@@ -5,9 +5,9 @@ package com.raphtory.core.clustersetup
   */
 import akka.actor.{Actor, Props}
 import com.raphtory.core.analysis.API.Analyser
-import com.raphtory.core.analysis.Managers.RangeManagers.{RangeAnalysisManager, WindowedRangeAnalysisManager}
-import com.raphtory.core.analysis.Managers.ViewManagers.ViewAnalysisManager
-import com.raphtory.core.analysis.Managers.LiveManagers.LiveAnalysisManager
+import com.raphtory.core.analysis.Managers.RangeManagers.{BWindowedRangeAnalysisManager, RangeAnalysisManager, WindowedRangeAnalysisManager}
+import com.raphtory.core.analysis.Managers.ViewManagers.{BWindowedViewAnalysisManager, ViewAnalysisManager, WindowedViewAnalysisManager}
+import com.raphtory.core.analysis.Managers.LiveManagers.{BWindowedLiveAnalysisManager, LiveAnalysisManager, WindowedLiveAnalysisManager}
 
 case class LiveAnalysisNode(seedLoc: String, name:String)
     extends DocSvr {
@@ -19,11 +19,33 @@ case class LiveAnalysisNode(seedLoc: String, name:String)
 
   sys.env.getOrElse("LAMTYPE", "LAM").toString match {
     case "LAM" => { // live graph
-      system.actorOf(Props(new LiveAnalysisManager(jobID,analyser)), s"LiveAnalysisManager_$name")
+      sys.env.getOrElse("WINDOWING", "false") match {
+        case "false" => {
+          system.actorOf(Props(new LiveAnalysisManager(jobID,analyser)), s"LiveAnalysisManager_$name")
+        }
+        case "true" => {
+          system.actorOf(Props(new WindowedLiveAnalysisManager(jobID,analyser)), s"LiveAnalysisManager_$name")
+        }
+        case "batched" => {
+          system.actorOf(Props(new BWindowedLiveAnalysisManager(jobID,analyser)), s"LiveAnalysisManager_$name")
+        }
+      }
     }
     case "VAM" => { //view of the graph
       val time = sys.env.getOrElse("TIMESTAMP", "0").toLong
-      system.actorOf(Props(new ViewAnalysisManager(jobID,analyser,time)), s"ViewAnalysisManager_$name")
+      sys.env.getOrElse("WINDOWING", "false") match {
+        case "false" => {
+          system.actorOf(Props(new ViewAnalysisManager(jobID,analyser,time)), s"ViewAnalysisManager_$name")
+        }
+        case "true" => {
+          val window = sys.env.getOrElse("WINDOW", "0").toLong
+          system.actorOf(Props(new WindowedViewAnalysisManager(jobID,analyser,time,window)), s"ViewAnalysisManager_$name")
+        }
+        case "batched" => {
+          val windowset = sys.env.getOrElse("WINDOWSET", "0").split(",").map(f=>f.toLong)
+          system.actorOf(Props(new BWindowedViewAnalysisManager(jobID,analyser,time,windowset)), s"ViewAnalysisManager_$name")        }
+      }
+
     }
     case "RAM" => { //range query through history
       val start = sys.env.getOrElse("START", "0").toLong
@@ -35,8 +57,18 @@ case class LiveAnalysisNode(seedLoc: String, name:String)
       val start = sys.env.getOrElse("START", "0").toLong
       val end = sys.env.getOrElse("END", "0").toLong
       val jump = sys.env.getOrElse("JUMP", "0").toLong
-      val window = sys.env.getOrElse("WINDOW", "0").toLong
-      system.actorOf(Props(new WindowedRangeAnalysisManager(jobID,analyser,start,end,jump,window)), s"WindowAnalysisManager_$name")
+      sys.env.getOrElse("WINDOWING", "false") match {
+        case "false" => {
+          system.actorOf(Props(new RangeAnalysisManager(jobID,analyser,start,end,jump)), s"ViewAnalysisManager_$name")
+        }
+        case "true" => {
+          val window = sys.env.getOrElse("WINDOW", "0").toLong
+          system.actorOf(Props(new WindowedRangeAnalysisManager(jobID,analyser,start,end,jump,window)), s"ViewAnalysisManager_$name")
+        }
+        case "batched" => {
+          val windowset = sys.env.getOrElse("WINDOWSET", "0").split(",").map(f=>f.toLong)
+          system.actorOf(Props(new BWindowedRangeAnalysisManager(jobID,analyser,start,end,jump,windowset)), s"ViewAnalysisManager_$name")        }
+      }
     }
 
   }
