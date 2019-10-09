34b5a840d61d66701c2495bd28ecb7aa9b315535
miratepuffin <piratemuffin@hotmail.co.uk>
2019-10-09 11:00:24 +0100

    Added single node component

--- mainproject/cluster/src/main/scala/com/raphtory/core/clustersetup/singlenode/SingleNodeSetup.scala
+++ mainproject/cluster/src/main/scala/com/raphtory/core/clustersetup/singlenode/SingleNodeSetup.scala
@@ -5,7 +5,10 @@ import com.raphtory.core.components.ClusterManagement.{RaphtoryReplicator, SeedA
 import com.raphtory.core.clustersetup.DocSvr
 import com.raphtory.core.storage.RaphtoryDBWrite
 import com.typesafe.config.{Config, ConfigFactory}
-
+import com.raphtory.core.analysis.API.Analyser
+import com.raphtory.core.analysis.Managers.RangeManagers.{BWindowedRangeAnalysisManager, RangeAnalysisManager, WindowedRangeAnalysisManager}
+import com.raphtory.core.analysis.Managers.ViewManagers.{BWindowedViewAnalysisManager, ViewAnalysisManager, WindowedViewAnalysisManager}
+import com.raphtory.core.analysis.Managers.LiveManagers.{BWindowedLiveAnalysisManager, LiveAnalysisManager, WindowedLiveAnalysisManager}
 import scala.language.postfixOps
 import scala.sys.process._
 
@@ -13,14 +16,67 @@ case class SingleNodeSetup(seedLoc:String,routerClassName:String,UpdaterName:Str
   val conf : Config = ConfigFactory.load()
   implicit val system = init(List(seedLoc))
   //"redis-server --daemonize yes" ! //start redis running on manager partition
-  Process("cassandra").lineStream //run cassandara in background on manager
-  Thread.sleep(5000)
-  RaphtoryDBWrite.createDB()
+  //Process("cassandra").lineStream //run cassandara in background on manager
+  //Thread.sleep(5000)
+  //RaphtoryDBWrite.createDB()
   system.actorOf(Props(new SeedActor(this)), "cluster")
   system.actorOf(Props(new WatchDog(partitionNumber,minimumRouters)), "WatchDog")
   system.actorOf(Props(RaphtoryReplicator("Router",1, routerClassName)), s"Routers")
   system.actorOf(Props(RaphtoryReplicator("Partition Manager",1)), s"PartitionManager")
   system.actorOf(Props(Class.forName(UpdaterName)), "UpdateGen")
- // system.actorOf(Props(Class.forName(LAMName)), s"LiveAnalysisManager_$LAMName")
 
+  val jobID = sys.env.getOrElse("JOBID", "Default").toString
+  val analyser = Class.forName(LAMName).newInstance().asInstanceOf[Analyser]
+
+  Thread.sleep(15000)
+
+  sys.env.getOrElse("LAMTYPE", "Live").toString match {
+    case "Live" => { // live graph
+      sys.env.getOrElse("WINDOWTYPE", "false") match {
+        case "false" => {
+          system.actorOf(Props(new LiveAnalysisManager(jobID, analyser)), s"LiveAnalysisManager_$LAMName")
+        }
+        case "true" => {
+          system.actorOf(Props(new WindowedLiveAnalysisManager(jobID, analyser)), s"LiveAnalysisManager_$LAMName")
+        }
+        case "batched" => {
+          system.actorOf(Props(new BWindowedLiveAnalysisManager(jobID, analyser)), s"LiveAnalysisManager_$LAMName")
+        }
+      }
+    }
+    case "View" => { //view of the graph
+      val time = sys.env.getOrElse("TIMESTAMP", "0").toLong
+      sys.env.getOrElse("WINDOWTYPE", "false") match {
+        case "false" => {
+          system.actorOf(Props(new ViewAnalysisManager(jobID, analyser, time)), s"ViewAnalysisManager_$LAMName")
+        }
+        case "true" => {
+          val window = sys.env.getOrElse("WINDOW", "0").toLong
+          system.actorOf(Props(new WindowedViewAnalysisManager(jobID, analyser, time, window)), s"ViewAnalysisManager_$LAMName")
+        }
+        case "batched" => {
+          val windowset = sys.env.getOrElse("WINDOWSET", "0").split(",").map(f => f.toLong)
+          system.actorOf(Props(new BWindowedViewAnalysisManager(jobID, analyser, time, windowset)), s"ViewAnalysisManager_$LAMName")
+        }
+      }
+    }
+    case "Range" => { // windowed range query through history
+      val start = sys.env.getOrElse("START", "0").toLong
+      val end = sys.env.getOrElse("END", "0").toLong
+      val jump = sys.env.getOrElse("JUMP", "0").toLong
+      sys.env.getOrElse("WINDOWTYPE", "false") match {
+        case "false" => {
+          system.actorOf(Props(new RangeAnalysisManager(jobID, analyser, start, end, jump)), s"RangeAnalysisManager_$LAMName")
+        }
+        case "true" => {
+          val window = sys.env.getOrElse("WINDOW", "0").toLong
+          system.actorOf(Props(new WindowedRangeAnalysisManager(jobID, analyser, start, end, jump, window)), s"RangeAnalysisManager_$LAMName")
+        }
+        case "batched" => {
+          val windowset = sys.env.getOrElse("WINDOWSET", "0").split(",").map(f => f.toLong)
+          system.actorOf(Props(new BWindowedRangeAnalysisManager(jobID, analyser, start, end, jump, windowset)), s"RangeAnalysisManager_$LAMName")
+        }
+      }
+    }
+  }
 }
