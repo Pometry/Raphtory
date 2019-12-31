06779009e2793815255e3a46bb2276d20ec8274d
miratepuffin <piratemuffin@hotmail.co.uk>
2019-12-31 17:49:41 +0000

    Added Parsing for Dashcoin

--- mainproject/cluster/src/main/scala/com/raphtory/tests/SingleNodeTest.scala
+++ mainproject/cluster/src/main/scala/com/raphtory/tests/SingleNodeTest.scala
@@ -20,8 +20,8 @@ object SingleNodeTest extends App {
   val minimumRouters = 1
 
 
- var UpdaterName = "com.raphtory.examples.blockchain.litecoin.actors.LitecoinSpout"
- var routerClassName = "com.raphtory.examples.blockchain.litecoin.actors.LitecoinRouter"
+ var UpdaterName = "com.raphtory.examples.blockchain.dashcoin.actors.DashcoinSpout"
+ var routerClassName = "com.raphtory.examples.blockchain.dashcoin.actors.DashcoinRouter"
  val Analyser = "com.raphtory.core.analysis.Algorithms.ConnectedComponents"
  //var UpdaterName = "com.raphtory.examples.ethereum.actors.EthereumPostgresSpout"
  //var routerClassName = "com.raphtory.examples.ethereum.actors.EthereumTransactionRouter"
