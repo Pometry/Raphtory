a556df3f3563279f8217cdff62c6e837c630eae4
miratepuffin <piratemuffin@hotmail.co.uk>
2020-01-24 14:34:21 +0000

    Changed git example spout to use file name env variable

--- mainproject/cluster/src/main/scala/com/raphtory/examples/gab/actors/GabExampleSpout.scala
+++ mainproject/cluster/src/main/scala/com/raphtory/examples/gab/actors/GabExampleSpout.scala
@@ -13,10 +13,8 @@ class GabExampleSpout extends SpoutTrait {
 
 
   val directory = System.getenv().getOrDefault("GAB_DIRECTORY", "/app").trim
-
-
-
-  val fileLines = io.Source.fromFile(directory+"/gabNetwork500.csv").getLines.drop(1).toArray
+  val file_name = System.getenv().getOrDefault("GAB_FILE_NAME", "gabNetwork500.csv").trim
+  val fileLines = io.Source.fromFile(directory+"/"+file_name).getLines.drop(1).toArray
 // upstream/master
   var position = 0
   var linesNumber=fileLines.length
