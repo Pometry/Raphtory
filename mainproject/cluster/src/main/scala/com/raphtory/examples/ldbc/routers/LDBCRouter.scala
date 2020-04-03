c6d7261a8528475f8f02aae5dd524f195b46d225
miratepuffin <piratemuffin@hotmail.co.uk>
2020-04-03 19:59:09 +0100

    Small LDBC changes

--- mainproject/cluster/src/main/scala/com/raphtory/examples/ldbc/routers/LDBCRouter.scala
+++ mainproject/cluster/src/main/scala/com/raphtory/examples/ldbc/routers/LDBCRouter.scala
@@ -9,6 +9,7 @@ import com.raphtory.core.model.communication.EdgeDelete
 import com.raphtory.core.model.communication.Type
 import com.raphtory.core.model.communication.VertexAdd
 import com.raphtory.core.model.communication.VertexDelete
+import com.raphtory.examples.random.actors.RandomSpout
 
 class LDBCRouter(routerId: Int, override val initialManagerCount: Int) extends RouterWorker {
   override protected def parseTuple(value: Any): Unit = {
@@ -18,11 +19,14 @@ class LDBCRouter(routerId: Int, override val initialManagerCount: Int) extends R
     val date2              = fileLine(2).substring(0, 10) + fileLine(1).substring(11, 23); //extract the day of the event
     val creationDate: Long = new SimpleDateFormat("yyyy-MM-ddHH:mm:ss.SSS").parse(date).getTime()
     val deletionDate: Long = new SimpleDateFormat("yyyy-MM-ddHH:mm:ss.SSS").parse(date2).getTime()
+    val vertexDeletion = sys.env.getOrElse("LDBC_VERTEX_DELETION", "false").trim.toBoolean
+    val edgeDeletion = sys.env.getOrElse("LDBC_EDGE_DELETION", "false").trim.toBoolean
     fileLine(0) match {
       case "person" =>
         sendGraphUpdate(VertexAdd(creationDate, assignID("person" + fileLine(3)), Type("person")))
         //sendGraphUpdate(VertexAdd(creationDate, fileLine(3).toLong,Type("person")))
-        sendGraphUpdate(VertexDelete(deletionDate, assignID("person" + fileLine(3))))
+        if(vertexDeletion)
+          sendGraphUpdate(VertexDelete(deletionDate, assignID("person" + fileLine(3))))
       case "person_knows_person" =>
         //sendGraphUpdate(EdgeAdd(creationDate, fileLine(3).toLong,fileLine(4).toLong,Type("person_knows_person")))
         sendGraphUpdate(
@@ -33,7 +37,8 @@ class LDBCRouter(routerId: Int, override val initialManagerCount: Int) extends R
                         Type("person_knows_person")
                 )
         )
-      //sendGraphUpdate(EdgeDelete(deletionDate, assignID("person"+fileLine(3)),assignID("person"+fileLine(4))))
+        if(edgeDeletion)
+          sendGraphUpdate(EdgeDelete(deletionDate, assignID("person"+fileLine(3)),assignID("person"+fileLine(4))))
     }
   }
 }
