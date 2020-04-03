4d8bee120aa62a9ba55fab9c148fca636e8ce332
miratepuffin <piratemuffin@hotmail.co.uk>
2020-04-03 04:10:38 +0100

    Fixed the rest api

--- mainproject/cluster/src/main/scala/com/raphtory/core/clustersetup/LiveAnalysisNode.scala
+++ mainproject/cluster/src/main/scala/com/raphtory/core/clustersetup/LiveAnalysisNode.scala
@@ -3,49 +3,13 @@ package com.raphtory.core.clustersetup
 /**
   * Created by Mirate on 29/09/2017.
   */
-import akka.actor.ActorRef
-import akka.actor.ActorSystem
+
 import akka.actor.Props
-import com.raphtory.core.analysis.API.Analyser
-import com.raphtory.core.analysis.Tasks.AnalysisTask
-import com.raphtory.core.analysis.Tasks.LiveTasks.BWindowedLiveAnalysisTask
-import com.raphtory.core.analysis.Tasks.LiveTasks.LiveAnalysisTask
-import com.raphtory.core.analysis.Tasks.LiveTasks.WindowedLiveAnalysisTask
-import com.raphtory.core.analysis.Tasks.RangeTasks.BWindowedRangeAnalysisTask
-import com.raphtory.core.analysis.Tasks.RangeTasks.RangeAnalysisTask
-import com.raphtory.core.analysis.Tasks.RangeTasks.WindowedRangeAnalysisTask
-import com.raphtory.core.analysis.Tasks.ViewTasks.BWindowedViewAnalysisTask
-import com.raphtory.core.analysis.Tasks.ViewTasks.ViewAnalysisTask
-import com.raphtory.core.analysis.Tasks.ViewTasks.WindowedViewAnalysisTask
-
-import akka.actor.Actor
-import akka.actor.Props
-import com.raphtory.core.analysis.API.Analyser
-import com.raphtory.core.analysis.Tasks.RangeTasks.BWindowedRangeAnalysisTask
-import com.raphtory.core.analysis.Tasks.RangeTasks.RangeAnalysisTask
-import com.raphtory.core.analysis.Tasks.RangeTasks.WindowedRangeAnalysisTask
-import com.raphtory.core.analysis.Tasks.ViewTasks.BWindowedViewAnalysisTask
-import com.raphtory.core.analysis.Tasks.ViewTasks.ViewAnalysisTask
-import com.raphtory.core.analysis.Tasks.ViewTasks.WindowedViewAnalysisTask
-import com.raphtory.core.analysis.Tasks.LiveTasks.BWindowedLiveAnalysisTask
-import com.raphtory.core.analysis.Tasks.LiveTasks.LiveAnalysisTask
-import com.raphtory.core.analysis.Tasks.LiveTasks.WindowedLiveAnalysisTask
-import akka.actor._
-import akka.http.scaladsl.Http
-import akka.http.scaladsl.model.HttpMethods._
-import akka.http.scaladsl.model.{HttpRequest, HttpResponse, _}
-import akka.pattern.ask
-import akka.stream.ActorMaterializer
-import akka.stream.scaladsl.{Sink, Source}
-import akka.util.Timeout
-import com.typesafe.config.ConfigFactory
-
-import scala.concurrent.ExecutionContext.Implicits.global
-import scala.concurrent.duration._
-import scala.concurrent.{Await, Future}
-
-case class LiveAnalysisNode(seedLoc: String, name: String) extends DocSvr {
+import com.raphtory.core.analysis.{AnalysisManager, AnalysisRestApi}
+case class LiveAnalysisNode(seedLoc: String) extends DocSvr {
   implicit val system = initialiseActorSystem(List(seedLoc))
+  system.actorOf(Props[AnalysisManager], s"AnalysisManager")
+  AnalysisRestApi(system)
 }
 
 
--- mainproject/cluster/src/main/scala/com/raphtory/core/clustersetup/singlenode/SingleNodeSetup.scala
+++ mainproject/cluster/src/main/scala/com/raphtory/core/clustersetup/singlenode/SingleNodeSetup.scala
@@ -1,7 +1,8 @@
 package com.raphtory.core.clustersetup.singlenode
 
-import akka.actor.Props
+import akka.actor.{ActorSystem, Props}
 import com.raphtory.core.analysis.API.Analyser
+import com.raphtory.core.analysis.{AnalysisManager, AnalysisRestApi}
 import com.raphtory.core.analysis.Tasks.LiveTasks.BWindowedLiveAnalysisTask
 import com.raphtory.core.analysis.Tasks.LiveTasks.LiveAnalysisTask
 import com.raphtory.core.analysis.Tasks.LiveTasks.WindowedLiveAnalysisTask
@@ -15,6 +16,7 @@ import com.raphtory.core.clustersetup.DocSvr
 import com.raphtory.core.components.ClusterManagement.RaphtoryReplicator
 import com.raphtory.core.components.ClusterManagement.SeedActor
 import com.raphtory.core.components.ClusterManagement.WatchDog
+import com.raphtory.tests.SingleNodeTest.{UpdaterName, minimumRouters, partitionNumber, routerClassName}
 import com.typesafe.config.Config
 import com.typesafe.config.ConfigFactory
 
@@ -23,23 +25,16 @@ import scala.language.postfixOps
 case class SingleNodeSetup(
     seedLoc: String,
     routerClassName: String,
-    UpdaterName: String,
-    LAMName: String,
-    partitionNumber: Int,
-    minimumRouters: Int
+    UpdaterName: String
 ) extends DocSvr {
   val conf: Config    = ConfigFactory.load()
   implicit val system = initialiseActorSystem(List(seedLoc))
-
+  AnalysisRestApi(system)
   system.actorOf(Props(new SeedActor(this)), "cluster")
   system.actorOf(Props(new WatchDog(partitionNumber, minimumRouters)), "WatchDog")
   system.actorOf(Props(RaphtoryReplicator("Router", 1, routerClassName)), s"Routers")
   system.actorOf(Props(RaphtoryReplicator("Partition Manager", 1)), s"PartitionManager")
   system.actorOf(Props(Class.forName(UpdaterName)), "UpdateGen")
-
-
-
-  Thread.sleep(30000)
-
+  system.actorOf(Props[AnalysisManager], s"AnalysisManager")
 
 }
