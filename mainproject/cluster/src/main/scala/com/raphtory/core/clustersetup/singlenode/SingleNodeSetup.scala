a93c226c0a0dbf10ec2595bf0e62bdcf367ad3cd
miratepuffin <piratemuffin@hotmail.co.uk>
2020-06-03 02:49:47 +0100

    Sync with fixed dashboards

--- mainproject/cluster/src/main/scala/com/raphtory/core/clustersetup/singlenode/SingleNodeSetup.scala
+++ mainproject/cluster/src/main/scala/com/raphtory/core/clustersetup/singlenode/SingleNodeSetup.scala
@@ -27,7 +27,6 @@ case class SingleNodeSetup(
     routerClassName: String,
     UpdaterName: String
 ) extends DocSvr {
-  println("hello2")
   val conf: Config    = ConfigFactory.load()
   implicit val system = initialiseActorSystem(List(seedLoc))
   AnalysisRestApi(system)
@@ -36,7 +35,6 @@ case class SingleNodeSetup(
   system.actorOf(Props(RaphtoryReplicator("Router", 1, routerClassName)), s"Routers")
   system.actorOf(Props(RaphtoryReplicator("Partition Manager", 1)), s"PartitionManager")
   system.actorOf(Props[AnalysisManager], s"AnalysisManager")
-  println("hello")
 
   system.actorOf(Props(Class.forName(UpdaterName)), "Spout")
 
