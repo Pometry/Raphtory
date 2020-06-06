5ff4a930501d187e6e45990f28ee30d6fbbbf0f1
miratepuffin <piratemuffin@hotmail.co.uk>
2020-06-06 01:17:32 +0100

    Finished new watermarking recording

--- mainproject/cluster/src/main/scala/com/raphtory/core/clustersetup/WatchDogNode.scala
+++ mainproject/cluster/src/main/scala/com/raphtory/core/clustersetup/WatchDogNode.scala
@@ -5,11 +5,10 @@ package com.raphtory.core.clustersetup
   */
 import akka.actor.ActorSystem
 import akka.actor.Props
-import com.raphtory.core.components.ClusterManagement.WatchDog
+import com.raphtory.core.components.ClusterManagement.{WatchDog, WatermarkManager}
 
 case class WatchDogNode(seedLoc: String, partitionNumber: Int, minimumRouters: Int) extends DocSvr {
   implicit val system: ActorSystem = initialiseActorSystem(seeds = List(seedLoc))
-
-  val actorName: String = "WatchDog"
-  system.actorOf(Props(new WatchDog(managerCount = partitionNumber, minimumRouters = minimumRouters)), actorName)
+  system.actorOf(Props(new WatermarkManager(managerCount = 1)),"WatermarkManager")
+  system.actorOf(Props(new WatchDog(managerCount = partitionNumber, minimumRouters = minimumRouters)), "WatchDog")
 }
