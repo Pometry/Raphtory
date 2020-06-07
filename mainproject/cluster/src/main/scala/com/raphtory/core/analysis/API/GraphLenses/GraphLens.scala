36bf0f6e573e884f63b86dabca549e583f78b61f
miratepuffin <piratemuffin@hotmail.co.uk>
2020-06-07 17:10:28 +0100

    Fixed old bug where messages between readers were not recorded properly

--- mainproject/cluster/src/main/scala/com/raphtory/core/analysis/API/GraphLenses/GraphLens.scala
+++ mainproject/cluster/src/main/scala/com/raphtory/core/analysis/API/GraphLenses/GraphLens.scala
@@ -5,7 +5,7 @@ import com.raphtory.core.analysis.API.ManagerCount
 import com.raphtory.core.analysis.API.entityVisitors.VertexVisitor
 import com.raphtory.core.model.graphentities.Vertex
 import com.raphtory.core.storage.EntityStorage
-import monix.execution.atomic.AtomicInt
+import java.util.concurrent.atomic.AtomicInteger
 
 import scala.collection.parallel.mutable.ParTrieMap
 
@@ -18,12 +18,10 @@ abstract class GraphLens(
     storage: EntityStorage,
     managerCount: ManagerCount
 ) {
-  private var messages = AtomicInt(0)
-  //val messageQueues:ParTrieMap[String,ConcurrentHashMap.KeySetView[(Long,Long,Any),java.lang.Boolean]] = ParTrieMap[String,ConcurrentHashMap.KeySetView[(Long,Long,Any),java.lang.Boolean]]()
-  //Utils.getAllReaders(managerCount.count).foreach(reader => messageQueues put (reader,java.util.concurrent.ConcurrentHashMap.newKeySet[(Long,Long,Any)]()))
+  private val messages = new AtomicInteger(0)
 
-  protected var voteCount                        = AtomicInt(0)
-  def job()                                      = jobID
+  protected var voteCount                             = new AtomicInteger(0)
+  def job()                                   = jobID
   def superStep()                                = superstep
   def getVerticesSet(): ParTrieMap[Long, Vertex] = storage.vertices
 
@@ -32,7 +30,7 @@ abstract class GraphLens(
   }
 
   def recordMessage(sourceID: Long, vertexID: Long, data: Any) =
-    messages.increment()
+    messages.incrementAndGet()
 
   def getMessages() = messages.get
 
@@ -42,11 +40,12 @@ abstract class GraphLens(
   def getTotalVerticesSet() =
     storage.vertices.keySet
 
-  def latestTime: Long = storage.newestTime
+  def latestTime: Long =
+    storage.newestTime
 
-  def vertexVoted() = voteCount.increment()
+  def vertexVoted() =
+    voteCount.incrementAndGet()
 
   def checkVotes(workerID: Int): Boolean =
-    //
     storage.vertices.size == voteCount.get
 }
