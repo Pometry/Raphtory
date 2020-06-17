0c452a7391da28315630c51a7347000864b5ed5e
Naomi Arnold <n.a.arnold@qmul.ac.uk>
2020-06-17 19:20:53 +0100

    Adding stack exchange spout, router and test file

---
+++ mainproject/cluster/src/main/scala/com/raphtory/examples/stackex/SXSpout.scala
@@ -0,0 +1,39 @@
+package com.raphtory.examples.stackex
+
+import java.time.LocalDateTime
+
+import com.raphtory.core.components.Spout.SpoutTrait
+
+import scala.concurrent.duration._
+import scala.io
+import scala.language.postfixOps
+
+class SXSpout extends SpoutTrait {
+
+  val directory = System.getenv().getOrDefault("SX_DIRECTORY", "/app").trim
+  val file_name = System.getenv().getOrDefault("SX_FILE_NAME", "sx_reordered.txt").trim
+  val fileLines = io.Source.fromFile(directory + "/" + file_name).getLines.drop(1).toArray
+  // upstream/master
+  var position    = 0
+  var linesNumber = fileLines.length
+  println("Start: " + LocalDateTime.now())
+
+  protected def ProcessSpoutTask(message: Any): Unit = message match {
+    case StartSpout => AllocateSpoutTask(Duration(1, NANOSECONDS), "newLine")
+    case "newLine" =>
+      try {
+        if (position < linesNumber) {
+          for (i <- 1 to 100) {
+            var line = fileLines(position)
+            sendTuple(line)
+            position += 1
+          }
+          AllocateSpoutTask(Duration(1, NANOSECONDS), "newLine")
+        }
+        else {
+          println("Finished ingestion")
+        }
+      }catch {case e:Exception => println("Finished ingestion")}
+    case _ => println("message not recognized!")
+  }
+}
