a58b1cd80d333689fb5e6e5aafb9de36e4056c01
miratepuffin <piratemuffin@hotmail.co.uk>
2020-06-21 19:19:47 +0100

    Added back pressure to kafka spout

--- mainproject/cluster/src/main/scala/com/raphtory/tests/SingleNodeTest.scala
+++ mainproject/cluster/src/main/scala/com/raphtory/tests/SingleNodeTest.scala
@@ -44,9 +44,9 @@ object SingleNodeTest extends App {
     val end =   31525368897000L
 
     val jump =    3600000
-    var SpoutName = "com.raphtory.examples.gab.actors.GabExampleSpout"
+   // var SpoutName = "com.raphtory.examples.gab.actors.GabExampleSpout"
     //var SpoutName = "com.raphtory.examples.random.actors.RandomSpout"
-    var routerClassName = "com.raphtory.examples.gab.actors.GabUserGraphRouter"
+   // var routerClassName = "com.raphtory.examples.gab.actors.GabUserGraphRouter"
     //var routerClassName = "com.raphtory.examples.random.actors.RandomRouter"
 
 
@@ -57,7 +57,9 @@ object SingleNodeTest extends App {
 
 //chainalysisAB
 //  var UpdaterName = "com.raphtory.examples.blockchain.spouts.ChainalysisABSpout"
+  var SpoutName = "com.raphtory.spouts.KafkaSpout"
 //  var routerClassName = "com.raphtory.examples.blockchain.routers.ChainalysisABRouter"
+  var routerClassName = "com.raphtory.examples.blockchain.routers.EthereumKafkaRouter"
 
   val system = ActorSystem("Single-Node-test")
 
