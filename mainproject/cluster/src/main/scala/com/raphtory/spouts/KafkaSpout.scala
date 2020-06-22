30e7b1f21d3bf13dcae6b12009b00c3f74bba814
miratepuffin <piratemuffin@hotmail.co.uk>
2020-06-22 13:17:18 +0100

    Router Running Poorly, need to investigate why

--- mainproject/cluster/src/main/scala/com/raphtory/spouts/KafkaSpout.scala
+++ mainproject/cluster/src/main/scala/com/raphtory/spouts/KafkaSpout.scala
@@ -22,13 +22,14 @@ import com.raphtory.core.utils.SchedulerUtil
 
 import scala.util.Random
 class KafkaSpout extends SpoutTrait {
+  println("Starting kafka")
   var kafkaServer     = System.getenv().getOrDefault("KAFKA_ADDRESS", "127.0.0.1").trim
   var kafkaIP     = System.getenv().getOrDefault("KAFKA_PORT", "9092").trim
   var offset     = System.getenv().getOrDefault("KAFKA_OFFSET", "earliest").trim
   val x     = new Random().nextLong()
   var groupID   = System.getenv().getOrDefault("KAFKA_GROUP", "group" + x).trim
   var topic   = System.getenv().getOrDefault("KAFKA_TOPIC", "sample_topic").trim
-  var restart   = System.getenv().getOrDefault("RESTART_RATE", "1").trim
+  var restart   = System.getenv().getOrDefault("RESTART_RATE", "10").trim
 
   val queue = new LinkedBlockingQueue[String]
 
@@ -42,7 +43,7 @@ class KafkaSpout extends SpoutTrait {
   consumer.subscribe(util.Arrays.asList(topic))
 
 
-  val helper = context.system.actorOf(Props(new KafkaSpoutBackPressure(queue)), "Spout_Helper")
+  //val helper = context.system.actorOf(Props(new KafkaSpoutBackPressure(queue)), "Spout_Helper")
 
   protected def ProcessSpoutTask(message: Any): Unit = message match {
     case StartSpout => AllocateSpoutTask(Duration(1, MILLISECONDS), "newLine")
@@ -52,13 +53,15 @@ class KafkaSpout extends SpoutTrait {
 
   def consumeFromKafka() = {
     //println("Consuming")
-    val record = consumer.poll(java.time.Duration.ofMillis(1000)).asScala
-    for (data <- record.iterator)
-      queue.put(data.value())
-    AllocateSpoutTask(Duration(restart.toInt, MICROSECONDS), "newLine")
+    val record = consumer.poll(java.time.Duration.ofMillis(5000)).asScala
+    for (data <- record.iterator) {
+      sendTuple(data.value())
+      //helper ! KafkaData(data.value())
+    }
+    AllocateSpoutTask(Duration(restart.toInt, MILLISECONDS), "newLine")
   }
 }
-
+case class KafkaData(data:String)
 class KafkaSpoutBackPressure(queue:LinkedBlockingQueue[String]) extends SpoutTrait {
   var startingSpeed     = System.getenv().getOrDefault("STARTING_SPEED", "1000").trim.toInt
   var increaseBy        = System.getenv().getOrDefault("INCREASE_BY", "1000").trim.toInt
@@ -68,16 +71,17 @@ class KafkaSpoutBackPressure(queue:LinkedBlockingQueue[String]) extends SpoutTra
   }
   override protected def ProcessSpoutTask(receivedMessage: Any): Unit = receivedMessage match {
     case StartSpout => AllocateSpoutTask(Duration(1, MILLISECONDS), "newLine")
+    case KafkaData(data) => queue.put(data)
     case "newLine"  => consumeFromQueue
     case "increase"  => startingSpeed+=increaseBy
     case _          => println("message not recognized!")
   }
   def consumeFromQueue() = {
-    for(i<-0 to startingSpeed/1000){
+    for(i<-0 to startingSpeed/100){
       if(!queue.isEmpty) {
         sendTuple(queue.take())
       }
     }
-    AllocateSpoutTask(Duration(1, MILLISECONDS), "newLine")
+    AllocateSpoutTask(Duration(10, MILLISECONDS), "newLine")
   }
 }
