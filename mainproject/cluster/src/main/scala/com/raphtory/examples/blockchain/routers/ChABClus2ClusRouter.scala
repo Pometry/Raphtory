f80126bd42bf656384c01faa8912a2b793df4820
imanehaf <hafmane@gmail.com>
2020-07-09 12:18:58 +0100

    fixed CHAB spout to read multiple files

--- mainproject/cluster/src/main/scala/com/raphtory/examples/blockchain/routers/ChABClus2ClusRouter.scala
+++ mainproject/cluster/src/main/scala/com/raphtory/examples/blockchain/routers/ChABClus2ClusRouter.scala
@@ -1,7 +1,5 @@
 package com.raphtory.examples.blockchain.routers
 
-import java.text.SimpleDateFormat
-
 import com.raphtory.core.components.Router.RouterWorker
 import com.raphtory.core.model.communication.Type
 import com.raphtory.core.model.communication._
@@ -9,27 +7,30 @@ import com.raphtory.core.model.communication._
 class ChABClus2ClusRouter(override val routerId: Int,override val workerID:Int, override val initialManagerCount: Int) extends RouterWorker {
 
   def parseTuple(record: Any): Unit = {
-    val dp = formatLine(record.asInstanceOf[String].split(",").map(_.trim))
-    val transactionTime = dp.time
-    val srcClusterId = dp.srcCluster
-    val dstClusterId = dp.dstCluster
-    val transactionId = dp.txid
-    val btcAmount = dp.amount
-    val usdAmount = dp.usd
+    val lines = record.asInstanceOf[String].split("\n").map(_.trim)
+    for(line <- lines){
+      val dp = formatLine(line.split(",").map(_.trim))
+      val transactionTime = dp.time
+      val srcClusterId = dp.srcCluster
+      val dstClusterId = dp.dstCluster
+      val transactionId = dp.txid
+      val btcAmount = dp.amount
+      val usdAmount = dp.usd
 
-    sendGraphUpdate(VertexAdd(msgTime = transactionTime, srcID = srcClusterId, Type("Cluster")))
-    sendGraphUpdate(VertexAdd(msgTime = transactionTime, srcID = dstClusterId, Type("Cluster")))
+      sendGraphUpdate(VertexAdd(msgTime = transactionTime, srcID = srcClusterId, Type("Cluster")))
+      sendGraphUpdate(VertexAdd(msgTime = transactionTime, srcID = dstClusterId, Type("Cluster")))
 
-    sendGraphUpdate(
-      EdgeAddWithProperties(msgTime = transactionTime,
-        srcID = srcClusterId,
-        dstID = dstClusterId,
-        Properties(DoubleProperty("BitCoin", btcAmount),
-                  DoubleProperty("USD",usdAmount),
-                  DoubleProperty("Transaction", transactionId)),
-        Type("Transfer")
+      sendGraphUpdate(
+        EdgeAddWithProperties(msgTime = transactionTime,
+          srcID = srcClusterId,
+          dstID = dstClusterId,
+          Properties(DoubleProperty("BitCoin", btcAmount),
+            DoubleProperty("USD", usdAmount),
+            DoubleProperty("Transaction", transactionId)),
+          Type("Transfer")
+        )
       )
-    )
+    }
   }
 
   //converts the line into a case class which has all of the data via the correct name and type
@@ -41,7 +42,6 @@ class ChABClus2ClusRouter(override val routerId: Int,override val workerID:Int, 
       line(4).toLong * 1000,            //Time of transaction in seconds (milli in Raph)
       line(5).toLong, 			            //ID of transaction, can be similar for many records
       line(6).toDouble / 100000 			    //Amount of transaction in USD
-
     )
 
   def longCheck(data: String): Option[Long] = if (data equals "") None else Some(data.toLong)
