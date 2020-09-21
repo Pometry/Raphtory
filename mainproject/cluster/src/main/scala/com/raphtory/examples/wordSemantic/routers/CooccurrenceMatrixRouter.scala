836eea56005a278047a3b75d5727eba6b52f31f1
imanehaf <hafmane@gmail.com>
2020-09-21 12:16:58 +0100

    word-semantic ingestion modifs

--- mainproject/cluster/src/main/scala/com/raphtory/examples/wordSemantic/routers/CooccurrenceMatrixRouter.scala
+++ mainproject/cluster/src/main/scala/com/raphtory/examples/wordSemantic/routers/CooccurrenceMatrixRouter.scala
@@ -6,27 +6,33 @@ import com.raphtory.core.model.communication.{Type, _}
 class CooccurrenceMatrixRouter(override val routerId: Int, override val workerID:Int, override val initialManagerCount: Int) extends RouterWorker {
 
   def parseTuple(record: Any): Unit = {
+    //println(record)
     var dp = record.asInstanceOf[String].split(" ").map(_.trim)
     val occurenceTime = dp.head.toLong//DateFormatting(dp.head) //.slice(4, dp.head.length)
-    dp = dp.last.split("\t")
-    val srcClusterId = assignID(dp.head)
-    val len = dp.length
-    for (i <- 1 until len by 2) {
-      val dstClusterId = assignID(dp(i))
-      val coocWeight = dp(i+1).toLong
+    try {
+      dp = dp.last.split("\t")
+      val srcClusterId = assignID(dp.head)
+      val len = dp.length
+      for (i <- 1 until len by 2) {
+        val dstClusterId = assignID(dp(i))
+        val coocWeight = dp(i + 1).toLong
 
-      sendGraphUpdate(
-        EdgeAddWithProperties(msgTime = occurenceTime,
-          srcID = srcClusterId,
-          dstID = dstClusterId,
-          Properties(DoubleProperty("Frequency", coocWeight))
+        sendGraphUpdate(
+         EdgeAddWithProperties(
+            msgTime = occurenceTime,
+            srcID = srcClusterId,
+            dstID = dstClusterId,
+            Properties(DoubleProperty("Frequency", coocWeight))
+          )
         )
-      )
+      }
+    }catch {
+      case e: Exception => println(e, dp.length, record.asInstanceOf[String])
     }
   }
 
   def DateFormatting(date: String): Long = {
     val format = new java.text.SimpleDateFormat("yyyyMM")
-    format.parse(date).getTime()
+    format.parse(date).getTime
   }
 }
