e7ec23543d465448e3a4e3533795a5a41b340a3f
Miratepuffin <b.a.steer@qmul.ac.uk>
2020-11-30 02:44:36 +0000

    Refactored the whole cluster setup file into RaphtoryServer class

--- mainproject/src/main/scala/com/raphtory/RaphtoryGraph.scala
+++ mainproject/src/main/scala/com/raphtory/RaphtoryGraph.scala
@@ -10,11 +10,18 @@ import kamon.Kamon
 import org.slf4j.LoggerFactory
 
 object RaphtoryGraph {
-  def apply[T](dataSource: Spout[T], graphBuilder: GraphBuilder[T]) : RaphtoryGraph[T] =
-    new RaphtoryGraph(dataSource, graphBuilder)
+  def apply[T](spout: Spout[T], graphBuilder: GraphBuilder[T]) : RaphtoryGraph[T] =
+    new RaphtoryGraph(spout, graphBuilder)
+
+  def apply[T](spoutPath: String, graphBuilderPath: String) : RaphtoryGraph[T] ={
+    val spout = Class.forName(spoutPath).getConstructor().newInstance().asInstanceOf[Spout[T]]
+    val graphBuilder = Class.forName(graphBuilderPath).getConstructor().newInstance().asInstanceOf[GraphBuilder[T]]
+    new RaphtoryGraph(spout, graphBuilder)
+  }
+
 }
 
-class RaphtoryGraph[T](dataSource: Spout[T], graphBuilder: GraphBuilder[T]) {
+class RaphtoryGraph[T](spout: Spout[T], graphBuilder: GraphBuilder[T]) {
   Kamon.init() //start tool logging
 
 //  val root = LoggerFactory.getLogger(org.slf4j.Logger.ROOT_LOGGER_NAME).asInstanceOf[ch.qos.logback.classic.Logger]
@@ -23,12 +30,12 @@ class RaphtoryGraph[T](dataSource: Spout[T], graphBuilder: GraphBuilder[T]) {
 
   val partitionNumber = 1
   val minimumRouters  = 1
-  system.actorOf(Props(new WatermarkManager(managerCount = 1)),"WatermarkManager")
+  system.actorOf(Props(new WatermarkManager(partitionNumber)),"WatermarkManager")
   system.actorOf(Props(new WatchDog(partitionNumber, minimumRouters)), "WatchDog")
 
-  system.actorOf(Props(new SpoutAgent(dataSource)), "Spout")
-  system.actorOf(Props(RaphtoryReplicator.apply("Router", 1, 1,graphBuilder)), s"Routers")
-  system.actorOf(Props(RaphtoryReplicator("Partition Manager", 1,1)), s"PartitionManager")
+  system.actorOf(Props(new SpoutAgent(spout)), "Spout")
+  system.actorOf(Props(RaphtoryReplicator.apply("Router", partitionNumber, minimumRouters,graphBuilder)), s"Routers")
+  system.actorOf(Props(RaphtoryReplicator("Partition Manager", partitionNumber,minimumRouters)), s"PartitionManager")
 
   system.actorOf(Props[AnalysisManager], s"AnalysisManager")
   AnalysisRestApi(system)
