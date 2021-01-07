9b57961b256267928fb6f7ac523aed73bfe2c21a
imanehaf <hafmane@gmail.com>
2021-01-07 16:17:25 +0000

    incorporated motif counting in CBOD

--- mainproject/src/main/scala/com/raphtory/algorithms/CommunityOutlierDetection.scala
+++ mainproject/src/main/scala/com/raphtory/algorithms/CommunityOutlierDetection.scala
@@ -6,53 +6,42 @@ import scala.collection.mutable.ArrayBuffer
 import scala.collection.parallel.immutable
 
 class CommunityOutlierDetection(args:Array[String]) extends LPA(args) {
+  val thr: Double = System.getenv().getOrDefault("OUTLIER_DETECTION_THRESHOLD", "0.1").trim.toDouble
+  val topnum: Int = System.getenv().getOrDefault("OUTLIER_DETECTION_NUMBER", "100").trim.toInt // TODO: add this as arguments
 
   override def doSomething(v: VertexVisitor, neighborLabels: Array[Long]): Unit = {
       val vlabel = v.getState[Long]("lpalabel")
-      val outlierScore = 1 - (neighborLabels.count(_==vlabel)/neighborLabels.length.toFloat)
+      val outlierScore = 1 - (neighborLabels.count(_==vlabel)/neighborLabels.length.toDouble)
       v.setState("outlierscore", outlierScore)
   }
 
-
-
-
   override def returnResults(): Any =
     view.getVertices()
-      .map(vertex => (vertex.ID(), vertex.getState[Float]("outlierscore")))
-//      .groupBy(f=> f._2)
-//      .map(f => f._2)
-
-
+      .map(vertex => (vertex.ID(), vertex.getState[Double]("outlierscore")))
 
   override def processResults(results: ArrayBuffer[Any], timestamp: Long, viewCompleteTime: Long): Unit = {
-    val endResults = results.asInstanceOf[ArrayBuffer[immutable.ParHashMap[Long, Float]]].flatten
-    val thr = System.getenv().getOrDefault("OUTLIER_DETECTION_THRESHOLD", "0.5").trim.toFloat
-    val topnum = System.getenv().getOrDefault("OUTLIER_DETECTION_NUMBER", "100").trim.toInt
+    val endResults = results.asInstanceOf[ArrayBuffer[immutable.ParHashMap[Long, Double]]].flatten
     val outliers = endResults.filter(_._2 >= thr)
     val sorted = outliers.sortBy(- _._2)
     val sortedstr = sorted.map(x => s""""${x._1}":${x._2}""")
     val top = sorted.map(_._1).take(5)
     val total = outliers.length
-    val proportion = total/endResults.length.toFloat
+    val proportion = total/endResults.length.toDouble
     val text = s"""{"time":$timestamp,"total":$total,"top5":[${top.mkString(",")}],"outliers":{${sortedstr.take(topnum).mkString(",")}},"proportion":$proportion,"viewTime":$viewCompleteTime}"""
-    writeLines(output_file, text, "{\"views\":[")
+//    writeLines(output_file, text, "{\"views\":[")
     println(text)
-    publishData(text)
   }
 
   override def processWindowResults(results: ArrayBuffer[Any], timestamp: Long, windowSize: Long, viewCompleteTime: Long): Unit = {
-    val endResults = results.asInstanceOf[ArrayBuffer[immutable.ParHashMap[Long, Float]]].flatten
-    val thr = System.getenv().getOrDefault("OUTLIER_DETECTION_THRESHOLD", "0.5").trim.toFloat
-    val topnum = System.getenv().getOrDefault("OUTLIER_DETECTION_NUMBER", "100").trim.toInt
+    val endResults = results.asInstanceOf[ArrayBuffer[immutable.ParHashMap[Long, Double]]].flatten
     val outliers = endResults.filter(_._2 >= thr)
     val sorted = outliers.sortBy(- _._2)
     val sortedstr = sorted.map(x => s""""${x._1}":${x._2}""")
     val top = sorted.map(_._1).take(5)
     val total = outliers.length
-    val proportion = total/endResults.length.toFloat
+    val proportion = total/endResults.length.toDouble
     val text = s"""{"time":$timestamp,"windowsize":$windowSize,"total":$total,"top5":[${top.mkString(",")}],"outliers":{${sortedstr.take(topnum).mkString(",")}},"proportion":$proportion,"viewTime":$viewCompleteTime},"""
-    writeLines(output_file, text, "{\"views\":[")
+//    writeLines(output_file, text, "{\"views\":[")
     println(text)
-    publishData(text)
   }
 }
--- mainproject/src/main/scala/com/raphtory/algorithms/MotifCounting.scala
+++ mainproject/src/main/scala/com/raphtory/algorithms/MotifCounting.scala
@@ -1,97 +1,90 @@
 package com.raphtory.algorithms
 
 import com.raphtory.api.Analyser
-import com.raphtory.core.model.analysis.entityVisitors.{EdgeVisitor, VertexVisitor}
+import com.raphtory.core.model.analysis.entityVisitors.EdgeVisitor
 
 import scala.collection.mutable.ArrayBuffer
 import scala.collection.parallel.mutable.{ParArray, ParIterable, ParMap}
 
-class MotifCounting(args: Array[String]) extends Analyser(args) { //args(delta)
-  val delta: Long = if (args.isEmpty) throw new Exception else args.head.toLong //todo remove exception later
-  val PROP = "weight"
+class MotifCounting(args: Array[String]) extends Analyser(args) { //todo better manage args
+  val Array(delta, step) = if (args.isEmpty) throw new Exception else args.map(_.toLong) //todo remove exception later
+  val PROP: String       = System.getenv().getOrDefault("EDGE_PROPERTY", "weight").trim
 
-  override def setup(): Unit = {
+  override def setup(): Unit =
     view.getVertices.foreach { v =>
-      val inc = v.getIncEdges
-      val t_in = inc.flatMap(e => e.getHistory().keys).toArray.sorted
+      val inc  = v.getIncEdges
       val outc = v.getOutEdges
-      val t_out = outc.flatMap(e => e.getHistory().keys).toArray.sorted
-      //      println(v.ID() + "     " + (v.getIncEdges ++ v.getOutEdges).map(e => e.getHistory().size).sum)
-      if (inc.nonEmpty & outc.nonEmpty) {
-        val count1 = motifCounting(1, t_in, t_out)
-        val count2 = motifCounting(2, t_in, t_out)
-        v.setState("motifs", (count1,count2))
-      }
-
-      def motifCounting(mType: Int, in: Array[Long], out: Array[Long]): Long = {
-        var (t_in, t_out) = (in.clone(), out.clone()) //todo <-- optimize
-        mType match {
-          case 1 => //todo change to fraction count
-            t_in = t_in.reverse.dropWhile(_ >= t_out.last).reverse
-            val tmp = t_in.groupBy(identity)
-            t_in.distinct.foldLeft(0L) { case (a, t) =>
-              t_out = t_out.filter(_ > t)
-              a + t_out.count(_ <= t + delta) * tmp(t).length
-            }
-          case 2 =>
-            val (mn, mx) = (t_in ++ t_out).foldLeft((t_in.head, t_in.head)) { case ((min, max), e) => (math.min(min, e), math.max(max, e)) }
-//            var total = 0
-            (for (dt <- mn to mx by delta) yield dt).count { dt =>
-              //              if (checkActivity(v, dt, dt+delta)) total += 1 //todo for getting fractions
-              val gamma1 = mean(inc.flatMap(getTimes(_, dt)).toParArray) < mean(outc.flatMap(getTimes(_, dt)).toParArray)
-              val q = getProperties(inc, dt, PROP)
-              val qq = getProperties(outc, dt, PROP)
-//              val gamma2 = q.sum > qq.sum
-              val gamma2 = getProperties(inc, dt, PROP).sum > getProperties(outc, dt, PROP).sum
-              gamma1 & gamma2
-            } // / total
-          case _ => 0L
-        }
-      }
+//            println(v.ID() + "     " + (v.getIncEdges ++ v.getOutEdges).map(e => e.getHistory().size).sum)
+      val count1 = motifCounting(1, inc, outc)
+      val count2 = motifCounting(2, inc, outc)
+      v.setState("motifs", (count1, count2))
     }
-  }
-
-  def mean(a: ParArray[Long]): Double ={ //the fact i have to build this is maddening dont touch me
-    if (a.nonEmpty) a.sum/a.length.toDouble else 0.0
-  }
-  def checkActivity(v: VertexVisitor, t1: Long, t2: Long): Boolean = {
-    v.getInCEdgesBetween(t1, t2) ++ v.getOutEdgesBetween(t1, t2) nonEmpty
-  }
-
-  def getTimes(edge: EdgeVisitor, time: Long): Iterable[Long] = {
-    edge.getHistory().filter { case (t, true) => t >= time & t < time + delta }.keys
-  }
-
-  def getProperties(edges: ParIterable[EdgeVisitor], time: Long, prop: String): ParIterable[Long] = {
-    edges.map(e => getTimes(e, time).foldLeft(0L) { case (a, b) =>
-      a + e.getPropertyValueAt(prop, b).getOrElse(0L).asInstanceOf[Long]
-    })
-  }
 
   override def analyse(): Unit = {}
 
-  override def returnResults(): Any = {
-    view.getVertices()
-      .map(vertex => (vertex.ID(), vertex.getOrSetState[(Float,Float)]("motifs", (0.0, 0.0)))).toMap
-  }
+  override def returnResults(): Any =
+    view.getVertices().map(vertex => (vertex.ID(), vertex.getOrSetState[(Double, Double)]("motifs", (0.0, 0.0)))).toMap
 
   override def processResults(results: ArrayBuffer[Any], timestamp: Long, viewCompleteTime: Long): Unit = {
-    val endResults = results.asInstanceOf[ArrayBuffer[ParMap[Long, (Float, Float)]]].flatten
-    val count = endResults.sortBy(_._1).map(x => s""""${x._1}":${x._2}""")
-    val nl = "\n"
-    val text = s"""{"time":$timestamp,"motifs":{ $nl${count.mkString(",\n")} $nl},"viewTime":$viewCompleteTime}"""
+    val endResults = results.asInstanceOf[ArrayBuffer[ParMap[Long, (Double, Double)]]].flatten
+    val count      = endResults.sortBy(_._1).map(x => s""""${x._1}":${x._2}""")
+    val nl         = "\n"
+    val text       = s"""{"time":$timestamp,"motifs":{ $nl${count.mkString(",\n")} $nl},"viewTime":$viewCompleteTime}"""
     //  writeLines(output_file, text, "{\"views\":[")
     println(text)
   }
 
-  override def processWindowResults(results: ArrayBuffer[Any], timestamp: Long, windowSize: Long, viewCompleteTime: Long): Unit = {
-    val endResults = results.asInstanceOf[ArrayBuffer[ParMap[Long, Float]]].flatten
-    val count = endResults.map(x => s""""${x._1}":${x._2}""")
-    val text = s"""{"time":$timestamp,"windowsize":$windowSize,"motifs":{${count.mkString(",")}},"viewTime":$viewCompleteTime}"""
+  override def processWindowResults(
+      results: ArrayBuffer[Any],
+      timestamp: Long,
+      windowSize: Long,
+      viewCompleteTime: Long
+  ): Unit = {
+    val endResults = results.asInstanceOf[ArrayBuffer[ParMap[Long, Double]]].flatten
+    val count      = endResults.map(x => s""""${x._1}":${x._2}""")
+    val text =
+      s"""{"time":$timestamp,"windowsize":$windowSize,"motifs":{${count.mkString(",")}},"viewTime":$viewCompleteTime}"""
     //    writeLines(output_file, text, "{\"views\":[")
     println(text)
     publishData(text)
   }
 
   override def defineMaxSteps(): Int = 100
+
+  def motifCounting(mType: Int, inc: ParIterable[EdgeVisitor], outc: ParIterable[EdgeVisitor]): Double = {
+    var t_in   = inc.flatMap(e => e.getHistory().keys).toArray.sorted
+    var t_out  = outc.flatMap(e => e.getHistory().keys).toArray.sorted
+    val tEdges = t_in ++ t_out
+    mType match {
+      case 1 =>
+        if (inc.nonEmpty & outc.nonEmpty) {
+          val total = nChoosek(tEdges.length) - tEdges.groupBy(identity).values.toArray.map(x => nChoosek(x.length)).sum
+          t_in = t_in.reverse.dropWhile(_ >= t_out.last).reverse
+          val tmp = t_in.groupBy(identity)
+          t_in.distinct.foldLeft(0L) {
+            case (a, t) =>
+              t_out = t_out.filter(_ > t)
+              a + t_out.count(_ <= t + delta) * tmp(t).length
+          } / total.toDouble
+        } else 0.0
+      case 2 =>
+        val (mn, mx) = tEdges.foldLeft((tEdges.head, tEdges.head)) {
+          case ((min, max), e) => (math.min(min, e), math.max(max, e))
+        }
+        var total = 0
+        (for (dt <- mn to mx by step) yield dt).count { dt =>
+          if (checkActivity(inc ++ outc, dt, dt + delta)) total += 1
+          val gamma1 = mean(inc.flatMap(getTimes(_, dt)).toParArray) < mean(outc.flatMap(getTimes(_, dt)).toParArray)
+          val gamma2 = mean(getProperties(inc, dt, PROP).toParArray) > mean(getProperties(outc, dt, PROP).toParArray)
+          gamma1 & gamma2
+        } / total.toDouble
+      case _ => 0
+    }
+  }
+  def nChoosek(n: Long, k: Long = 2): Long = if (k == 0L) 1L else (n * nChoosek(n - 1, k - 1)) / k
+  def mean(a: ParArray[Long]): Double =    if (a.nonEmpty) a.sum / a.length.toDouble else 0.0 //the fact i have to build this is maddening dont touch me
+  def checkActivity(edges: ParIterable[EdgeVisitor], t1: Long, t2: Long): Boolean = {    edges.exists(e => e.getHistory().exists(k => k._1 >= t1 && k._1 < t2))  }
+  def getTimes(edge: EdgeVisitor, time: Long): Iterable[Long] =  edge.getHistory().filter { case (t, true) => t >= time & t < time + delta }.keys
+  def getProperties(edges: ParIterable[EdgeVisitor], time: Long, prop: String): ParIterable[Long] =
+    edges.map(e =>   getTimes(e, time).foldLeft(0L) {  case (a, b) => a + e.getPropertyValueAt(prop, b).getOrElse(0L).asInstanceOf[Long]  })
 }
