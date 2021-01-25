1e3c1716f8720190ba10b3109748af574d329c42
imanehaf <hafmane@gmail.com>
2021-01-25 17:16:44 +0000

    clean up: LPA, CBOD

--- mainproject/src/main/scala/com/raphtory/algorithms/CommunityOutlierDetection.scala
+++ mainproject/src/main/scala/com/raphtory/algorithms/CommunityOutlierDetection.scala
@@ -1,49 +1,57 @@
 package com.raphtory.algorithms
 
 import com.raphtory.core.model.analysis.entityVisitors.VertexVisitor
-
 import scala.collection.mutable.ArrayBuffer
 import scala.collection.parallel.immutable
 import scala.reflect.io.Path
 
-class CommunityOutlierDetection(args:Array[String]) extends LPA(args) {
-  val thr: Double = System.getenv().getOrDefault("OUTLIER_DETECTION_THRESHOLD", "0.0").trim.toDouble
-  val topnum: Int = System.getenv().getOrDefault("OUTLIER_DETECTION_NUMBER", "-1").trim.toInt // TODO: add this as arguments
+class CommunityOutlierDetection(args: Array[String]) extends LPA(args) {
+  //args = [top output, edge property, threshold, maxIter]
+  val topnum: Int           = if (arg.length == 0) 0 else arg.head.toInt
+  val thr: Double           = if (arg.length < 3) 0.0 else arg(2).toDouble
+  override val maxIter: Int = if (arg.length < 4) 500 else arg(3).toInt
 
   override def doSomething(v: VertexVisitor, neighborLabels: Array[Long]): Unit = {
-      val vlabel = v.getState[Long]("lpalabel")
-      val outlierScore = 1 - (neighborLabels.count(_==vlabel)/neighborLabels.length.toDouble)
-      v.setState("outlierscore", outlierScore)
+    val vlabel       = v.getState[Long]("lpalabel")
+    val outlierScore = 1 - (neighborLabels.count(_ == vlabel) / neighborLabels.length.toDouble)
+    v.setState("outlierscore", outlierScore)
   }
 
   override def returnResults(): Any =
-    view.getVertices().filter(v=> v.Type()==nodeType)
-      .map(vertex => (vertex.ID(), vertex.getOrSetState[Double]("outlierscore", -1.0)))
+    view
+      .getVertices()
+      .filter(v => v.Type() == nodeType)
+      .map(vertex => (vertex.ID(), vertex.getOrSetState[Double]("outlierscore", -1.0))) //TODO: IM: temp fix -- to be remove later
 
   override def processResults(results: ArrayBuffer[Any], timestamp: Long, viewCompleteTime: Long): Unit = {
     val endResults = results.asInstanceOf[ArrayBuffer[immutable.ParHashMap[Long, Double]]].flatten
-    val outliers = endResults.filter(_._2 >= thr)
-    val sorted = outliers.sortBy(- _._2)
-    val sortedstr = sorted.map(x => s""""${x._1}":${x._2}""")
-    val top = sorted.map(_._1).take(5)
-    val total = outliers.length
-    val proportion = total/endResults.length.toDouble
-    val out = if (topnum == -1) sortedstr else sortedstr.take(topnum)
-    val text = s"""{"time":$timestamp,"total":$total,"top5":[${top.mkString(",")}],"outliers":{${out.mkString(",")}},"proportion":$proportion,"viewTime":$viewCompleteTime}"""
+    val outliers   = endResults.filter(_._2 >= thr)
+    val sorted     = outliers.sortBy(-_._2)
+    val sortedstr  = sorted.map(x => s""""${x._1}":${x._2}""")
+    val top        = sorted.map(_._1).take(5)
+    val total      = outliers.length
+    val out        = if (topnum == 0) sortedstr else sortedstr.take(topnum)
+    val text = s"""{"time":$timestamp,"total":$total,"top5":[${top.mkString(",")}],"outliers":{${out
+      .mkString(",")}},"viewTime":$viewCompleteTime}"""
     Path(output_file).createFile().appendAll(text + "\n")
     //    println(text)
   }
 
-  override def processWindowResults(results: ArrayBuffer[Any], timestamp: Long, windowSize: Long, viewCompleteTime: Long): Unit = {
+  override def processWindowResults(
+      results: ArrayBuffer[Any],
+      timestamp: Long,
+      windowSize: Long,
+      viewCompleteTime: Long
+  ): Unit = {
     val endResults = results.asInstanceOf[ArrayBuffer[immutable.ParHashMap[Long, Double]]].flatten
-    val outliers = endResults.filter(_._2 >= thr)
-    val sorted = outliers.sortBy(- _._2)
-    val sortedstr = sorted.map(x => s""""${x._1}":${x._2}""")
-    val top = sorted.map(_._1).take(5)
-    val total = outliers.length
-    val proportion = total/endResults.length.toDouble
-    val out = if (topnum == -1) sortedstr else sortedstr.take(topnum)
-    val text = s"""{"time":$timestamp,"windowsize":$windowSize,"total":$total,"top5":[${top.mkString(",")}],"outliers":{${out.mkString(",")}},"proportion":$proportion,"viewTime":$viewCompleteTime}"""
+    val outliers   = endResults.filter(_._2 >= thr)
+    val sorted     = outliers.sortBy(-_._2)
+    val sortedstr  = sorted.map(x => s""""${x._1}":${x._2}""")
+    val top        = sorted.map(_._1).take(5)
+    val total      = outliers.length
+    val out        = if (topnum == 0) sortedstr else sortedstr.take(topnum)
+    val text = s"""{"time":$timestamp,"windowsize":$windowSize,"total":$total,"top5":[${top
+      .mkString(",")}],"outliers":{${out.mkString(",")}},"viewTime":$viewCompleteTime}"""
     Path(output_file).createFile().appendAll(text + "\n")
 //    println(text)
   }
--- mainproject/src/main/scala/com/raphtory/algorithms/LPA.scala
+++ mainproject/src/main/scala/com/raphtory/algorithms/LPA.scala
@@ -2,107 +2,118 @@ package com.raphtory.algorithms
 
 import com.raphtory.api.Analyser
 import com.raphtory.core.model.analysis.entityVisitors.VertexVisitor
+
 import scala.collection.mutable.ArrayBuffer
 import scala.collection.parallel.immutable
 import scala.collection.parallel.mutable.ParArray
 import scala.reflect.io.Path
 
-class LPA(args:Array[String]) extends Analyser(args){ //TODO needs Major cleanup
-  val arg = args.map(_.trim)//.head
-  val top_c = if (arg.length==0 || arg.head=="-1") 0 else arg.head.toInt // TODO: youknwo the drill
-  val PROP = "weight"
-  val output_file = System.getenv().getOrDefault("LPA_OUTPUT_PATH", "/home/tsunade/app/out.json").trim
-  val nodeType = System.getenv().getOrDefault("NODE_TYPE", "Addr").trim
+object LPA {
+  def apply(): LPA = new LPA(Array())
+}
 
-  override def setup(): Unit = {
+class LPA(args: Array[String]) extends Analyser(args) {
+  //args = [top output, edge property, max iterations]
+  val arg: Array[String] = args.map(_.trim) // TODO: IM: add argument (maxiter)
+  val top_c: Int         = if (arg.length == 0) 0 else arg.head.toInt
+  val PROP: String       = if (arg.length < 2) "" else arg(1)
+  val maxIter: Int       = if (arg.length < 3) 500 else arg(2).toInt
+
+  val output_file: String = System.getenv().getOrDefault("LPA_OUTPUT_PATH", "/app/out.json").trim
+  val nodeType: String    = System.getenv().getOrDefault("NODE_TYPE", "").trim
+
+  override def setup(): Unit =
     view.getVertices().foreach { vertex =>
       val lab = scala.util.Random.nextLong()
       vertex.setState("lpalabel", lab)
-      vertex.messageAllNeighbours((vertex.ID(),lab))
+      vertex.messageAllNeighbours((vertex.ID(), lab))
     }
-  }
 
-  override def analyse(): Unit = {
+  override def analyse(): Unit =
     view.getMessagedVertices().foreach { vertex =>
       try {
-        val vlabel = vertex.getState[Long]("lpalabel") //, scala.util.Random.nextLong())
+        val Vlabel = vertex.getState[Long]("lpalabel")
+
+        // Get neighbourhood Frequencies -- relevant to weighted LPA
         val vneigh = vertex.getOutEdges ++ vertex.getIncEdges
-        val neigh_freq = vneigh.map { e => (e.ID(), e.getPropertyValue(PROP).getOrElse(1L).asInstanceOf[Long]) }
-          .groupBy(_._1).mapValues(x => x.map(_._2).sum)
+        val neigh_freq = vneigh
+          .map(e => (e.ID(), e.getPropertyValue(PROP).getOrElse(1L).asInstanceOf[Long]))
+          .groupBy(_._1)
+          .mapValues(x => x.map(_._2).sum)
         val vfreq = if (vneigh.nonEmpty) neigh_freq.values.sum / vneigh.map(_.ID()).toSet.size else 1L
-        val gp = vertex.messageQueue[(Long, Long)].map { v => (v._2, if (neigh_freq.contains(v._1)) neigh_freq(v._1) else 1) }
-        gp.append((vlabel, vfreq))
+
+        // Process neighbour labels into (label, frequency)
+        val gp = vertex.messageQueue[(Long, Long)].map(v => (v._2, neigh_freq(v._1)))
+        gp.append((Vlabel, vfreq))
+
+        // Update node label and broadcast
         val newLabel = gp.groupBy(_._1).mapValues(_.map(_._2).sum).maxBy(_._2)._1
-        if (newLabel == vlabel) {
-          vertex.voteToHalt()
-        } else {
-          vertex.setState("lpalabel", newLabel)
+        newLabel match {
+          case Vlabel => vertex.voteToHalt()
+          case _      => vertex.setState("lpalabel", newLabel)
         }
+
         vertex.messageAllNeighbours((vertex.ID(), newLabel))
         doSomething(vertex, gp.dropRight(1).map(_._1).toArray)
       } catch {
         case e: Exception => println(e, vertex.ID())
       }
     }
-  }
-
-  def labelProbability(gp: Map[Long, ArrayBuffer[Long]]): Long ={
-//    label probability function to get rid of oscillation phenomena of synchronous LPA from [1]
-//    [1] https://doi.org/10.1016/j.neucom.2014.04.084
-
-    val f = gp.map{lab =>  Math.pow(lab._2.sum, 2)}//TODO: implement full method from paper
-    val p = f.toArray.map(i => i/f.toArray.sum)
-    val gpp = gp.keys zip p
-    gpp.filter(x=>x._2 == gpp.maxBy(_._2)._2).maxBy(_._1)._1
-  }
 
   def doSomething(v: VertexVisitor, gp: Array[Long]): Unit = {}
 
   override def returnResults(): Any =
-    view.getVertices().filter(v=> v.Type()==nodeType)
+    view
+      .getVertices()
+      .filter(v => v.Type() == nodeType)
       .map(vertex => (vertex.getState[Long]("lpalabel"), vertex.ID()))
       .groupBy(f => f._1)
       .map(f => (f._1, f._2.map(_._2)))
 
-
   override def processResults(results: ArrayBuffer[Any], timestamp: Long, viewCompleteTime: Long): Unit = {
-    val er = extractData(results)
-    val commtxt = er.communities.map { x => s"""[${x.mkString(",")}]""" }
-    val text = s"""{"time":$timestamp,"top5":[${er.top5.mkString(",")}],"total":${er.total},"totalIslands":${er.totalIslands},"communities": [${commtxt.mkString(",")}],"proportion":${er.proportion}, "viewTime":$viewCompleteTime}"""
+    val er      = extractData(results)
+    val commtxt = er.communities.map(x => s"""[${x.mkString(",")}]""")
+    val text = s"""{"time":$timestamp,"top5":[${er.top5
+      .mkString(",")}],"total":${er.total},"totalIslands":${er.totalIslands},"communities": [${commtxt
+      .mkString(",")}], "viewTime":$viewCompleteTime}"""
     Path(output_file).createFile().appendAll(text + "\n")
 //    println(text)
   }
 
-  override def processWindowResults(results: ArrayBuffer[Any], timestamp: Long, windowSize: Long, viewCompleteTime: Long): Unit = {
-    val er = extractData(results)
-    val commtxt = er.communities.map{x=> s"""[${x.mkString(",")}]"""}
-    val text = s"""{"time":$timestamp,"windowsize":$windowSize,"top5":[${er.top5.mkString(",")}],"total":${er.total},"totalIslands":${er.totalIslands},"communities": [${commtxt.mkString(",")}],"proportion":${er.proportion}, "viewTime":$viewCompleteTime}"""
+  override def processWindowResults(
+      results: ArrayBuffer[Any],
+      timestamp: Long,
+      windowSize: Long,
+      viewCompleteTime: Long
+  ): Unit = {
+    val er      = extractData(results)
+    val commtxt = er.communities.map(x => s"""[${x.mkString(",")}]""")
+    val text = s"""{"time":$timestamp,"windowsize":$windowSize,"top5":[${er.top5
+      .mkString(",")}],"total":${er.total},"totalIslands":${er.totalIslands},"communities": [${commtxt
+      .mkString(",")}], "viewTime":$viewCompleteTime}"""
     Path(output_file).createFile().appendAll(text + "\n")
 //    println(text)
   }
 
-  def extractData(results:ArrayBuffer[Any]):fd ={
-    val endResults = results.asInstanceOf[ArrayBuffer[immutable.ParHashMap[Long,  ParArray[String]]]]
+  def extractData(results: ArrayBuffer[Any]): fd = {
+    val endResults = results.asInstanceOf[ArrayBuffer[immutable.ParHashMap[Long, ParArray[String]]]]
     try {
-      val grouped = endResults.flatten.groupBy(f => f._1).mapValues(x => x.flatMap(_._2))
-      val groupedNonIslands = grouped.filter(x => x._2.size > 1)
-      val biggest = grouped.maxBy(_._2.size)._2.size
-      val sorted = grouped.toArray.sortBy(_._2.size)(sortOrdering)//
-      val top5 = sorted.map(x=>x._2.size).take(5)
-      val total = grouped.size
+      val grouped             = endResults.flatten.groupBy(f => f._1).mapValues(x => x.flatMap(_._2))
+      val groupedNonIslands   = grouped.filter(x => x._2.size > 1)
+      val sorted              = grouped.toArray.sortBy(_._2.size)(sortOrdering) //
+      val top5                = sorted.map(_._2.size).take(5)
+      val total               = grouped.size
       val totalWithoutIslands = groupedNonIslands.size
-      val totalIslands = total - totalWithoutIslands
-      val proportion = biggest.toFloat / grouped.map(x => x._2.size).sum
-      val communities =  sorted.map(x=>x._2).take(if(top_c==0) sorted.length else top_c)//.values//.toArray
-      fd(top5,total,totalIslands,proportion, communities)
-    }  catch {
-      case e: UnsupportedOperationException => fd(Array(0),0,0,0, Array(ArrayBuffer("0")))
+      val totalIslands        = total - totalWithoutIslands
+      val communities         = if (top_c == 0) sorted.map(_._2) else sorted.map(_._2).take(top_c)
+      fd(top5, total, totalIslands, communities)
+    } catch {
+      case e: UnsupportedOperationException => fd(Array(0), 0, 0, Array(ArrayBuffer("0")))
     }
   }
 
-  override def defineMaxSteps(): Int = 500
+  override def defineMaxSteps(): Int = maxIter
 
 }
 
-case class fd(top5:Array[Int],total:Int,totalIslands:Int,proportion:Float, communities: Array[ArrayBuffer[String]])
-
+case class fd(top5: Array[Int], total: Int, totalIslands: Int, communities: Array[ArrayBuffer[String]])
