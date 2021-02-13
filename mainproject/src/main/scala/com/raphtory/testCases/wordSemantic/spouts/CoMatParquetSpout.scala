09d88e420d9e1ac4717fc2e9560eb3b7dc2388dc
imanehaf <hafmane@gmail.com>
2021-02-13 09:24:05 +0000

    modified Spout to read multiple parquets

--- mainproject/src/main/scala/com/raphtory/testCases/wordSemantic/spouts/CoMatParquetSpout.scala
+++ mainproject/src/main/scala/com/raphtory/testCases/wordSemantic/spouts/CoMatParquetSpout.scala
@@ -1,39 +1,111 @@
 package com.raphtory.testCases.wordSemantic.spouts
 
+import java.io.File
 import java.util
 
 import com.raphtory.core.actors.Spout.Spout
-import org.apache.spark.sql.{DataFrame, Row, SparkSession}
+import com.raphtory.spouts.FileManager.{joiner, logger}
+import org.apache.spark.sql.{Row, SparkSession}
+import com.typesafe.scalalogging.LazyLogging
 
-class CoMatParquetSpout(args: Array[String]) extends Spout[Row] {
-  //args: [range, thr]
-  lazy val spark: SparkSession =
-    SparkSession.builder().master("local").getOrCreate()
+class CoMatParquetSpout() extends Spout[Row] {
+  private val directory = "/home/tsunade/qmul/datasets/word_semantics/tmp/sample-0.1"//System.getenv().getOrDefault("FILE_SPOUT_DIRECTORY", "/app").trim
+  private val fileName = ""//System.getenv().getOrDefault("FILE_SPOUT_FILENAME", "").trim //gabNetwork500.csv
+  private val dropHeader = System.getenv().getOrDefault("FILE_SPOUT_DROP_HEADER", "false").trim.toBoolean
 
-  private val directory          = System.getenv().getOrDefault("FILE_SPOUT_DIRECTORY", "/app").trim
-  val Array(r1, r2)              = if (args.isEmpty) Array(2004L, 2008L) else args.head.trim.split("-").map(_.toLong)
-  val thr: Double                = if (args.length < 1) 0.1 else args(1).trim.toDouble
-  var merged: util.Iterator[Row] = spark.emptyDataFrame.toLocalIterator()
+  private var fileManager = ParquetManager(directory, fileName, dropHeader)
 
-  override def setupDataSource(): Unit = {
-    val source        = "%s/sample-%s".format(directory, thr)
-    var df: DataFrame = spark.createDataFrame(Seq.empty[(Long, String, String, Long)])
-    for (y <- r1 to r2) {
-      val mergedDF =
-        spark.read.option("mergeSchema", "true").parquet("%s/D-%s-%s".format(source, thr, y))
-      df = df.union(mergedDF)
-    }
-    merged = df.toLocalIterator()
-  }
+  override def setupDataSource(): Unit = {}
 
-  override def generateData(): Option[Row] = //im: rewrite this to read pne parquet at a time
-    if (merged.hasNext)
-      Some(merged.next())
-    else {
+  override def generateData(): Option[Row] =
+    if (fileManager.allCompleted) {
       dataSourceComplete()
       None
+    }
+    else {
+      val (newParquetManager, line) = fileManager.nextLine()
+      fileManager = newParquetManager
+      if (line!= Row.empty) Some(line) else None
     }
 
   override def closeDataSource(): Unit = {}
 
+}
+
+final case class ParquetManager private (
+                                       currentFileReader: Option[util.Iterator[Row]],
+                                       restFiles: List[File],
+                                       dropHeader: Boolean
+                                     ) extends LazyLogging {
+  def nextFile(): ParquetManager = this.copy(currentFileReader = None)
+
+  lazy val allCompleted: Boolean = currentFileReader.isEmpty && restFiles.isEmpty
+  lazy val spark: SparkSession =   SparkSession.builder().master("local").getOrCreate()
+
+  def nextLine(): (ParquetManager, Row) = currentFileReader match {
+    case None =>
+      restFiles match {
+        case Nil => (this, Row.empty)
+        case head :: tail =>
+          val reader             = getFileReader(head)
+          val (block, endOfFile) = readBlockAndIsEnd(reader)
+          val currentReader      = if (endOfFile) None else Some(reader)
+          (this.copy(currentFileReader = currentReader, restFiles = tail), block)
+      }
+    case Some(reader) =>
+      val (block, endOfFile) = readBlockAndIsEnd(reader)
+      if (endOfFile) (this.copy(currentFileReader = None), block)
+      else (this, block)
+
+  }
+
+  private def readBlockAndIsEnd(reader: util.Iterator[Row]): (Row, Boolean) = {
+    if (reader.hasNext)
+      (reader.next(), false)
+    else{
+      (Row.empty ,true)
+    }
+  }
+
+  private def getFileReader(file: File): util.Iterator[Row] = {
+    logger.info(s"Reading file ${file.getCanonicalPath}")
+    val br = spark.read.parquet(file.getCanonicalPath)
+    br.toLocalIterator()
+  }
+}
+
+object ParquetManager extends LazyLogging {
+  private val joiner     = System.getenv().getOrDefault("FILE_SPOUT_JOINER", "/").trim //gabNetwork500.csv
+  def apply(dir: String, fileName: String, dropHeader: Boolean): ParquetManager = {
+    val filesToRead =
+      if (fileName.isEmpty)
+        getListOfFiles(dir)
+      else {
+        val file = new File(dir + joiner + fileName)
+        if (file.exists && file.isFile)
+          List(file)
+        else {
+          logger.error(s"File $dir$joiner$fileName does not exist or is not file ")
+          List.empty
+        }
+      }
+    ParquetManager(None, filesToRead, dropHeader)
+  }
+
+  private def getListOfFiles(dir: String): List[File] = {
+    val d = new File(dir)
+    if (d.exists && d.isDirectory) {
+      val files = getRecursiveListOfFiles(d)
+      files.filter(f => f.isFile && !f.isHidden && f.getName.endsWith(".parquet"))
+    }
+    else {
+      logger.error(s"Directory $dir does not exist or is not directory")
+      List.empty
+    }
+  }
+
+  private def getRecursiveListOfFiles(dir: File): List[File] = {
+    val these = dir.listFiles.toList
+    these ++ these.filter(_.isDirectory).sorted.flatMap(getRecursiveListOfFiles)
+  }
 }
