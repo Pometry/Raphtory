87b8a48c2092001b74957428fe9abb580ee3bf17
Miratepuffin <b.a.steer@qmul.ac.uk>
2021-03-18 12:19:13 +0000

    Minor changes based on Hamza's notes and removed dev for merge

--- src/main/scala/com/raphtory/dev/gab/rawgraphmodel/GabEntityType.scala
+++
@@ -1,7 +0,0 @@
-package com.raphtory.examples.gab.rawgraphmodel
-
-object GabEntityType extends Enumeration {
-  val post: Value  = Value("post")
-  val user: Value  = Value("user")
-  val topic: Value = Value("topic")
-}
