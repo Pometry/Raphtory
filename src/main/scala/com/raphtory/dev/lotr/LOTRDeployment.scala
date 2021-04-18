6cdf637f8f422196dde987e3eab9a9be4c0b5544
Miratepuffin <b.a.steer@qmul.ac.uk>
2021-04-18 23:55:03 +0100

    Implemented Mongo Serialiser

--- src/main/scala/com/raphtory/dev/lotr/LOTRDeployment.scala
+++ src/main/scala/com/raphtory/dev/lotr/LOTRDeployment.scala
@@ -18,5 +18,5 @@ object LOTRDeployment extends App{
 
   //rg.viewQuery(DegreeBasic(),timestamp = 10000,arguments)
   //rg.viewQuery(DegreeBasic(),timestamp = 10000,window=100,arguments)
-  rg.viewQuery(ConnectedComponents(), new MongoSerialiser, timestamp = 10000, arguments)
+  rg.viewQuery(ConnectedComponents(), new MongoSerialiser, timestamp = 32674, arguments)
 }
