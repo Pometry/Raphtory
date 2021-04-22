68b91476b5ca7a25bcfe7cfe3352221bc57e0b2f
imanehaf <hafmane@gmail.com>
2021-04-22 13:35:02 +0100

    Update: incorporated island fixes to MultiLayerLPA et co.

--- src/main/scala/com/raphtory/dev/algorithms/MultiLayerLPA.scala
+++ src/main/scala/com/raphtory/dev/algorithms/MultiLayerLPA.scala
@@ -11,12 +11,13 @@ object MultiLayerLPA {
 }
 
 class MultiLayerLPA(args: Array[String]) extends LPA(args) {
-  //args = [top, weight, maxiter, start, end, layer-size, omega]
+  //args = [top, weight, maxiter, start, end, layer-size, omega, scaled]
   val snapshotSize: Long        = args(5).toLong
-  val startTime: Long           = args(3).toLong // * snapshotSize //imlater: change this when done with wsdata
-  val endTime: Long             = args(4).toLong //* snapshotSize
+  val startTime: Long           = args(3).toLong
+  val endTime: Long             = args(4).toLong
   val snapshots: Iterable[Long] = for (ts <- startTime to endTime by snapshotSize) yield ts
   val omega: String             = if (arg.length < 7) "1" else args(6)
+  val scaled: Boolean = if (arg.length < 8) true else args(7).toBoolean
 
   override def setup(): Unit =
     view.getVertices().foreach { vertex =>
@@ -24,11 +25,10 @@ class MultiLayerLPA(args: Array[String]) extends LPA(args) {
       val tlabels =
         snapshots
           .filter(ts => vertex.aliveAtWithWindow(ts, snapshotSize))
-          .map(ts => (ts, (scala.util.Random.nextLong(), scala.util.Random.nextLong())))
+          .map(ts => (ts, rnd.nextLong()))
           .toArray
-//          .map(x => (x, (vertex.ID()+x,vertex.ID()-x))).toArray //IM: remove this after testing
       vertex.setState("mlpalabel", tlabels)
-      val message = (vertex.ID(), tlabels.map(x => (x._1, x._2._2)))
+      val message = (vertex.ID(), tlabels.map(x => (x._1, x._2)))
       vertex.messageAllNeighbours(message)
     }
 
@@ -36,16 +36,15 @@ class MultiLayerLPA(args: Array[String]) extends LPA(args) {
     val t1 = System.currentTimeMillis()
     try{
       view.getMessagedVertices().foreach { vertex =>
-        val vlabel    = vertex.getState[Array[(Long, (Long, Long))]]("mlpalabel").toMap
+        val vlabel    = vertex.getState[Array[(Long, Long)]]("mlpalabel").toMap
         val msgQueue  = vertex.messageQueue[(Long, Array[(Long, Long)])]
         var voteCount = 0
         val newLabel = vlabel.map { tv =>
           val ts     = tv._1
-          val Oldlab = tv._2._1
-          val Curlab = tv._2._2
+          val Curlab = tv._2
           // Get weights/labels of neighbours of vertex at time ts
           val nei_ts_freq = weightFunction(vertex, ts) // ID -> freq
-          val newlab = if (nei_ts_freq.nonEmpty) {
+          var newlab = if (nei_ts_freq.nonEmpty) {
             val nei_labs = msgQueue
               .filter(x => nei_ts_freq.keySet.contains(x._1)) // filter messages from neighbours at time ts only
               .map { msg =>
@@ -56,9 +55,10 @@ class MultiLayerLPA(args: Array[String]) extends LPA(args) {
 
             //Get labels of past/future instances of vertex //IMlater: links between non consecutive layers should persist or at least degrade?
             if (vlabel.contains(ts - snapshotSize))
-              nei_labs.append((vlabel(ts - snapshotSize)._2, interLayerWeights(omega, vertex, ts - snapshotSize)))
+              nei_labs.append((vlabel(ts - snapshotSize), interLayerWeights(omega, vertex, ts - snapshotSize)))
             if (vlabel.contains(ts + snapshotSize))
-              nei_labs.append((vlabel(ts + snapshotSize)._2, interLayerWeights(omega, vertex, ts)))
+              nei_labs.append((vlabel(ts + snapshotSize), interLayerWeights(omega, vertex, ts)))
+
             // Get label most prominent in neighborhood of vertex
 
             val max_freq = nei_labs.groupBy(_._1).mapValues(_.map(_._2).sum)
@@ -66,16 +66,13 @@ class MultiLayerLPA(args: Array[String]) extends LPA(args) {
           } else Curlab
 
           // Update node label and broadcast
-          (ts, newlab match {
-            case Curlab | Oldlab =>
-              voteCount += 1
-              if (Curlab > Oldlab) (Oldlab, Curlab) else (Curlab, Oldlab)
-            case _ => (Curlab, newlab)
-          })
+          if (newlab == Curlab) voteCount += 1
+          newlab = if (rnd.nextFloat() < SP) Curlab else newlab
+          (ts,newlab)
         }.toArray
 
         vertex.setState("mlpalabel", newLabel)
-        val message = (vertex.ID(), newLabel.map(x => (x._1, x._2._2)))
+        val message = (vertex.ID(), newLabel)
         vertex.messageAllNeighbours(message)
 
         // Vote to halt if all instances of vertex haven't changed their labels
@@ -84,36 +81,45 @@ class MultiLayerLPA(args: Array[String]) extends LPA(args) {
     } catch {
       case e: Exception => println("Something went wrong with mLPA!", e)
     }
-    if (workerID == 1)
+    if (debug & (workerID==1))
       println(
               s"Superstep: ${view.superStep()}    Time: ${LocalDateTime.now()}   ExecTime: ${System.currentTimeMillis() - t1}"
       )
   }
 
-  def interLayerWeights(x: String, v: VertexVisitor, ts: Long): Double =
+  def interLayerWeights(x: String, v: VertexVisitor, ts: Long): Float =
     x match {
       case "average" =>
-        val neilabs = weightFunction(v, ts) //imlater: a more conceptual implementation of temporal weights
+        val neilabs = weightFunction(v, ts)
         neilabs.values.sum / neilabs.size
-      case _ => omega.toDouble //imlater: possibly changing this to Double
+      case _ => omega.toFloat
     }
 
-  def weightFunction(v: VertexVisitor, ts: Long): ParMap[Long, Double] = //im: change this to use range and filter dead links
-    (v.getInCEdgesBetween(ts - snapshotSize, ts) ++ v.getOutEdgesBetween(ts - snapshotSize, ts))
-      .map(e => (e.ID(), e.getPropertyValue(weight).getOrElse(1.0).asInstanceOf[Double]))
-      .groupBy(_._1)
-      .mapValues(x => x.map(_._2).sum / x.size) // (ID -> Freq)
+  def weightFunction(v: VertexVisitor, ts: Long): ParMap[Long, Float] = {
+    var nei_weights =
+      (v.getInCEdgesBetween(ts - snapshotSize, ts) ++ v.getOutEdgesBetween(ts - snapshotSize, ts)).map(e =>
+        (e.ID(), e.getPropertyValue(weight).getOrElse(1.0F).asInstanceOf[Float])
+      )
+    if (scaled) {
+      val scale = scaling(nei_weights.map(_._2).toArray)
+      nei_weights = nei_weights.map(x => (x._1, x._2 / scale))
+
+    }
+    nei_weights.groupBy(_._1).mapValues(x => x.map(_._2).sum) // (ID -> Freq)
+  }
+
+  def scaling(freq: Array[Float]): Float = math.sqrt(freq.map(math.pow(_, 2)).sum).toFloat
 
   override def returnResults(): Any =
     view
       .getVertices()
       .map(vertex =>
         (
-                vertex.getState[Array[(Long, (Long, Long))]]("mlpalabel"),
+                vertex.getState[Array[(Long, Long)]]("mlpalabel"),
                 vertex.getPropertyValue("Word").getOrElse(vertex.ID()).toString
         )
       )
-      .flatMap(f => f._1.map(x => (x._2._2, f._2 + "_" + x._1.toString)))
+      .flatMap(f => f._1.map(x => (x._2, f._2 + "_" + x._1.toString)))
       //      .flatMap(f =>  f._1.toArray.map(x => (x._2._2, "\""+x._1.toString+f._2+"\"")))
       .groupBy(f => f._1)
       .map(f => (f._1, f._2.map(_._2)))
--- src/main/scala/com/raphtory/dev/algorithms/MultiLayerLPAVW.scala
+++ src/main/scala/com/raphtory/dev/algorithms/MultiLayerLPAVW.scala
@@ -14,37 +14,35 @@ object MultiLayerLPAVW {
 }
 
 class MultiLayerLPAVW(args: Array[String]) extends LPA(args) {
-  //args = [top, weight, maxiter, start, end, layer-size, omega]
+  //args = [top, weight, maxiter, start, end, layer-size, omega, scaled]
   val snapshotSize: Long        = args(5).toLong
   val startTime: Long           = args(3).toLong //* snapshotSize //imlater: change this when done with wsdata
   val endTime: Long             = args(4).toLong //* snapshotSize
   val snapshots: Iterable[Long] = for (ts <- startTime to endTime by snapshotSize) yield ts
-  val o: Double                 = if (arg.length < 7) 0.1 else args(6).toDouble
-  val omega: Array[Double]      = (o to 1.0 by o).toArray
-
-  val rnd    = new scala.util.Random
-  val scaled = true
-
+  val o: Float                 = if (arg.length < 7) 0.1F else args(6).toFloat
+  val omega: Array[Float]      = (o to 1.0F by o).toArray
+  val scaled: Boolean = if (arg.length < 8) true else args(7).toBoolean
+  var t1 = System.currentTimeMillis()
   override def setup(): Unit =
     view.getVertices().foreach { vertex =>
       // Assign random labels for all instances in time of a vertex as Map(ts, lab)
-      val labels = //im: send tuples instead of TreeMap and build the TM for processing only
+      val labels =
         snapshots
           .filter(t => vertex.aliveAtWithWindow(t, snapshotSize))
-          .map(x => (x, (rnd.nextLong(), rnd.nextLong())))
+          .map(x => (x,  rnd.nextLong()))
           .toArray
 //          .map(x => (x, (vertex.ID()+x,vertex.ID()-x))).toArray
       val tlabels = omega.flatMap(_ => labels)
       vertex.setState("mlpalabel", tlabels)
-      val message = (vertex.ID(), tlabels.map(x => (x._1, x._2._2)))
+      val message = (vertex.ID(), tlabels)
       vertex.messageAllNeighbours(message)
     }
 
   override def analyse(): Unit = {
-    val t1 = System.currentTimeMillis()
+
     try {
       view.getMessagedVertices().foreach { vertex =>
-        val vlabel    = vertex.getState[Array[(Long, (Long, Long))]]("mlpalabel").groupBy(_._1).mapValues(x => x.map(_._2))
+        val vlabel    = vertex.getState[Array[(Long, Long)]]("mlpalabel").groupBy(_._1).mapValues(x => x.map(_._2))
         val msgQueue  = vertex.messageQueue[(Long, Array[(Long, Long)])]
         var voteCount = 0
         val newLabel = vlabel
@@ -62,70 +60,67 @@ class MultiLayerLPAVW(args: Array[String]) extends LPA(args) {
 
             //Get labels of past/future instances of vertex //IMlater: links between non consecutive layers should persist or at least degrade?
             if (vlabel.contains(ts - snapshotSize))
-              vlabel(ts - snapshotSize).zipWithIndex.foreach(x => nei_labs.append((x._2, x._1._2, omega(x._2))))
+              vlabel(ts - snapshotSize).zipWithIndex.foreach(x => nei_labs.append((x._2, x._1, omega(x._2))))
             if (vlabel.contains(ts + snapshotSize))
-              vlabel(ts + snapshotSize).zipWithIndex.foreach(x => nei_labs.append((x._2, x._1._2, omega(x._2))))
+              vlabel(ts + snapshotSize).zipWithIndex.foreach(x => nei_labs.append((x._2, x._1, omega(x._2))))
 
             nei_labs.groupBy(_._1).values.toArray.map { x =>
               val w      = x.head._1
-              val Oldlab = tv._2(w)._1
-              val Curlab = tv._2(w)._2
+              val Curlab = tv._2(w)
 
               // Get label most prominent in neighborhood of vertex
-              val newlab = if (x.nonEmpty) {
+              var newlab = if (x.nonEmpty) {
                 val max_freq = x.groupBy(_._2).mapValues(_.map(_._3).sum)
                 max_freq.filter(_._2 == max_freq.values.max).keySet.max
               } else Curlab
 
               // Update node label and broadcast
-              (ts, newlab match {
-                case Curlab | Oldlab =>
-                  voteCount += 1
-                  if (Curlab > Oldlab) (Oldlab, Curlab) else (Curlab, Oldlab)
-                case _ => (Curlab, newlab)
-              })
-
+              if (newlab == Curlab) voteCount += 1
+              newlab = if (rnd.nextFloat() < SP) Curlab else newlab
+              (ts,newlab)
             }
           }
           .toArray
           .flatten
         vertex.setState("mlpalabel", newLabel)
-        val message = (vertex.ID(), newLabel.map(x => (x._1, x._2._2)))
+        val message = (vertex.ID(), newLabel)
         vertex.messageAllNeighbours(message)
 
         // Vote to halt if all instances of vertex haven't changed their labels
-        if (voteCount == vlabel.size * omega.length) vertex.voteToHalt()
+        if (voteCount > (vlabel.size * omega.length * 0.8).toInt) vertex.voteToHalt()
       }
     } catch {
-      case e: Exception => println("Something went wrong with mLPA!", e)
+      case e: Exception => println("Something went wrong with mLPAv!", e)
     }
-    if (workerID == 1)
+    if (debug & (workerID==1)) {
       println(
               s"Superstep: ${view.superStep()}    Time: ${LocalDateTime.now()}   ExecTime: ${System.currentTimeMillis() - t1}"
       )
+      t1 = System.currentTimeMillis()
+    }
   }
 
-  def weightFunction(v: VertexVisitor, ts: Long): ParMap[Long, Double] = {
+  def weightFunction(v: VertexVisitor, ts: Long): ParMap[Long, Float] = {
     var nei_weights =
       (v.getInCEdgesBetween(ts - snapshotSize, ts) ++ v.getOutEdgesBetween(ts - snapshotSize, ts)).map(e =>
-        (e.ID(), e.getPropertyValue(weight).getOrElse(1.0).asInstanceOf[Double])
+        (e.ID(), e.getPropertyValue(weight).getOrElse(1.0F).asInstanceOf[Float])
       )
     if (scaled) {
       val scale = scaling(nei_weights.map(_._2).toArray)
       nei_weights = nei_weights.map(x => (x._1, x._2 / scale))
 
     }
-    nei_weights.groupBy(_._1).mapValues(x => x.map(_._2).sum / x.size) // (ID -> Freq)
+    nei_weights.groupBy(_._1).mapValues(x => x.map(_._2).sum) // (ID -> Freq)
   }
 
-  def scaling(freq: Array[Double]): Double = math.sqrt(freq.map(math.pow(_, 2)).sum)
+  def scaling(freq: Array[Float]): Float = math.sqrt(freq.map(math.pow(_, 2)).sum).toFloat
 
   override def returnResults(): Any =
     view
       .getVertices()
       .map(vertex =>
         (
-                vertex.getState[Array[(Long, (Long, Long))]]("mlpalabel"),
+                vertex.getState[Array[(Long, Long)]]("mlpalabel"),
                 vertex.getPropertyValue("Word").getOrElse(vertex.ID()).toString
         )
       )
@@ -133,11 +128,11 @@ class MultiLayerLPAVW(args: Array[String]) extends LPA(args) {
         f._1
           .groupBy(_._1)
           .mapValues(x => x.map(_._2))
-          .flatMap(ts => ts._2.zipWithIndex.map { case ((_, lab), w) => (omega(w), lab, s"""${f._2}_${ts._1}""") })
+          .flatMap(ts => ts._2.zipWithIndex.map { case (lab, w) => (omega(w), lab, s"""${f._2}_${ts._1}""") })
       )
 
   override def processResults(results: ArrayBuffer[Any], timestamp: Long, viewCompleteTime: Long): Unit = {
-    val endResults = results.asInstanceOf[ArrayBuffer[ParIterable[(Double, Long, String)]]]
+    val endResults = results.asInstanceOf[ArrayBuffer[ParIterable[(Float, Long, String)]]]
     try {
       println(s"Printing output to $output_file")
       val groupedW = endResults.flatten.groupBy(f => f._1).mapValues(x => x.groupBy(_._2).mapValues(x => x.map(_._3)))
@@ -154,11 +149,11 @@ class MultiLayerLPAVW(args: Array[String]) extends LPA(args) {
 
         output_file match {
           case "" =>
-            //            val commtxt = communities.map(x => s"""[${x.mkString(",")}]""")
+                        val commtxt = communities.map(x => s"""[${x.mkString(",")}]""")
             val text =
               s"""{"time":$timestamp, "omega": $w, "top5":[${top5
                 .mkString(",")}],"total":$total,"totalIslands":$totalIslands,""" +
-                //          s""" "communities": [${commtxt.mkString(",")}],""" +
+                          s""" "communities": [${commtxt.mkString(",")}],""" +
                 s"""viewTime":$viewCompleteTime}"""
             println(text)
           case _ =>
