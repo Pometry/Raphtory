d5ee6679a3338030d90e41f7931fd04ae466e25f
imanehaf <hafmane@gmail.com>
2021-06-10 16:58:53 +0100

    updated classes app.conf

--- src/main/scala/com/raphtory/dev/algorithms/MultiLayerLPA.scala
+++ src/main/scala/com/raphtory/dev/algorithms/MultiLayerLPA.scala
@@ -1,17 +1,16 @@
 package com.raphtory.dev.algorithms
 
+import com.github.nscala_time.time.Imports.LocalDateTime
 import com.raphtory.algorithms.LPA
 import com.raphtory.core.analysis.entity.Vertex
 
-import java.time.LocalDateTime
-import scala.collection.parallel.ParMap
 
 object MultiLayerLPA {
   def apply(args: Array[String]): MultiLayerLPA = new MultiLayerLPA(args)
 }
 
 class MultiLayerLPA(args: Array[String]) extends LPA(args) {
-  //args = [top, weight, maxiter, start, end, layer-size, omega, stickiness prob, scaled]
+  //args = [top, weight, maxiter, start, end, layer-size, omega, stickiness prob, scaled, label initial prob]
   val snapshotSize: Long        = args(5).toLong
   val startTime: Long           = args(3).toLong
   val endTime: Long             = args(4).toLong
@@ -19,76 +18,72 @@ class MultiLayerLPA(args: Array[String]) extends LPA(args) {
   val omega: String             = if (arg.length < 7) "1" else args(6)
   override val SP: Float = if (arg.length < 8) 0.2F else args(7).toFloat
   val scaled: Boolean = if (arg.length < 9) true else args(8).toBoolean
-  val filter: Float = if (arg.length < 10) 1.0F else args(9).toFloat
+  val commprob: Float = if (arg.length < 10) 1.0F else args(9).toFloat
 
   override def setup(): Unit =
     view.getVertices().foreach { vertex =>
       // Assign random labels for all instances in time of a vertex as Map(ts, lab)
-    val slabel = rnd.nextLong()
+      val slabel = rnd.nextLong()
+      val prob = rnd.nextFloat()
       val tlabels =
         snapshots
           .filter(ts => vertex.aliveAtWithWindow(ts, snapshotSize))
-//          .map(ts => (ts, rnd.nextLong()))
-          .map(ts => (ts, slabel))
+          //          .map(ts => (ts, rnd.nextLong()))
+          .map(ts => (ts, if ( prob < commprob) slabel else rnd.nextLong()))
           .toArray
       vertex.setState("mlpalabel", tlabels)
-      val message = (vertex.ID(), tlabels.map(x => (x._1, x._2)))
+      val message = (vertex.ID(), tlabels.map(x => (x._1, x._2)).toList)
       vertex.messageAllNeighbours(message)
     }
 
   override def analyse(): Unit = {
     val t1 = System.currentTimeMillis()
-    try{
-      view.getMessagedVertices().foreach { vertex =>
-        val vlabel    = vertex.getState[Array[(Long, Long)]]("mlpalabel").toMap
-        val msgQueue  = vertex.messageQueue[(Long, Array[(Long, Long)])]
-        var voteCount = 0
-        val newLabel = vlabel.map { tv =>
-          val ts     = tv._1
-          val Curlab = tv._2
-          // Get weights/labels of neighbours of vertex at time ts
-          val nei_ts_freq = weightFunction(vertex, ts) // ID -> freq
-          var newlab = if (nei_ts_freq.nonEmpty) { //im: put this a bit lower
-            val nei_labs = msgQueue
-              .filter(x => nei_ts_freq.keySet.contains(x._1)) // filter messages from neighbours at time ts only
-              .map { msg =>
-                val freq     = nei_ts_freq(msg._1)
-                val label_ts = msg._2.filter(_._1 == ts).head._2
-                (label_ts, freq) //get label at time ts -> (lab, freq)
-              }
+    view.getMessagedVertices().foreach { vertex =>
+      val vlabel    = vertex.getState[Array[(Long, Long)]]("mlpalabel").toMap
+      val msgQueue  = vertex.messageQueue[(Long, List[(Long, Long)])]
+      var voteCount = 0
+      val newLabel = vlabel.map { tv =>
+        val ts     = tv._1
+        val Curlab = tv._2
+        // Get weights/labels of neighbours of vertex at time ts
+        val nei_ts_freq = weightFunction(vertex, ts) // ID -> freq
+        var newlab = if (nei_ts_freq.nonEmpty) { //im: put this a bit lower
+          val nei_labs = msgQueue
+            .filter(x => nei_ts_freq.keySet.contains(x._1)) // filter messages from neighbours at time ts only
+            .map { msg =>
+              val freq     = nei_ts_freq(msg._1)
+              val label_ts = msg._2.filter(_._1 == ts).head._2
+              (label_ts, freq) //get label at time ts -> (lab, freq)
+            }
 
-             if (vlabel.contains(ts - snapshotSize))
-              nei_labs ++ List((vlabel(ts - snapshotSize), interLayerWeights(omega, vertex, ts - snapshotSize)))
-            if (vlabel.contains(ts + snapshotSize))
-              nei_labs ++ List((vlabel(ts + snapshotSize), interLayerWeights(omega, vertex, ts)))
-//            nei_labs.appendAll(vlabel.filter(_._1!=ts).map(ts=>(ts._2,omega.toFloat)))
-            // Get label most prominent in neighborhood of vertex
-            selectiveProc(v = vertex, ts, gp = nei_labs.map(_._1).toArray )
-            val max_freq = nei_labs.groupBy(_._1).mapValues(_.map(_._2).sum)
-            max_freq.filter(_._2 == max_freq.values.max).keySet.max
-          } else Curlab
+          if (vlabel.contains(ts - snapshotSize))
+            nei_labs ++ List((vlabel(ts - snapshotSize), interLayerWeights(omega, vertex, ts - snapshotSize)))
+          if (vlabel.contains(ts + snapshotSize))
+            nei_labs ++ List((vlabel(ts + snapshotSize), interLayerWeights(omega, vertex, ts)))
+          //            nei_labs.appendAll(vlabel.filter(_._1!=ts).map(ts=>(ts._2,omega.toFloat)))
+          // Get label most prominent in neighborhood of vertex
+          selectiveProc(v = vertex, ts, gp = nei_labs.map(_._1).toArray )
+          val max_freq = nei_labs.groupBy(_._1).mapValues(_.map(_._2).sum)
+          max_freq.filter(_._2 == max_freq.values.max).keySet.max
+        } else Curlab
 
-          // Update node label and broadcast
-          if (newlab == Curlab) voteCount += 1
-          newlab = if (rnd.nextFloat() < SP) Curlab else newlab
-          (ts,newlab)
-        }.toArray
+        // Update node label and broadcast
+        if (newlab == Curlab) voteCount += 1
+        newlab = if (rnd.nextFloat() < SP) Curlab else newlab
+        (ts,newlab)
+      }.toArray
 
-        vertex.setState("mlpalabel", newLabel)
-        val message = (vertex.ID(), newLabel)
-        vertex.messageAllNeighbours(message)
+      vertex.setState("mlpalabel", newLabel)
+      val message = (vertex.ID(), newLabel.toList)
+      vertex.messageAllNeighbours(message)
 
-        // Vote to halt if all instances of vertex haven't changed their labels
-        if (voteCount == vlabel.size) vertex.voteToHalt()
-      }
-    } catch {
-      case e: Exception => println("Something went wrong with mLPA!", e)
+      // Vote to halt if all instances of vertex haven't changed their labels
+      if (voteCount == vlabel.size) vertex.voteToHalt()
     }
-
-    if (debug & (workerID==1))
-      println(
-              s"Superstep: ${view.superStep}    Time: ${LocalDateTime.now()}   ExecTime: ${System.currentTimeMillis() - t1}"
-      )
+        if (workerID==1)
+          println(
+                  s"Superstep: ${view.superStep}    Time: ${LocalDateTime.now()}   ExecTime: ${System.currentTimeMillis() - t1}"
+          )
   }
   def selectiveProc(v: Vertex, ts: Long, gp: Array[Long]): Unit = {}
   def interLayerWeights(x: String, v: Vertex, ts: Long): Float =
@@ -110,13 +105,13 @@ class MultiLayerLPA(args: Array[String]) extends LPA(args) {
       nei_weights = nei_weights.map(x => (x._1, x._2 / scale))
 
     }
-//    nei_weights =
-      var nei_filt = nei_weights.toArray.sortBy(-_._2)
-      nei_filt = nei_filt.take((nei_weights.size*filter).toInt)
-//    nei_weights
+    //    nei_weights =
+    //      var nei_filt = nei_weights.toArray.sortBy(-_._2)
+    //      nei_filt = nei_filt.take((nei_weights.size*filter).toInt)
+    //    nei_weights
 
-      nei_filt = if (nei_filt.nonEmpty) nei_filt else nei_weights.toArray.take(1)
-      nei_filt.groupBy(_._1).mapValues(x => x.map(_._2).sum) // (ID -> Freq)
+    //      nei_filt = if (nei_filt.nonEmpty) nei_filt else nei_weights.toArray.take(1)
+    nei_weights.toArray.groupBy(_._1).mapValues(x => x.map(_._2).sum) // (ID -> Freq)
   }
 
   def scaling(freq: Array[Float]): Float = math.sqrt(freq.map(math.pow(_, 2)).sum).toFloat
@@ -126,8 +121,8 @@ class MultiLayerLPA(args: Array[String]) extends LPA(args) {
       .getVertices()
       .map(vertex =>
         (
-                vertex.getState[Array[(Long, Long)]]("mlpalabel"),
-                vertex.getPropertyValue("Word").getOrElse(vertex.ID()).toString
+          vertex.getState[Array[(Long, Long)]]("mlpalabel"),
+          vertex.getPropertyValue("Word").getOrElse(vertex.ID()).toString
         )
       )
       .flatMap(f => f._1.map(x => (x._2, f._2 + "_" + x._1.toString)))
--- src/main/scala/com/raphtory/dev/allcommands/AllCommandTest.scala
+++ src/main/scala/com/raphtory/dev/allcommands/AllCommandTest.scala
@@ -1,34 +1,37 @@
 package com.raphtory.dev.allcommands
 
 import com.raphtory.RaphtoryComponent
+
 import scala.language.postfixOps
 
 object AllCommandTest extends App {
   val partitionCount =3
   val routerCount =3
+  val source = "com.raphtory.dev.allcommands.AllCommandsSpout"
+  val builder = "com.raphtory.dev.allcommands.AllCommandsBuilder"
   new RaphtoryComponent("seedNode",partitionCount,routerCount,1600)
   new RaphtoryComponent("watchdog",partitionCount,routerCount,1601)
   new RaphtoryComponent("analysisManager",partitionCount,routerCount,1602)
-  new RaphtoryComponent("spout",partitionCount,routerCount,1603,"com.raphtory.dev.allcommands.AllCommandsSpout")
-  new RaphtoryComponent("router",partitionCount,routerCount,1604,"com.raphtory.dev.allcommands.AllCommandsBuilder")
-  new RaphtoryComponent("router",partitionCount,routerCount,1605,"com.raphtory.dev.allcommands.AllCommandsBuilder")
-  new RaphtoryComponent("router",partitionCount,routerCount,1606,"com.raphtory.dev.allcommands.AllCommandsBuilder")
-//  new RaphtoryComponent("router",partitionCount,routerCount,1607,"com.raphtory.dev.allcommands.AllCommandsBuilder")
-//  new RaphtoryComponent("router",partitionCount,routerCount,1608,"com.raphtory.dev.allcommands.AllCommandsBuilder")
-//  new RaphtoryComponent("router",partitionCount,routerCount,1609,"com.raphtory.dev.allcommands.AllCommandsBuilder")
-//  new RaphtoryComponent("router",partitionCount,routerCount,1610,"com.raphtory.dev.allcommands.AllCommandsBuilder")
-//  new RaphtoryComponent("router",partitionCount,routerCount,1611,"com.raphtory.dev.allcommands.AllCommandsBuilder")
-//  new RaphtoryComponent("router",partitionCount,routerCount,1612,"com.raphtory.dev.allcommands.AllCommandsBuilder")
-//  new RaphtoryComponent("router",partitionCount,routerCount,1613,"com.raphtory.dev.allcommands.AllCommandsBuilder")
+  new RaphtoryComponent("spout",partitionCount,routerCount,1603,source)
+  new RaphtoryComponent("router",partitionCount,routerCount,1604,builder)
+  new RaphtoryComponent("router",partitionCount,routerCount,1605,builder)
+  new RaphtoryComponent("router",partitionCount,routerCount,1606,builder)
+  //  new RaphtoryComponent("router",partitionCount,routerCount,1607,builder)
+  //  new RaphtoryComponent("router",partitionCount,routerCount,1608,builder)
+  //  new RaphtoryComponent("router",partitionCount,routerCount,1609,builder)
+  //  new RaphtoryComponent("router",partitionCount,routerCount,1610,builder)
+  //  new RaphtoryComponent("router",partitionCount,routerCount,1611,builder)
+  //  new RaphtoryComponent("router",partitionCount,routerCount,1612,builder)
+  //  new RaphtoryComponent("router",partitionCount,routerCount,1613,builder)
   new RaphtoryComponent("partitionManager",partitionCount,routerCount,1614)
   new RaphtoryComponent("partitionManager",partitionCount,routerCount,1615)
   new RaphtoryComponent("partitionManager",partitionCount,routerCount,1616)
-//  new RaphtoryComponent("partitionManager",partitionCount,routerCount,1617)
-//  new RaphtoryComponent("partitionManager",partitionCount,routerCount,1618)
-//  new RaphtoryComponent("partitionManager",partitionCount,routerCount,1619)
-//  new RaphtoryComponent("partitionManager",partitionCount,routerCount,1620)
-//  new RaphtoryComponent("partitionManager",partitionCount,routerCount,1621)
-//  new RaphtoryComponent("partitionManager",partitionCount,routerCount,1622)
-//  new RaphtoryComponent("partitionManager",partitionCount,routerCount,1623)
+  //  new RaphtoryComponent("partitionManager",partitionCount,routerCount,1617)
+  //  new RaphtoryComponent("partitionManager",partitionCount,routerCount,1618)
+  //  new RaphtoryComponent("partitionManager",partitionCount,routerCount,1619)
+  //  new RaphtoryComponent("partitionManager",partitionCount,routerCount,1620)
+  //  new RaphtoryComponent("partitionManager",partitionCount,routerCount,1621)
+  //  new RaphtoryComponent("partitionManager",partitionCount,routerCount,1622)
+  //  new RaphtoryComponent("partitionManager",partitionCount,routerCount,1623)
 
 }
