40d923b014174edeeda5d316344c4286e0792f77
Miratepuffin <b.a.steer@qmul.ac.uk>
2021-06-17 15:43:41 +0100

    Package Refactoring

---
+++ src/main/scala/com/raphtory/core/actors/analysismanager/AnalysisManager.scala
@@ -0,0 +1,226 @@
+package com.raphtory.core.actors.analysismanager
+
+import akka.actor.ActorLogging
+import akka.actor.ActorRef
+import akka.actor.Props
+import akka.actor.Stash
+import akka.cluster.pubsub.DistributedPubSub
+import akka.cluster.pubsub.DistributedPubSubMediator
+import com.raphtory.core.actors.analysismanager.AnalysisManager.Message.{JobKilled, _}
+import com.raphtory.core.actors.analysismanager.AnalysisManager.State
+import com.raphtory.core.actors.analysismanager.AnalysisRestApi.message._
+import com.raphtory.core.actors.analysismanager.tasks.AnalysisTask.Message.FailedToCompile
+import com.raphtory.core.actors.analysismanager.tasks.subtasks._
+import com.raphtory.core.actors.clustermanagement.WatchDog.Message._
+import com.raphtory.core.actors.RaphtoryActor
+import com.raphtory.core.analysis.api.{AggregateSerialiser, Analyser, LoadExternalAnalyser}
+import com.raphtory.core.utils.AnalyserUtils
+
+import scala.concurrent.ExecutionContext
+import scala.concurrent.duration._
+import scala.language.postfixOps
+import scala.util.Failure
+import scala.util.Success
+import scala.util.Try
+
+final case class AnalysisManager() extends RaphtoryActor with ActorLogging with Stash {
+  implicit val executionContext: ExecutionContext = context.system.dispatcher
+
+  final protected val mediator = DistributedPubSub(context.system).mediator
+  mediator ! DistributedPubSubMediator.Put(self)
+
+  override def preStart() {
+    context.system.scheduler.scheduleOnce(Duration(5, SECONDS), self, StartUp)
+  }
+
+  override def receive: Receive = init()
+
+  def init(): Receive = {
+    case StartUp =>
+      mediator ! new DistributedPubSubMediator.Send(
+              "/user/WatchDog",
+              ClusterStatusRequest
+      ) //ask if the cluster is safe to use
+
+    case ClusterStatusResponse(clusterUp, _, _) =>
+      if (clusterUp) mediator ! new DistributedPubSubMediator.Send("/user/WatchDog", RequestPartitionCount)
+      else context.system.scheduler.scheduleOnce(Duration(1, SECONDS), self, StartUp)
+
+    case PartitionsCount(count) =>
+      context.become(work(State(count, Map.empty)))
+      unstashAll()
+
+    case _: AnalysisRequest => stash()
+
+    case unhandled => log.error(s"unexpected message $unhandled during init stage")
+  }
+
+  def work(state: State): Receive = {
+    case StartUp => // do nothing as it is ready
+
+    case PartitionsCount(newValue) =>
+      context.become(work(state.copy(managerCount = newValue)))
+
+    case request: LiveAnalysisRequest =>
+      val newState = state.updateCurrentTask(_ ++ spawnLiveAnalysisManager(state.managerCount, request))
+      context.become(work(newState))
+
+    case request: ViewAnalysisRequest =>
+      val newState = state.updateCurrentTask(_ ++ spawnViewAnalysisManager(state.managerCount, request))
+      context.become(work(newState))
+
+    case request: RangeAnalysisRequest =>
+      val newState = state.updateCurrentTask(_ ++ spawnRangeAnalysisManager(state.managerCount, request))
+      context.become(work(newState))
+
+    case RequestResults(jobId) =>
+      state.currentTasks.get(jobId) match {
+        case Some(actor) =>
+          actor forward RequestResults(jobId)
+        case None => sender ! JobDoesntExist
+      }
+
+    case req: KillTask =>
+      state.currentTasks.get(req.jobId) match {
+        case Some(actor) =>
+          context.become(work(state.updateCurrentTask(_ - req.jobId)))
+          actor forward KillTask
+        case None => sender ! JobDoesntExist
+      }
+
+    case unhandled => log.error(s"unexpected message $unhandled")
+  }
+
+  private def spawnLiveAnalysisManager(managerCount: Int, request: LiveAnalysisRequest): Option[(String, ActorRef)] = {
+    import request._
+    val jobId = analyserName + "_" + System.currentTimeMillis()
+    log.info(s"Live Analysis Task received, your job ID is $jobId")
+
+    getAnalyser(analyserName, args, request.rawFile).map {
+      case (newAnalyser, analyser) =>
+        val ref = context.system.actorOf(
+                Props(
+                        LiveAnalysisTask(
+                                managerCount,
+                                jobId,
+                                args,
+                                analyser,
+                                getSerialiser(serialiserName), //TODO tidy up
+                                repeatTime,
+                                eventTime,
+                                windowSet,
+                                newAnalyser,
+                                rawFile
+                        )
+                ).withDispatcher("analysis-dispatcher"),
+                s"LiveAnalysisTask_$jobId"
+        )
+
+        (jobId, ref)
+    }
+  }
+
+  private def spawnViewAnalysisManager(managerCount: Int, request: ViewAnalysisRequest): Option[(String, ActorRef)] = {
+    import request._
+    val jobId = analyserName + "_" + System.currentTimeMillis()
+    log.info(s"View Analysis Task received, your job ID is $jobId")
+    getAnalyser(analyserName, args, rawFile).map {
+      case (newAnalyser, analyser) =>
+        val ref =
+
+          context.system.actorOf(
+                  Props(
+                          ViewAnalysisTask(
+                                  managerCount,
+                                  jobId,
+                                  args,
+                                  analyser,
+                                  getSerialiser(serialiserName), //TODO tidy up
+                                  timestamp,
+                                  windowSet,
+                                  newAnalyser,
+                                  rawFile
+                          )
+                  ).withDispatcher("analysis-dispatcher"),
+                  s"ViewAnalysisTask_$jobId"
+          )
+        (jobId, ref)
+    }
+  }
+
+  private def spawnRangeAnalysisManager(
+      managerCount: Int,
+      request: RangeAnalysisRequest
+  ): Option[(String, ActorRef)] = {
+    import request._
+    val jobId = analyserName + "_" + System.currentTimeMillis()
+    log.info(
+            s"Range Analysis Task received, your job ID is $jobId, running $analyserName, between $start and $end jumping $jump at a time."
+    )
+    getAnalyser(analyserName, args, rawFile).map {
+      case (newAnalyser, analyser) =>
+        val ref =
+          context.system.actorOf(
+                  Props(
+                          RangeAnalysisTask(
+                                  managerCount,
+                                  jobId,
+                                  args,
+                                  analyser,
+                                  getSerialiser(serialiserName), //TODO tidy up
+                                  start,
+                                  end,
+                                  jump,
+                                  windowSet,
+                                  newAnalyser,
+                                  rawFile
+                          )
+                  ).withDispatcher("analysis-dispatcher"),
+                  s"RangeAnalysisTask_$jobId"
+          )
+        (jobId, ref)
+    }
+  }
+
+  private def getAnalyser(
+      analyserName: String,
+      args: Array[String],
+      rawFile: String
+  ): Option[(Boolean, Analyser[Any])] = {
+    val tryExist = AnalyserUtils.loadPredefinedAnalyser(analyserName, args)
+
+    tryExist match {
+      case Success(analyser) => Some((false, analyser))
+      case Failure(_)        => compileNewAnalyser(rawFile, args).map((true, _))
+    }
+  }
+
+  private def getSerialiser(
+       serialiserName: String,
+  ):  AggregateSerialiser= AnalyserUtils.loadPredefinedSerialiser(serialiserName)
+
+
+  private def compileNewAnalyser(rawFile: String, args: Array[String]): Option[Analyser[Any]] =
+    AnalyserUtils.compileNewAnalyser(rawFile, args) match {
+      case Success(analyser) => Some(analyser)
+      case Failure(e) =>
+        sender ! FailedToCompile(e.getStackTrace.mkString(","))
+        log.info(e.getMessage)
+        None
+    }
+}
+
+object AnalysisManager {
+  private case class State(managerCount: Int, currentTasks: Map[String, ActorRef]) {
+    def updateCurrentTask(f: Map[String, ActorRef] => Map[String, ActorRef]): State =
+      copy(currentTasks = f(currentTasks))
+  }
+  object Message {
+    case object StartUp
+    case class RequestResults(jobId: String)
+    case class KillTask(jobId: String)
+    case object JobKilled
+    case class ResultsForApiPI(results: Array[String])
+    case object JobDoesntExist
+  }
+}
---
+++ src/main/scala/com/raphtory/core/actors/analysismanager/AnalysisRestApi.scala
@@ -0,0 +1,168 @@
+package com.raphtory.core.actors.analysismanager
+
+import akka.actor._
+import akka.cluster.pubsub._
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
+import akka.http.scaladsl.server.Directives._
+import akka.http.scaladsl.server.Route
+import akka.pattern.ask
+import akka.stream.ActorMaterializer
+import akka.util.Timeout
+import com.raphtory.core.actors.analysismanager.AnalysisManager.Message._
+import com.raphtory.core.actors.analysismanager.AnalysisRestApi.http._
+import com.raphtory.core.actors.analysismanager.AnalysisRestApi.message._
+import spray.json.DefaultJsonProtocol._
+import spray.json.RootJsonFormat
+
+import scala.concurrent.ExecutionContext
+import scala.concurrent.duration._
+
+final case class AnalysisRestApi(system: ActorSystem) {
+  implicit val systemImpl: ActorSystem = system
+  implicit val materializer: ActorMaterializer = ActorMaterializer()
+  implicit val ex: ExecutionContext = system.dispatcher
+  implicit val timeout: Timeout = 15.seconds
+  private val port              = 8081
+  private val iface             = "0.0.0.0" //config//java.net.InetAddress.getLocalHost().getHostAddress()
+  private val mediator          = DistributedPubSub(system).mediator
+
+  val route: Route = {
+    (path("LiveAnalysisRequest") & post) {
+        entity(as[LiveAnalysisPost]) { in =>
+          val request = LiveAnalysisRequest(
+            in.analyserName,
+            in.serialiserName,
+            in.repeatTime.getOrElse(0),
+            in.eventTime.getOrElse(false),
+            in.windowSet.getOrElse(List.empty),
+            in.args.getOrElse(Array()),
+            in.rawFile.getOrElse("")
+          )
+          mediator ! new DistributedPubSubMediator.Send("/user/AnalysisManager", request)
+          complete(s"Your Task Has been successfully submitted as a Live Analysis Task!")
+        }
+    } ~ (path("ViewAnalysisRequest") & post) {
+      entity(as[ViewAnalysisPost]) { in =>
+        val request = ViewAnalysisRequest(
+          in.analyserName,
+          in.serialiserName,
+          in.timestamp,
+          in.windowSet.getOrElse(List.empty),
+          in.args.getOrElse(Array()),
+          in.rawFile.getOrElse("")
+        )
+        mediator ! new DistributedPubSubMediator.Send("/user/AnalysisManager", request)
+        complete(s"Your Task Has been successfully submitted as a View Analysis Task!")
+      }
+    } ~ (path("RangeAnalysisRequest") & post) {
+      entity(as[RangeAnalysisPost]) { in =>
+        val request = RangeAnalysisRequest(
+          in.analyserName,
+          in.serialiserName,
+          in.start,
+          in.end,
+          in.jump,
+          in.windowSet.getOrElse(List.empty),
+          in.args.getOrElse(Array()),
+          in.rawFile.getOrElse("")
+        )
+        mediator ! new DistributedPubSubMediator.Send("/user/AnalysisManager", request)
+        complete(s"Your Task Has been successfully submitted as a Range Analysis Task!")
+      }
+    }
+  } ~ (path("AnalysisResults") & get & parameter("jobID")) { jobId =>
+   val result =  (mediator ? new DistributedPubSubMediator.Send("/user/AnalysisManager", RequestResults(jobId))).map {
+      case ResultsForApiPI(results) => results.mkString("{results:[", ",", "]}")
+      case JobDoesntExist           => "JobID given doesn't exist"
+    }
+    complete(result)
+  } ~ (path("KillTask") & get & parameter("jobID")) { jobId =>
+    val result =  (mediator ? new DistributedPubSubMediator.Send("/user/AnalysisManager", KillTask(jobId))).map {
+      case JobKilled      => s"Analysis has been stopped for $jobId"
+      case JobDoesntExist => "JobID given doesn't exist"
+    }
+    complete(result)
+  }
+
+  Http().bindAndHandle(route, iface, port)
+}
+
+object AnalysisRestApi {
+  object http {
+    case class LiveAnalysisPost(
+        analyserName: String,
+        serialiserName:String,
+        windowSet: Option[List[Long]],
+        repeatTime: Option[Long],
+        eventTime: Option[Boolean],
+        args: Option[Array[String]],
+        rawFile: Option[String]
+    )
+    object LiveAnalysisPost {
+      implicit val formatter: RootJsonFormat[LiveAnalysisPost] = jsonFormat7(LiveAnalysisPost.apply)
+    }
+
+    case class ViewAnalysisPost(
+        analyserName: String,
+        serialiserName:String,
+        timestamp: Long,
+        windowSet: Option[List[Long]],
+        args: Option[Array[String]],
+        rawFile: Option[String]
+    )
+    object ViewAnalysisPost {
+      implicit val formatter: RootJsonFormat[ViewAnalysisPost] = jsonFormat6(ViewAnalysisPost.apply)
+    }
+
+    case class RangeAnalysisPost(
+        analyserName: String,
+        serialiserName:String,
+        start: Long,
+        end: Long,
+        jump: Long,
+        windowSet: Option[List[Long]],
+        args: Option[Array[String]],
+        rawFile: Option[String]
+    )
+
+    object RangeAnalysisPost {
+      implicit val formatter: RootJsonFormat[RangeAnalysisPost] = jsonFormat8(RangeAnalysisPost.apply)
+    }
+  }
+
+  object message {
+    sealed trait AnalysisRequest
+
+    case class LiveAnalysisRequest(
+        analyserName: String,
+        serialiserName:String,
+        repeatTime: Long,
+        eventTime: Boolean,
+        windowSet: List[Long],
+        args: Array[String],
+        rawFile: String
+    ) extends AnalysisRequest
+
+    case class ViewAnalysisRequest(
+        analyserName: String,
+        serialiserName:String,
+        timestamp: Long,
+        windowSet: List[Long],
+        args: Array[String],
+        rawFile: String
+    ) extends AnalysisRequest
+
+    case class RangeAnalysisRequest(
+        analyserName: String,
+        serialiserName:String,
+        start: Long,
+        end: Long,
+        jump: Long,
+        windowSet: List[Long],
+        args: Array[String],
+        rawFile: String
+    ) extends AnalysisRequest
+  }
+
+}
