1eb9feec34154d43896f39a09a0a8d62f3758619
imanehaf <hafmane@gmail.com>
2021-07-30 08:23:14 +0100

    tidy up code

--- src/main/scala/com/raphtory/dev/allcommands/AllCommandTest.scala
+++ src/main/scala/com/raphtory/dev/allcommands/AllCommandTest.scala
@@ -5,8 +5,8 @@ import com.raphtory.RaphtoryComponent
 import scala.language.postfixOps
 
 object AllCommandTest extends App {
-  val partitionCount =1
-  val routerCount =1
+  val partitionCount =2
+  val routerCount =2
   val source = "com.raphtory.dev.allcommands.AllCommandsSpout"
   val builder = "com.raphtory.dev.allcommands.AllCommandsBuilder"
 //  val source = "com.raphtory.dev.wordSemantic.spouts.CoMatSpout"
@@ -16,7 +16,7 @@ object AllCommandTest extends App {
   new RaphtoryComponent("analysisManager",partitionCount,routerCount,1602)
   new RaphtoryComponent("spout",partitionCount,routerCount,1603,source)
   new RaphtoryComponent("router",partitionCount,routerCount,1604,builder)
-//  new RaphtoryComponent("router",partitionCount,routerCount,1605,builder)
+  new RaphtoryComponent("router",partitionCount,routerCount,1605,builder)
 //  new RaphtoryComponent("router",partitionCount,routerCount,1606,builder)
   //  new RaphtoryComponent("router",partitionCount,routerCount,1607,builder)
   //  new RaphtoryComponent("router",partitionCount,routerCount,1608,builder)
@@ -26,7 +26,7 @@ object AllCommandTest extends App {
   //  new RaphtoryComponent("router",partitionCount,routerCount,1612,builder)
   //  new RaphtoryComponent("router",partitionCount,routerCount,1613,builder)
   new RaphtoryComponent("partitionManager",partitionCount,routerCount,1614)
-//  new RaphtoryComponent("partitionManager",partitionCount,routerCount,1615)
+  new RaphtoryComponent("partitionManager",partitionCount,routerCount,1615)
 //  new RaphtoryComponent("partitionManager",partitionCount,routerCount,1616)
   //  new RaphtoryComponent("partitionManager",partitionCount,routerCount,1617)
   //  new RaphtoryComponent("partitionManager",partitionCount,routerCount,1618)
