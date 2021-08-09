4cf72d7c77a8604f09e777c141923daada133ca0
Narnolddd <narnolddd@Mirates-iMac.local>
2021-08-09 16:22:03 +0100

    Updated Triangle Count in line with new serialisation method for algorithm outputs

--- src/main/scala/com/raphtory/dev/gab/GabDeployment.scala
+++ src/main/scala/com/raphtory/dev/gab/GabDeployment.scala
@@ -1,9 +1,9 @@
 package com.raphtory.dev.gab
 
 import com.raphtory.RaphtoryGraph
-import com.raphtory.algorithms.{ConnectedComponents, DegreeBasic}
+import com.raphtory.algorithms.{ConnectedComponents, DegreeBasic, TriangleCount}
 import com.raphtory.dev.gab.graphbuilders.GabUserGraphBuilder
-import com.raphtory.serialisers.MongoSerialiser
+import com.raphtory.serialisers.{DefaultSerialiser, MongoSerialiser}
 import com.raphtory.spouts.FileSpout
 
 object GabDeployment extends App{
@@ -12,7 +12,9 @@ object GabDeployment extends App{
   val rg = RaphtoryGraph[String](source,builder)
   val arguments = Array[String]()
 
-  rg.rangeQuery(ConnectedComponents(),new MongoSerialiser, start= 1470797917000L,end = 1476113868000L,increment = 86400000L,windowBatch=List(3600000L,86400000L,604800000L,2592000000L,31536000000L),arguments)
+  rg.rangeQuery(ConnectedComponents(),new DefaultSerialiser, start= 1470797917000L,end = 1476113868000L,increment = 86400000L,windowBatch=List(3600000L,86400000L,604800000L,2592000000L,31536000000L),arguments)
+  rg.rangeQuery(new TriangleCount(Array()), new DefaultSerialiser, start=1470797917000L,
+    end = 1525368897000L,increment = 86400000L,window=86400000L, arguments)
   //rg.rangeQuery(ConnectedComponents(),start = 1,end = 32674,increment = 100,window=100,arguments)
   //rg.rangeQuery(ConnectedComponents(),start = 1,end = 32674,increment = 100,windowBatch=Array(3600,36000,360000),arguments)
 
