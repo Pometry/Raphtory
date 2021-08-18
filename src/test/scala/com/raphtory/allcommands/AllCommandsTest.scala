d50dc7a31856271a0e8d8dbc2be12383bd11dbfe
Ben Steer <bensteer@Bens-MacBook-Pro.local>
2021-08-18 17:04:58 +0100

    State test and Connected Components test fully implemented for All commands

--- src/test/scala/com/raphtory/allcommands/AllCommandsTest.scala
+++ src/test/scala/com/raphtory/allcommands/AllCommandsTest.scala
@@ -5,16 +5,18 @@ import com.raphtory.RaphtoryComponent
 import org.scalatest.FunSuite
 import akka.pattern.ask
 import akka.util.Timeout
-import com.raphtory.algorithms.StateTest
+import com.raphtory.algorithms.{ConnectedComponents, StateTest}
 import com.raphtory.core.actors.analysismanager.AnalysisManager.Message.{AreYouFinished, ManagingTask, TaskFinished}
 import com.raphtory.core.actors.analysismanager.AnalysisRestApi.message.RangeAnalysisRequest
 import com.raphtory.core.actors.orchestration.clustermanager.WatermarkManager.Message.{WatermarkTime, WhatsTheTime}
 import com.raphtory.core.analysis.api.Analyser
-import com.raphtory.resultcomparison.{StateCheckResult, TimeParams}
+import com.raphtory.resultcomparison.comparisonJsonProtocol._
+import com.raphtory.resultcomparison.{ConnectedComponentsResults, RaphtoryResultComparitor, StateCheckResult, TimeParams, comparisonJsonProtocol}
 import com.raphtory.serialisers.DefaultSerialiser
 import spray.json._
-import com.raphtory.resultcomparison.comparisonJsonProtocol._
 
+import java.io.File
+import scala.collection.mutable.HashMap
 import scala.concurrent.{Await, ExecutionContext}
 import scala.concurrent.duration._
 
@@ -38,19 +40,6 @@ class AllCommandsTest extends FunSuite {
 
 
 
-
-  test ("scratch space"){
-    val testData = scala.io.Source.fromFile("src/test/scala/com/raphtory/data/allcommands/output").getLines()
-      .map(line=>line.parseJson.convertTo[StateCheckResult])
-      .map(state=>(TimeParams(state.time,state.windowsize),state))
-    val standardData = scala.io.Source.fromFile("src/test/scala/com/raphtory/data/allcommands/statetest.json").getLines()
-      .map(line=> line.parseJson.convertTo[StateCheckResult])
-      .map(state=>(TimeParams(state.time,state.windowsize),state))
-
-    //    if(stateResult.fold(true){(x,y) =>x&&y})
-    assert(true)
-  }
-
   test("Warmup and Ingestion Test") {
       implicit val timeout: Timeout = 20.second
       try {
@@ -73,17 +62,62 @@ class AllCommandsTest extends FunSuite {
     try {
       //First we run the test and see if it finishes in a reasonable time
       implicit val timeout: Timeout = 120.second
-      val future      = analysisManager.getAnalysisManager.get ? RangeAnalysisRequest(stateTest,serialiser,1,290001,10000, List(1000,10000,100000,1000000),Array(),"")
+      val future = analysisManager.getAnalysisManager.get ? RangeAnalysisRequest(stateTest, serialiser, 1, 290001, 10000, List(1000, 10000, 100000, 1000000), Array(), "")
       val taskManager = Await.result(future, timeout.duration).asInstanceOf[ManagingTask].actor
-      val future2     = taskManager ? AreYouFinished
-      val result      = Await.result(future2, timeout.duration).asInstanceOf[TaskFinished].result
+      val future2 = taskManager ? AreYouFinished
+      val result = Await.result(future2, timeout.duration).asInstanceOf[TaskFinished].result
 
+      val testData = HashMap[TimeParams, StateCheckResult]() ++=
+        scala.io.Source.fromFile("src/test/scala/com/raphtory/data/allcommands/output").getLines()
+          .map(line => line.parseJson.convertTo[StateCheckResult])
+          .map(state => (TimeParams(state.time, state.windowsize), state))
+      val standardData = HashMap[TimeParams, StateCheckResult]() ++=
+        scala.io.Source.fromFile("src/test/scala/com/raphtory/data/allcommands/statetest.json").getLines()
+          .map(line => line.parseJson.convertTo[StateCheckResult])
+          .map(state => (TimeParams(state.time, state.windowsize), state))
 
-      assert(result)
-    } catch {
+      val correctResults = testData.map(row => row._2.compareTo(standardData(row._1))).fold(true) { (x, y) => x && y }
+      if (correctResults) {
+        new File("src/test/scala/com/raphtory/data/allcommands/output").delete()
+      }
+      assert(correctResults)
+    }
+      catch {
       case _: java.util.concurrent.TimeoutException => assert(false)
     }
-
   }
+
+  test("Connected Components Test"){
+    val connectedComponents = ConnectedComponents().getClass.getCanonicalName
+    val serialiser = new DefaultSerialiser().getClass.getCanonicalName
+    try {
+      //First we run the test and see if it finishes in a reasonable time
+      implicit val timeout: Timeout = 300.second
+      val future = analysisManager.getAnalysisManager.get ? RangeAnalysisRequest(connectedComponents, serialiser, 1, 290001, 10000, List(1000, 10000, 100000, 1000000), Array(), "")
+      val taskManager = Await.result(future, timeout.duration).asInstanceOf[ManagingTask].actor
+      val future2 = taskManager ? AreYouFinished
+      val result = Await.result(future2, timeout.duration).asInstanceOf[TaskFinished].result
+
+      val testData = HashMap[TimeParams, ConnectedComponentsResults]() ++=
+        scala.io.Source.fromFile("src/test/scala/com/raphtory/data/allcommands/output").getLines()
+          .map(line => line.parseJson.convertTo[ConnectedComponentsResults])
+          .map(state => (TimeParams(state.time, state.windowsize), state))
+      val standardData = HashMap[TimeParams, ConnectedComponentsResults]() ++=
+        scala.io.Source.fromFile("src/test/scala/com/raphtory/data/allcommands/connectedcomponents.json").getLines()
+          .map(line => line.parseJson.convertTo[ConnectedComponentsResults])
+          .map(state => (TimeParams(state.time, state.windowsize), state))
+
+      val correctResults = testData.map(row => row._2.compareTo(standardData(row._1))).fold(true) { (x, y) => x && y }
+      if (correctResults) {
+        new File("src/test/scala/com/raphtory/data/allcommands/output").delete()
+      }
+      assert(correctResults)
+    }
+    catch {
+      case _: java.util.concurrent.TimeoutException => assert(false)
+    }
+  }
+
+
 
 }
