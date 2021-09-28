f0fb44d3f80c3c84063232e5334c3d98268833b5
miratepuffin <b.a.steer@qmul.ac.uk>
2021-09-28 22:44:19 +0100

    Added First Implentation of usable GraphPerspective/Table

--- src/main/scala/com/raphtory/core/model/algorithm/GraphPerspective.scala
+++ src/main/scala/com/raphtory/core/model/algorithm/GraphPerspective.scala
@@ -2,14 +2,15 @@ package com.raphtory.core.model.algorithm
 
 import com.raphtory.core.model.graph.visitor.Vertex
 
-sealed trait RaphtoryFunction
-case   class Step(f:(Vertex)=>Unit) extends RaphtoryFunction
-case   class Iterate(f:(Vertex)=>Unit, iterations:Int) extends RaphtoryFunction
-case   class Filter(f:(Vertex)=>Boolean) extends RaphtoryFunction
+sealed trait GraphFunction
+case   class Step(f:(Vertex)=>Unit) extends GraphFunction
+case   class Iterate(f:(Vertex)=>Unit, iterations:Int) extends GraphFunction
+case   class VertexFilter(f:(Vertex)=>Boolean) extends GraphFunction
+case   class Select(f:Vertex=>Row) extends GraphFunction
 
 abstract class GraphPerspective{
   def filter(f:(Vertex)=>Boolean):GraphPerspective
   def step(f:(Vertex)=>Unit):GraphPerspective
   def iterate(f:(Vertex)=>Unit, iterations:Int):GraphPerspective
-  def select[S](f:Vertex=>S):Table
+  def select(f:Vertex=>Row):Table
 }
