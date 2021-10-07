f364579bddf9a9548c7820c45ee3651d39c45881
Haaroon Yousaf <haaroon.yousaf@gmail.com>
2021-10-07 14:29:17 +0100

    add skeleton analysis example

---
+++ src/main/scala/com/raphtory/dev/ethereum/analysis/TaintTracking.scala
@@ -0,0 +1,30 @@
+package com.raphtory.dev.ethereum.analysis
+
+import com.raphtory.core.model.algorithm.{GraphAlgorithm, GraphPerspective, Row}
+
+class TestAlgorithm extends GraphAlgorithm{
+  override def algorithm(graph: GraphPerspective): Unit = {
+    graph
+      .step({
+        vertex =>
+          vertex.setState("cclabel", vertex.ID)
+          vertex.messageAllNeighbours(vertex.ID)
+      })
+      .iterate({
+        vertex =>
+          val label = vertex.messageQueue[Long].min
+          if (label < vertex.getState[Long]("cclabel")) {
+            vertex.setState("cclabel", label)
+            vertex messageAllNeighbours label
+          }
+          else
+            vertex.voteToHalt()
+      }, 100)
+      .select(vertex => Row(Array(vertex.ID(),vertex.getState[Long]("cclabel"))))
+      //.filter(r=> r.get(0).asInstanceOf[Long]==18174)
+      .writeTo("/Users/bensteer/github/output")
+  }
+}
+object TestAlgorithm{
+  def apply() = new TestAlgorithm()
+}
