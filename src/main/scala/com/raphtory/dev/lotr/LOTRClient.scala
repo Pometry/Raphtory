d706a7513cf50ca656741071b744410449f529d1
miratepuffin <b.a.steer@qmul.ac.uk>
2021-10-16 13:18:16 +0100

    Adding jobID handler for anon functions

--- src/main/scala/com/raphtory/dev/lotr/LOTRClient.scala
+++ src/main/scala/com/raphtory/dev/lotr/LOTRClient.scala
@@ -4,7 +4,17 @@ import com.raphtory.core.build.client.RaphtoryClient
 import com.raphtory.core.model.algorithm.{GraphAlgorithm, GraphPerspective, Row}
 object LOTRClient extends App {
   val client = new RaphtoryClient("127.0.0.1:1601",1700)
-  client.pointQuery(ConnectedComponents("/Users/bensteer/github/output"),10000,List(10000, 1000,100))
+
+  client.pointQuery(new testAlgo,10000,List(10000, 1000,100))
 }
 
+
+class testAlgo()extends GraphAlgorithm {
+  override def algorithm(graph: GraphPerspective): Unit = {
+  val count = graph.nodeCount()
+  graph.step(v=> v.setState("key",count))
+  .select(v => Row(v.ID(),v.getState("key")))
+  .writeTo("/Users/bensteer/github/output")
+}
+}
 
--- src/main/scala/com/raphtory/dev/lotr/LOTRDeployment.scala
+++ src/main/scala/com/raphtory/dev/lotr/LOTRDeployment.scala
@@ -7,6 +7,7 @@ object LOTRDeployment extends App{
   val source  = new LOTRSpout("src/main/scala/com/raphtory/dev/lotr/lotr.csv")
   val builder = new LOTRGraphBuilder()
   val rg = RaphtoryPD[String](source,builder)
+
   //rg.pointQuery(GraphState("/Users/bensteer/github/output"),32000)
   rg.pointQuery(ConnectedComponents("/Users/bensteer/github/output"),32000)
   //rg.rangeQuery(ConnectedComponents("/Users/bensteer/github/output"),10000,32000,1000,List(10000, 1000,100))
