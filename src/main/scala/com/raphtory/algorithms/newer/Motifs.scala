64d315665d037f7754ea3da7ad5eb49956feb005
Haaroon Yousaf <haaroon.yousaf@gmail.com>
2021-10-19 18:27:59 +0100

    2-3-1 motifs complete

--- src/main/scala/com/raphtory/algorithms/newer/Motifs.scala
+++ src/main/scala/com/raphtory/algorithms/newer/Motifs.scala
@@ -11,7 +11,7 @@ class Motifs(fileOutput:String) extends GraphAlgorithm {
           if (vertex.explodeInEdges().size > 0 & vertex.explodeOutEdges().size > 0) {
             val out = vertex.explodeInEdges()
               .map(inEdge => vertex.explodeOutEdges()
-                .filter( e => e.getTimestamp() >= inEdge.getTimestamp() & e.dst() != inEdge.src() & e.dst() != inEdge.dst()).size).sum
+                .filter( e => e.getTimestamp() > inEdge.getTimestamp() & e.dst() != inEdge.src() & e.dst() != inEdge.dst()).size).sum
             Row(vertex.ID(), out)
           }
           else{
