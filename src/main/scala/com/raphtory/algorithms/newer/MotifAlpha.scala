64d7725bce5c6bb817db7ba04d62bbc56274ffde
Haaroon Yousaf <haaroon.yousaf@gmail.com>
2021-10-25 13:15:55 +0100

    renamed alpha1 motif

--- src/main/scala/com/raphtory/algorithms/newer/MotifAlpha.scala
+++ src/main/scala/com/raphtory/algorithms/newer/MotifAlpha.scala
@@ -2,13 +2,19 @@ package com.raphtory.algorithms.newer
 
 import com.raphtory.core.model.algorithm.{GraphAlgorithm, GraphPerspective, Row}
 
-class Motifs(fileOutput:String) extends GraphAlgorithm {
+/*
+
+This is an implementation of the alpha_1 motif from the paper
+Detecting Mixing Services via Mining Bitcoin Transaction Network With Hybrid Motifs
+ */
+
+class MotifAlpha(fileOutput:String="/tmp/motif_alpha") extends GraphAlgorithm {
 
   override def algorithm(graph: GraphPerspective): Unit = {
     graph
       .select({
         vertex =>
-          if (vertex.explodeInEdges().size > 0 & vertex.explodeOutEdges().size > 0) {
+          if (vertex.explodeInEdges().nonEmpty & vertex.explodeOutEdges().nonEmpty) {
             val out = vertex.explodeInEdges()
               .map(inEdge => vertex.explodeOutEdges()
                 .filter( e => e.getTimestamp() > inEdge.getTimestamp() & e.dst() != inEdge.src() & e.dst() != inEdge.dst()).size).sum
@@ -18,12 +24,12 @@ class Motifs(fileOutput:String) extends GraphAlgorithm {
             Row(vertex.ID(), 0)
           }
       }
-      ).writeTo("/tmp/counter")
+      ).writeTo(fileOutput)
   }
 
 }
 
-object Motifs{
-  def apply(path:String) = new Motifs(path)
+object MotifAlpha{
+  def apply(path:String) = new MotifAlpha(path)
 }
 
