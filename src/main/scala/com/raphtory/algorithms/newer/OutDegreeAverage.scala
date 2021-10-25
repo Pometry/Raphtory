028e4efd33b86a129372120f1dc0e6fc2cab16e8
Haaroon Yousaf <haaroon.yousaf@gmail.com>
2021-10-25 16:34:27 +0100

    ported out degree, broken until ben fixes the nodeCount

---
+++ src/main/scala/com/raphtory/algorithms/newer/OutDegreeAverage.scala
@@ -0,0 +1,25 @@
+package com.raphtory.algorithms.newer
+
+import com.raphtory.core.model.algorithm.{GraphAlgorithm, GraphPerspective, Row}
+
+class OutDegreeAverage(output:String) extends GraphAlgorithm {
+  override def algorithm(graph: GraphPerspective): Unit = {
+    val nodeCount = graph.nodeCount()
+    graph
+      .select({
+        vertex =>
+          val id = vertex.ID()
+          val sized =
+            try vertex.getOutEdges().size.toDouble / nodeCount.toDouble
+            catch { case _: ArithmeticException => 0 }
+          Row(id, sized)
+      })
+      .writeTo(output)
+  }
+}
+
+
+object OutDegreeAverage{
+  def apply(output:String= "/tmp/outDegreeAverage") = new OutDegreeAverage(output)
+}
+
