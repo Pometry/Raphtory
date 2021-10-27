5b5ed09556512bead00e746f4e664e4400eba9a3
haaroony <haaroon.yousaf@gmail.com>
2021-10-27 17:01:31 +0100

    two hops done, it just needs a test now

--- src/main/scala/com/raphtory/algorithms/newer/twoHopNeighbour.scala
+++ src/main/scala/com/raphtory/algorithms/newer/twoHopNeighbour.scala
@@ -1,46 +1,56 @@
 package com.raphtory.algorithms.newer
 
-import com.raphtory.core.model.algorithm.{GraphAlgorithm, GraphPerspective}
+import com.raphtory.core.model.algorithm.{GraphAlgorithm, GraphPerspective, Row}
+
 import scala.collection.mutable
 
-class twoHopNeighbour(output:String = "/tmp/twoHopNeighbour") extends GraphAlgorithm{
+class twoHopNeighbour(output: String = "/tmp/twoHopNeighbour") extends GraphAlgorithm {
   override def algorithm(graph: GraphPerspective): Unit = {
-    graph.step({
-      vertex => vertex.getEdges().foreach(edge => edge.send(
-          ("twoHopRequest", vertex.ID, 0)
+    graph
+      .step(vertex => vertex.getEdges().foreach(edge => edge.send(("twoHopRequest", vertex.ID, 0))))
+      .iterate(
+        { vertex =>
+          val newMessages = vertex.messageQueue[(String, Long, Long)]
+          val requests = newMessages.distinct.filter(_._1 == "twoHopRequest")
+          val responses = newMessages.distinct.filter(_._1 == "twoHopResponse")
+          if (requests.nonEmpty) {
+            vertex.getAllNeighbours().foreach { neighbour =>
+              requests.foreach(msg =>
+                if (msg._2 != neighbour) {
+                  vertex.messageNeighbour(msg._2, ("twoHopResponse", neighbour, vertex.ID))
+                })
+            }
+          }
+          if (responses.nonEmpty) {
+            vertex.setState("twoHopResponse", true)
+            var twoHops = vertex.getOrSetState("twoHops", mutable.ListBuffer[(Long, Long)]())
+            responses.foreach(response => twoHops.append((response._2, response._3)))
+            twoHops = twoHops.distinct
+            vertex.setState("twoHops", twoHops)
+          }
+        }, 5
+      )
+      .select(vertex =>
+        Row(
+          vertex.getStateOrElse("twoHopResponse", false),
+          vertex.ID(),
+          vertex.getStateOrElse("twoHops", "")
         )
-        )
-    }).iterate({
-      vertex =>
-        val newMessages = vertex.messageQueue[(String, Long, Int)]
-        val request = newMessages.filter(item => item.get(0)=="twoHopRequest")
-        newMessages.filter(item => item.get(0)!="twoHopRequest").foreach(msg => vertex.messageSelf(msg))
-        request.foreach(
-          msg =>
-            vertex.messageNeighbour(
-              msg.get(1).get(0),
-            )
-        )
-
-        vertex.getAllNeighbours().foreach(
-          neighbour =>
-            request.map({
-              m =>
-                if (m.size == 1) {
-                  neighbour =>
-                  )
-                }
-        )
-        val twoHopStatus =
-
-        })
-        vertex.getAllNeighbours()
-    }, 50)
+      )
+      .filter(
+        row =>
+          row.get(0) == true)
+      .explode(
+        row =>
+          row.get(2).asInstanceOf[mutable.ListBuffer[(Long, Long)]]
+            .toList.map(hops =>
+              Row(row.get(1), hops._1, hops._2)
+          )
+      )
+      .writeTo(output)
   }
 }
 
-
-object twoHopNeighbour{
-  def apply(output:String) = new twoHopNeighbour(output)
+object twoHopNeighbour {
+  def apply(output: String) = new twoHopNeighbour(output)
 }
-
