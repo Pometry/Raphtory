cd999d788ca650c791b7672285819bcb8c6054dd
Haaroon Y <Haaroon@users.noreply.github.com>
2021-11-01 15:23:59 +0000

    undo

--- src/main/scala/com/raphtory/dev/lotr/LOTRDeployment.scala
+++ src/main/scala/com/raphtory/dev/lotr/LOTRDeployment.scala
@@ -8,6 +8,7 @@ object LOTRDeployment extends App{
   val source  = new LOTRSpout("src/main/scala/com/raphtory/dev/lotr/lotr.csv")
   val builder = new LOTRGraphBuilder()
   val rg = RaphtoryPD[String](source,builder)
+  
   //rg.pointQuery(GraphState("/Users/bensteer/github/output"),32000)
   rg.pointQuery(TriangleCount("/Users/naomiarnold/CODE/lotroutput"),31816)
   //rg.rangeQuery(ConnectedComponents("/Users/bensteer/github/output"),10000,32000,1000,List(10000, 1000,100))
