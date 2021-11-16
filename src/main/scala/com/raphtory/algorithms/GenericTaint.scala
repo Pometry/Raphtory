df3c495aed1c7a30177e0afb960656aeeab0d283
Haaroon Yousaf <haaroon.yousaf@gmail.com>
2021-11-16 15:39:56 +0000

    generic taint completed

--- src/main/scala/com/raphtory/algorithms/GenericTaint.scala
+++ src/main/scala/com/raphtory/algorithms/GenericTaint.scala
@@ -2,7 +2,26 @@ package com.raphtory.algorithms
 
 import com.raphtory.core.model.algorithm.{GraphAlgorithm, GraphPerspective, Row}
 
-class GenericTaint(startTime: Long, infectedNodes: Set[String], stopNodes: Set[String] = Set()) extends GraphAlgorithm{
+/**
+Description
+  A tainting/infection algorithm for a directed graph. Given the start node(s) and time
+  this algorithm will spread taint/infect any node which has received an edge after this time.
+  The following node will then infect any of its neighbours that receive an edge after the
+  time it was first infected. This repeats until the latest timestamp/iterations are met.
+
+Parameters
+  startTime Long : Time to start spreading taint
+  infectedNodes Set[Long] : List of nodes that will start as tainted
+  stopNodes Set[Long] : If set, any nodes that will not propogate taint
+  output String : Path where the output will be written
+
+Returns
+  Infected ID (Long) : ID of the infected vertex
+  Edge ID (Long) : Edge that propogated the infection
+  Time (Long) : Time of infectino
+  Infector ID (Long) : Node that spread the infection
+**/
+class GenericTaint(startTime: Long, infectedNodes: Set[Long], stopNodes: Set[Long] = Set(), output: String = "/tmp/generic_taint") extends GraphAlgorithm{
 
   override def algorithm(graph: GraphPerspective): Unit = {
     graph
@@ -11,9 +30,9 @@ class GenericTaint(startTime: Long, infectedNodes: Set[String], stopNodes: Set[S
         // for each vertex in the graph
         vertex =>
           // check if it is one of our infected nodes
-          if (infectedNodes contains vertex.getPropertyOrElse("name", "").toString){
+          if (infectedNodes contains vertex.ID){
             // set its state to tainted
-            val result = List(("tainted", -1, startTime, "startPoint",))
+            val result = List(("tainted", -1, startTime, "startPoint"))
             // set this node as the beginning state
             vertex.setState("taintStatus", true)
             vertex.setState("taintHistory", result)
@@ -30,7 +49,7 @@ class GenericTaint(startTime: Long, infectedNodes: Set[String], stopNodes: Set[S
                       edge.send(Tuple4("tainted",
                         edge.ID(),
                         event.time,
-                        vertex.getPropertyOrElse("name", "")
+                        vertex.ID()
                         )
                       )
                     }
@@ -42,7 +61,7 @@ class GenericTaint(startTime: Long, infectedNodes: Set[String], stopNodes: Set[S
         vertex =>
           // check if any node has received a message
           // obtain the messages as a set
-          val newMessages = vertex.messageQueue[(String, Long, Long, String)].map(item => item).distinct
+          val newMessages = vertex.messageQueue[(String, Long, Long, Long)].map(item => item).distinct
           // obtain the min time it was infected
           val infectionTime = newMessages.map(item => item._3).min
           val status = newMessages.map(item => item._1).distinct
@@ -56,7 +75,7 @@ class GenericTaint(startTime: Long, infectedNodes: Set[String], stopNodes: Set[S
             }
             else {
               // otherwise set a brand new state, first get the old txs it was tainted by
-              val oldState: List[(String, Long, Long, String)] = vertex.getState("taintHistory")
+              val oldState: List[(String, Long, Long, Long)] = vertex.getState("taintHistory")
               // add the new transactions and old ones together
               val newState = List.concat(newMessages, oldState).distinct
               // set this as the new state
@@ -71,14 +90,14 @@ class GenericTaint(startTime: Long, infectedNodes: Set[String], stopNodes: Set[S
                     edge.send(Tuple4("tainted",
                       edge.ID(),
                       event.time,
-                      vertex.getPropertyOrElse("name", "")
+                      vertex.ID()
                       )
                     )
                   }
                 )
               )
             }
-            else if (!(stopNodes contains vertex.getPropertyOrElse("address", "").toString.toLowerCase.trim)) {
+            else if (!(stopNodes contains vertex.ID)) {
               // otherwise if we contain stop nodes and we dont have to stop then keep going
               // repeat like we did in the step
               vertex.getOutEdges(after = infectionTime).foreach(
@@ -87,7 +106,7 @@ class GenericTaint(startTime: Long, infectedNodes: Set[String], stopNodes: Set[S
                     edge.send(Tuple4("tainted",
                       edge.ID(),
                       event.time,
-                      vertex.getPropertyOrElse("name", "")
+                      vertex.ID()
                       )
                     )
                   }
@@ -100,20 +119,20 @@ class GenericTaint(startTime: Long, infectedNodes: Set[String], stopNodes: Set[S
       }, 100, true)
       // get all vertexes and their status
       .select(vertex => Row(
-        vertex.getPropertyOrElse("name", "").toString,
+        vertex.ID(),
         vertex.getStateOrElse("taintStatus", false),
         vertex.getStateOrElse[Any]("taintHistory","false")
       ))
       // filter for any that had been tainted and save to folder
       .filter(r=> r.get(1) == true)
-      .explode( row => row.get(2).asInstanceOf[List[(String, Long, Long, String)]].map(
+      .explode( row => row.get(2).asInstanceOf[List[(String, Long, Long, Long)]].map(
         tx => Row( row(0), tx._2, tx._3, tx._4)
       ))
-      .writeTo("/tmp/generic_taint")
+      .writeTo(output)
   }
 }
 
 object GenericTaint {
-  def apply(startTime: Int, infectedNodes: Set[String], stopNodes: Set[String]) =
-    new GenericTaint(startTime, infectedNodes, stopNodes)
+  def apply(startTime: Int, infectedNodes: Set[Long], stopNodes: Set[Long] = Set(), output: String) =
+    new GenericTaint(startTime, infectedNodes, stopNodes, output)
 }
