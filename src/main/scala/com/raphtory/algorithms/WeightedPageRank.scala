931783d54b61bb1ec9e16fe308c70b2975edf7c7
Naomi Arnold <n.a.arnold@qmul.ac.uk>
2021-11-16 11:50:00 +0000

    Weighted page rank appears to work and produce sensible answers.

---
+++ src/main/scala/com/raphtory/algorithms/WeightedPageRank.scala
@@ -0,0 +1,54 @@
+package com.raphtory.algorithms
+
+import com.raphtory.core.model.algorithm.{GraphAlgorithm, GraphPerspective, Row}
+
+class WeightedPageRank (dampingFactor:Float = 0.85F, iterateSteps:Int = 100, output:String = "/tmp/PageRank") extends  GraphAlgorithm{
+
+  override def algorithm(graph: GraphPerspective): Unit = {
+    graph.step({
+      vertex =>
+        val initLabel = 1.0F
+        vertex.setState("prlabel", initLabel)
+        val outWeight = vertex.getOutEdges()
+          .map(e => e.getPropertyOrElse("weight", e.history().size))
+          .sum
+        vertex.getOutEdges().foreach({
+          e =>
+            vertex.messageNeighbour(e.ID, e.getPropertyOrElse("weight", e.history().size).toFloat / outWeight)
+        })
+    })
+      .iterate({ vertex =>
+        val vname = vertex.getPropertyOrElse("name",vertex.ID().toString) // for logging purposes
+        val currentLabel = vertex.getState[Float]("prlabel")
+
+        val queue = vertex.messageQueue[Float]
+        val newLabel = (1 - dampingFactor) + dampingFactor * queue.sum
+        vertex.setState("prlabel", newLabel)
+
+        val outWeight = vertex.getOutEdges()
+          .map(e => e.getPropertyOrElse("weight", e.history().size))
+          .sum
+        vertex.getOutEdges().foreach({
+          e =>
+            vertex.messageNeighbour(e.ID, newLabel * e.getPropertyOrElse("weight", e.history().size).toFloat / outWeight)
+        })
+
+        if (Math.abs(newLabel - currentLabel) / currentLabel < 0.00001) {
+          vertex.voteToHalt()
+        }
+      }, iterateSteps, false)
+      .select({
+        vertex =>
+          Row(
+            vertex.getPropertyOrElse("name", vertex.ID()),
+            vertex.getStateOrElse("prlabel", -1)
+          )
+      })
+      .writeTo(output)
+  }
+}
+
+object WeightedPageRank{
+  def apply(dampingFactor:Float = 0.85F, iterateSteps:Int = 100, output:String = "/tmp/PageRank") =
+    new WeightedPageRank(dampingFactor, iterateSteps, output)
+}
