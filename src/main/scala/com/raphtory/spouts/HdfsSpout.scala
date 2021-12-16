25c242bfc5a0dfbe4f8fc88ae38cee3f48ae16bb
calzadaharo@gmail.com <calzadaharo@gmail.com>
2021-12-16 19:17:03 +0100

    New HDFS Spout

---
+++ src/main/scala/com/raphtory/spouts/HdfsSpout.scala
@@ -0,0 +1,55 @@
+package com.raphtory.spouts
+
+import com.raphtory.core.components.spout.Spout
+
+import java.io.{BufferedReader, InputStreamReader}
+import java.net.URI
+
+import org.apache.hadoop.conf.Configuration
+import org.apache.hadoop.fs.{FileSystem, Path}
+
+class HdfsSpout(headerUrlPort:String, route:String, dropHeader:Boolean=false) extends Spout[String] {
+
+  val fs = FileSystem.get(new URI(headerUrlPort), new Configuration())
+  private var status = fs.listStatus(new Path(route))
+  private var hdfsManager = HDFSManager()
+
+  override def setupDataSource(): Unit = {}
+
+  override def closeDataSource(): Unit = {}
+
+  override def generateData(): Option[String] = {
+    val line = hdfsManager.nextLine()
+    if (hdfsManager.finished(line)) {
+      dataSourceComplete()
+      None
+    } else {
+      Some(line)
+    }
+  }
+
+  final case class HDFSManager private () {
+    val stream = fs.open(status(0).getPath)
+    status = status.slice(1,status.length)
+    val bufferReader = new BufferedReader(new InputStreamReader(stream))
+
+    def nextLine(): String = {
+      bufferReader.readLine()
+    }
+
+    def finished(line: String): Boolean = {
+      if (line == null) {
+        if(status.length > 0) {
+          hdfsManager = HDFSManager()
+          println("Still loading Spout...")
+          false
+        } else {
+          println("Spout loaded!")
+          true
+        }
+      } else {
+        false
+      }
+    }
+  }
+}
