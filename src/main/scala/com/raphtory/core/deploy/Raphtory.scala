ca725f7408f4a78d3ba25bc3365d6619172a1355
miratepuffin <b.a.steer@qmul.ac.uk>
2022-02-22 15:26:41 +0000

    Reenabled file polling via scheduler

--- src/main/scala/com/raphtory/core/deploy/Raphtory.scala
+++ src/main/scala/com/raphtory/core/deploy/Raphtory.scala
@@ -108,20 +108,20 @@ object Raphtory {
       pulsarController: PulsarController
   ): SpoutExecutor[T] =
     spout match {
-      case spout: FileSpout[T]            =>
+      case spout: FileSpout[T] =>
         new FileSpoutExecutor[T](
                 spout.source,
                 spout.schema,
                 spout.lineConverter,
                 conf,
-                pulsarController
+                pulsarController,
+                scheduler
         )
-      case IdentitySpout()                => new IdentitySpoutExecutor[T](conf, pulsarController)
-      case ResourceSpout(resource)        =>
-        new ResourceSpoutExecutor(resource, conf, pulsarController).asInstanceOf[SpoutExecutor[T]]
+      case IdentitySpout() => new IdentitySpoutExecutor[T](conf, pulsarController, scheduler)
+      case ResourceSpout(resource) =>
+        new ResourceSpoutExecutor(resource, conf, pulsarController, scheduler).asInstanceOf[SpoutExecutor[T]]
       case StaticGraphSpout(fileDataPath) =>
-        new StaticGraphSpoutExecutor(fileDataPath, conf, pulsarController)
-          .asInstanceOf[SpoutExecutor[T]]
+        new StaticGraphSpoutExecutor(fileDataPath, conf, pulsarController, scheduler).asInstanceOf[SpoutExecutor[T]]
 
     }
 
