c3a60dcf913bbd52aea29f818550ab71a780e8ca
alnaimi- <al.alnaimi@gmail.com>
2022-02-23 15:26:17 +0000

    Added Marshalling for deep cloning objects

--- src/main/scala/com/raphtory/core/components/graphbuilder/BuilderExecutor.scala
+++ src/main/scala/com/raphtory/core/components/graphbuilder/BuilderExecutor.scala
@@ -2,22 +2,25 @@ package com.raphtory.core.components.graphbuilder
 
 import com.raphtory.core.components.Component
 import com.raphtory.core.config.PulsarController
+import com.raphtory.serialisers.Marshal
 import com.rits.cloning.Cloner
 import com.typesafe.config.Config
 import org.apache.pulsar.client.api.Consumer
 import org.apache.pulsar.client.api.Message
 import org.apache.pulsar.client.api.Schema
+import shapeless.Coproduct
 
 import scala.reflect.runtime.universe
 import scala.reflect.runtime.universe._
 
-class BuilderExecutor[T](
+class BuilderExecutor[T: Manifest](
     schema: Schema[T],
     graphBuilder: GraphBuilder[T],
     conf: Config,
     pulsarController: PulsarController
 ) extends Component[T](conf, pulsarController) {
-  private val safegraphBuilder = new Cloner().deepClone(graphBuilder)
+
+  private val safegraphBuilder = Marshal.deepCopy(graphBuilder)
   private val producers        = toWriterProducers
 
   var cancelableConsumer: Option[Consumer[T]] = None
