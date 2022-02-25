934e71459589e37316b87fd0a5c0a779ba239ab2
miratepuffin <b.a.steer@qmul.ac.uk>
2022-02-25 17:09:59 +0000

    Swapped from Manifest to ClassTag throughout project

--- src/main/scala/com/raphtory/serialisers/Marshal.scala
+++ src/main/scala/com/raphtory/serialisers/Marshal.scala
@@ -5,7 +5,7 @@ object Marshal {
   import scala.reflect.ClassTag
 
   def dump[A](o: A)(implicit t: ClassTag[A]): Array[Byte] = {
-    val ba = new ByteArrayOutputStream(512)
+    val ba  = new ByteArrayOutputStream(512)
     val out = new ObjectOutputStream(ba)
     out.writeObject(t)
     out.writeObject(o)
@@ -17,22 +17,23 @@ object Marshal {
   @throws(classOf[ClassCastException])
   @throws(classOf[ClassNotFoundException])
   def load[A](buffer: Array[Byte])(implicit expected: ClassTag[A]): A = {
-    val in = new ObjectInputStream(new ByteArrayInputStream(buffer))
+    val in    = new ObjectInputStream(new ByteArrayInputStream(buffer))
     val found = in.readObject.asInstanceOf[ClassTag[_]]
     try {
       found.runtimeClass.asSubclass(expected.runtimeClass)
       in.readObject.asInstanceOf[A]
-    } catch {
+    }
+    catch {
       case _: ClassCastException =>
         in.close()
-        throw new ClassCastException("type mismatch;"+
-          "\n found : "+found+
-          "\n required: "+expected)
+        throw new ClassCastException(
+                "type mismatch;" +
+                  "\n found : " + found +
+                  "\n required: " + expected
+        )
     }
   }
 
-  def deepCopy[A](a: A)(implicit m: reflect.Manifest[A]): A = {
+  def deepCopy[A](a: A)(implicit m: reflect.ClassTag[A]): A =
     Marshal.load[A](Marshal.dump(a))
-  }
 }
-
