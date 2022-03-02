36d44e03a3034f7f6ae58942d96748989a51658c
Lucas Jeub <lucas.jeub@pometry.com>
2022-03-02 10:00:10 +0100

    Add documentation to Chain and remove unnecessary conversion to list

--- src/main/scala/com/raphtory/core/algorithm/Chain.scala
+++ src/main/scala/com/raphtory/core/algorithm/Chain.scala
@@ -1,11 +1,41 @@
 package com.raphtory.core.algorithm
 
-import scala.Seq
-
+/**
+  * {s}`Chain(algorithms: GraphAlgorithm*)`
+  *  : Run algorithms in sequence with shared graph state
+  *
+  * ## Parameters
+  *
+  *  {s}`algorithms: GraphAlgorithm*`
+  *    : List of algorithms to run
+  *
+  * ## Class Signature
+  *
+  *  {s}`Chain(algorithms: Seq[GraphAlgorithm]) extends GraphAlgorithm`
+  *
+  * ## Methods
+  *
+  *  {s}`apply(graph: GraphPerspective): GraphPerspective`
+  *    : Apply each algorithm to the graph in sequence, clearing messages in-between.
+  *      This means that while the next algorithm has access to all the graph state set by previous algorithms,
+  *      it does not receive messages sent by the previous algorithm.
+  *
+  *  {s}`tabularise(graph: GraphPerspective): Table`
+  *    : Uses the `tabularise` method of the last algorithm in the chain to return results (if {s}`algorithms`
+  *       is empty, it returns an empty table).
+  *
+  *  {s}`->(graphAlgorithm: GraphAlgorithm): Chain`
+  *    : Append a new graph algorithm to the chain.
+  *
+  *  ```{seealso}
+  *  [](com.raphtory.core.algorithm.GraphAlgorithm), [](com.raphtory.core.algorithm.GraphPerspective),
+  *  [](com.raphtory.core.algorithm.Table)
+  *  ```
+  */
 class Chain(algorithms: Seq[GraphAlgorithm]) extends GraphAlgorithm {
 
-  override def apply(graphPerspective: GraphPerspective): GraphPerspective = {
-    var gp = graphPerspective
+  override def apply(graph: GraphPerspective): GraphPerspective = {
+    var gp = graph
     if (algorithms.nonEmpty)
       for (algorithm <- algorithms)
         gp = algorithm.apply(gp).asInstanceOf[GenericGraphPerspective].clearMessages()
@@ -19,11 +49,9 @@ class Chain(algorithms: Seq[GraphAlgorithm]) extends GraphAlgorithm {
       super.tabularise(graph)
 
   override def ->(graphAlgorithm: GraphAlgorithm): Chain =
-    Chain(algorithms.toList ++ List(graphAlgorithm))
-
+    Chain(algorithms :+ graphAlgorithm: _*)
 }
 
 object Chain {
-  def apply(algorithms: GraphAlgorithm*)      = new Chain(algorithms)
-  def apply(algorithms: List[GraphAlgorithm]) = new Chain(algorithms)
+  def apply(algorithms: GraphAlgorithm*) = new Chain(algorithms)
 }
