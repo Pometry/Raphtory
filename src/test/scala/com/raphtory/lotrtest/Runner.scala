0958025894e22b741b522096e10a3aa10e07c71d
miratepuffin <b.a.steer@qmul.ac.uk>
2022-03-05 00:49:29 +0000

    Fixed GraphFunctionCompleteWithState and added tests for accumulators

--- src/test/scala/com/raphtory/lotrtest/Runner.scala
+++ src/test/scala/com/raphtory/lotrtest/Runner.scala
@@ -1,6 +1,7 @@
 package com.raphtory.lotrtest
 
 import com.google.protobuf.ByteString.Output
+import com.raphtory.GlobalState
 import com.raphtory.GraphState
 import com.raphtory.algorithms.generic.ConnectedComponents
 import com.raphtory.core.components.spout.instance.FileSpout
@@ -12,7 +13,7 @@ object Runner extends App {
   val spout        = FileSpout("/tmp/lotr.csv")
   val graphBuilder = new LOTRGraphBuilder()
   val graph        = Raphtory.createGraph(spout, graphBuilder)
-  graph.pointQuery(ConnectedComponents(), FileOutputFormat("/tmp"), 30000).waitForJob()
+  graph.pointQuery(new GlobalState(), FileOutputFormat("/tmp"), 30000).waitForJob()
   graph.stop()
 
 }
