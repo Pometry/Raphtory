d4ca8583ebcca55bbf3d263d10a120fbc40a9b23
rutujasurve94 <rutuja.r.surve@gmail.com>
2022-03-08 16:23:19 +0530

    add docs for marshal serialiser

--- src/main/scala/com/raphtory/serialisers/Marshal.scala
+++ src/main/scala/com/raphtory/serialisers/Marshal.scala
@@ -1,5 +1,50 @@
 package com.raphtory.serialisers
 
+
+/**
+  * {s}`Marshal()`
+  *
+  * : support serialisation (`dump`) and deserialisation (`load`) for `ByteArray` input and output streams
+  *
+  *
+  * ## Methods
+  *
+  *    {s}`dump[A](o: A)(implicit t: ClassTag[A]): Array[Byte]`
+  *      : Serialise to ByteArray
+  *
+  *        {s}`t: ClassTag[A]`
+  *           : serialise object to Array[Byte]
+  *
+  *    {s}`load[A](buffer: Array[Byte])(implicit expected: ClassTag[A])`
+  *      : Deserialise from Array[Byte]
+  *
+  *        {s}`buffer: Array[Byte]`
+  *           : deserialise from byte buffer to object
+  *
+  *    {s}`deepCopy[A](a: A)(implicit m: reflect.ClassTag[A])`
+  *      : Deep copy
+  *
+  *        {s}`a: ClassTag[A]`
+  *           : deserialise and persist as deep copy by serialising
+  *
+  * Example Usage:
+  *
+  * ```{code-block} scala
+  * import com.raphtory.serialisers.PulsarKryoSerialiser
+  * import com.raphtory.core.config.PulsarController
+  * import com.raphtory.serialisers.Marshal
+  * import com.raphtory.core.components.graphbuilder.GraphBuilder
+  *
+  * def setGraphBuilder(): GraphBuilder[T]
+  * val graphBuilder = setGraphBuilder()
+  * val safegraphBuilder = Marshal.deepCopy(graphBuilder)
+  * ```
+  *
+  *
+  * ```{seealso}
+  * [](com.raphtory.serialisers.PulsarKryoSerialiser)
+  * ```
+  */
 object Marshal {
   import java.io._
   import scala.reflect.ClassTag
