f14a95a6c350b9ade3e81755559cb92d2daf868c
miratepuffin <b.a.steer@qmul.ac.uk>
2022-03-09 16:14:17 +0000

    Added batch ingestion functions for the graph builder to remove the need to an array

--- src/main/scala/com/raphtory/core/components/graphbuilder/GraphBuilder.scala
+++ src/main/scala/com/raphtory/core/components/graphbuilder/GraphBuilder.scala
@@ -17,6 +17,7 @@ trait GraphBuilder[T] extends Serializable {
   private var partitionIDs: mutable.Set[Int]                 = _
   private var batchWriters: mutable.Map[Int, BatchWriter[T]] = _
   private var batching: Boolean                              = false
+  private var totalPartitions: Int                           = 1
 
   def parseTuple(tuple: T): Unit
 
@@ -43,40 +44,66 @@ trait GraphBuilder[T] extends Serializable {
     toReturn.toList
   }
 
-  protected def addVertex(updateTime: Long, srcId: Long): Unit = {
-    val update = VertexAdd(updateTime, srcId, Properties(), None)
-
-    if (batching)
-      update
-    else
-      updates += update
+  private[core] def setupBatchIngestion(
+      IDs: mutable.Set[Int],
+      writers: mutable.Map[Int, BatchWriter[T]],
+      partitions: Int
+  ) = {
+    partitionIDs = IDs
+    batchWriters = writers
+    batching = true
+    totalPartitions = partitions
   }
 
-  protected def addVertex(updateTime: Long, srcId: Long, properties: Properties): Unit =
-    updates += VertexAdd(updateTime, srcId, properties, None)
+  protected def addVertex(updateTime: Long, srcId: Long): Unit = {
+    val update = VertexAdd(updateTime, srcId, Properties(), None)
+    handleVertexAdd(update)
+  }
 
-  protected def addVertex(updateTime: Long, srcId: Long, vertexType: Type): Unit =
-    updates += VertexAdd(updateTime, srcId, Properties(), Some(vertexType))
+  protected def addVertex(updateTime: Long, srcId: Long, properties: Properties): Unit = {
+    val update = VertexAdd(updateTime, srcId, properties, None)
+    handleVertexAdd(update)
+  }
+
+  protected def addVertex(updateTime: Long, srcId: Long, vertexType: Type): Unit = {
+    val update = VertexAdd(updateTime, srcId, Properties(), Some(vertexType))
+    handleVertexAdd(update)
+  }
 
   protected def addVertex(
       updateTime: Long,
       srcId: Long,
       properties: Properties,
       vertexType: Type
-  ): Unit =
-    updates += VertexAdd(updateTime, srcId, properties, Some(vertexType))
+  ): Unit = {
+    val update = VertexAdd(updateTime, srcId, properties, Some(vertexType))
+    handleVertexAdd(update)
+  }
 
   protected def deleteVertex(updateTime: Long, srcId: Long): Unit =
     updates += VertexDelete(updateTime, srcId)
 
-  protected def addEdge(updateTime: Long, srcId: Long, dstId: Long): Unit =
-    updates += EdgeAdd(updateTime, srcId, dstId, Properties(), None)
+  protected def addEdge(updateTime: Long, srcId: Long, dstId: Long): Unit = {
+    val update = EdgeAdd(updateTime, srcId, dstId, Properties(), None)
+    handleEdgeAdd(update)
+  }
 
-  protected def addEdge(updateTime: Long, srcId: Long, dstId: Long, properties: Properties): Unit =
-    updates += EdgeAdd(updateTime, srcId, dstId, properties, None)
+  protected def addEdge(
+      updateTime: Long,
+      srcId: Long,
+      dstId: Long,
+      properties: Properties
+  ): Unit = {
+    val update = EdgeAdd(updateTime, srcId, dstId, properties, None)
+    handleEdgeAdd(update)
 
-  protected def addEdge(updateTime: Long, srcId: Long, dstId: Long, edgeType: Type): Unit =
-    updates += EdgeAdd(updateTime, srcId, dstId, Properties(), Some(edgeType))
+  }
+
+  protected def addEdge(updateTime: Long, srcId: Long, dstId: Long, edgeType: Type): Unit = {
+    val update = EdgeAdd(updateTime, srcId, dstId, Properties(), Some(edgeType))
+    handleEdgeAdd(update)
+
+  }
 
   protected def addEdge(
       updateTime: Long,
@@ -84,12 +111,45 @@ trait GraphBuilder[T] extends Serializable {
       dstId: Long,
       properties: Properties,
       edgeType: Type
-  ): Unit =
-    updates += EdgeAdd(updateTime, srcId, dstId, properties, Some(edgeType))
+  ): Unit = {
+    val update = EdgeAdd(updateTime, srcId, dstId, properties, Some(edgeType))
+    handleEdgeAdd(update)
+
+  }
 
   protected def deleteEdge(updateTime: Long, srcId: Long, dstId: Long): Unit =
     updates += EdgeDelete(updateTime, srcId, dstId)
 
+  private def handleVertexAdd(update: VertexAdd) =
+    if (batching) {
+      val partitionForTuple = checkPartition(update.srcId)
+      if (partitionIDs contains partitionForTuple)
+        batchWriters(partitionForTuple).handleMessage(update)
+    }
+    else
+      updates += update
+
+  private def handleEdgeAdd(update: EdgeAdd) = {
+    val partitionForSrc = checkPartition(update.srcId)
+    val partitionForDst = checkPartition(update.dstId)
+    if (partitionIDs contains partitionForSrc)
+      batchWriters(partitionForSrc).handleMessage(update)
+    if (
+            (partitionIDs contains partitionForDst) && (partitionForDst != partitionForSrc)
+    ) //TODO doesn't see to currently work
+      batchWriters(partitionForDst).handleMessage(
+              BatchAddRemoteEdge(
+                      update.updateTime,
+                      update.srcId,
+                      update.dstId,
+                      update.properties,
+                      update.eType
+              )
+      )
+  }
+
+  private def checkPartition(id: Long): Int =
+    (id.abs % totalPartitions).toInt
 }
 
 object GraphBuilder {
--- src/main/scala/com/raphtory/core/components/partition/LocalBatchHandler.scala
+++ src/main/scala/com/raphtory/core/components/partition/LocalBatchHandler.scala
@@ -21,49 +21,25 @@ class LocalBatchHandler[T: ClassTag](
     scheduler: Scheduler
 ) extends Component[GraphAlteration](conf: Config, pulsarController: PulsarController) {
 
+  graphBuilder.setupBatchIngestion(partitionIDs, batchWriters, totalPartitions)
+
   override def handleMessage(
       msg: GraphAlteration
   ): Unit = {} //No messages received by this component
 
-  override def run(): Unit = {
+  override def run(): Unit =
     runIngestion
-    if (spout.reschedule())
-      reschedule()
 
-  }
-
-  private def runIngestion() =
+  private def runIngestion() = {
     while (spout.hasNext())
       spout.next() match {
         case Some(tuple) =>
-          graphBuilder.getUpdates(tuple)(failOnError = false).foreach {
-            case update: VertexAdd    =>
-              val partitionForTuple = checkPartition(update.srcId)
-              if (partitionIDs contains partitionForTuple)
-                batchWriters(partitionForTuple).handleMessage(update)
-            case update: EdgeAdd      =>
-              val partitionForSrc = checkPartition(update.srcId)
-              val partitionForDst = checkPartition(update.dstId)
-              if (partitionIDs contains partitionForSrc)
-                batchWriters(partitionForSrc).handleMessage(update)
-              if (
-                      (partitionIDs contains partitionForDst) && (partitionForDst != partitionForSrc)
-              ) //TODO doesn't see to currently work
-                batchWriters(partitionForDst).handleMessage(
-                        BatchAddRemoteEdge(
-                                update.updateTime,
-                                update.srcId,
-                                update.dstId,
-                                update.properties,
-                                update.eType
-                        )
-                )
-            case update: EdgeDelete   => //Not currently supported by batch ingestion
-            case update: VertexDelete => //Not currently supported by batch ingestion
-
-          }
+          graphBuilder.parseTuple(tuple)
         case None        =>
       }
+    if (spout.reschedule())
+      reschedule()
+  }
 
   private def checkPartition(id: Long): Int =
     (id.abs % totalPartitions).toInt
