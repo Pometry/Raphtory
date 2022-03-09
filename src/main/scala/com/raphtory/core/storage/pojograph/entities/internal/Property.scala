6c8f54c69479a8b60d428a8d763c92d9b548eaa5
Lucas Jeub <lucas.jeub@pometry.com>
2022-03-09 12:15:17 +0100

    fix handling of history for ImmutableProperty such that it has a value provided before is later than creation time

--- src/main/scala/com/raphtory/core/storage/pojograph/entities/internal/Property.scala
+++ src/main/scala/com/raphtory/core/storage/pojograph/entities/internal/Property.scala
@@ -4,7 +4,7 @@ abstract class Property {
   def update(msgTime: Long, newValue: Any): Unit
   def valueAt(time: Long): Option[Any]
   def values(): Array[(Long, Any)]
-  def valuesAfter(time: Long): Array[Any]
+  def valueHistory(after: Long = Long.MinValue, before: Long = Long.MaxValue): Array[(Long, Any)]
   def currentValue(): Any
   def creation(): Long
 }
