948eaa5bed0d0b98a76003125fe4b2cfcd95df2a
Lucas Jeub <lucas.jeub@pometry.com>
2022-03-10 23:11:49 +0100

    MaxFlow algorithm that can pass the simplest test

---
+++ src/test/scala/com/raphtory/algorithms/MaxFlowTest.scala
@@ -0,0 +1,21 @@
+package com.raphtory.algorithms
+
+import com.raphtory.algorithms.generic.EdgeList
+import com.raphtory.algorithms.generic.MaxFlow
+import com.raphtory.algorithms.generic.NodeList
+import com.raphtory.core.components.graphbuilder.GraphBuilder
+
+class MaxFlowTest extends BaseCorrectnessTest {
+  override def setGraphBuilder(): GraphBuilder[String] = WeightedGraphBuilder()
+  test("test on line graph") {
+    assert(
+            correctnessTest(
+                    MaxFlow[Long]("1", "3"),
+                    "MaxFlow/minimalTest.csv",
+                    "MaxFlow/minimalResult.csv",
+                    2
+            )
+    )
+  }
+
+}
