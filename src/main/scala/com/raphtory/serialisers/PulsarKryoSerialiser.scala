d6b23b664a44208a5c614142c9452df26712b203
Rutuja Surve <rutuja.r.surve@gmail.com>
2022-03-11 20:21:36 +0530

    docs: serialisers (#260)
    * add docs for pulsar serialiser
    
    * add docs for marshal serialiser
    
    * add doc for avro schema
    
    * fix docs for serialisers
    
    * add usage for serialisers

--- src/main/scala/com/raphtory/serialisers/PulsarKryoSerialiser.scala
+++ src/main/scala/com/raphtory/serialisers/PulsarKryoSerialiser.scala
@@ -5,9 +5,43 @@ import com.twitter.chill.ScalaKryoInstantiator
 
 import scala.reflect.runtime.universe._
 
-/** @DoNotDocument */
+/**
+  *  {s}`PulsarKryoSerialiser()`
+  *    : support serialisation and deserialisation using ScalaKryoInstantiator from twitter.chill package
+  *
+  *  ## Methods
+  *
+  *    {s}`serialise[T](value: T): Array[Byte]`
+  *      : serialise value to byte array
+  *
+  *    {s}`deserialise[T](bytes: Array[Byte]): T`
+  *      : deserialise byte array to object
+  *
+  *
+  *  Example Usage:
+  *
+  * ```{code-block} scala
+  * import com.raphtory.serialisers.PulsarKryoSerialiser
+  * import com.raphtory.core.config.PulsarController
+  * import org.apache.pulsar.client.api.Schema
+  *
+  * val schema: Schema[Array[Byte]] = Schema.BYTES
+  * val kryo = PulsarKryoSerialiser()
+  *
+  * val pulsarController = new PulsarController(config)
+  * val client         = pulsarController.accessClient
+  * val producer_topic = "test_lotr_graph_input_topic"
+  * val producer       = client.newProducer(Schema.BYTES).topic(producer_topic).create()
+  * producer.sendAsync(kryo.serialise("Gandalf,Benjamin,400"))
+  * ```
+  *
+  *
+  * ```{seealso}
+  * [](com.raphtory.core.client.RaphtoryClient)
+  * ```
+  */
 class PulsarKryoSerialiser {
-  private val kryo: KryoPool = ScalaKryoInstantiator.defaultPool
+  private val kryo = ScalaKryoInstantiator.defaultPool
 
   def serialise[T](value: T): Array[Byte] = kryo.toBytesWithClass(value)
 
