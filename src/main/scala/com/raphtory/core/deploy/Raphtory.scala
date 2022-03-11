61a4fa9876934c2f1631dabecb9d0af99d5455f8
miratepuffin <b.a.steer@qmul.ac.uk>
2022-03-11 19:49:51 +0000

    Make scheduler private again

--- src/main/scala/com/raphtory/core/deploy/Raphtory.scala
+++ src/main/scala/com/raphtory/core/deploy/Raphtory.scala
@@ -21,7 +21,7 @@ import scala.reflect.runtime.universe._
 
 object Raphtory {
 
-  val scheduler = new MonixScheduler().scheduler
+  private val scheduler = new MonixScheduler().scheduler
 
   def streamGraph[T: TypeTag: ClassTag](
       spout: Spout[T] = new IdentitySpout[T](),
