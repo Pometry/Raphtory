c7bd1089ad517af8f4cb39debc14f63d7d3c1227
Ben Steer <b.a.steer@qmul.ac.uk>
2022-03-15 09:59:25 +0000

    Created new spout API and added batchloading to the partitions (#271)

--- src/test/scala/com/raphtory/lotrtest/Runner.scala
+++ src/test/scala/com/raphtory/lotrtest/Runner.scala
@@ -4,16 +4,14 @@ import com.google.protobuf.ByteString.Output
 import com.raphtory.GlobalState
 import com.raphtory.GraphState
 import com.raphtory.algorithms.generic.ConnectedComponents
-import com.raphtory.core.components.spout.instance.FileSpout
 import com.raphtory.core.deploy.Raphtory
 import com.raphtory.output.FileOutputFormat
+import com.raphtory.spouts.FileSpout
 
 object Runner extends App {
 
-  val spout        = FileSpout("/tmp/lotr.csv")
+  val spout        = FileSpout("/tmp")
   val graphBuilder = new LOTRGraphBuilder()
-  val graph        = Raphtory.createGraph(spout, graphBuilder)
-  graph.pointQuery(new GlobalState(), FileOutputFormat("/tmp"), 30000).waitForJob()
-  graph.stop()
+  val graph        = Raphtory.batchLoadGraph(spout, graphBuilder)
 
 }
