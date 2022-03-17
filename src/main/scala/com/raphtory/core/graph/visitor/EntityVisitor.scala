45ba879d4af6646b79ceb074700bb41a0140fb87
Lucas Jeub <lucas.jeub@pometry.com>
2022-03-17 12:21:52 +0100

    add pointer and seealso section to HistoricEvent

--- src/main/scala/com/raphtory/core/graph/visitor/EntityVisitor.scala
+++ src/main/scala/com/raphtory/core/graph/visitor/EntityVisitor.scala
@@ -264,6 +264,7 @@ abstract class EntityVisitor {
   * {s}`HistoricEvent(time: Long, event: Boolean)`
   *  : Case class for encoding additions and deletions
   *
+  * This class is returned by the history access methods of [{s}`EntityVisitor`](com.raphtory.core.graph.visitor.EntityVisitor).
   *  ## Parameters
   *
   *  {s}`time: Long`
@@ -271,5 +272,11 @@ abstract class EntityVisitor {
   *
   *  {s}`event: Boolean`
   *    : {s}`true` if event is an addition or {s}`false` if event is a deletion
+  *
+  * ```{seealso}
+  * [](com.raphtory.core.graph.visitor.EntityVisitor),
+  * [](com.raphtory.core.graph.visitor.Vertex),
+  * [](com.raphtory.core.graph.visitor.Edge)
+  * `
   */
 case class HistoricEvent(time: Long, event: Boolean)
