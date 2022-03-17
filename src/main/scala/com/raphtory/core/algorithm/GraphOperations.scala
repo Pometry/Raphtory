693e34919a35665be7f093f42c70d333fe7fa91b
Pedro Rico Pinazo <ricopinazo@gmail.com>
2022-03-17 11:36:22 +0000

    Move graph function definitions into GraphOperations.scala

--- src/main/scala/com/raphtory/core/algorithm/GraphOperations.scala
+++ src/main/scala/com/raphtory/core/algorithm/GraphOperations.scala
@@ -1,6 +1,46 @@
 package com.raphtory.core.algorithm
 
+import com.raphtory.core.components.querymanager.QueryManagement
 import com.raphtory.core.graph.visitor.Vertex
+
+sealed trait GraphFunction                    extends QueryManagement
+final case class Setup(f: GraphState => Unit) extends GraphFunction
+final case class Step(f: (Vertex) => Unit)    extends GraphFunction
+
+final case class StepWithGraph(
+    f: (Vertex, GraphState) => Unit,
+    graphState: GraphStateImplementation = GraphStateImplementation.empty
+) extends GraphFunction
+
+final case class Iterate(f: (Vertex) => Unit, iterations: Int, executeMessagedOnly: Boolean)
+        extends GraphFunction
+
+final case class IterateWithGraph(
+    f: (Vertex, GraphState) => Unit,
+    iterations: Int,
+    executeMessagedOnly: Boolean,
+    graphState: GraphStateImplementation = GraphStateImplementation.empty
+)                                                     extends GraphFunction
+final case class VertexFilter(f: (Vertex) => Boolean) extends GraphFunction
+
+final case class VertexFilterWithGraph(
+    f: (Vertex, GraphState) => Boolean,
+    graphState: GraphStateImplementation = GraphStateImplementation.empty
+)                                         extends GraphFunction
+final case class Select(f: Vertex => Row) extends GraphFunction
+
+final case class SelectWithGraph(
+    f: (Vertex, GraphState) => Row,
+    graphState: GraphStateImplementation = GraphStateImplementation.empty
+) extends GraphFunction
+
+final case class GlobalSelect(
+    f: GraphState => Row,
+    graphState: GraphStateImplementation = GraphStateImplementation.empty
+)                                                      extends GraphFunction
+final case class ExplodeSelect(f: Vertex => List[Row]) extends GraphFunction
+final case class ClearChain()                          extends GraphFunction
+final case class PerspectiveDone()                     extends GraphFunction
 
 /**
   * {s}`GraphOperations[G <: GraphOperations[G]]`
