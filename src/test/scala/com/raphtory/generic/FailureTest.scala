159c677e30a8fbbd0a7927ddd8a7d3487c88e1a9
Lucas Jeub <lucas.jeub@pometry.com>
2022-03-21 18:44:34 +0100

    cleaner test for failure check

--- src/test/scala/com/raphtory/generic/FailureTest.scala
+++ src/test/scala/com/raphtory/generic/FailureTest.scala
@@ -1,8 +1,12 @@
 package com.raphtory.generic
 
 import com.raphtory.algorithms.BaseCorrectnessTest
+import com.raphtory.algorithms.BasicGraphBuilder
 import com.raphtory.core.algorithm.GraphAlgorithm
 import com.raphtory.core.algorithm.GraphPerspective
+import com.raphtory.core.deploy.Raphtory
+import com.raphtory.output.FileOutputFormat
+import org.scalatest.funsuite.AnyFunSuite
 
 class FailingAlgo extends GraphAlgorithm {
 
@@ -12,9 +16,16 @@ class FailingAlgo extends GraphAlgorithm {
   }
 }
 
-class FailureTest extends BaseCorrectnessTest {
+class FailureTest extends AnyFunSuite {
   test("test failure propagation") {
-    assert(correctnessTest(new FailingAlgo, "./blank.csv", "./blank.csv", 0))
+    val graph = Raphtory.streamGraph(graphBuilder = BasicGraphBuilder())
+    val query = graph.pointQuery(new FailingAlgo, FileOutputFormat("/tmp"), 0)
+    for (i <- 1 to 10 if !query.isJobDone())
+      Thread.sleep(1000)
+    assert(
+            query.isJobDone()
+    ) // if query failed to terminate after 10 seconds, assuming infinite loop
+    graph.stop()
   }
 
 }
