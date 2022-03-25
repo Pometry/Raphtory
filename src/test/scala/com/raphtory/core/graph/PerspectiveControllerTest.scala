a705a27ed3adc953d34dd083c7c8e1e53e26fec2
Pedro Rico Pinazo <ricopinazo@gmail.com>
2022-03-25 20:27:28 +0000

    Fix broken test for the PerspectiveController

--- src/test/scala/com/raphtory/core/graph/PerspectiveControllerTest.scala
+++ src/test/scala/com/raphtory/core/graph/PerspectiveControllerTest.scala
@@ -6,9 +6,6 @@ import com.raphtory.core.components.querymanager.Query
 import com.raphtory.core.time.DateTimeParser.{defaultParse => parseDateTime}
 import com.raphtory.core.time.IntervalParser.{parse => parseInterval}
 import org.scalatest.funsuite.AnyFunSuite
-import com.raphtory.core.time.TimeConverters._
-
-import java.time.Instant
 
 class PerspectiveControllerTest extends AnyFunSuite {
   test("A range of perspectives is correctly generated") {
@@ -16,7 +13,7 @@ class PerspectiveControllerTest extends AnyFunSuite {
     val startTime  = parseDateTime("2021-01-01 00:00:00")
     val firstStep  = parseDateTime("2021-03-01 00:00:00")
     val secondStep = parseDateTime("2021-05-01 00:00:00")
-    val endTime    = parseDateTime("2021-06-01 00:00:00")
+    val endTime    = parseDateTime("2021-07-01 00:00:00")
     val query      = Query(
             timelineStart = startTime,
             timelineEnd = endTime,
@@ -24,11 +21,6 @@ class PerspectiveControllerTest extends AnyFunSuite {
             windowAlignment = Alignment.END
     )
     val controller = PerspectiveController(0, Long.MaxValue, query)
-
-    println(increment.size)
-    println(Instant.ofEpochMilli(startTime + increment))
-
-    parseInterval("2 years")
 
     assert(controller.nextPerspective().get === Perspective(startTime, None))
     assert(controller.nextPerspective().get === Perspective(firstStep, None))
