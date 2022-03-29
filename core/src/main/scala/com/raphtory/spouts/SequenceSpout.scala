21e15a3089d4830457ff6deb1e38bd6a5b3e25f3
Lucas Jeub <lucas.jeub@pometry.com>
2022-03-29 10:47:45 +0200

    implement SequenceSpout for sending small test data to raphtory without having to create a file

---
+++ core/src/main/scala/com/raphtory/spouts/SequenceSpout.scala
@@ -0,0 +1,22 @@
+package com.raphtory.spouts
+
+import com.raphtory.components.spout.Spout
+
+/**
+  * {s}`SequenceSpout[T](seq: T*)`
+  *  : output input messages to Raphtory
+  *
+  * This spout is mainly useful for testing functionality
+  */
+class SequenceSpout[T](seq: Seq[T]) extends Spout[T] {
+  val seqIterator                          = seq.iterator
+  override def spoutReschedules(): Boolean = false
+
+  override def hasNext: Boolean = seqIterator.hasNext
+
+  override def next(): T = seqIterator.next()
+}
+
+object SequenceSpout {
+  def apply[T](seq: T*) = new SequenceSpout[T](seq)
+}
