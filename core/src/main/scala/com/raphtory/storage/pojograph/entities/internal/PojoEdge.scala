e6aa0685556ea9ad50fcac40c631c1f820c35eb3
Lucas Jeub <lucas.jeub@pometry.com>
2022-03-29 10:47:30 +0200

    sorted, deduplicated history

--- core/src/main/scala/com/raphtory/storage/pojograph/entities/internal/PojoEdge.scala
+++ core/src/main/scala/com/raphtory/storage/pojograph/entities/internal/PojoEdge.scala
@@ -1,10 +1,12 @@
 package com.raphtory.storage.pojograph.entities.internal
 
+import com.raphtory.util.OrderedBuffer._
+
 /** @DoNotDocument */
 class PojoEdge(msgTime: Long, srcId: Long, dstId: Long, initialValue: Boolean)
         extends PojoEntity(msgTime, initialValue) {
 
-  def killList(vKills: List[Long]): Unit = history ++= vKills.map(x => (x, false))
+  def killList(vKills: List[Long]): Unit = history sortedExtend vKills.map(x => (x, false))
 
   def getSrcId: Long = srcId
   def getDstId: Long = dstId
