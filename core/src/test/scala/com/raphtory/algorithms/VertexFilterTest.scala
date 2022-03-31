b6f6ae879e94a673f531330b526e9df80bd2ebb5
Lucas Jeub <lucas.jeub@pometry.com>
2022-03-31 13:50:55 +0200

    fix VertexFilterTest

--- core/src/test/scala/com/raphtory/algorithms/VertexFilterTest.scala
+++ core/src/test/scala/com/raphtory/algorithms/VertexFilterTest.scala
@@ -3,30 +3,17 @@ package com.raphtory.algorithms
 import com.raphtory.algorithms.generic.EdgeList
 import com.raphtory.algorithms.api.GraphAlgorithm
 import com.raphtory.algorithms.api.GraphPerspective
+import com.raphtory.algorithms.api.VertexFilter
 import com.raphtory.graph.visitor.Vertex
-
-class VertexFilter extends GraphAlgorithm {
-
-  override def apply(graph: GraphPerspective): GraphPerspective =
-    graph
-      .filter { (vertex: Vertex) =>
-        println(vertex.getProperty("name").get)
-        vertex.getProperty("name").get == 3
-      }
-}
-
-object VertexFilter {
-  def apply() = new VertexFilter()
-}
 
 class VertexFilterTest extends BaseCorrectnessTest {
 
   test("Vertex is being filtered") {
     assert(
             correctnessTest(
-                    VertexFilter() -> EdgeList(),
+                    VertexFilter(_.ID() != 1) -> EdgeList(),
                     "MotifCount/motiftest.csv",
-                    "MotifCount/motifFilterTest.csv",
+                    "VertexFilter/motifFilterTest.csv",
                     23
             )
     )
