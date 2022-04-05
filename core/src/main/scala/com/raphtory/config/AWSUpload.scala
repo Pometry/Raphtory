ef2f2f9af650fd6c40ff21f3d4feda81c1aed806
Rutuja Surve <rutuja.r.surve@gmail.com>
2022-04-05 13:50:10 +0530

    upload to s3
    Upload to S3

---
+++ core/src/main/scala/com/raphtory/config/AWSUpload.scala
@@ -0,0 +1,32 @@
+package com.raphtory.core.deploy
+
+import com.amazonaws.services.s3.AmazonS3Client
+import com.amazonaws.auth.BasicAWSCredentials
+import com.amazonaws.SDKGlobalConfiguration
+import java.io.File
+import com.raphtory.core.deploy.Raphtory
+import com.typesafe.config.Config
+
+object AWSUpload extends App {
+
+  val raphtoryConfig: Config = Raphtory.getDefaultConfig()
+  val bucketName: String        = raphtoryConfig.getString("raphtory.aws.bucket_name")
+  val fileToUpload: String      = raphtoryConfig.getString("raphtory.aws.input_file_path")
+  val uploadFileName: String    = raphtoryConfig.getString("raphtory.aws.upload_file_name")
+
+  System.setProperty(SDKGlobalConfiguration.ENABLE_S3_SIGV4_SYSTEM_PROPERTY, "true")
+
+  //file to upload
+  val fileToUpload = new File(fileToUpload)
+
+  /* These Keys available  in “Security Credentials” of Amazon S3 account */
+  val AWS_ACCESS_KEY = raphtoryConfig.getString("raphtory.aws.access_key")
+  val AWS_SECRET_KEY = raphtoryConfig.getString("raphtory.aws.secret_access_key")
+  val AWSCredentials = new BasicAWSCredentials(AWS_ACCESS_KEY, AWS_SECRET_KEY)
+  val amazonS3Client = new AmazonS3Client(AWSCredentials)
+
+  // This will create a bucket for storage
+  amazonS3Client.createBucket(bucketName)
+
+  amazonS3Client.putObject(bucketName, uploadFileName, fileToUpload)
+}
