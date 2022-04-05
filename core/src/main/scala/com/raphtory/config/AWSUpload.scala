a4aaf73b0486897205d8d203742563120b0d7203
Rutuja Surve <rutuja.r.surve@gmail.com>
2022-04-05 19:50:27 +0530

    set signature for aws upload

--- core/src/main/scala/com/raphtory/config/AWSUpload.scala
+++ core/src/main/scala/com/raphtory/config/AWSUpload.scala
@@ -13,6 +13,8 @@ object AWSUpload extends App {
   val bucketName: String        = raphtoryConfig.getString("raphtory.aws.bucket_name")
   val fileToUpload: String      = raphtoryConfig.getString("raphtory.aws.input_file_path")
   val uploadFileName: String    = raphtoryConfig.getString("raphtory.aws.upload_file_name")
+  val clientConfiguration = new ClientConfiguration()
+  clientConfiguration.setSignerOverride("AWSS3V4SignerType")
 
   def getAWSClient(): AmazonS3Client = {
     System.setProperty(SDKGlobalConfiguration.ENABLE_S3_SIGV4_SYSTEM_PROPERTY, "true")
@@ -24,7 +26,8 @@ object AWSUpload extends App {
     val AWS_ACCESS_KEY = raphtoryConfig.getString("raphtory.aws.access_key")
     val AWS_SECRET_KEY = raphtoryConfig.getString("raphtory.aws.secret_access_key")
     val AWSCredentials = new BasicAWSCredentials(AWS_ACCESS_KEY, AWS_SECRET_KEY)
-    val amazonS3Client = new AmazonS3Client(AWSCredentials)
+    val amazonS3Client = new AmazonS3Client(AWSCredentials, clientConfiguration)
+    amazonS3Client
   }
 
   // This will create a bucket for storage
