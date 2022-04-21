18b4c4807c274071dd682522bf1e4007777dd818
rachchan <25484244+rachchan@users.noreply.github.com>
2022-04-21 10:50:06 +0100

    DEV-546 Time Series Library (#354)
    * Adding scala time series library
    
    * Removing unnecessary methods
    
    * TimeSeries Property History method
    
    * Remove unused imports
    
    * remove changes to lotrtest
    
    * added docs for getpropertytimeseries
    
    * In progress implementing timeSeriesHistory
    
    * Implement timeSeriesHistory() and unit tests
    
    * OutEdgesHistory

---
+++ core/src/test/scala/com/raphtory/TimeSeriesGraphState.scala
@@ -0,0 +1,35 @@
+package com.raphtory
+
+import com.raphtory.algorithms.api.GraphAlgorithm
+import com.raphtory.algorithms.api.GraphPerspective
+import com.raphtory.algorithms.api.Row
+import com.raphtory.algorithms.api.Table
+
+class TimeSeriesGraphState() extends GraphAlgorithm {
+
+  override def tabularise(graph: GraphPerspective): Table =
+    graph.select { vertex =>
+      val propertyhistory =
+        vertex.getPropertySet().toArray.map(x => vertex.getTimeSeriesPropertyHistory(x).size).sum
+
+      val inEdges  = vertex.getInEdges()
+      val outEdges = vertex.getOutEdges()
+
+      val inEdgesHistory =
+        inEdges.map(e => e.timeSeriesHistory().size).sum
+
+      val outEdgesHistory =
+        outEdges.map(e => e.timeSeriesHistory().size).sum
+
+      Row(
+              vertex.ID(),
+              propertyhistory,
+              inEdgesHistory,
+              outEdgesHistory
+      )
+    }
+}
+
+object TimeSeriesGraphState {
+  def apply() = new TimeSeriesGraphState()
+}
