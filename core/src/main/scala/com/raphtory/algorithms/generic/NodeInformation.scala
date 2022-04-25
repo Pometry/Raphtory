c230ff7c80378b5b35c4688e09fccc43f456d465
rachelchan <rachelchan1497@gmail.com>
2022-04-25 14:06:19 +0100

    Added docs for nodeinformation.scala

--- core/src/main/scala/com/raphtory/algorithms/generic/NodeInformation.scala
+++ core/src/main/scala/com/raphtory/algorithms/generic/NodeInformation.scala
@@ -5,6 +5,55 @@ import com.raphtory.algorithms.api.GraphPerspective
 import com.raphtory.algorithms.api.Row
 import com.raphtory.algorithms.api.Table
 
+/**
+  * {s}`NodeInformation(initialID: Long, hopsAway: Int = 1)`
+  *   : Finds information about node and neighbours of node X hops away (default = 1).
+  *
+  *  ## Usage
+  *     Used with JsonOutputRunner.scala (located in the lotrTopic example) and JsonOutputFormat.scala to output node information in Json format.
+  *
+  *  ## Parameters
+  *     {s}`initialID: Long`
+  *       : initial vertex ID that user inputs
+  *
+  *     {s}`hopAway: Int = 1`
+  *       : information about neighbours nodes set to X number of hops away from original vertex. Default is set to 1 hop away.
+  *
+  *   ## States
+  *
+  *   {s}`vertexInvolved: Boolean`
+  *     : Boolean flag of whether vertex should be included in the final tabularised output.
+  *
+  *  ## Returns
+  *
+  *   {
+  *      "label": "Goldilocks",
+  *      "metadata": {
+  *        "id": "5415127257870295999"
+  *      },
+  *      "edges": [
+  *        {
+  *          "source": "5415127257870295999",
+  *          "target": "2751396522352401052",
+  *          "metadata": {
+  *            "value": 0
+  *          }
+  *        },
+  *        {
+  *          "source": "5415127257870295999",
+  *          "target": "-6281184428558944342",
+  *          "metadata": {
+  *            "value": 0
+  *          }
+  *        }
+  *      ]
+  *    }
+  *
+  *   ```{note}
+  *   To be used with JsonOutputFormat.scala and JsonOutputRunner.scala (in lotrTopic example) to return this Json format.
+  *   ```
+  */
+
 class NodeInformation(initialID: Long, hopsAway: Int = 1) extends GraphAlgorithm {
 
   case class Node(label: String, metadata: NodeData, edges: Array[EdgeInfo])
@@ -40,8 +89,8 @@ class NodeInformation(initialID: Long, hopsAway: Int = 1) extends GraphAlgorithm
           .getEdges()
           .map { edge =>
             EdgeInfo(
-                    edge.src().toString,
                     edge.dst().toString,
+                    edge.src().toString,
                     EdgeData(edge.weight(weightProperty = "Character Co-occurence", 0))
             )
           }
