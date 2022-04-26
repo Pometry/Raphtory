ac3552064677d1dfc208610535a53808d18ffa67
Haaroon Yousaf <Haaroon@users.noreply.github.com>
2022-04-26 16:12:53 +0100

    example ith new api

---
+++ python/example.py
@@ -0,0 +1,28 @@
+from raphtoryclient import client
+
+raphtoryClient = client.client(raphtory_deployment_id="raphtory_249183142")
+
+raphtoryClient.java_import("com.raphtory.algorithms.generic.ConnectedComponents")
+connectedComponentsAlgorithm = raphtoryClient.gateway.jvm.ConnectedComponents
+
+raphtoryClient.java_import("com.raphtory.output.FileOutputFormat")
+fileOutputFormat = raphtoryClient.gateway.jvm.FileOutputFormat
+
+raphtoryClient.raphtory_client.pointQuery(
+    connectedComponentsAlgorithm(),
+    fileOutputFormat.apply("/tmp/adsasdads"),
+    30000)
+
+
+# queryHandler = raphtoryClient.raphtory_client
+#     .at(30000)
+#     .past()
+#     .execute(connectedComponentsAlgorithm())
+#     .writeTo(fileOutputFormat.apply("/tmp/pythonCC"))
+
+
+
+# raphtoryClient.raphtory_client.pointQuery(
+#     connectedComponentsAlgorithm(),
+#     ,
+#     30000)
