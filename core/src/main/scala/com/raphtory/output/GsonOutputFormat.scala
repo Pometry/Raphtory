251745449bf8956ea403fa7cf9ec73a93d0f1516
Rachel Chan <25484244+rachchan@users.noreply.github.com>
2022-04-29 13:49:23 +0100

    Initial JSON output for UI (#372)
    * Implemented initial JSON output for UI
    
    * Added some comments and removed unused imports
    
    * Fixed JsonOutputFormat
    
    * JsonOutputFormat to Json standard format
    
    * changed edgedata label name to metadata
    
    * Removed unused case class
    
    * Added docs for nodeinformation.scala
    
    * added filter to row
    
    * Output JSON to file
    
    * removed unnecessary line
    
    * Renamed jsonformat to Gson
    
    Co-authored-by: miratepuffin <b.a.steer@qmul.ac.uk>

---
+++ core/src/main/scala/com/raphtory/output/GsonOutputFormat.scala
@@ -0,0 +1,40 @@
+package com.raphtory.output
+
+import com.google.gson.GsonBuilder
+import com.raphtory.algorithms.api.OutputFormat
+import com.raphtory.algorithms.api.Row
+import com.raphtory.time.Interval
+
+import java.io.File
+
+/**
+  * {s}`GsonOutputFormat()`
+  *   : writes output for Raphtory Job to Gson Format
+  */
+
+class GsonOutputFormat(filePath: String) extends OutputFormat {
+
+  override def write(
+      timestamp: Long,
+      window: Option[Interval],
+      jobID: String,
+      row: Row,
+      partitionID: Int
+  ): Unit = {
+    val dir = new File(s"$filePath/$jobID")
+    dir.mkdirs()
+
+    val gsonBuilder = new GsonBuilder()
+      .setPrettyPrinting()
+      .create()
+
+    val value = s"${gsonBuilder.toJson(row.getValues())}"
+    reflect.io.File(s"$filePath/$jobID/partition-$partitionID").appendAll(value)
+  }
+}
+
+object GsonOutputFormat {
+
+  def apply(filePath: String) =
+    new GsonOutputFormat(filePath)
+}
