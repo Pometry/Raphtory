07e6274b6f1ad8347e493f0dc1f8cf923538bad5
Pedro Rico Pinazo <ricopinazo@gmail.com>
2022-05-04 15:38:46 +0200

    Reorder Component class

--- core/src/main/scala/com/raphtory/components/Component.scala
+++ core/src/main/scala/com/raphtory/components/Component.scala
@@ -9,14 +9,12 @@ abstract class Component[T](conf: Config) {
 
   val logger: Logger = Logger(LoggerFactory.getLogger(this.getClass))
 
-  // TODO: remove this from here
   val partitionServers: Int    = conf.getInt("raphtory.partitions.serverCount")
   val partitionsPerServer: Int = conf.getInt("raphtory.partitions.countPerServer")
   val totalPartitions: Int     = partitionServers * partitionsPerServer
 
+  def getWriter(srcId: Long): Int = (srcId.abs % totalPartitions).toInt
   def handleMessage(msg: T): Unit
   def run(): Unit
   def stop(): Unit = {}
-
-  protected def getWriter(srcId: Long): Int = (srcId.abs % totalPartitions).toInt
 }
