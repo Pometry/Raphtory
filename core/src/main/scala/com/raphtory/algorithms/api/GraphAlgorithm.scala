c4f893ba388d0cd36c0667d5b13830cbb4e501e9
Haaroon Yousaf <Haaroon@users.noreply.github.com>
2022-05-11 17:01:32 +0100

    forgot to do graph algorithm

--- core/src/main/scala/com/raphtory/algorithms/api/GraphAlgorithm.scala
+++ core/src/main/scala/com/raphtory/algorithms/api/GraphAlgorithm.scala
@@ -3,47 +3,48 @@ package com.raphtory.algorithms.api
 import com.typesafe.scalalogging.Logger
 import org.slf4j.LoggerFactory
 
-/**
-  * `GraphAlgorithm`
-  *  : Base class for writing graph algorithms
+/** Base class for writing graph algorithms
   *
-  * ## Attributes
-  *
-  *  `logger: Logger`
-  *    : Logger instance for writing out log messages
-  *
-  * ## Methods
   *
   *  `apply(graph: GraphPerspective): GraphPerspective`
-  *    : Default implementation returns the graph unchanged
+  *    :
   *
   *   `tabularise(graph: GraphPerspective): Table`
-  *    : Return tabularised results (default implementation returns empty table)
+  *    :
   *
   *   `run(graph: GraphPerspective): Unit`
-  *      : Run graph algorithm and output results (called internally by the query API to execute the algorithm).
-  *        Normally, this method should not be overriden. Overriding this method can mean that the algorithm will
-  *        behave differently, depending on whether it is called as part of a [](com.raphtory.algorithms.api.Chain)
-  *        or on its own.
+  *      :
   *
   *   `->(graphAlgorithm: GraphAlgorithm): Chain`
-  *      : Create a new algorithm [](com.raphtory.algorithms.api.Chain) which runs this algorithm first before
-  *        running the other algorithm.
+  *      :
   *
   *        `graphAlgorithm: GraphAlgorithm)`
-  *          : next algorithm to run in the chain
+  *          :
   */
 abstract class GraphAlgorithm extends Serializable {
+  /** Logger instance for writing out log messages */
   val logger: Logger = Logger(LoggerFactory.getLogger(this.getClass))
 
+  /** Default implementation returns the graph unchanged
+   * @param graph graph to run function upon */
   def apply(graph: GraphPerspective): GraphPerspective =
     graph
 
+  /** Return tabularised results (default implementation returns empty table)
+   * @param graph graph to run function upon */
   def tabularise(graph: GraphPerspective): Table =
     graph.select(vertex => Row())
 
+  /** Run graph algorithm and output results (called internally by the query API to execute the algorithm).
+   *  Normally, this method should not be overriden. Overriding this method can mean that the algorithm will
+   *  behave differently, depending on whether it is called as part of a [](com.raphtory.algorithms.api.Chain)
+   *  or on its own.
+   * @param graph graph to run function upon */
   def run(graph: GraphPerspective): Table = tabularise(apply(graph))
 
+  /** Create a new algorithm [](com.raphtory.algorithms.api.Chain) which runs this algorithm first before
+   *  running the other algorithm.
+   *  @param graphAlgorithm next algorithm to run in the chain */
   def ->(graphAlgorithm: GraphAlgorithm): Chain =
     Chain(this, graphAlgorithm)
 }
