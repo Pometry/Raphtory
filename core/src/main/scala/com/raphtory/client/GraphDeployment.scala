6067bb73da413fa0814129d8f2f1ae4586078938
miratepuffin <b.a.steer@qmul.ac.uk>
2022-05-22 22:52:12 +0100

    Added Temporal Graph Connection Class which can 'disconnect' removing allocated resources.

--- core/src/main/scala/com/raphtory/client/GraphDeployment.scala
+++ core/src/main/scala/com/raphtory/client/GraphDeployment.scala
@@ -8,7 +8,9 @@ import com.raphtory.config.Scheduler
 import com.raphtory.config.ThreadedWorker
 import com.raphtory.config.ZookeeperIDManager
 import com.typesafe.config.Config
+import com.typesafe.scalalogging.Logger
 import io.prometheus.client.exporter.HTTPServer
+import org.slf4j.LoggerFactory
 
 import java.io.IOException
 import scala.language.postfixOps
@@ -18,23 +20,18 @@ import scala.reflect.runtime.universe._
 /** Graph Deployment extends Raphtory Client to initialise Query Manager, Partitions, Spout Worker and GraphBuilder Worker for a deployment ID
   *  `GraphDeployment` should not be created directly. To create a `GraphDeployment` use
   *  `Raphtory.createClient(deploymentID: String = "", customConfig: Map[String, Any] = Map())`.
-  *  The query methods for `GraphDeployment` are similar to `RaphtoryClient`
-  * @see [[com.raphtory.client.RaphtoryClient]], [[com.raphtory.deployment.Raphtory]]
   */
 private[raphtory] class GraphDeployment[T: ClassTag: TypeTag](
     batchLoading: Boolean,
     spout: Spout[T],
     graphBuilder: GraphBuilder[T],
-    private val querySender: QuerySender,
     private val conf: Config,
     private val componentFactory: ComponentFactory,
     private val scheduler: Scheduler
-) extends RaphtoryClient(
-                querySender,
-                conf
-        ) {
+) {
 
   allowIllegalReflection()
+  val logger: Logger = Logger(LoggerFactory.getLogger(this.getClass))
 
   private val deploymentID: String = conf.getString("raphtory.deploy.id")
   private val spoutTopic: String   = conf.getString("raphtory.spout.topic")
