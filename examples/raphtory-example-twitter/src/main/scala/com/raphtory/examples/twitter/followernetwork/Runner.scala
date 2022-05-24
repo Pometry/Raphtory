17428106b6db6a0a722023e87f45acd4aa939d17
rachelchan <rachelchan1497@gmail.com>
2022-05-24 12:13:05 +0100

    Refactored twitter

--- examples/raphtory-example-twitter/src/main/scala/com/raphtory/examples/twitter/followernetwork/Runner.scala
+++ examples/raphtory-example-twitter/src/main/scala/com/raphtory/examples/twitter/followernetwork/Runner.scala
@@ -1,24 +1,22 @@
-package com.raphtory.examples.twittercircles
+package com.raphtory.examples.twitter.followernetwork
 
 import com.raphtory.algorithms.api.Alignment
-import com.raphtory.algorithms.generic.ConnectedComponents
-import com.raphtory.algorithms.generic.EdgeList
+import com.raphtory.algorithms.generic.{ConnectedComponents, EdgeList}
 import com.raphtory.deployment.Raphtory
-import com.raphtory.output.FileOutputFormat
+import com.raphtory.examples.twitter.followernetwork.graphbuilders.TwitterCirclesGraphBuilder
 import com.raphtory.output.PulsarOutputFormat
 import com.raphtory.spouts.StaticGraphSpout
-import com.raphtory.examples.twittercircles.graphbuilders.TwitterCirclesGraphBuilder
 import com.raphtory.util.FileUtils
 
 object Runner extends App {
   // Create Graph
   val path = "/tmp/snap-twitter.csv"
-  val url  = "https://raw.githubusercontent.com/Raphtory/Data/main/snap-twitter.csv"
+  val url = "https://raw.githubusercontent.com/Raphtory/Data/main/snap-twitter.csv"
   FileUtils.curlFile(path, url)
 
-  val source  = StaticGraphSpout(path)
+  val source = StaticGraphSpout(path)
   val builder = new TwitterCirclesGraphBuilder()
-  val graph   = Raphtory.stream(spout = source, graphBuilder = builder)
+  val graph = Raphtory.stream(spout = source, graphBuilder = builder)
   Thread.sleep(20000)
   graph
     .at(88234)
