8e6418ac797986fb8a85012ee4626e5fa24edf15
Pedro Rico Pinazo <ricopinazo@gmail.com>
2022-05-27 16:55:37 +0200

    Add scaladocs for Sink trait

--- core/src/main/scala/com/raphtory/output/Sink.scala
+++ core/src/main/scala/com/raphtory/output/Sink.scala
@@ -1,24 +1,41 @@
 package com.raphtory.output
 
-// A Sink is responsible fot outputting one or several entities of some type OutputType to the outside.
-// An entity is a piece of information meaningful by itself and isolated from the rest of entities.
-// Entities can be separated from the others using a natural separation existing for the technology in use.
-// For instance, in a message system, each entity might be sent in a separate message
-// If there doesn't exist such kind of separation, some kind of delimiter might be used.
-// For instance, in a file output, a character delimiter as a "\n" might be used.
-// If we write just one entity into the output, there should be any delimiter symbols as they are not needed
-// The behavior expected from combining calls to append/closeEntity and writeEntity is undefined
+/** An abstract sink of entities to be written out
+  *
+  * An entity is a piece of information meaningful by itself and isolated from the rest of entities.
+  * Entities can be separated from the others using a natural separation existing for the technology in use.
+  * For instance, in a message system, each entity might be sent in a separate message.
+  * If there doesn't exist such kind of separation, some kind of delimiter might be used.
+  * For instance, in a file output, a character delimiter as a "\n" might be used.
+  * If we write just one entity into the output, there shouldn't be any delimiter symbols as they are not needed.
+  *
+  * Implementations of this trait are intended to be used by sequential calls to either `writeEntity` or
+  * `append`/`closeEntity`
+  * The behavior expected from combining both styles is undefined.
+  *
+  * @tparam OutputType The type of output: e.g. `String`
+  */
 trait Sink[OutputType] {
-  // output an entity. Several calls to writeEntity in a row must output the entities properly separated between them.
+
+  /** Outputs an `entity`.
+    * Several calls to writeEntity in a row must output the entities properly separated between them.
+    * @param entity The entity to be output.
+    */
   def writeEntity(entity: OutputType): Unit
 
-  // intended to be used in combination with closeEntity. When the Sink is created, and append is called for the first time
-  // the partial entity is assumed to be appended to an empty entity, conforming therefore the initial part of the first entity
+  /** Append the `partialEntity` to the current entity being output.
+    * When the Sink is created, and append is called for the first time the partial entity is assumed to be appended to
+    * an empty entity, conforming therefore the initial part of the first entity.
+    * @note Intended to be used in combination with `closeEntity`.
+    * @param partialEntity
+    */
   def append(partialEntity: OutputType): Unit
 
-  // intended to be used in combination with append
+  /** Closes the current entity
+    * @note Intended to be used in combination with append.
+    */
   def closeEntity(): Unit
 
-  // ensures that the output of this sink completed and free up all the resources used by it
+  /** Ensures that the output of this sink completed and free up all the resources used by it. */
   def close(): Unit
 }
