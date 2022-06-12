c58d522a0ecc1ec8da7f1b3670cd593e1871b1c8
Naomi Arnold <33124479+narnolddd@users.noreply.github.com>
2022-06-12 13:14:42 +0100

    Feature/extend filters (#431)
    * Uniform node and edge sampling implemented as a util class.
    
    * Implemented disparity filter but haven't yet tested it works.
    
    * Fixed compile error :rotating_light:
    
    * Added documentation for DisparityFilter

---
+++ core/src/main/scala/com/raphtory/algorithms/generic/filters/UniformEdgeSample.scala
@@ -0,0 +1,29 @@
+package com.raphtory.algorithms.generic.filters
+
+import com.raphtory.api.analysis.algorithm.Generic
+import com.raphtory.api.analysis.graphview.GraphPerspective
+
+import scala.util.Random
+
+/**
+  * {s} `UniformEdgeSample(p:Float, pruneNodes:Boolean=true)`
+  *   : Filtered view of the graph achieved by taking a uniform random sample of the edges
+  *
+  *   This retains each edge of the graph with probability p. Additionally, if pruneNodes is set to true,
+  *   nodes which become isolated by this edge removal are also removed.
+  *
+  * ```{seealso}
+  * [](com.raphtory.algorithms.generic.filters.EdgeFilter)
+  * [](com.raphtory.algorithms.generic.filters.UniformVertexSample)
+  * ```
+  */
+
+class UniformEdgeSample(p:Float, pruneNodes:Boolean=true) extends Generic{
+  override def apply(graph: GraphPerspective): graph.Graph = {
+    graph.edgeFilter(_=> Random.nextFloat()<p, pruneNodes = pruneNodes)
+  }
+}
+
+object UniformEdgeSample {
+  def apply(p:Float,pruneNodes:Boolean=true) = new UniformEdgeSample(p,pruneNodes)
+}
