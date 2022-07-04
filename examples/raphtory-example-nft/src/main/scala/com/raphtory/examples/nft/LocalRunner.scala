8d9700aa647ae76855ce8270bb80a8182fe7fa8a
Haaroon Yousaf <Haaroon@users.noreply.github.com>
2022-07-04 14:12:08 +0100

    data cleaned and results exported

--- examples/raphtory-example-nft/src/main/scala/com/raphtory/examples/nft/LocalRunner.scala
+++ examples/raphtory-example-nft/src/main/scala/com/raphtory/examples/nft/LocalRunner.scala
@@ -15,10 +15,11 @@ object LocalRunner extends App {
         val path500k   = "/Users/haaroony/OneDrive - Pometry Ltd/nft_andrea/Data_API_500k.csv"
         val pathBad =         "/Users/haaroony/OneDrive - Pometry Ltd/nft_andrea/bad_nft.csv"
         val fullDataCleaned = "/Users/haaroony/OneDrive - Pometry Ltd/nft_andrea/Data_API_cleaned_ETH_only.csv"
+        val smallDataCleanedEthOnly = "/Users/haaroony/OneDrive - Pometry Ltd/nft_andrea/Data_API_clean_nfts_ETH_only.csv"
 
         val t0 = System.currentTimeMillis()
 
-        val source  = FileSpout(fullDataCleaned)
+        val source  = FileSpout(smallDataCleanedEthOnly)
         val builder = new NFTGraphBuilder()
 
         val graph   = Raphtory.load(spout = source, graphBuilder = builder)
@@ -27,10 +28,11 @@ object LocalRunner extends App {
         val output   = FileSink(filepath, format = CsvFormat())
 
         // 1597947153 - 2020-08-20 19:12:33
-        // 1619562578 - '2021-04-27 23:29:38' test1m / 250k / 500k / ALL / eth cleaned
+        // 1619562578 - '2021-04-27 23:29:38' test1m / 250k / 500k / ALL / eth cleaned / smallEthCleaned
         //   1613266168         - 2021-02-14 01:29:28
         // 1595884980  - 2020-07-27 22:23:00 testOne
         // 1540432748 2018-10-25 02:59:08 // badnft2
+        //            - 2021-04-27 23:59:51
         val atTime = 1619562578
 
         graph
