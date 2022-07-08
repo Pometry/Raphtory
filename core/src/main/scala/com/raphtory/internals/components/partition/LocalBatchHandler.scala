cbd77d0f03cc27f38d8baee3d318b87fbda5cc5d
HTK55 <80215162+HTK55@users.noreply.github.com>
2022-07-08 15:43:01 +0100

    Added catch to BatchHandler to catch and print error for lines in graphbuilder ingestion which fail parsing (#490)
    * Fixed BatchHandler to catch lines that fail parsing
    
    * This has revealed a very strange bug with AllCommands which is now fixed
    
    * Changed file
    
    Co-authored-by: miratepuffin <b.a.steer@qmul.ac.uk>

--- core/src/main/scala/com/raphtory/internals/components/partition/LocalBatchHandler.scala
+++ core/src/main/scala/com/raphtory/internals/components/partition/LocalBatchHandler.scala
@@ -51,7 +51,14 @@ private[raphtory] class LocalBatchHandler[T: ClassTag](
   private def runIngestion(): Unit = {
     while (spout.hasNextIterator()) {
       startIngesting()
-      spout.nextIterator().foreach(graphBuilder.parseTuple)
+      spout.nextIterator().foreach(line => {
+        try {
+          graphBuilder.parseTuple(line)
+        }
+        catch {
+          case e: Exception => logger.info(s"Could not parse: ${line}. Fails with exception ${e.toString}")
+        }
+      })
     }
 
     stopIngesting()
