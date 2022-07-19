a58eea62a9ead9a41d049fce00d1730d712387e3
Fabian Murariu <murariu.fabian@gmail.com>
2022-07-19 18:04:45 +0100

    [DEV-1068] adding some mechanism to get results back into python

--- core/src/main/scala/com/raphtory/internals/management/python/PythonEntrypoint.scala
+++ core/src/main/scala/com/raphtory/internals/management/python/PythonEntrypoint.scala
@@ -2,7 +2,9 @@ package com.raphtory.internals.management.python
 
 import com.raphtory.algorithms.generic.ConnectedComponents
 import com.raphtory.api.analysis.graphview.GraphPerspective
-import com.raphtory.sinks.FileSink
+import com.raphtory.sinks.{FileSink, LocalQueueSink}
+
+import java.util.concurrent.ArrayBlockingQueue
 
 class PythonEntrypoint(graph: GraphPerspective) {
 
@@ -11,4 +13,9 @@ class PythonEntrypoint(graph: GraphPerspective) {
   def connectedComponents = new ConnectedComponents
 
   def fileSink(path: String): FileSink = FileSink(path)
+
+  def newSyncQueue = new ArrayBlockingQueue[String](Int.MaxValue)
+
+  def localSink(queue: ArrayBlockingQueue[String]) =
+    new LocalQueueSink(queue)
 }
