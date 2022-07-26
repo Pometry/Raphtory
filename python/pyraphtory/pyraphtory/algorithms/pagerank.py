cb371a2ed7515b113e664b4b55e7ccf45d29f9f2
miratepuffin <b.a.steer@qmul.ac.uk>
2022-07-26 11:11:06 +0100

    Syncing with Haaroon

--- python/pyraphtory/pyraphtory/algorithms/pagerank.py
+++ python/pyraphtory/pyraphtory/algorithms/pagerank.py
@@ -5,19 +5,7 @@ from pyraphtory.builder import *
 from pyraphtory.context import BaseContext
 from pyraphtory.graph import TemporalGraph
 
-class LotrGraphBuilder(BaseBuilder):
-    def __init__(self):
-        super(LotrGraphBuilder, self).__init__()
 
-    def parse_tuple(self, line: str):
-        src_node, target_node, timestamp, *_ = line.split(",")
-
-        src_id = self.assign_id(src_node)
-        tar_id = self.assign_id(target_node)
-
-        self.add_vertex(int(timestamp), src_id, [ImmutableProperty("name", src_node)], "Character")
-        self.add_vertex(int(timestamp), tar_id, [ImmutableProperty("name", target_node)], "Character")
-        self.add_edge(int(timestamp), src_id, tar_id, [], "Character Co-occurence")
 
 PR_LABEL = 'prlabel'
 
