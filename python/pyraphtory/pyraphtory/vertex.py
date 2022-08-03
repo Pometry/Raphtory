9e906550f095cd31cc0409323383882b28f17254
Lucas Jeub <lucas.jeub@pometry.com>
2022-08-03 11:33:13 +0200

    implement vertex as proxy

--- python/pyraphtory/pyraphtory/vertex.py
+++ python/pyraphtory/pyraphtory/vertex.py
@@ -1,51 +1,17 @@
-class Vertex(object):
-    def __init__(self, jvm_v):
-        self.v = jvm_v
+from pyraphtory.proxy import GenericScalaProxy
 
+
+class Vertex(GenericScalaProxy):
     def __setitem__(self, key, value):
-        self.v.set_state(key, value)
+        self.set_state(key, value)
 
     def __getitem__(self, key):
-        return self.v.get_state(key, True)
-
-    def id(self):
-        return self.v.ID()
-
-    def message_all_neighbours(self, msg):
-        self.v.message_all_neighbours(msg)
-
-    def message_queue(self):
-        return self.v.message_queue()
-
-    def vote_to_halt(self):
-        self.v.vote_to_halt()
-
-    def in_degree(self):
-        return self.v.in_degree()
-
-    def out_degree(self):
-        return self.v.out_degree()
-
-    def message_outgoing_neighbours(self, msg):
-        self.v.message_outgoing_neighbours(msg)
-
-    def name(self, name_property="name"):
-        return self.v.name(name_property)
-
-    def neighbours(self):
-        return self.v.neighbours()
+        return self.get_state(key, True)
 
 
-class GraphState(object):
-    def __init__(self, jvm_global_state):
-        self.jvm_global_state = jvm_global_state
-
+class GraphState(GenericScalaProxy):
     def __setitem__(self, key, value):
-        self.jvm_global_state.set_state_num(key, value)
+        self.set_state_num(key, value)
 
     def __getitem__(self, key):
-        return self.jvm_global_state.get_state_num(key)
-
-    def numAdder(self, name: str, initial_value: int, retain_state: bool):
-        self.jvm_global_state.numAdder(name, initial_value, retain_state)
-
+        return self.get_state_num(key)
