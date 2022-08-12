b5ed13f629243e551d514d62ba6a6558eede30d1
rachelchan <rachelchan1497@gmail.com>
2022-08-12 10:40:06 +0100

    add stream method

--- examples/companies-house/src/main/scala/com/raphtory/examples/coho/companiesStream/CompaniesHouseTest.scala
+++ examples/companies-house/src/main/scala/com/raphtory/examples/coho/companiesStream/CompaniesHouseTest.scala
@@ -11,10 +11,10 @@ object CompaniesHouseTest {
 
   def main(args: Array[String]) {
 
-    val source = FileSpout("/home/ubuntu/persons-with-significant-control-snapshot-2022-08-09.txt", regexPattern = "^.*\\.([jJ][sS][oO][nN]??)$")
+    val source = FileSpout("/Users/rachelchan/Downloads/persons-with-significant-control-snapshot-2022-08-09.txt", regexPattern = "^.*\\.([jJ][sS][oO][nN]??)$")
     val builder = new CompanyToPscBulkGraphBuilder()
-    val output = FileSink("/home/ubuntu/psc")
-    val graph = Raphtory.load[String](source, builder)
+    val output = FileSink("/Users/rachelchan/Downloads/psc")
+    val graph = Raphtory.stream[String](source, builder)
 
     graph
       .range("2000-01-01", "2022-08-04", "1 day")
