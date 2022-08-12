30ae94e253433dd66afd1288eca136d1b8c3c2da
Lucas Jeub <lucas.jeub@pometry.com>
2022-08-12 14:49:51 +0200

    start adding some documentation to the python wrappers

--- python/pyraphtory/pyraphtory/proxy.py
+++ python/pyraphtory/pyraphtory/proxy.py
@@ -6,10 +6,16 @@ from pyraphtory.interop import logger, register
 
 
 class ScalaProxyBase(object):
+    """Base class for wrapping jvm objects"""
     _jvm_object = None
 
 
 class GenericScalaProxy(ScalaProxyBase):
+    """Base class for proxy objects that are not constructable from python
+
+    If a subclass defines a '_classname' attribute, it will be automatically
+    registered as the base class for proxy objects of that java class.
+    """
     _methods = None
     _initialised = False
     _classname = None
@@ -17,6 +23,11 @@ class GenericScalaProxy(ScalaProxyBase):
 
     @classmethod
     def _init_methods(cls, jvm_object):
+        """
+        Create method wrappers for all jvm methods (only called the first time a wrapper is created)
+
+        :param jvm_object: java object to wrap
+        """
         if not cls._initialised:
             logger.trace(f"uninitialised class {cls.__name__}")
             if jvm_object is None:
@@ -29,6 +40,7 @@ class GenericScalaProxy(ScalaProxyBase):
 
     @property
     def classname(self):
+        """The name of the underlying jvm class of this wrapper"""
         if self._classname is None:
             if self._jvm_object is not None:
                 self._classname = self._jvm_object.getClass().getName()
@@ -39,10 +51,14 @@ class GenericScalaProxy(ScalaProxyBase):
         return self._classname
 
     def __call__(self, *args, **kwargs):
+        """
+        Calling a wrapper calls the 'apply' method of the jvm object
+        """
         logger.trace(f"{self!r} called with {args=} and {kwargs=}")
         return self.apply(*args, **kwargs)
 
     def __getattr__(self, item):
+        """Triggers method initialisation if the class is uninitialised, otherwise just raises AttributeError"""
         if self._initialised:
             raise AttributeError(f"Attribute {item} does not exist for {self!r}")
         else:
@@ -58,9 +74,10 @@ class GenericScalaProxy(ScalaProxyBase):
         try:
             return self._jvm_object.toString()
         except Exception as e:
-            return repr(self._jvm_object)
+            return f"{self.__class__.__name__}({self._jvm_object!r})"
 
     def __new__(cls, jvm_object=None):
+        """Create a new instance and trigger method initialisation if needed"""
         if jvm_object is not None:
             if not cls._initialised:
                 cls._init_methods(jvm_object)
@@ -69,12 +86,14 @@ class GenericScalaProxy(ScalaProxyBase):
         return self
 
     def __init_subclass__(cls, **kwargs):
+        """automatically register wrappers that have a '_classname' defined"""
         super().__init_subclass__(**kwargs)
         if cls._classname is not None:
             register(cls)
 
 
 class MethodProxyDescriptor(object):
+    """Descriptor object for adding scala methods to class"""
     def __init__(self, name, methods):
         self.name = name
         self.methods = methods
@@ -90,6 +109,8 @@ class MethodProxyDescriptor(object):
 
 
 class GenericMethodProxy(object):
+    """Proxy object for scala method with support for default arguments and implicits"""
+    # TODO: This needs to be optimised
     def __init__(self, name: str, jvm_object, methods):
         self.name = name
         self._jvm_object = jvm_object
@@ -137,14 +158,17 @@ class GenericMethodProxy(object):
         raise ValueError(f"No matching implementation of method {self.name} with arguments {args=} and {kwargs=} and implicits {self._implicits}")
 
     def __getitem__(self, item):
+        """support specifying implicit arguments with [val] syntax"""
         self._implicits.append(interop.to_jvm(item))
         return self
 
 
 class ScalaObjectProxy(ScalaProxyBase, type):
+    """Metaclass for wrapping Scala companion objects"""
     _base_initialised = False
 
     def __new__(mcs, name, bases, attrs, **kwargs):
+        """Injects an additional specialised type to avoid interference between different classes"""
         if "_classname" not in attrs:
             actual_mcs = mcs
         else:
@@ -152,6 +176,7 @@ class ScalaObjectProxy(ScalaProxyBase, type):
         return type.__new__(actual_mcs, name, bases, attrs, **kwargs)
 
     def __getattr__(self, item):
+        """Lazy initialisation of class attributes to avoid import errors due to missing java connection"""
         if self._base_initialised:
             raise AttributeError(f"Attribute {item} does not exist for {self!r}")
         else:
@@ -164,6 +189,7 @@ class ScalaObjectProxy(ScalaProxyBase, type):
 
     @classmethod
     def _init_base_methods(mcs, jvm_object):
+        """add companion object methods to metaclass"""
         if not mcs._base_initialised:
             logger.trace(f"uninitialised class {mcs.__name__}")
             if jvm_object is None:
@@ -175,6 +201,7 @@ class ScalaObjectProxy(ScalaProxyBase, type):
             mcs._base_initialised = True
 
     def __call__(cls, *args, jvm_object=None, **kwargs):
+        """Calls scala constructor if no 'jvm_object' is given, otherwise initialises the wrapper"""
         logger.trace(f"ScalaCompanionObjectProxy called with {args=}, {jvm_object=}, {kwargs=}")
         if jvm_object is not None:
             # Construct from existing jvm object
@@ -190,16 +217,19 @@ class ScalaObjectProxy(ScalaProxyBase, type):
 
 
 class ScalaClassProxy(GenericScalaProxy, metaclass=ScalaObjectProxy):
+    """Base class for wrapper objects that are constructable from python"""
     pass
 
 @register(name="Iterable")
 class IterableScalaProxy(GenericScalaProxy, Iterable):
+    """Proxy object that converts scala iterables into python iterables"""
     def __iter__(self):
         return self.iterator()
 
 
 @register(name="Iterator")
 class IteratorScalaProxy(GenericScalaProxy, Iterator):
+    """Proxy object that converts scala iterators into python iterators"""
 
     def __next__(self):
         if self.has_next():
@@ -209,14 +239,20 @@ class IteratorScalaProxy(GenericScalaProxy, Iterator):
 
 
 class Function1(ScalaClassProxy):
+    """Proxy object for wrapping python functions with 1 argument"""
     _classname = "com.raphtory.internals.management.PythonFunction1"
 
 
 class Function2(ScalaClassProxy):
+    """Proxy object for wrapping python functions with 2 arguments"""
     _classname = "com.raphtory.internals.management.PythonFunction2"
 
 
 class BuiltinAlgorithm(ScalaProxyBase):
+    """Proxy object for looking up built-in algorithms based on path
+
+    (This actually could be used for looking up other classes as well if needed)
+    """
     @property
     def _jvm_object(self):
         return interop.find_class(self._path)
