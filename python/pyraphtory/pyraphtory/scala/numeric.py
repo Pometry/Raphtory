f0786a05022a6313fc35411c2af3b3af9f57cafb
Lucas Jeub <lucas.jeub@pometry.com>
2022-08-12 15:35:49 +0200

    document algorithm and numeric

--- python/pyraphtory/pyraphtory/scala/numeric.py
+++ python/pyraphtory/pyraphtory/scala/numeric.py
@@ -1,9 +1,13 @@
+"""Wrappers for providing implicit numeric value required by accumulators"""
+
 from pyraphtory.proxy import ScalaClassProxy
 
 
 class Int(ScalaClassProxy):
+    """Wrapper for Long implicit instance"""
     _classname = "scala.math.Numeric.LongIsIntegral"
 
 
 class Double(ScalaClassProxy):
+    """Wrapper for Double implicit instance"""
     _classname = "scala.math.Numeric.DoubleIsFractional"
