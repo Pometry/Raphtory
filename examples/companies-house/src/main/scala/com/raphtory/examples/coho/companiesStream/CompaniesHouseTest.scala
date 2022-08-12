4106b83dd2f9174d811d875fcab34a38b3da6882
rachelchan <rachelchan1497@gmail.com>
2022-08-12 10:46:24 +0100

    correct files

--- examples/companies-house/src/main/scala/com/raphtory/examples/coho/companiesStream/CompaniesHouseTest.scala
+++ examples/companies-house/src/main/scala/com/raphtory/examples/coho/companiesStream/CompaniesHouseTest.scala
@@ -11,9 +11,9 @@ object CompaniesHouseTest {
 
   def main(args: Array[String]) {
 
-    val source = FileSpout("/Users/rachelchan/Downloads/persons-with-significant-control-snapshot-2022-08-09.txt", regexPattern = "^.*\\.([jJ][sS][oO][nN]??)$")
+    val source = FileSpout("/home/ubuntu/persons-with-significant-control-snapshot-2022-08-09.txt", regexPattern = "^.*\\.([jJ][sS][oO][nN]??)$")
     val builder = new CompanyToPscBulkGraphBuilder()
-    val output = FileSink("/Users/rachelchan/Downloads/psc")
+    val output = FileSink("/home/ubuntu/psc")
     val graph = Raphtory.stream[String](source, builder)
 
     graph
