7c313b77637b41ccff8a0abf955c0950cf767f2c
James Alford <james.alford@live.co.uk>
2022-08-15 14:31:24 +0100

    fix up spacing in conf file and adjusted dockerfile to remove args inline with service change

--- bin/docker/raphtory/bin/entrypoint.sh
+++ bin/docker/raphtory/bin/entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-java -cp "/raphtory/jars/*" $RAPHTORY_JAVA_RUN_ARGS $RAPHTORY_JAVA_RUN_CLASS $RAPHTORY_JAVA_COMPONENT_NAME
+java -cp "/raphtory/jars/*" $RAPHTORY_JAVA_RUN_ARGS $RAPHTORY_JAVA_RUN_CLASS
