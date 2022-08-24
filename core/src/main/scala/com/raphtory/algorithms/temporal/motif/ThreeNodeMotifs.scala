744bfac8c424945736d8314dfe6dd1055c620ebc
narnolddd <n.a.arnold@qmul.ac.uk>
2022-08-24 15:51:23 +0100

    Edge state and temporal motifs working TM, need to test and find a better way of returning output to the user

--- core/src/main/scala/com/raphtory/algorithms/temporal/motif/ThreeNodeMotifs.scala
+++ core/src/main/scala/com/raphtory/algorithms/temporal/motif/ThreeNodeMotifs.scala
@@ -6,8 +6,6 @@ import com.raphtory.api.analysis.table.{Row, Table}
 import com.raphtory.api.analysis.visitor.{ExplodedEdge, ReducedVertex, Vertex}
 
 import scala.collection.mutable
-import scala.collection.mutable.ListBuffer
-import scala.util.control.Breaks.{break, breakable}
 
 
 class ThreeNodeMotifs(delta:Long=3600) extends GenericReduction {
@@ -18,7 +16,8 @@ class ThreeNodeMotifs(delta:Long=3600) extends GenericReduction {
         v=>
           val neighbours = v.neighbours.toSet
           neighbours.foreach{nb =>
-              v.messageVertex(nb,(v.ID, neighbours))}
+              v.messageVertex(nb,(v.ID, neighbours))
+          }
           v.setState("triCounts",Array.fill(8)(0))
       }
       // this step gets a list of (static) edges that form the opposite edge of a triangle with a node. this is ordered
@@ -34,7 +33,7 @@ class ThreeNodeMotifs(delta:Long=3600) extends GenericReduction {
                 // largest id node sends to the smallest id node the exploded edges.
                 if (nb < w && w.max(nb).max(v.ID)==v.ID) {
                   // message the edge and the size of the edge history
-                  v.messageVertex(nb,(v.ID.min(nb), v.ID.max(nb), v.explodedEdge(nb).getOrElse(List()).size))
+                  v.messageVertex(nb,(v.ID.min(w), v.ID.max(w), v.explodedEdge(w).getOrElse(List()).size))
                 }
             }
         }
@@ -42,7 +41,6 @@ class ThreeNodeMotifs(delta:Long=3600) extends GenericReduction {
       // in this step only the smallest id node in the triangle is looking after the process of finding emax.
       .step {
         v =>
-
           val opEdgeSizes = v.messageQueue[(Long,Long,Int)]
           opEdgeSizes.foreach {
             edge =>
@@ -52,13 +50,63 @@ class ThreeNodeMotifs(delta:Long=3600) extends GenericReduction {
                 triMap.put((u,w),size)
                 triMap.put((v.ID,u), v.explodedEdge(u).getOrElse(List()).size)
                 triMap.put((v.ID,w), v.explodedEdge(w).getOrElse(List()).size)
-                println(triMap)
                 // order first by size then by id1 then id2 to ensure uniqueness.
                 val eMax = triMap.maxBy(x => (x._2, x._1._1, x._1._2))._1
-
+              if (eMax._1 == v.ID) {
+                // request the history of the other edges
+                v.messageVertex(eMax._2,RequestEdges(v.ID,if (eMax._2 == u) w else u))
+                val appendedEdge = v.getEdge(eMax._2).minBy(_.src)
+                v.explodedEdge(if (eMax._2 == u) w else u).getOrElse(List()).foreach(e=> appendedEdge.appendToState[(Long,Long,Long)]("a_e",(e.src,e.dst,e.timestamp)))
+              } else {
+                // send to the vertex looking after eMax
+                v.messageVertex(u,(w, v.explodedEdge(w).getOrElse(List()).map(e => (e.src,e.dst,e.timestamp))))
+              }
           }
-          if( v.name() == "Gandalf")
-            println("did step 3")
+      }
+      // Either return edges to the requested vertex or set exploded edges received as edge state
+      .step{
+      v =>
+        val queue = v.messageQueue[Any]
+        queue.foreach{
+          case RequestEdges(sendTo, dst) =>
+            v.messageVertex(sendTo, (v.ID, v.explodedEdge(dst).getOrElse(List()).map(e => (e.src,e.dst,e.timestamp))))
+          case edges:(Long,List[(Long,Long,Long)]) =>
+            edges._2.foreach( e => v.getEdge(edges._1).minBy(_.src).appendToState[(Long,Long,Long)]("a_e",e))
+            val dst = if (edges._2.head._1 == edges._1) edges._2.head._2 else edges._2.head._1
+            val appendedEdge = v.getEdge(edges._1).minBy(_.src)
+            v.explodedEdge(dst).getOrElse(List()).foreach(e => appendedEdge.appendToState[(Long,Long,Long)]("a_e",(e.src,e.dst,e.timestamp)))
+        }
+    }
+      .step{
+        v =>
+          v.messageQueue[(Long,List[(Long,Long,Long)])].foreach{
+            edges =>
+              edges._2.foreach {
+                e =>
+                  v.getEdge(edges._1)
+                  .minBy(_.src)
+                  .appendToState[(Long,Long,Long)]("a_e",e)
+              }
+          }
+      }
+      .step{
+        v=>
+          v.neighbours.filter(_>v.ID).foreach{
+            nb =>
+              val edge = v.getEdge(nb).minBy(_.src)
+              val a_e = edge.getStateOrElse[Array[(Long,Long,Long)]]("a_e",Array())
+              if (a_e.nonEmpty) {
+                val mc = new TriadMotifCounter(v.ID, nb)
+                val inputEdges = a_e
+                  .appendedAll(v.explodedEdge(nb).getOrElse(List())
+                  .map(e=> (e.src,e.dst,e.timestamp)))
+                  .toList
+                  .sortBy(_._3)
+                mc.execute(inputEdges,delta)
+                val triadCounts = v.getState[Array[Int]]("triCounts")
+                v.setState("triCounts", triadCounts.zip(mc.getCounts).map{ case(x,y)=> x+y})
+              }
+          }
       }
       .step({
         v =>
@@ -131,7 +179,7 @@ class TriadMotifCounter(uid: Long, vid: Long) {
   val outgoing: Int = 1
   val uorv: Map[Long,Int] = Map(uid -> 0, vid -> 1)
 
-  val dirsToMap = List((0,0,0),(0,0,1),(0,1,0),(0,1,1),(1,0,0),(1,0,1),(1,1,0),(1,1,1)).map { k=> (k,0)}
+  val dirsToMap: Seq[((Int, Int, Int), Int)] = List((0,0,0),(0,0,1),(0,1,0),(0,1,1),(1,0,0),(1,0,1),(1,1,0),(1,1,1)).map { k=> (k,0)}
   val preNodes: mutable.Map[(Int, Int, Long), Int] = mutable.Map[(Int,Int,Long),Int]().withDefaultValue(0)
   val postNodes: mutable.Map[(Int, Int, Long), Int] = mutable.Map[(Int,Int,Long),Int]().withDefaultValue(0)
   val preSum: mutable.Map[(Int, Int, Int), Int] = mutable.Map[(Int,Int,Int),Int]()++=dirsToMap
@@ -143,9 +191,9 @@ class TriadMotifCounter(uid: Long, vid: Long) {
     if ((curEdge._1.min(curEdge._2),curEdge._1.max(curEdge._2))==(uid,vid))
       return
     val (isUorV,nb, dir) = if (curEdge._2 == uid || curEdge._2 ==vid) (uorv(curEdge._2),curEdge._1,incoming) else (uorv(curEdge._1),curEdge._2,outgoing)
-    curSum((1-isUorV,incoming,dir))+=curNodes((1-isUorV,incoming,nb))
-    curSum((1-isUorV,outgoing,dir))+=curNodes((1-isUorV,outgoing,nb))
-    curNodes(isUorV,dir,nb)+=1
+    curSum((1-isUorV,incoming,dir))+=curNodes((incoming,1-isUorV,nb))
+    curSum((1-isUorV,outgoing,dir))+=curNodes((outgoing,1-isUorV,nb))
+    curNodes(dir,isUorV,nb)+=1
   }
 
   def execute(edges:List[(Long,Long,Long)], delta:Long): Unit = {
@@ -173,17 +221,18 @@ class TriadMotifCounter(uid: Long, vid: Long) {
     if ((curEdge._1.min(curEdge._2),curEdge._1.max(curEdge._2))==(uid,vid))
       return
     val (isUorV,nb, dir) = if (curEdge._2 == uid || curEdge._2 ==vid) (uorv(curEdge._2),curEdge._1,incoming) else (uorv(curEdge._1),curEdge._2,outgoing)
-    curNodes(isUorV,dir,nb)-=1
-    curSum((isUorV,dir,incoming))+=curNodes((1-isUorV,incoming,nb))
-    curSum((isUorV,dir,outgoing))+=curNodes((1-isUorV,outgoing,nb))
+    curNodes(dir,isUorV,nb)-=1
+    curSum((isUorV,dir,incoming))-=curNodes((incoming,1-isUorV,nb))
+    curSum((isUorV,dir,outgoing))-=curNodes((outgoing,1-isUorV,nb))
   }
+
   def processCurrent(curEdge: (Long, Long, Long)): Unit = {
     if ((curEdge._1.min(curEdge._2),curEdge._1.max(curEdge._2))!=(uid,vid)) {
       val (isUorV, nb, dir) = if (curEdge._2 == uid || curEdge._2 == vid) (uorv(curEdge._2), curEdge._1, incoming) else (uorv(curEdge._1), curEdge._2, outgoing)
-      midSum((1-isUorV,incoming,dir))+=preNodes((1-isUorV,incoming,nb))
-      midSum((1-isUorV,outgoing,dir))+=preNodes((1-isUorV,outgoing,nb))
-      midSum((isUorV,dir,incoming))+=postNodes((1-isUorV,incoming,nb))
-      midSum((isUorV,dir,outgoing))+=postNodes((1-isUorV,outgoing,nb))
+      midSum((1-isUorV,incoming,dir))-=preNodes((incoming,1-isUorV,nb))
+      midSum((1-isUorV,outgoing,dir))-=preNodes((outgoing,1-isUorV,nb))
+      midSum((isUorV,dir,incoming))+=postNodes((incoming,1-isUorV,nb))
+      midSum((isUorV,dir,outgoing))+=postNodes((outgoing,1-isUorV,nb))
     } else {
       val (utov,dir) = if (curEdge._1==uid) (1,outgoing) else (0,incoming)
       finalCounts(0,0,0) += midSum(utov, 0,0) + postSum(utov,0,1) + preSum(1-utov,1,1)
@@ -302,6 +351,8 @@ class TwoNodeMotifs(vid: Long) {
 
   def getCounts:Array[Int] = count3d.values.toArray
 }
+
+case class RequestEdges(sendTo:Long, dst:Long)
 
 object ThreeNodeMotifs {
   def apply(delta:Long=3600) = new ThreeNodeMotifs(delta)
