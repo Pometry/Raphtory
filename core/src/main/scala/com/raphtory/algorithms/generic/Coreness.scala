3c1fad01bf0124eadd86187d209eadbb86343f0c
Natalie Shapiro <natalie.shapiro@pometry.com>
2022-09-01 14:10:35 +0100

    Added documentation, other clean up

--- core/src/main/scala/com/raphtory/algorithms/generic/Coreness.scala
+++ core/src/main/scala/com/raphtory/algorithms/generic/Coreness.scala
@@ -3,34 +3,64 @@ package com.raphtory.algorithms.generic
 import com.raphtory.api.analysis.algorithm.Generic
 import com.raphtory.api.analysis.graphview.GraphPerspective
 import com.raphtory.api.analysis.table.{Row, Table}
-import com.raphtory.algorithms.generic.KCore
+import com.raphtory.algorithms.generic.KCore.EFFDEGREE
+
+/**
+* {s}`Coreness(1, 6)`
+* : Identify the maximal k for each vertex (the coreness, or k-core number) for which the vertex is in a k-core, here trying values of k from 1 to 6.
+*
+* https://en.wikipedia.org/wiki/Degeneracy_(graph_theory)
+*
+* ## Parameters
+*
+*  {s}`start: Int`
+*    : The value of k to start trying values from (inclusive).
+*
+*  {s}`end: Int`
+*    : The value of k to end at (inclusive).
+*
+* ## States
+*
+* {s}`CORENESS: Int`
+* : Stores the k value of maximum k-core the vertex is in out of the k values that have been tried.
+*
+* ## Returns
+*
+* | vertex name       | coreness           |
+* | ----------------- | ------------------ |
+* | {s}`name: String` | {s}`CORENESS: Int` |
+*
+* ## Implementation
+*
+* The algorithm repeats KCore for start to end values of k, using the resetStates = false flag on KCore so it only runs on the nodes in the previous (as the KCores with k = 3 is a subgraph of the KCores with k = 2 etc).
+* It updates the CORENESS state for all the nodes still alive in that KCore run, so the CORENESS state contains the highest value of k run of which that vertex was in the k-cores.
+*/
 
 class Coreness(start: Int, end: Int) extends Generic {
-  // start and end determine the range of k values that KCore runs to determine the coreness
   final val CORENESS = "CORENESS"
-  final val EFFDEGREE = "effectiveDegree" // todo get dynamically from KCORE?
 
-  // todo add checks
+  if (start > end | start < 0) { // out of range
+    throw new IllegalArgumentException(s"start and end parameters must be non-negative and start <= end. You inputted start=$start, end=$end.")
+  }
+
   override def apply(graph: GraphPerspective): graph.Graph = {
     var g = graph.step { vertex =>
-      println("hello world")
       vertex.setState(CORENESS, 0)
     }
     for(k <- start to end) {
       g = KCore(k, resetStates = false).apply(g).step { vertex =>
-        if (vertex.getStateOrElse[Int](EFFDEGREE, 0) >= k) { // if still in the core graph for this value of k, increment the coreness //todo error handle better
+        if (vertex.getState[Int](EFFDEGREE) >= k) { // if still in the core graph for this value of k, increment the coreness
           vertex.setState(CORENESS, k)
         }
       }
     }
     g
-    //graph.asInstanceOf[graph.Graph] // TODO type mismatch with just graph: found "graph.type", required: "graph.Graph"
   }
 
   override def tabularise(graph: GraphPerspective): Table =
     graph
-      .select(vertex =>{println("tab")
-        Row(vertex.name, vertex.getStateOrElse[Int](CORENESS, -1))}
+      .select(vertex =>
+        Row(vertex.name, vertex.getStateOrElse[Int](CORENESS, throw new IllegalStateException(s"Failed to get '$CORENESS' for vertex '${vertex.name}'.")))
       )
 }
 
--- core/src/main/scala/com/raphtory/algorithms/generic/KCore.scala
+++ core/src/main/scala/com/raphtory/algorithms/generic/KCore.scala
@@ -1,19 +1,54 @@
 package com.raphtory.algorithms.generic
 
+import com.raphtory.algorithms.generic.KCore.EFFDEGREE
 import com.raphtory.api.analysis.algorithm.Generic
 import com.raphtory.api.analysis.graphview.GraphPerspective
 import com.raphtory.api.analysis.table.{Row, Table}
 
-/*
-* Step: check if already dead (i.e. degree < k), if so broadcast death to neighbours. Initialise effective degree as degree
-* Iterate: if receive message saying neighbour died then decrement effective degree
-*   Then check if the new degree is too low, in which case die and broadcast death
-*
-* */
-
+/**
+  * {s}`KCore(3)`
+  * : Identify the K-Cores of a graph with given k
+  *
+  * A k-core of a graph is a maximal subgraph in which every vertex has degree at least k. This algorithm identifies the k-cores of a graph.
+  * (https://en.wikipedia.org/w/index.php?title=Degeneracy_(graph_theory)&action=edit&section=3).
+  *
+  * ## Parameters
+  *
+  *  {s}`k: Int`
+  *    : The value of k to run k-core with.
+  *
+  *  {s}`resetStates: Bool = True`
+  *    : When true this resets all the effectiveDegree states at the start of running so the algorithm runs from scratch, when false reuses previous effectiveDegree data and runs on the remaining alive nodes (e.g. for nested KCore iterations like in Coreness.)
+  *    : Defaults to true, generally should be true.
+  *
+  * ## States
+  *
+  * {s}`effectiveDegree: Int`
+  * : Stores the current number of alive neighbours of each vertex (decreases as nodes get deleted).
+  * : At the end of the algorithm the nodes in the k-cores are those with effectiveDegree => k.
+  * : In a tabularise this can be filtered using the boolean {s}`vertex.getState[Int]("effectiveDegree") >= k`
+  *
+  * ## Returns
+  *
+  * | vertex name       | if in the k-core (only returns true rows)               |
+  * | ----------------- | ------------------------------------------------------- |
+  * | {s}`name: String` | {s}`vertex.getState[Int]("effectiveDegree") >= k: Bool` |
+  *
+  * ## Implementation
+  *
+  * The algorithm works by recursively removing nodes that don't have enough alive neighbours for them to remain in the graph.
+  *
+  *  1. Each node sets is effectiveDegree to it's actual degree (as all it's neighbours are currently alive). If it has degree < k, it can't be in the k-core, so dies and broadcasts its death.
+  *     If resetStates == false, we start from previous effectiveDegree values instead.
+  *
+  *  2. When a node receives a message saying its neighbour has died, it decrements its effective degree.
+  *
+  *  3. It then checks if its effectiveDegree < k, in which case it can't be in the k-core, so dies and broadcasts its death.
+  *
+  *  4. This continues until no more nodes are dying.
+  *
+  */
 class KCore(k: Int, resetStates: Boolean = true) extends Generic {
-
-  final val EFFDEGREE = "effectiveDegree" // effective degree
 
   override def apply(graph: GraphPerspective): graph.Graph =
     graph
@@ -21,9 +56,10 @@ class KCore(k: Int, resetStates: Boolean = true) extends Generic {
 
         if ((resetStates == false) & (vertex.containsState(EFFDEGREE))) {  // i.e. you're reusing the previous states
           // we assume here that the previous iteration of KCore was run with a smaller value of k (this allows e.g. Coreness to run)
-          // therefore the effective degree is only going to decrease for each node, so any node with a previous effective degree < current k dies straight away
+          // therefore the effective degree is only going to decrease for each node, so any node with a previous effective degree = k-1
+          // (so this node was alive last time i.e. is counted in other nodes' effective degree, but can't be in the current k core: dies straight )
           val effDegree = vertex.getStateOrElse(EFFDEGREE, vertex.degree)
-          if (effDegree < k) {
+          if (effDegree == k-1) { // i.e. in previous round was alive but dead straight away in this round
             vertex.messageAllNeighbours(0)
           }
         } else {
@@ -50,17 +86,20 @@ class KCore(k: Int, resetStates: Boolean = true) extends Generic {
           }
 
         },
-        iterations = 20,
+        iterations = 100,
         executeMessagedOnly = true
       )
 
   override def tabularise(graph: GraphPerspective): Table =
     graph
       .select(vertex =>
-        Row(vertex.name, vertex.getState[Int](EFFDEGREE) >= k) //todo errorhandle or else here?
+        Row(vertex.name, vertex.getState[Int](EFFDEGREE) >= k)
       ).filter(r => r.getBool(1))
 }
 
 object KCore {
+
+  final val EFFDEGREE = "effectiveDegree"
   def apply(k: Int, resetStates: Boolean = true) = new KCore(k, resetStates)
+
 }
