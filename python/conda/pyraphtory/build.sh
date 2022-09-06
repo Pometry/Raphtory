996603e9977e68d984cc745a0f63c3cda5ab1a74
Haaroon Yousaf <Haaroon@users.noreply.github.com>
2022-09-06 19:20:57 +0100

    brought back poetry, got it to work with conda, all builds within a vagrant vm

--- python/conda/pyraphtory/build.sh
+++ python/conda/pyraphtory/build.sh
@@ -1,10 +1,16 @@
 echo "Building Raphtory from source via SBT..."
 set -e && (cd $SRC_DIR && sbt "core/assembly")
+echo "Getting Raphtory version"
+set -e && (cd $SRC_DIR && sbt -Dsbt.supershell=false -error "print core/version" > $SRC_DIR/raphtory_version)
 echo "Creating a directory for the jar..."
 set -e && mkdir -p $SRC_DIR/python/pyraphtory/lib/
-echo "Moving the raphtory jar to the pyraphtory lib folder..."
-set -e && mv $SRC_DIR/core/target/scala-2.13/core-assembly-0.1.0.jar $SRC_DIR/python/pyraphtory/lib/
-echo "Building pyraphtory..."
-set -e && (cd $SRC_DIR/python/pyraphtory/ && $PYTHON -m build --no-isolation)
-echo "Installing pyraphtory..."
-set e && (cd $SRC_DIR/python/pyraphtory/ && $PYTHON -m pip install dist/pyraphtory-0.0.1.tar.gz)
+echo "Moving raphtory core jar to the pyraphtory lib folder..."
+set -e && mv $SRC_DIR/core/target/scala-2.13/core-assembly-$(cat $SRC_DIR/raphtory_version).jar $SRC_DIR/python/pyraphtory/lib/
+echo "Building pyraphtory via poetry..."
+set -e && (cd $SRC_DIR/python/pyraphtory/ && poetry build)
+echo "Installing via poetry..."
+set -e && (cd $SRC_DIR/python/pyraphtory/ && poetry install)
+echo "Installing package via pip..."
+set -e && (pip3 install $SRC_DIR/python/pyraphtory/dist/pyraphtory-$(cat $SRC_DIR/raphtory_version).tar.gz)
+echo "Clean-up"
+rm -r $SRC_DIR/raphtory_version
