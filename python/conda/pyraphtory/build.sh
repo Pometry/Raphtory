a203dcd6d719ba99b9fc615a7ee27f10251f18dd
Haaroon Yousaf <Haaroon@users.noreply.github.com>
2022-09-06 16:34:57 +0100

    for some reason it doesnt pull the jar, i might switch bad to poetry, but this seems to build in my vagrant box and passes a local test fine.

--- python/conda/pyraphtory/build.sh
+++ python/conda/pyraphtory/build.sh
@@ -1,11 +1,10 @@
-# Build raphtory from source
-#echo "BUILDING SBT"
-#set -e && (cd $SRC_DIR && sbt "core/assembly")
-#echo "Make a directory for the lib file"
-#set -e && mkdir -p $SRC_DIR/python/pyraphtory/lib/
-#echo "Copy the raphtory jar to the pyraphtory lib folder"
-#set -e && cp $SRC_DIR/core/target/scala-2.13/core-assembly-0.1.0.jar $SRC_DIR/python/pyraphtory/lib/
-echo "build"
-set -e && (cd $SRC_DIR/python/pyraphtory/ && $PYTHON -m build)
-echo "install"
-set e && (cd $SRC_DIR/python/pyraphtory/ && $PYTHON -m pip install dist/pyraphtory-0.0.1.tar.gz)   # Python command to install the script.
+echo "Building Raphtory from source via SBT..."
+set -e && (cd $SRC_DIR && sbt "core/assembly")
+echo "Creating a directory for the jar..."
+set -e && mkdir -p $SRC_DIR/python/pyraphtory/lib/
+echo "Moving the raphtory jar to the pyraphtory lib folder..."
+set -e && mv $SRC_DIR/core/target/scala-2.13/core-assembly-0.1.0.jar $SRC_DIR/python/pyraphtory/lib/
+echo "Building pyraphtory..."
+set -e && (cd $SRC_DIR/python/pyraphtory/ && $PYTHON -m build --no-isolation)
+echo "Installing pyraphtory..."
+set e && (cd $SRC_DIR/python/pyraphtory/ && $PYTHON -m pip install dist/pyraphtory-0.0.1.tar.gz)
