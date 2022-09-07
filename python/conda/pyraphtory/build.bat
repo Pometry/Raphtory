118f2836c8ea014117f6e7db7b469cdaecd23756
Haaroon Yousaf <Haaroon@users.noreply.github.com>
2022-09-07 10:57:20 +0100

    add built.bat file, clean up .sh with nicer spaces, upgrade conda python version to match poetry, restore pometry.toml file and use active environment,

--- python/conda/pyraphtory/build.bat
+++ python/conda/pyraphtory/build.bat
@@ -1,2 +1,27 @@
-"%PYTHON%" setup.py install
-if errorlevel 1 exit 1
+@echo off
+
+echo "Building Raphtory from source via SBT..."
+cd "%SRC_DIR%" && sbt "core\assembly"
+
+echo "Getting Raphtory version"
+cd "%SRC_DIR%" && sbt "-Dsbt.supershell=false" "-error" "print core\version" > "%SRC_DIR%\raphtory_version"
+set /p RAPHTORY_VERSION=<"%SRC_DIR%\raphtory_version"
+
+
+echo "Creating a directory for the jar..."
+mkdir "-p" "%SRC_DIR%\python\pyraphtory\lib\"
+
+echo "Moving raphtory core jar to the pyraphtory lib folder..."
+mv "%SRC_DIR%\core\target\scala-2.13\core-assembly-%RAPHTORY_VERSION%.jar" "%SRC_DIR%\python\pyraphtory\lib\"
+
+echo "Building pyraphtory via poetry..."
+cd "%SRC_DIR%\python\pyraphtory\" && poetry "build"
+
+echo "Installing via poetry..."
+cd "%SRC_DIR%\python\pyraphtory\" && poetry "install"
+
+echo "Installing package via pip..."
+pip "install" "%SRC_DIR%\python\pyraphtory\dist\pyraphtory-%RAPHTORY_VERSION%.tar.gz"
+
+echo "Clean-up"
+DEL /S "%SRC_DIR%\raphtory_version"
--- python/conda/pyraphtory/build.sh
+++ python/conda/pyraphtory/build.sh
@@ -1,16 +1,23 @@
 echo "Building Raphtory from source via SBT..."
 set -e && (cd $SRC_DIR && sbt "core/assembly")
+
 echo "Getting Raphtory version"
 set -e && (cd $SRC_DIR && sbt -Dsbt.supershell=false -error "print core/version" > $SRC_DIR/raphtory_version)
+
 echo "Creating a directory for the jar..."
 set -e && mkdir -p $SRC_DIR/python/pyraphtory/lib/
+
 echo "Moving raphtory core jar to the pyraphtory lib folder..."
 set -e && mv $SRC_DIR/core/target/scala-2.13/core-assembly-$(cat $SRC_DIR/raphtory_version).jar $SRC_DIR/python/pyraphtory/lib/
+
 echo "Building pyraphtory via poetry..."
 set -e && (cd $SRC_DIR/python/pyraphtory/ && poetry build)
+
 echo "Installing via poetry..."
 set -e && (cd $SRC_DIR/python/pyraphtory/ && poetry install)
+
 echo "Installing package via pip..."
 set -e && (pip install $SRC_DIR/python/pyraphtory/dist/pyraphtory-$(cat $SRC_DIR/raphtory_version).tar.gz)
+
 echo "Clean-up"
 rm -r $SRC_DIR/raphtory_version
