0be3c6ef0d6b061f251bbb07e72c69a4d183fde5
Lucas Jeub <lucas.jeub@pometry.com>
2022-09-08 13:05:01 +0200

    tweak doc strings

--- core/src/main/scala/com/raphtory/internals/components/output/TableOutputSink.scala
+++ core/src/main/scala/com/raphtory/internals/components/output/TableOutputSink.scala
@@ -16,47 +16,21 @@ private[raphtory] case object EndOutput                                         
 private[raphtory] class TableOutputSinkExecutor(endPoint: EndPoint[OutputMessages]) extends SinkExecutor {
   private var currentPerspective: Perspective = _
 
-  /** Sets up the perspective to be written out.
-    * This method gets called every time a new graph perspective is going to be written out so this `SinkExecutor` can
-    * handle it if needed.
-    *
-    * @param perspective the perspective to be written out
-    */
   override def setupPerspective(perspective: Perspective): Unit =
     currentPerspective = perspective
 
-  /** Writes out one row.
-    * The implementation of this method doesn't need to be thread-safe as it is wrapped by `threadSafeWriteRow` to
-    * handle synchronization.
-    *
-    * @param row the row of data to write out
-    */
   override protected def writeRow(row: Row): Unit = endPoint.sendAsync(RowOutput(currentPerspective, row))
 
   override def threadSafeWriteRow(row: Row): Unit = writeRow(row)
 
-  /** Closes the writing of the current graph perspective.
-    * This method gets called every time all the rows from one graph perspective have been successfully written out so
-    * this `SinkExecutor` can handle it if needed.
-    */
   override def closePerspective(): Unit =
     endPoint.sendSync(EndPerspective(currentPerspective))
 
-  /** Closes this `SinkExecutor` after writing the complete table.
-    *
-    * This method should free up all the resources in use.
-    */
   override def close(): Unit = endPoint.closeWithMessage(EndOutput)
 }
 
 private[raphtory] case object TableOutputSink extends Sink {
 
-  /**
-    * @param jobID       the ID of the job that generated the table
-    * @param partitionID the ID of the partition of the table
-    * @param config      the configuration provided by the user
-    * @return the `SinkExecutor` to be used for writing out results
-    */
   override def executor(jobID: String, partitionID: Int, config: Config, topics: TopicRepository): SinkExecutor =
     new TableOutputSinkExecutor(topics.output(jobID).endPoint)
 }
