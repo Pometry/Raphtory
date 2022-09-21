3a4943bf46f8209942666b8517f0402fa93cf7f7
Lucas Jeub <lucas.jeub@pometry.com>
2022-09-21 17:47:00 +0200

    fairly comprehensive docstring conversion handling code examples with some tests

--- python/pyraphtory/pyraphtory/_docstring.py
+++ python/pyraphtory/pyraphtory/_docstring.py
@@ -1,13 +1,16 @@
-from parsy import string, whitespace, any_char, seq, peek, alt, eof, string_from
+from parsy import string, whitespace, any_char, seq, peek, alt, eof, string_from, Parser, regex, letter, decimal_digit, generate
 
+code_conversions = {}
+
+
+def camel_to_snake(name):
+    from pyraphtory.interop import camel_to_snake as convert
+    from pyraphtory._codegen import clean_identifier
+    return clean_identifier(convert(name))
 
 
 def parse_name(name):
-    from pyraphtory.interop import camel_to_snake
-    from pyraphtory._codegen import clean_identifier
-    converted = camel_to_snake(name)
-    clean = clean_identifier(converted)
-    return clean
+    return camel_to_snake(name)
 
 
 def finalise_param(s):
@@ -33,22 +36,59 @@ def ignore(s):
 def capitalise(s: str):
     return s.capitalize()
 
-start = (whitespace.optional("") >> string("/**") >> string(" ").optional("")).map(ignore)
-linestart = (string("\n") << whitespace.optional("") << string("*") << string(" ").optional(""))
-end = (string("*").many() >> string("/")).map(ignore)
+
+def report_unparsed(s: str):
+    print(f"unparsed: {s}")
+    return s
+
+
+def with_optional_whitespace(parser: Parser):
+    return whitespace.optional("") + parser + whitespace.optional("")
+
+
+def linestart_clean(s: str):
+    return "\n"
+
+
+non_newline_whitespace = regex(r"[^\S\r\n]+")
+
+
+# scaladoc comment noise
+start = (whitespace.optional("") + string("/**") + string(" ").optional("")).map(ignore)
+linestart = (non_newline_whitespace.optional("") + string("\n") + whitespace.optional("") + string("*").times(1, 1).concat() + string(" ").optional("")).map(linestart_clean)
+end = (whitespace.optional("") + string("*").at_least(1).concat() + string("/")).map(ignore)
+
+# convert tags
 token = any_char.until(whitespace | eof, consume_other=False, min=1).map(join_tokens)
 name = token.map(parse_name)
 param_id = string("@param").map(convert_param)
 param = (param_id + whitespace + name).map(finalise_param)
 return_id = string("@return").map(convert_returns)
-boolean = string_from('`true`', '`false`').map(capitalise)
 
-doc_converter = alt(start, end, linestart, whitespace,
-                    boolean,
+# convert code expressions
+code_others = string("`").should_fail("not end of code") >> any_char  # match anything that is not end of code and return unchanged
+boolean = string_from('true', 'false').map(capitalise)
+string_expr = string("String").result("str")
+int_expr = string_from("Int", "Long", "Integer", "Short").result("int")
+method_or_variable_name = (string("_").many().concat() + regex(r"[a-z]") + (letter | decimal_digit).many().concat() + string("_").many().concat()).map(camel_to_snake)
+class_name = (string("_").many().concat() + regex(r"[A-Z]") + (letter | decimal_digit).many().concat() + string("_").many().concat())
+code_expr = (boolean | string_expr | int_expr | method_or_variable_name | class_name | code_others).many().map(join_tokens)
+code = string("`") + code_expr + string("`")
+code_unparsed = (string("`") + any_char.until(string("`"), consume_other=True).concat()).map(report_unparsed)
+
+# find links (only extracts text for now)
+link = string("[[") >>((token >> whitespace >> (class_name | method_or_variable_name)) | (class_name | method_or_variable_name)) << string("]]")
+
+
+doc_converter = alt(start, end, linestart, whitespace, link,
+                    code, code_unparsed,
                     param, return_id,
                     token).many().map(join_tokens)
 
 
 def convert_docstring(docs):
-    cleaned = doc_converter.parse(docs)
-    return cleaned.rstrip("\n")
+    if docs:
+        cleaned = doc_converter.parse(docs)
+        return cleaned.rstrip("\n")
+    else:
+        return docs
---
+++ python/pyraphtory/tests/test_docstring.py
@@ -0,0 +1,45 @@
+import pytest
+
+from pyraphtory._docstring import convert_docstring, string_expr, code
+from pyraphtory.context import PyRaphtory
+
+
+@pytest.fixture(scope="module", autouse=True)
+def pyraphtory():
+    pr = PyRaphtory().open()
+    yield pr
+    pr.shutdown()
+
+
+
+def test_class_name_conversion():
+    assert convert_docstring("`ClassName`") == "`ClassName`"
+
+
+def test_variable_name_conversion():
+    assert convert_docstring("`variableNameConvention1`") == "`variable_name_convention1`"
+
+
+def test_combined_expression():
+    assert convert_docstring("`ClassName.variableName(otherName=true) = 1`") == "`ClassName.variable_name(other_name=True) = 1`"
+
+
+def test_string_conversion():
+    assert convert_docstring("`String`") == "`str`"
+
+
+def test_scaladoc_comment_removal():
+    assert convert_docstring("/** * \n * s\n */") == "*\ns"
+
+
+def test_direct_conversion():
+    assert string_expr.parse("String") == "str"
+    assert code.parse("`String`") == "`str`"
+
+
+def test_link_conversion():
+    assert convert_docstring("[[Vertex]]") == "Vertex"
+
+
+def test_link_conversion_with_target():
+    assert convert_docstring("[[visitor.Vertex Vertex]]") == "Vertex"
