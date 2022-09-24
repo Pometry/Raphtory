e224e802feee16f6418a7643b1c74c7479f0b7de
Shivam Kapoor <shivamkapoor@POM-MBP7.lan>
2022-09-24 20:30:20 +0100

    fix race condition in instantiating queryexecutors

--- core/src/main/scala/com/raphtory/internals/management/arrow/LocalHostAddressProvider.scala
+++ core/src/main/scala/com/raphtory/internals/management/arrow/LocalHostAddressProvider.scala
@@ -8,13 +8,23 @@ import com.raphtory.internals.communication.repositories.ArrowFlightRepository.s
 import com.typesafe.config.Config
 import org.apache.arrow.memory.RootAllocator
 
+import java.util.concurrent.TimeUnit
+
 class LocalHostAddressProvider(config: Config, server: ArrowFlightServer)
         extends ArrowFlightHostAddressProvider(config) {
 
   private val interface = server.getInterface
   private val port      = server.getPort
 
-  override def getAddressAcrossPartitions(topic: String): Map[String, ArrowFlightHostAddress] = addresses.toMap
+  override def getAddressAcrossPartitions(topic: String): Map[String, ArrowFlightHostAddress] = {
+    // When QueryExecutors across all partitions register themselves there is a potential race between them in
+    // registering a listeners against topics and fetching endpoints for those topics. This is why is it important
+    // to wait before returning from this method.
+    while (!addresses.contains(topic))
+      TimeUnit.SECONDS.sleep(1)
+
+    addresses.toMap
+  }
 
   override def startAndPublishAddress[T](
       topics: Seq[CanonicalTopic[T]],
