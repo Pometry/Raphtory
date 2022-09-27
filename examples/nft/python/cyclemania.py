72d46a6d57230bbf2ab984c5e8d47a7e942c82eb
Haaroon Yousaf <Haaroon@users.noreply.github.com>
2022-09-27 14:04:21 +0100

    fixed issue with dataclass fixed issue with -1 not being recognised by pemja moved tabularise out of dataclass add filter for nonempty rows

--- examples/nft/python/cyclemania.py
+++ examples/nft/python/cyclemania.py
@@ -6,29 +6,7 @@ import pyraphtory.scala.collection
 
 CYCLES_FOUND: str = "CYCLES_FOUND"
 
-
-@dataclass(frozen=True)
-class Sale:
-    buyer: str
-    price_usd: float
-    time: int
-    tx_hash: str
-    nft_id: str
-
-
-@dataclass(frozen=False)
-class Cycle:
-    sales: list[Sale]
-
-
-@dataclass(frozen=True)
-class CycleData:
-    buyer: str
-    profit_usd: float
-    cycle: Cycle
-
-
-class CycleManiaj(PyAlgorithm):
+class CycleMania(PyAlgorithm):
     def __call__(self, graph: TemporalGraph) -> TemporalGraph:
         def step(v: Vertex):
             if v.type() != "NFT":
@@ -38,12 +16,11 @@ class CycleManiaj(PyAlgorithm):
             all_purchases = sorted(v.explode_in_edges(), key=lambda e: e.timestamp())
 
             purchasers = list(map(lambda e:
-                                  {
-                                      "buyer":e.get_property_or_else("buyer_address", "_UNKNOWN_"),
-                                      "price_usd":float(e.get_property_or_else("price_usd", 0.0)),
-                                      "time":e.timestamp(),
-                                      "tx_hash":e.get_property_or_else("transaction_hash", ""),
-                                      "nft_id":e.get_property_or_else("token_id", "_UNKNOWN_")},
+                                  dict(buyer=e.get_property_or_else("buyer_address", "_UNKNOWN_"),
+                                       price_usd=float(e.get_property_or_else("price_usd", 0.0)),
+                                       time=e.timestamp(),
+                                       tx_hash=e.get_property_or_else("transaction_hash", ""),
+                                       nft_id=e.get_property_or_else("token_id", "_UNKNOWN_")),
                                   all_purchases))
             if len(purchasers) > 2:
                 buyers_seen = {}
@@ -59,49 +36,36 @@ class CycleManiaj(PyAlgorithm):
                         # print(f"prev {prev_price} : current: {current_price}")
                         if prev_price < current_price:
                             # print(f"Money Cycle found, item from {buyer_id} to {pos}")
-                            all_cycles.append([purchasers[prev_pos:pos + 1]])
+                            all_cycles.append(purchasers[prev_pos:pos + 1])
             if len(all_cycles):
-                print("found cycles. Saved to vertex")
-                print(all_cycles)
                 v[CYCLES_FOUND] = all_cycles
-                print("saved")
             else:
                 v[CYCLES_FOUND] = []
 
         return graph.reduced_view().step(step)
-
-    # def tabularise(self, graph: TemporalGraph) -> Table:
-    #     return graph.select(lambda v: Row(v['PURCHASE']))
 
     def tabularise(self, graph: TemporalGraph):
         def get_cycles(v: Vertex):
             vertex_type = v.type()
             rows_found = [Row()]
             if vertex_type == "NFT" and len(v[CYCLES_FOUND]):
-                nft_id = v.id()
+                nft_id = str(v.id())
                 cycles_found = v[CYCLES_FOUND]
                 nft_collection = v.get_property_or_else('collection', '_UNKNOWN_')
                 nft_category = v.get_property_or_else('category', '_UNKNOWN_')
-                # for single_cycle in cycles_found:
                 rows_found = list(map(lambda single_cycle:
                                       Row(
                                           nft_id,
                                           nft_collection,
                                           nft_category,
-                                          len(single_cycle.sales),
-                                          CycleData(
-                                              buyer=single_cycle.sales[0].buyer,
-                                              profit_usd=single_cycle.sales[-1].price_usd - single_cycle.sales[
-                                                  0].price_usd,
-                                              cycle=single_cycle
-                                          )
+                                          len(single_cycle),
+                                          dict(buyer=str(single_cycle[0]['buyer']),
+                                               profit_usd=float(single_cycle[len(single_cycle) - 1]['price_usd']) -
+                                                          float(single_cycle[0]['price_usd']),
+                                               cycle=single_cycle)
                                       ), cycles_found))
             return rows_found
-        return graph.explode_select(lambda v: get_cycles(v))
+
+        return graph.explode_select(lambda v: get_cycles(v)).filter(lambda row: len(row.get_values()) > 0)
 
 
-qp = graph \
-    .at(at_time) \
-    .past() \
-    .execute(CycleManiaj()) \
-    .write_to(FileSink('/tmp/raphtory_nft_python', format = JsonFormat()))
