604dd2a7d06f565613bfd5be77fe3e4749c45d46
Shivam Kapoor <shivamkapoor@POM-MBP7.local>
2022-10-18 13:13:36 +0100

    impl raphtoryapp

--- examples/lotr/src/main/scala/com/raphtory/examples/lotr/TutorialRunner.scala
+++ examples/lotr/src/main/scala/com/raphtory/examples/lotr/TutorialRunner.scala
@@ -1,6 +1,8 @@
 package com.raphtory.examples.lotr
 
-import com.raphtory.Raphtory
+import cats.effect.IO
+import cats.effect.Resource
+import com.raphtory.RaphtoryApp
 import com.raphtory.algorithms.generic.ConnectedComponents
 import com.raphtory.algorithms.generic.NodeList
 import com.raphtory.algorithms.generic.centrality.Degree
@@ -9,67 +11,73 @@ import com.raphtory.api.input.Graph.assignID
 import com.raphtory.api.input.ImmutableProperty
 import com.raphtory.api.input.Properties
 import com.raphtory.api.input.Type
+import com.raphtory.internals.context.RaphtoryContext
+import com.raphtory.internals.context.RaphtoryContext.RaphtoryContextBuilder
+import com.raphtory.protocol.RaphtoryService
 import com.raphtory.sinks.FileSink
 import com.raphtory.utils.FileUtils
 import scala.language.postfixOps
 
-object TutorialRunner extends App {
+object TutorialRunner extends RaphtoryApp {
 
-  Raphtory.local().runWithNewGraph(destory = true) {
-    graph =>
+  override def buildContext(
+      ctxBuilder: RaphtoryContextBuilder
+  ): (Resource[IO, RaphtoryService[IO]], RaphtoryContext) =
+    ctxBuilder.remote("127.0.0.1", 1736)
 
-    val path = "/tmp/lotr.csv"
-    val url = "https://raw.githubusercontent.com/Raphtory/Data/main/lotr.csv"
-    FileUtils.curlFile(path, url)
+  override def run(ctx: RaphtoryContext): Unit =
+    ctx.runWithNewGraph(destory = true) { graph =>
+      val path = "/tmp/lotr.csv"
+      val url  = "https://raw.githubusercontent.com/Raphtory/Data/main/lotr.csv"
+      FileUtils.curlFile(path, url)
 
-    val file = scala.io.Source.fromFile(path)
-    file.getLines.foreach { line =>
-      val fileLine = line.split(",").map(_.trim)
-      val sourceNode = fileLine(0)
-      val srcID = assignID(sourceNode)
-      val targetNode = fileLine(1)
-      val tarID = assignID(targetNode)
-      val timeStamp = fileLine(2).toLong
+      val file = scala.io.Source.fromFile(path)
+      file.getLines.foreach { line =>
+        val fileLine   = line.split(",").map(_.trim)
+        val sourceNode = fileLine(0)
+        val srcID      = assignID(sourceNode)
+        val targetNode = fileLine(1)
+        val tarID      = assignID(targetNode)
+        val timeStamp  = fileLine(2).toLong
 
-      graph.addVertex(timeStamp, srcID, Properties(ImmutableProperty("name", sourceNode)), Type("Character"))
-      graph.addVertex(timeStamp, tarID, Properties(ImmutableProperty("name", targetNode)), Type("Character"))
-      graph.addEdge(timeStamp, srcID, tarID, Type("Character Co-occurrence"))
+        graph.addVertex(timeStamp, srcID, Properties(ImmutableProperty("name", sourceNode)), Type("Character"))
+        graph.addVertex(timeStamp, tarID, Properties(ImmutableProperty("name", targetNode)), Type("Character"))
+        graph.addEdge(timeStamp, srcID, tarID, Type("Character Co-occurrence"))
+      }
+
+      //Get simple metrics
+      graph
+        .execute(Degree())
+        .writeTo(FileSink("/tmp/raphtory"))
+        .waitForJob()
+
+      //PageRank
+      graph
+        .at(32674)
+        .past()
+        .transform(PageRank())
+        .execute(NodeList(Seq("prlabel")))
+        .writeTo(FileSink("/tmp/raphtory"))
+        .waitForJob()
+
+      //Connected Components
+      graph
+        .at(32674)
+        .past()
+        .execute(ConnectedComponents)
+        .writeTo(FileSink("/tmp/raphtory"))
+        .waitForJob()
+
+      //Chained Example
+      graph
+        .at(32674)
+        .past()
+        .transform(PageRank())
+        .transform(ConnectedComponents)
+        .transform(Degree())
+        .execute(NodeList(Seq("prlabel", "cclabel", "inDegree", "outDegree", "degree")))
+        .writeTo(FileSink("/tmp/raphtory"))
+        .waitForJob()
+
     }
-
-    //Get simple metrics
-    graph
-      .execute(Degree())
-      .writeTo(FileSink("/tmp/raphtory"))
-      .waitForJob()
-
-    //PageRank
-    graph
-      .at(32674)
-      .past()
-      .transform(PageRank())
-      .execute(NodeList(Seq("prlabel")))
-      .writeTo(FileSink("/tmp/raphtory"))
-      .waitForJob()
-
-    //Connected Components
-    graph
-      .at(32674)
-      .past()
-      .execute(ConnectedComponents)
-      .writeTo(FileSink("/tmp/raphtory"))
-      .waitForJob()
-
-    //Chained Example
-    graph
-      .at(32674)
-      .past()
-      .transform(PageRank())
-      .transform(ConnectedComponents)
-      .transform(Degree())
-      .execute(NodeList(Seq("prlabel", "cclabel", "inDegree", "outDegree", "degree")))
-      .writeTo(FileSink("/tmp/raphtory"))
-      .waitForJob()
-
-  }
-
 }
