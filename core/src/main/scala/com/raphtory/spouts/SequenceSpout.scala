8bbee87c5143befb6799cb362bbf888dd769fdc8
Lucas Jeub <lucas.jeub@pometry.com>
2022-10-21 14:18:55 +0200

    improve sequence spout documentation

--- core/src/main/scala/com/raphtory/spouts/SequenceSpout.scala
+++ core/src/main/scala/com/raphtory/spouts/SequenceSpout.scala
@@ -3,16 +3,24 @@ package com.raphtory.spouts
 import com.raphtory.api.input.Spout
 import com.raphtory.api.input.SpoutInstance
 
-/** Output input messages to Raphtory.
-  * This spout is mainly useful for testing functionality
+/** Spout that takes an Iterable or Iterator and pushes the values into Raphtory
   */
 class SequenceSpout[T](seq: IterableOnce[T]) extends Spout[T] {
   override def buildSpout(): SpoutInstance[T] = new SequenceSpoutInstance(seq)
 }
 
 object SequenceSpout {
+
+  /** Create sequence spout from arguments
+    *
+    * @return `SequenceSpout` that returns the input arguments one-by-one
+    */
   def apply[T](first: T, seq: T*): SequenceSpout[T] = new SequenceSpout[T](first +: seq)
-  def apply[T](seq: IterableOnce[T])                = new SequenceSpout[T](seq)
+
+  /** Create sequence spout from existing `Iterable` or `Iterator`
+    * @return `SequenceSpout` wrapping the input
+    */
+  def apply[T](seq: IterableOnce[T]) = new SequenceSpout[T](seq)
 }
 
 class SequenceSpoutInstance[T](seqInput: IterableOnce[T]) extends SpoutInstance[T] {
