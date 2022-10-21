1545d18655a3a6904920ebfe691a39c909d0869e
Shivam Kapoor <shivamkapoor@pom-mbp7.home>
2022-10-21 14:27:21 +0100

    ref tests

--- core/src/test/scala/com/raphtory/api/analysis/table/TestTableOutput.scala
+++ core/src/test/scala/com/raphtory/api/analysis/table/TestTableOutput.scala
@@ -5,7 +5,7 @@ import cats.effect.Resource
 import com.raphtory.algorithms.generic.EdgeList
 import com.raphtory.api.analysis.graphview.DeployedTemporalGraph
 import com.raphtory.api.time.Perspective
-import com.raphtory.defaultConf
+import com.raphtory._
 import com.raphtory.internals.components.RaphtoryServiceBuilder
 import com.raphtory.internals.context.RaphtoryContext
 import com.raphtory.internals.graph._
@@ -13,30 +13,24 @@ import munit.CatsEffectSuite
 
 class TestTableOutput extends CatsEffectSuite {
 
-  def run[R](f: DeployedTemporalGraph => R): R =
-    RaphtoryServiceBuilder
-      .standalone[IO](defaultConf)
-      .use { standalone =>
-        IO {
-          val ctx = new RaphtoryContext(Resource.eval(IO(standalone)), defaultConf)
-          ctx.runWithNewGraph() { graph =>
-            f(graph)
-          }
-        }
-      }
-      .unsafeRunSync()
+  lazy val f: Fixture[DeployedTemporalGraph] = ResourceSuiteLocalFixture(
+          "graph",
+          new RaphtoryContext(RaphtoryServiceBuilder.standalone[IO](defaultConf), defaultConf)
+            .newIOGraph(failOnNotFound = false, destroy = true)
+  )
+
+  override def munitFixtures: Seq[Fixture[_]] = List(f)
 
   test("test table get()") {
-    val result = run { graph =>
-      graph.addVertex(0, 0)
-      graph.addVertex(0, 1)
-      graph.addVertex(1, 2)
-      graph.addEdge(0, 0, 1)
-      graph.addEdge(1, 1, 2)
+    val graph = f()
+    graph.addVertex(0, 0)
+    graph.addVertex(0, 1)
+    graph.addVertex(1, 2)
+    graph.addEdge(0, 0, 1)
+    graph.addEdge(1, 1, 2)
 
-      val perspectives = graph.range(0, 1, 1).past().execute(EdgeList()).get()
-      perspectives.map(out => out.perspective -> Set.from(out.rows)).toMap
-    }
+    val perspectives = graph.range(0, 1, 1).past().execute(EdgeList()).get()
+    val result       = perspectives.map(out => out.perspective -> Set.from(out.rows)).toMap
 
     val correctResult = Map[Perspective, Set[Row]](
             Perspective(0, None, Long.MinValue, 0) -> Set(Row("0", "1")),
