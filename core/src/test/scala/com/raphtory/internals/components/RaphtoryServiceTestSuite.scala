cfdb4ec986f4a810df12a6a562f2e0dfb1adc3f9
Shivam Kapoor <mailtoshivamk@gmail.com>
2022-10-26 13:47:31 +0100

    intro raphtoryservice get graph tests

---
+++ core/src/test/scala/com/raphtory/internals/components/RaphtoryServiceTestSuite.scala
@@ -0,0 +1,45 @@
+package com.raphtory.internals.components
+
+import cats.effect.IO
+import com.raphtory._
+import com.raphtory.protocol.ClientGraphId
+import com.raphtory.protocol.GetGraph
+import com.raphtory.protocol.RaphtoryService
+import com.raphtory.protocol.Status
+import munit.CatsEffectSuite
+
+class RaphtoryServiceTestSuite extends CatsEffectSuite {
+
+  val f: Fixture[RaphtoryService[IO]] = ResourceSuiteLocalFixture(
+          "standalone",
+          RaphtoryServiceBuilder.standalone[IO](defaultConf)
+  )
+
+  override def munitFixtures: Seq[Fixture[_]] = List(f)
+
+  test("Validate that a graph gets established successfully with a given graphId") {
+    val standalone = f()
+    standalone
+      .establishGraph(ClientGraphId("raphtory-client-1", "raphtory-graph-1"))
+      .map { status =>
+        assertEquals(status.success, true)
+      }
+  }
+
+  test("Validate that a graph doesn't exists with a given graphId if the graph is not established already") {
+    val standalone = f()
+    standalone.getGraph(GetGraph("raphtory-graph-1")).map(res => assertEquals(res, Status()))
+  }
+
+  test("Validate that a graph exists with a given graphId if the graph is established already") {
+    val standalone = f()
+    standalone
+      .establishGraph(ClientGraphId("raphtory-client-1", "raphtory-graph-1"))
+      .flatMap { status =>
+        if (status.success) standalone.getGraph(GetGraph("raphtory-graph-1")) else IO(Status())
+      }
+      .map { status =>
+        assertEquals(status.success, true)
+      }
+  }
+}
