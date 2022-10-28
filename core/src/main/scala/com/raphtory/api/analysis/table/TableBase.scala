deb1d463a623e6567eb77fedc245f86b03d55378
Shivam Kapoor <mailtoshivamk@gmail.com>
2022-10-28 18:35:54 +0100

    ref progress tracker (#643)
    * ref progress tracker
    
    * restruct project
    
    * ref
    
    * ref

--- core/src/main/scala/com/raphtory/api/analysis/table/TableBase.scala
+++ core/src/main/scala/com/raphtory/api/analysis/table/TableBase.scala
@@ -1,8 +1,8 @@
 package com.raphtory.api.analysis.table
 
 import com.raphtory.api.output.sink.Sink
-import com.raphtory.api.querytracker.QueryProgressTracker
-import com.raphtory.api.querytracker.QueryProgressTrackerBase
+import com.raphtory.api.progresstracker.QueryProgressTracker
+import com.raphtory.api.progresstracker.ProgressTracker
 import com.raphtory.sinks.FileSink
 
 trait TableBase {
@@ -18,29 +18,29 @@ trait TableBase {
   def explode(f: Row => IterableOnce[Row]): TableBase
 
   /** Write out data and
-    * return [[com.raphtory.api.querytracker.QueryProgressTracker QueryProgressTracker]]
+    * return [[com.raphtory.api.progresstracker.QueryProgressTracker QueryProgressTracker]]
     * with custom job name
     *
     * @param sink [[com.raphtory.api.output.sink.Sink Sink]] for writing results
     * @param jobName Name for job
     */
-  def writeTo(sink: Sink, jobName: String): QueryProgressTrackerBase
+  def writeTo(sink: Sink, jobName: String): ProgressTracker
 
   /** Write out data and
-    * return [[com.raphtory.api.querytracker.QueryProgressTracker QueryProgressTracker]]
+    * return [[com.raphtory.api.progresstracker.QueryProgressTracker QueryProgressTracker]]
     * with default job name
     *
     * @param sink [[com.raphtory.api.output.sink.Sink Sink]] for writing results
     */
-  def writeTo(sink: Sink): QueryProgressTrackerBase
+  def writeTo(sink: Sink): ProgressTracker
 
   /** Write out data to files and
-    * return [[com.raphtory.api.querytracker.QueryProgressTracker QueryProgressTracker]]
+    * return [[com.raphtory.api.progresstracker.QueryProgressTracker QueryProgressTracker]]
     * with default job name
     *
     * @param name folder path for writing results
     */
-  def writeToFile(name: String): QueryProgressTrackerBase =
+  def writeToFile(name: String): ProgressTracker =
     writeTo(FileSink(name))
 
 }
