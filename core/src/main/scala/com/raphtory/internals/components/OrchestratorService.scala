ff36c7bbbee60fc4f76e1f0978c40a207f27bed9
Shivam Kapoor <mailtoshivamk@gmail.com>
2022-10-31 14:34:28 +0000

    ref

--- core/src/main/scala/com/raphtory/internals/components/OrchestratorService.scala
+++ core/src/main/scala/com/raphtory/internals/components/OrchestratorService.scala
@@ -1,8 +1,7 @@
 package com.raphtory.internals.components
 
-import cats.effect.Async
-import cats.effect.Concurrent
-import cats.effect.Ref
+import cats.Monad
+import cats.effect._
 import cats.effect.std.Supervisor
 import cats.syntax.all._
 import com.raphtory.internals.components.OrchestratorService.Graph
@@ -13,7 +12,6 @@ import com.raphtory.protocol.success
 
 abstract class OrchestratorService[F[_]: Concurrent, T](graphs: GraphList[F, T]) {
 
-  // Methods to override by instances of this class
   protected def makeGraphData(graphId: String): F[T]
 
   protected def graphExecution(graph: Graph[F, T]): F[Unit] = Concurrent[F].unit
@@ -30,20 +28,34 @@ abstract class OrchestratorService[F[_]: Concurrent, T](graphs: GraphList[F, T])
 
   final def destroyGraph(req: GraphInfo): F[Status] = destroyGraph(req.graphId).as(success)
 
-  final protected def attachExecutionToGraph(graphId: String, execution: Graph[F, T] => F[Unit]): F[Unit] =
-    for {
-      graph <- graphs.get.map(graphs => graphs(graphId))
-      _     <- graph.supervisor.supervise(execution(graph))
-    } yield ()
-
   final protected def destroyGraph(graphId: String): F[Unit] =
     for {
       graph <- graphs.modify(graphs => (graphs - graphId, graphs(graphId)))
       _     <- graph.release
+    } yield ()
+
+  final protected def attachExecutionToGraph(graphId: String, execution: Graph[F, T] => F[Unit]): F[Unit] =
+    for {
+      graph <- graphs.get.map(graphs => graphs(graphId))
+      _     <- graph.supervisor.supervise(execution(graph))
     } yield ()
 }
 
 object OrchestratorService {
   case class Graph[F[_], T](id: String, supervisor: Supervisor[F], release: F[Unit], data: T)
   type GraphList[F[_], T] = Ref[F, Map[String, Graph[F, T]]]
+
+  private[components] def makeGraphList[F[_]: Concurrent, T]: Resource[F, GraphList[F, T]] = {
+    def releaseAllGraphs[C[_]: Monad, R](graphs: GraphList[C, R]): C[Unit] =
+      for {
+        graphs <- graphs.modify(graphs => (Map(), graphs)) // Empty the list and retrieve the graphs on it
+        _      <- graphs.values.map(graph => graph.release).toList.sequence // Sequence the releasing
+      } yield ()
+
+    Resource.make(Ref.of(Map[String, Graph[F, T]]()))(graphs => releaseAllGraphs(graphs))
+  }
+}
+
+abstract class NoGraphDataOrchestratorService[F[_]: Async](graphs: GraphList[F, Unit]) extends OrchestratorService(graphs) {
+  protected def makeGraphData(graphId: String): F[Unit] = Async[F].unit
 }
--- core/src/main/scala/com/raphtory/internals/components/ingestion/IngestionServiceInstance.scala
+++ core/src/main/scala/com/raphtory/internals/components/ingestion/IngestionServiceInstance.scala
@@ -1,47 +1,42 @@
 package com.raphtory.internals.components.ingestion
 
 import cats.effect.Async
-import cats.effect.Ref
 import cats.effect.Resource
 import cats.syntax.all._
 import com.raphtory.internals.communication.TopicRepository
-import com.raphtory.internals.components.OrchestratorService.Graph
 import com.raphtory.internals.components.OrchestratorService.GraphList
-import com.raphtory.internals.components.GrpcServiceDescriptor
-import com.raphtory.internals.components.OrchestratorService
-import com.raphtory.internals.components.OrchestratorServiceBuilder
-import com.raphtory.internals.components.ServiceDescriptor
-import com.raphtory.internals.components.ServiceRepository
-import com.raphtory.internals.components.querymanager.IngestData
-import com.raphtory.internals.components.querymanager.TryIngestData
+import com.raphtory.internals.components._
+import com.raphtory.internals.components.querymanager._
 import com.raphtory.protocol
 import com.raphtory.protocol.IngestionService
 import com.raphtory.protocol.Status
 import com.raphtory.protocol.failure
 import com.raphtory.protocol.success
 import com.typesafe.config.Config
+import com.typesafe.scalalogging.Logger
+import org.slf4j.LoggerFactory
 
 class IngestionServiceInstance[F[_]: Async](graphs: GraphList[F, Unit], repo: TopicRepository, config: Config)
-        extends OrchestratorService(graphs)
+        extends NoGraphDataOrchestratorService(graphs)
         with IngestionService[F] {
-
-  override def makeGraphData(graphId: String): F[Unit] = Async[F].unit
 
   override def ingestData(request: protocol.IngestData): F[Status] =
     request match {
       case protocol.IngestData(TryIngestData(scala.util.Success(req)), _) =>
-        attachExecutionToGraph(req.graphID, _ => processSource(req).onError(_ => destroyGraph(req.graphID))).as(success)
+        attachExecutionToGraph(
+                req.graphID,
+                _ =>
+                  IngestionExecutor(req.graphID, req.source, req.blocking, req.sourceId, config, repo)
+                    .onError(_ => destroyGraph(req.graphID))
+        ).as(success)
       case _                                                              => failure[F]
-    }
-
-  private def processSource(req: IngestData): F[Unit] =
-    req match {
-      case IngestData(_, graphID, sourceId, source, blocking) =>
-        IngestionExecutor(graphID, source, blocking, sourceId, config, repo)
     }
 }
 
-object IngestionServiceInstance extends OrchestratorServiceBuilder {
+object IngestionServiceInstance {
+  import OrchestratorService._
+
+  val logger: Logger = Logger(LoggerFactory.getLogger(this.getClass))
 
   def apply[F[_]: Async](repo: ServiceRepository[F], config: Config): Resource[F, Unit] =
     for {
