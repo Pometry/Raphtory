d7a911058020b970f63570bb19e337c67ab06ba1
Pedro Rico Pinazo <ricopinazo@gmail.com>
2022-11-01 17:20:37 +0100

    Fix bug preventing some exceptions from ingestion from bubbling up (#651)
    * Fix bug preventing some exceptions from ingestion bubbling up
    
    * Add default error handling to OrchestratorService
    
    * Add NonFatal check to OrchestratorService
    
    Co-authored-by: Ben Steer <b.a.steer@qmul.ac.uk>

--- core/src/main/scala/com/raphtory/internals/components/OrchestratorService.scala
+++ core/src/main/scala/com/raphtory/internals/components/OrchestratorService.scala
@@ -1,7 +1,6 @@
 package com.raphtory.internals.components
 
 import cats.effect.Async
-import cats.effect.Concurrent
 import cats.effect.Ref
 import cats.effect.std.Supervisor
 import cats.syntax.all._
@@ -10,13 +9,19 @@ import com.raphtory.internals.components.OrchestratorService.GraphList
 import com.raphtory.protocol.GraphInfo
 import com.raphtory.protocol.Status
 import com.raphtory.protocol.success
+import com.typesafe.scalalogging.Logger
+import org.slf4j.LoggerFactory
 
-abstract class OrchestratorService[F[_]: Concurrent, T](graphs: GraphList[F, T]) {
+import scala.util.control.NonFatal
+
+abstract class OrchestratorService[F[_]: Async, T](graphs: GraphList[F, T]) {
+
+  protected val logger: Logger = Logger(LoggerFactory.getLogger(this.getClass))
 
   // Methods to override by instances of this class
   protected def makeGraphData(graphId: String): F[T]
 
-  protected def graphExecution(graph: Graph[F, T]): F[Unit] = Concurrent[F].unit
+  protected def graphExecution(graph: Graph[F, T]): F[Unit] = Async[F].unit
 
   final def establishGraph(req: GraphInfo): F[Status] =
     for {
@@ -33,8 +38,10 @@ abstract class OrchestratorService[F[_]: Concurrent, T](graphs: GraphList[F, T])
   final protected def attachExecutionToGraph(graphId: String, execution: Graph[F, T] => F[Unit]): F[Unit] =
     for {
       graph <- graphs.get.map(graphs => graphs(graphId))
-      _     <- graph.supervisor.supervise(execution(graph))
+      _     <- graph.supervisor.supervise(execution(graph).onError { case NonFatal(e) => logError(e) })
     } yield ()
+
+  private def logError(e: Throwable) = Async[F].delay(logger.error(s"Exception found in orchestrator service: '$e'"))
 
   final protected def destroyGraph(graphId: String): F[Unit] =
     for {
