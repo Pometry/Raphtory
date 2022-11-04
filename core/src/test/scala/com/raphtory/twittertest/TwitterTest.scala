28cf93b204952495cc5066100208465540eb4f14
Naomi Arnold <33124479+narnolddd@users.noreply.github.com>
2022-11-04 16:10:18 +0000

    fix to stop timeout issues and brought twitter test back in (#666)

--- core/src/test/scala/com/raphtory/twittertest/TwitterTest.scala
+++ core/src/test/scala/com/raphtory/twittertest/TwitterTest.scala
@@ -33,7 +33,7 @@ import scala.language.postfixOps
  * Reference: https://snap.stanford.edu/data/ego-Twitter.html
  *
  * */
-@IgnoreSuite
+
 class TwitterTest extends BaseRaphtoryAlgoTest[String] {
   override val outputDirectory: String = "/tmp/raphtoryTwitterTest"
 
