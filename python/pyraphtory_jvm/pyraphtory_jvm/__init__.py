d211c6553fffde0802c9030ce5a755adfeac6053
Haaroon Yousaf <Haaroon@users.noreply.github.com>
2022-11-16 17:39:10 +0000

    fix version number

--- python/pyraphtory_jvm/pyraphtory_jvm/__init__.py
+++ python/pyraphtory_jvm/pyraphtory_jvm/__init__.py
@@ -2,4 +2,4 @@ import pyraphtory_jvm.jre
 import pyraphtory_jvm._version
 
 
-__version__ = pyraphtory_jvm._version.__version__
+__version__ = '0.2.0a7' # pyraphtory_jvm._version.__version__
--- python/pyraphtory_jvm/pyraphtory_jvm/_version.py
+++ python/pyraphtory_jvm/pyraphtory_jvm/_version.py
@@ -1,6 +1,6 @@
-try:
-    from importlib import metadata
-except ImportError: # for Python<3.8
-    import importlib_metadata as metadata
+#try:
+#    from importlib import metadata
+#except ImportError: # for Python<3.8
+#    import importlib_metadata as metadata
 
-__version__ = metadata.version("pyraphtory_jvm")
+# __version__ = metadata.version("pyraphtory_jvm")
