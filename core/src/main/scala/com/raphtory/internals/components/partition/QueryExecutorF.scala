c999ac97f35bc45ed05704edc97254f0058e9bfb
Pedro Rico Pinazo <ricopinazo@gmail.com>
2022-11-17 14:44:14 +0000

    Start implementation of query executor

--- core/src/main/scala/com/raphtory/internals/components/partition/QueryExecutorF.scala
+++ core/src/main/scala/com/raphtory/internals/components/partition/QueryExecutorF.scala
@@ -1,7 +1,9 @@
 package com.raphtory.internals.components.partition
 
+import cats.effect.std.Queue
 import com.raphtory.internals.components.querymanager.Query
 import com.raphtory.internals.graph.GraphPartition
+import com.raphtory.internals.graph.LensInterface
 import com.raphtory.protocol.CreatePerspective
 import com.raphtory.protocol.Empty
 import com.raphtory.protocol.GraphId
@@ -15,11 +17,16 @@ class QueryExecutorF[F[_]](
     query: Query,
     partitionID: Int,
     storage: GraphPartition,
+    graphLens: Queue[F, LensInterface],
     conf: Config
 ) {
   def processMessages(req: VertexMessage): F[Empty] = ???
 
-  def establishPerspective(req: CreatePerspective): F[Empty] = ???
+  def establishPerspective(req: CreatePerspective): F[Empty] = {
+    val start = req.perspective.actualStart
+    val end = req.perspective.actualEnd
+    graphLens.offer(storage.lens(query.name, start, end, ))
+  }
 
   def executeOperation(req: Operation): F[Empty] = ???
 
