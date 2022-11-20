e1395438aa439bd82f47c6e5ac97d73c46a63932
Jatinder <jatinder@sangha.co.uk>
2022-11-20 10:08:49 +0000

    Added perf test example based on the alphabay loader.

---
+++ examples/nft/src/main/scala/com/raphtory/examples/nft/ThrowawayRunner.scala
@@ -0,0 +1,29 @@
+package com.raphtory.examples.nft
+
+import com.raphtory.RaphtoryApp
+import com.raphtory.algorithms.generic.centrality.Degree
+import com.raphtory.api.input.Source
+import com.raphtory.internals.context.RaphtoryContext
+import com.raphtory.sinks.FileSink
+import com.raphtory.spouts.FileSpout
+
+object ThrowawayRunner extends RaphtoryApp.ArrowLocal[NoProps, Price] {
+
+  val path = "alphabay_sorted.csv"
+  val builder = new AlphaBayBuilder()
+
+  override def run(args: Array[String], ctx: RaphtoryContext): Unit = {
+    ctx.runWithNewGraph() { graph =>
+      val source = Source(FileSpout(path), builder)
+      graph.addDynamicPath("com.raphtory.crypto")
+      graph.load(source)
+
+      graph.execute(Degree())
+        .writeTo(FileSink("/tmp/raphtory"))
+        .waitForJob()
+    }
+  }
+}
+
+case class NoProps()
+case class Price(valUSD: Long)
