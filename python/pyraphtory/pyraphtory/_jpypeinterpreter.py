dc5b8e84bc7114bd2a0ae0c2ed6ded3ebe716325
Lucas Jeub <lucas.jeub@pometry.com>
2022-11-23 11:58:59 +0100

    comment out debug hooks as they crash when not running in debugger and add verbose exception printouts

--- python/pyraphtory/pyraphtory/_jpypeinterpreter.py
+++ python/pyraphtory/pyraphtory/_jpypeinterpreter.py
@@ -1,6 +1,7 @@
 import jpype.imports
 from jpype import JImplements, JOverride, JString, JObject, JClass
 import pydevd
+from traceback import print_exc
 
 Interpreter = JClass("pemja.core.Interpreter")
 from java.util import Map
@@ -13,55 +14,71 @@ class JPypeInterpreter:
 
     @JOverride
     def set(self, name: JString, value: JObject):
-        pydevd.connected = True
-        pydevd.settrace(suspend=False)
-        self._locals[name] = value
+        # pydevd.connected = True
+        # pydevd.settrace(suspend=False)
+        try:
+            self._locals[name] = value
+        except Exception as e:
+            print_exc()
+            raise e
 
     @JOverride
     def get(self, *args):
-        pydevd.connected = True
-        pydevd.settrace(suspend=False)
-        obj = self._locals[args[0]]
-        if len(args) == 1:
-            return obj
-        else:
-            return JObject(obj, args[1])
+        # pydevd.connected = True
+        # pydevd.settrace(suspend=False)
+        try:
+            obj = self._locals[args[0]]
+            if len(args) == 1:
+                return obj
+            else:
+                return JObject(obj, args[1])
+        except Exception as e:
+            print_exc()
+            raise e
 
     @JOverride
     def invoke(self, *args):
-        pydevd.connected = True
-        pydevd.settrace(suspend=False)
-        fun = self._locals[args[0]]
-        if isinstance(args[1], Map):
-            return fun(**args[1])
-        else:
-            if len(args) > 2:
-                return fun(*args[1], **args[2])
+        # pydevd.connected = True
+        # pydevd.settrace(suspend=False)
+        try:
+            fun = self._locals[args[0]]
+            if isinstance(args[1], Map):
+                return fun(**args[1])
             else:
-                return fun(*args[1])
+                if len(args) > 2:
+                    return fun(*args[1], **args[2])
+                else:
+                    return fun(*args[1])
+        except Exception as e:
+            print_exc()
+            raise e
 
     @JOverride
     def invokeMethod(self, obj: JString, method: JString, args):
-        pydevd.connected = True
-        pydevd.settrace(suspend=False)
-        obj = self._locals[obj]
-        method = getattr(obj, str(method))
-        result = method(*args)
+        # pydevd.connected = True
+        # pydevd.settrace(suspend=False)
+        try:
+            obj = self._locals[obj]
+            method = getattr(obj, str(method))
+            result = method(*args)
+        except Exception as e:
+            print_exc()
+            raise e
         return result
 
     @JOverride
     def exec(self, code: JString):
-        pydevd.connected = True
-        pydevd.settrace(suspend=False)
+        # pydevd.connected = True
+        # pydevd.settrace(suspend=False)
         try:
             exec(str(code), globals(), self._locals)
         except Exception as e:
-            print(e)
+            print_exc()
             raise e
         return
 
     @JOverride
     def close(self):
-        pydevd.connected = True
-        pydevd.settrace(suspend=False)
+        # pydevd.connected = True
+        # pydevd.settrace(suspend=False)
         self._locals = {}
