a7a31dbf5061c631ed1f9f838f62e919edb77005
Jatinder <jatinder@sangha.co.uk>
2022-11-23 16:57:44 +0000

    Reverted this file as I couldn't get it to compile!

--- core/src/main/scala/com/raphtory/internals/storage/arrow/ArrowPartition.scala
+++ core/src/main/scala/com/raphtory/internals/storage/arrow/ArrowPartition.scala
@@ -189,23 +189,27 @@ class ArrowPartition(graphID: String, val par: RaphtoryArrowPartition, partition
       // handle dst
       val dst = idsRepo.resolve(dstId)
 
-      val matchingEdges = src.findAllOutgoingEdges(dstId, dst.isLocal)
-      if (matchingEdges.hasNext) {
-        matchingEdges.next()
-        val foundEdge = matchingEdges.getEdge
-        updateExistingEdge(sourceID, msgTime, index, srcId, dstId, properties, dst, foundEdge)
-      }
-      else {
-        if (dst.isLocal) {
-          val dstV = addVertexInternal(dstId, msgTime, Properties())
-          addLocalVerticesToEdge(src, dstV, msgTime, properties)
-          None
+      src.outgoingEdges.find { e =>
+        dst match {
+          case NotFound(_)           => false
+          case GlobalId(id)          => id == e.getDstVertex && e.isDstGlobal
+          case ExistsOnPartition(id) => id == e.getDstVertex && !e.isDstGlobal
         }
-        else {
-          addRemoteOutgoingEdge(src, dst.id, msgTime, properties)
+      } match {
+        case Some(e) =>
+          updateExistingEdge(sourceID, msgTime, index, srcId, dstId, properties, dst, e)
+        case None    =>
+          if (dst.isLocal) {
+            val dstV = addVertexInternal(dstId, msgTime, Properties())
+            addLocalVerticesToEdge(src, dstV, msgTime, properties)
+            None
+          }
+          else {
+            addRemoteOutgoingEdge(src, dst.id, msgTime, properties)
 
-          Some(SyncNewEdgeAdd(sourceID, msgTime, index, srcId, dstId, properties, Nil, edgeType))
-        }
+            Some(SyncNewEdgeAdd(sourceID, msgTime, index, srcId, dstId, properties, Nil, edgeType))
+          }
+
       }
     }
 
