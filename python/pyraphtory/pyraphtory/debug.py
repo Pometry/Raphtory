5ebba800748870d5179304cefc5e5f5faf38d54e
Lucas Jeub <lucas.jeub@pometry.com>
2022-11-24 15:42:18 +0100

    clean up debug-mode check to avoid ugly error message in notebooks

--- python/pyraphtory/pyraphtory/debug.py
+++ python/pyraphtory/pyraphtory/debug.py
@@ -1,19 +1,22 @@
-try:
-    import pydevd
-    pydevd.connected = True
-    pydevd.settrace(suspend=False)
-    def enable_pydev_debug():
-        """
-        Use this function to ensure the pydevd debugger (used by e.g. Pycharm)
-        works for python code called from a thread created by the jvm.
+import sys
 
-        """
+
+def enable_pydev_debug():
+    pass
+
+
+if hasattr(sys, "gettrace") and sys.gettrace() is not None:
+    try:
+        import pydevd
         pydevd.connected = True
         pydevd.settrace(suspend=False)
-except Exception as e:
-    def enable_pydev_debug():
+        def enable_pydev_debug():
+            """
+            Use this function to ensure the pydevd debugger (used by e.g. Pycharm)
+            works for python code called from a thread created by the jvm.
+
+            """
+            pydevd.connected = True
+            pydevd.settrace(suspend=False)
+    except Exception:
         pass
-
-
-
-
