c1a9ba78188ba73bf4d9b40c81b603a418c3c84a
Fabian Murariu <murariu.fabian@gmail.com>
2022-12-07 17:48:35 +0000

    stuff finally compiles and filters the vertices

--- src/graph.rs
+++ src/graph.rs
@@ -1,37 +1,58 @@
 use std::{
     collections::BTreeMap,
-    ops::{Range, RangeBounds},
-    sync::{Arc, RwLock},
+    ops::RangeBounds,
+    sync::{Arc, Mutex},
 };
 
 use dashmap::DashMap;
-use roaring::{RoaringBitmap, RoaringTreemap};
+use guardian::ArcMutexGuardian;
+use roaring::RoaringTreemap;
 
-use crate::TemporalGraphStorage;
+use crate::{TemporalGraphStorage, TVertexView};
 
 #[derive(Debug)]
 enum Adj {
     Empty,
     List { out: Vec<u64>, into: Vec<u64> },
 }
-type TsIndex = RwLock<BTreeMap<u64, RoaringTreemap>>;
+
+type TsIndex = Arc<Mutex<BTreeMap<u64, RoaringTreemap>>>;
 
 pub struct TemporalGraph {
     gs: Arc<DashMap<u64, Adj>>,
-    t_index: Arc<TsIndex>,
+    t_index: ConcurrentTsIndex,
 }
 
 impl TemporalGraph {
     pub fn new_mem() -> Self {
         TemporalGraph {
             gs: Arc::new(DashMap::default()),
-            t_index: Arc::new(RwLock::new(BTreeMap::default())),
+            t_index: ConcurrentTsIndex::new(BTreeMap::default()),
         }
     }
 }
 
-fn update_adj_list_and_ts_index(_adj: &mut Adj, t: u64, v: u64, ts_index: Arc<TsIndex>) {
-    if let Ok(mut index) = ts_index.write() {
+struct ConcurrentTsIndex {
+    index: TsIndex,
+}
+
+impl ConcurrentTsIndex {
+    fn new(val: BTreeMap<u64, RoaringTreemap>) -> Self {
+        Self {
+            index: Arc::new(Mutex::new(val)),
+        }
+    }
+
+    fn iterable_lock<R: RangeBounds<u64>>(&self, r: R) -> TVertexView<R> {
+        TVertexView {
+            r,
+            guard: ArcMutexGuardian::take(Arc::clone(&self.index)).unwrap(),
+        }
+    }
+}
+
+fn update_adj_list_and_ts_index(_adj: &mut Adj, t: u64, v: u64, ts_index: TsIndex) {
+    if let Ok(mut index) = ts_index.lock() {
         index
             .entry(t)
             .and_modify(|set| {
@@ -47,11 +68,12 @@ fn update_adj_list_and_ts_index(_adj: &mut Adj, t: u64, v: u64, ts_index: Arc<Ts
 
 impl TemporalGraphStorage for TemporalGraph {
     fn add_vertex(&self, v: u64, t: u64) -> &Self {
+        let idx = self.t_index.index.clone();
         self.gs
             .entry(v)
-            .and_modify(|adj| update_adj_list_and_ts_index(adj, t, v, self.t_index.clone()))
+            .and_modify(|adj| update_adj_list_and_ts_index(adj, t, v, idx.clone()))
             .or_insert_with(|| {
-                update_adj_list_and_ts_index(&mut Adj::Empty, t, v, self.t_index.clone());
+                update_adj_list_and_ts_index(&mut Adj::Empty, t, v, idx.clone());
                 Adj::Empty
             });
         self
@@ -61,13 +83,10 @@ impl TemporalGraphStorage for TemporalGraph {
         self.gs.iter().map(|entry| *entry.key()).collect()
     }
 
-    fn enumerate_vs_at<R>(&self, t: R) -> Vec<u64> 
-    where R: RangeBounds<u64>
+    fn enumerate_vs_at<R: RangeBounds<u64>>(&self, r: R) -> TVertexView<R>
+    where
+        R: RangeBounds<u64>,
     {
-        if let Ok(index) = self.t_index.read() {
-            index.range(t).flat_map(|(_, vs)| vs.iter()).collect()
-        } else {
-            vec![]
-        }
+        self.t_index.iterable_lock(r)
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,31 @@
-use std::ops::RangeBounds;
+use std::{collections::BTreeMap, ops::RangeBounds};
+
+use guardian::ArcMutexGuardian;
+use roaring::RoaringTreemap;
 
 mod graph;
+
+struct TVertexView<R: RangeBounds<u64>> {
+    r: R,
+    guard: ArcMutexGuardian<BTreeMap<u64, RoaringTreemap>>,
+}
+
+impl<R: RangeBounds<u64> + Clone> TVertexView<R> {
+    fn iter(&self) -> Box<dyn Iterator<Item = u64> + '_> {
+        let iter = self
+            .guard
+            .range(self.r.clone())
+            .flat_map(|(_, vs)| vs.iter());
+        Box::new(iter)
+    }
+}
 
 trait TemporalGraphStorage {
     fn add_vertex(&self, v: u64, t: u64) -> &Self;
 
     fn enumerate_vertices(&self) -> Vec<u64>;
 
-    fn enumerate_vs_at<R: RangeBounds<u64>>(&self, t: R) -> Vec<u64>;
+    fn enumerate_vs_at<R: RangeBounds<u64>>(&self, t: R) -> TVertexView<R>;
 }
 
 #[cfg(test)]
@@ -30,9 +48,11 @@ mod tests {
         g.add_vertex(9, 1);
         g.add_vertex(1, 2);
 
-        assert_eq!(g.enumerate_vs_at(0..=1), vec![9]);
-        assert_eq!(g.enumerate_vs_at(2..10), vec![1]);
-        assert_eq!(g.enumerate_vs_at(0..10), vec![9,1]);
-
+        let actual: Vec<u64> = g.enumerate_vs_at(0..=1).iter().collect();
+        assert_eq!(actual, vec![9]);
+        let actual: Vec<u64> = g.enumerate_vs_at(2..10).iter().collect();
+        assert_eq!(actual, vec![1]);
+        let actual: Vec<u64> = g.enumerate_vs_at(0..10).iter().collect();
+        assert_eq!(actual, vec![9, 1]);
     }
 }
