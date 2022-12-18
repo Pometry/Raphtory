dd0483e6076ecd5e22b99229a1ce4f4320afcd05
Fabian Murariu <murariu.fabian@gmail.com>
2022-12-18 15:33:26 +0000

    some refactoring and some extra tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,18 @@
-use std::ops::Range;
+use std::ops::{Range, RangeBounds};
 
-pub mod graph;
-pub mod tvec;
-pub mod tcell;
-pub mod sortedvec;
 pub mod bitset;
+pub mod graph;
+mod misc;
+pub mod sortedvec;
+pub mod tcell;
+pub mod tvec;
+
+#[derive(Clone, Copy)]
+pub enum Direction {
+    OUT,
+    IN,
+    BOTH,
+}
 
 pub trait TemporalGraphStorage {
     fn add_vertex(&mut self, v: u64, t: u64) -> &mut Self;
@@ -13,34 +21,68 @@ pub trait TemporalGraphStorage {
      * adds the edge in an idempotent manner
      * if src doesn't exit it's added at time t
      * if dst doesn't exit it's added at time t
-     * 
+     *
      * both src and dst get an index at time t if they do exist
      */
     fn add_edge(&mut self, src: u64, dst: u64, t: u64) -> &mut Self;
 
     fn iter_vs(&self) -> Box<dyn Iterator<Item = &u64> + '_>;
 
-    fn iter_vs_window(&self, r:Range<u64>) ->  Box<dyn Iterator<Item = u64> + '_>;
+    fn iter_vs_window(&self, r: Range<u64>) -> Box<dyn Iterator<Item = u64> + '_>;
 
-    fn outbound(&self, src: u64) ->  Box<dyn Iterator<Item = &u64> + '_>;
+    fn neighbours(&self, v: u64, d: Direction) -> Box<dyn Iterator<Item = &u64> + '_>;
 
-    fn inbound(&self, dst: u64) ->  Box<dyn Iterator<Item = &u64> + '_>;
+    fn neighbours_window(
+        &self,
+        w: Range<u64>,
+        v: u64,
+        d: Direction,
+    ) -> Box<dyn Iterator<Item = &u64> + '_>;
 
-    fn outbound_window(&self, src: u64, r:Range<u64>) ->  Box<dyn Iterator<Item = &u64> + '_>;
+    fn neighbours_window_t(
+        &self,
+        w: Range<u64>,
+        v: u64,
+        d: Direction,
+    ) -> Box<dyn Iterator<Item = (&u64, &u64)> + '_>;
 
-    fn inbound_window(&self, dst: u64, r:Range<u64>) ->  Box<dyn Iterator<Item = &u64> + '_>;
+    fn outbound(&self, src: u64) -> Box<dyn Iterator<Item = &u64> + '_> {
+        self.neighbours(src, Direction::OUT)
+    }
 
-    fn outbound_window_t(&self, src: u64, r:Range<u64>) ->  Box<dyn Iterator<Item = (&u64, &u64)> + '_>;
+    fn inbound(&self, dst: u64) -> Box<dyn Iterator<Item = &u64> + '_> {
+        self.neighbours(dst, Direction::IN)
+    }
 
-    fn inbound_window_t(&self, dst: u64, r:Range<u64>) ->  Box<dyn Iterator<Item = (&u64, &u64)> + '_>;
+    fn outbound_window(&self, src: u64, r: Range<u64>) -> Box<dyn Iterator<Item = &u64> + '_> {
+        self.neighbours_window(r, src, Direction::OUT)
+    }
+
+    fn inbound_window(&self, dst: u64, r: Range<u64>) -> Box<dyn Iterator<Item = &u64> + '_> {
+        self.neighbours_window(r, dst, Direction::IN)
+    }
+
+    fn outbound_window_t(
+        &self,
+        src: u64,
+        r: Range<u64>,
+    ) -> Box<dyn Iterator<Item = (&u64, &u64)> + '_> {
+        self.neighbours_window_t(r, src, Direction::OUT)
+    }
+
+    fn inbound_window_t(
+        &self,
+        dst: u64,
+        r: Range<u64>,
+    ) -> Box<dyn Iterator<Item = (&u64, &u64)> + '_>{
+        self.neighbours_window_t(r, dst, Direction::IN)
+    }
 
     fn len(&self) -> usize;
 
     fn outbound_degree(&self, src: u64) -> usize;
     fn inbound_degree(&self, dst: u64) -> usize;
 
-    fn outbound_degree_t(&self, dst: u64, r:Range<u64>) -> usize;
-    fn inbound_degree_t(&self, dst: u64, r:Range<u64>) -> usize;
-
+    fn outbound_degree_t(&self, dst: u64, r: Range<u64>) -> usize;
+    fn inbound_degree_t(&self, dst: u64, r: Range<u64>) -> usize;
 }
-
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+#![allow(unused_imports)]
 use std::collections::HashMap;
 use std::env;
 
