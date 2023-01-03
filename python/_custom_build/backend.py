490d394ff3f9a11018c015e5d2766439fd0eb3d3
Lucas Jeub <lucas.jeub@pometry.com>
2023-01-03 16:23:45 +0100

    fix platform tags for wheels

--- python/_custom_build/backend.py
+++ python/_custom_build/backend.py
@@ -7,6 +7,7 @@ from pathlib import Path
 from ivy import get_and_run_ivy
 from java import check_system_dl_java
 from sbt_build import make_python_build
+from check_platform import get_platform_tag
 
 prepare_metadata_for_build_wheel = _orig.prepare_metadata_for_build_wheel
 prepare_metadata_for_build_editable = _orig.prepare_metadata_for_build_editable
@@ -15,7 +16,7 @@ get_requires_for_build_sdist = _orig.get_requires_for_build_sdist
 get_requires_for_build_editable = _orig.get_requires_for_build_editable
 
 
-platform = next(tags.sys_tags()).platform
+platform = get_platform_tag()
 build_folder = Path(__file__).resolve().parent
 package_folder = build_folder.parent / "pyraphtory"
 
@@ -28,10 +29,13 @@ def build_sdist(sdist_directory, config_settings=None):
     return _orig.build_sdist(sdist_directory, config_settings)
 
 
+def get_java_dependencies():
+    java_bin = check_system_dl_java(jre_folder)
+    get_and_run_ivy(java_bin, build_folder / "ivy_data", lib_folder, package_folder / "ivy")
+
 def build_wheel(wheel_directory, config_settings: dict=None, metadata_directory=None):
     print(sys.path)
-    java_bin = check_system_dl_java(jre_folder)
-    get_and_run_ivy(java_bin, build_folder / "ivy_data", lib_folder)
+    get_java_dependencies()
 
     # make wheel platform-specific as the jre downloaded will be different
     if config_settings is not None:
@@ -45,6 +49,5 @@ def build_wheel(wheel_directory, config_settings: dict=None, metadata_directory=
 
 def build_editable(wheel_directory, config_settings=None, metadata_directory=None):
     make_python_build()
-    java_bin = check_system_dl_java(jre_folder)
-    get_and_run_ivy(java_bin, build_folder / "ivy_data", lib_folder, package_folder / "ivy")
+    get_java_dependencies()
     return _orig.build_editable(wheel_directory, config_settings, metadata_directory)
--- python/_custom_build/check_platform.py
+++ python/_custom_build/check_platform.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 import platform
+from packaging import tags
 
 
 OS_MAC = 'Darwin'
@@ -23,3 +24,13 @@ def getArch() -> str:
         return OS_AARCH64
     else:
         raise Exception("Unsupported Architecture. Cannot install.")
+
+
+def get_platform_tag() -> str:
+    if getOS() == OS_MAC:
+        if getArch() == OS_X64:
+            return str(next(tags.mac_platforms((10, 11))))
+        else:
+            return str(tags.mac_platforms((11, 0)))
+    else:
+        return str(next(tags.sys_tags()).platform)
