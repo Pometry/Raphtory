ba6f4da8ab0ae134c18e0fe24c55460a880b5793
Lucas Jeub <lucas.jeub@pometry.com>
2023-01-04 10:22:50 +0100

    start fixing entrypoint script

--- bin/docker/raphtory/bin/entrypoint.sh
+++ bin/docker/raphtory/bin/entrypoint.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 set -x
 
-DEP_CP=$(python3 -c "from pyraphtory_jvm.jre import get_local_ivy_loc; print(get_local_ivy_loc())")
-CORE_CP=$(python3 -c "import site; print(site.getsitepackages()[0] + '/lib/')")
+CORE_CP="$(raphtory-classpath)"
 
-java -cp "$DEP_CP/compile/*:${CORE_CP}/*:/raphtory/jars/*" $RAPHTORY_JAVA_RUN_ARGS $RAPHTORY_JAVA_RUN_CLASS $RAPHTORY_JAVA_COMPONENT_NAME
+java -cp "$CORE_CP" $RAPHTORY_JAVA_RUN_ARGS $RAPHTORY_JAVA_RUN_CLASS $RAPHTORY_JAVA_COMPONENT_NAME
