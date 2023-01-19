0fefd6d0acf2aacf1f1f97fcda15b24fae2d9a53
Fabian Murariu <murariu.fabian@gmail.com>
2023-01-19 16:37:09 +0000

    added support to iterate edges without lifetimes

--- docbrown/core/src/tpartition.rs
+++ docbrown/core/src/tpartition.rs
@@ -4,39 +4,46 @@ use std::sync::Arc;
 use genawaiter::rc::gen;
 use genawaiter::yield_;
 
-use crate::graph::TemporalGraph;
-use crate::tadjset::AdjEdge;
-use crate::{Prop, Direction};
+use crate::graph::{EdgeView, TemporalGraph};
+use crate::{Direction, Prop};
 use itertools::*;
 
 #[derive(Clone, Debug, Default)]
 pub struct TemporalGraphPart(Arc<RwLock<TemporalGraph>>);
 
-pub struct TEdge{v: usize, edge_meta_id: AdjEdge, t: Option<u64>, outbound: bool}
+pub struct TEdge {
+    src: u64,
+    dst: u64,
+    // edge_meta_id: AdjEdge,
+    t: Option<u64>,
+    is_remote: bool,
+}
+
+impl<'a> From<EdgeView<'a, TemporalGraph>> for TEdge {
+    fn from(e: EdgeView<'a, TemporalGraph>) -> Self {
+        Self {
+            src: e.global_src(),
+            dst: e.global_dst(),
+            t: e.time(),
+            is_remote: e.is_remote(),
+        }
+    }
+}
 
 impl TemporalGraphPart {
     pub fn add_vertex(&self, t: u64, v: u64, props: &Vec<(String, Prop)>) {
         self.write_shard(|tg| tg.add_vertex(v, t))
     }
 
-    pub fn add_edge(
-        &self,
-        t: u64, src: u64, dst:u64, props: &Vec<(String, Prop)>
-    ) {
+    pub fn add_edge(&self, t: u64, src: u64, dst: u64, props: &Vec<(String, Prop)>) {
         self.write_shard(|tg| tg.add_edge_props(src, dst, t, props))
     }
 
-    pub fn add_edge_remote_out(
-        &self,
-        t: u64, src: u64, dst:u64, props: &Vec<(String, Prop)>
-    ) {
+    pub fn add_edge_remote_out(&self, t: u64, src: u64, dst: u64, props: &Vec<(String, Prop)>) {
         self.write_shard(|tg| tg.add_edge_remote_out(src, dst, t, props))
     }
 
-    pub fn add_edge_remote_into(
-        &self,
-        t: u64, src: u64, dst:u64, props: &Vec<(String, Prop)>
-    ) {
+    pub fn add_edge_remote_into(&self, t: u64, src: u64, dst: u64, props: &Vec<(String, Prop)>) {
         self.write_shard(|tg| tg.add_edge_remote_into(src, dst, t, props))
     }
 
@@ -45,8 +52,8 @@ impl TemporalGraphPart {
         &self,
         t_start: u64,
         t_end: u64,
-        chunk_size: usize
-    ) -> impl Iterator<Item = Vec<usize>>{
+        chunk_size: usize,
+    ) -> impl Iterator<Item = Vec<usize>> {
         let tg = self.clone();
         let vertices_iter = gen!({
             let g = tg.0.read();
@@ -60,17 +67,19 @@ impl TemporalGraphPart {
         vertices_iter.into_iter()
     }
 
-    pub fn neighbours_window(&self, t_start: u64, t_end: u64, v: u64, d: Direction) -> impl Iterator<Item = TEdge>{
-
+    pub fn neighbours_window(
+        &self,
+        t_start: u64,
+        t_end: u64,
+        v: u64,
+        d: Direction,
+    ) -> impl Iterator<Item = TEdge> {
         let tg = self.clone();
         let vertices_iter = gen!({
             let g = tg.0.read();
-            let chunks = (*g).neighbours_window((t_start .. t_end), v, d).map(|e| {
-
-                let oposite_v = if (d == Direction::OUT) {e.global_dst()} else {e.global_src()};
-                todo!()
-            }
-);
+            let chunks = (*g)
+                .neighbours_window((t_start..t_end), v, d)
+                .map(|e| e.into());
             let iter = chunks.into_iter();
             for v_id in iter {
                 yield_!(v_id)
@@ -80,9 +89,16 @@ impl TemporalGraphPart {
         vertices_iter.into_iter()
     }
 
-
     pub fn len(&self) -> usize {
         self.read_shard(|tg| tg.len())
+    }
+
+    pub fn contains(&self, v: u64) -> bool {
+        self.read_shard(|tg| tg.contains(v))
+    }
+
+    pub fn contains_t(&self, t_start: u64, t_end: u64, v: u64) -> bool {
+        self.read_shard(|tg| tg.contains_vertex_w(t_start..t_end, v))
     }
 
     #[inline(always)]
@@ -118,7 +134,11 @@ mod temporal_graph_partition_test {
         fn arbitrary(g: &mut quickcheck::Gen) -> Self {
             let mut some_nums = Vec::<u64>::arbitrary(g);
             some_nums.sort();
-            let intervals = some_nums.into_iter().tuple_windows().filter(|(a, b)| a != b).collect_vec();
+            let intervals = some_nums
+                .into_iter()
+                .tuple_windows()
+                .filter(|(a, b)| a != b)
+                .collect_vec();
             Intervals(intervals)
         }
     }
@@ -147,12 +167,12 @@ mod temporal_graph_partition_test {
             g.add_vertex(*t_start, v.try_into().unwrap(), &vec![])
         }
 
-       for (v, (t_start, t_end)) in intervals.0.iter().enumerate() {
-           let vertex_window = g.vertices_window(*t_start, *t_end, 1);
-           let iter = &mut vertex_window.into_iter().flatten();
-           let v_actual = iter.next();
-           assert_eq!(Some(v), v_actual);
-           assert_eq!(None, iter.next()); // one vertex per interval
+        for (v, (t_start, t_end)) in intervals.0.iter().enumerate() {
+            let vertex_window = g.vertices_window(*t_start, *t_end, 1);
+            let iter = &mut vertex_window.into_iter().flatten();
+            let v_actual = iter.next();
+            assert_eq!(Some(v), v_actual);
+            assert_eq!(None, iter.next()); // one vertex per interval
         }
     }
 }
