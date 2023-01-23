c8695cb3efafd13aa93f511cfed5c8776dd940da
Fabian Murariu <murariu.fabian@gmail.com>
2023-01-23 17:18:07 +0000

    csv loader can ingest bitcoin data

--- docbrown/core/src/tpartition.rs
+++ docbrown/core/src/tpartition.rs
@@ -94,6 +94,10 @@ impl TemporalGraphPart {
         self.read_shard(|tg| tg.len())
     }
 
+    pub fn edges_len(&self) -> usize {
+        self.read_shard(|tg| tg.edges_len())
+    }
+
     pub fn contains(&self, v: u64) -> bool {
         self.read_shard(|tg| tg.contains(v))
     }
