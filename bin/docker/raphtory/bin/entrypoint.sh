495ebc5a965a8cbababa74013034fb4fad598b4c
James Alford <james.alford@live.co.uk>
2023-02-06 10:01:40 +0000

    Adjust docker build scripts so that they work on multi platform and fix jar packaging (#799)
    Co-authored-by: James Alford <james.alford@pometry.com>

--- bin/docker/raphtory/bin/entrypoint.sh
+++ bin/docker/raphtory/bin/entrypoint.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
-#set -x
 set -euf -o pipefail
-
 RAPHTORY_JAVA_RUN_ARGS="${RAPHTORY_JAVA_RUN_ARGS:-}"
-
-#CORE_CP="$(raphtory-classpath)"
 CORE_CP="/opt/venv/lib/python3.10/site-packages/pyraphtory/lib/*"
-set -x
 java -cp "$CORE_CP:/raphtory/jars/*" $RAPHTORY_JAVA_RUN_ARGS $RAPHTORY_JAVA_RUN_CLASS
