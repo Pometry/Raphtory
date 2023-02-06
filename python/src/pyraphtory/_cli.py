ff7f0d04ad199b3c7da1faf7a6f788ae43621946
Lucas Jeub <lucas.jeub@pometry.com>
2023-02-06 17:51:50 +0100

    get path to lib directory form cli (for easier scala extension package dev)

--- python/src/pyraphtory/_cli.py
+++ python/src/pyraphtory/_cli.py
@@ -1,7 +1,8 @@
-from pyraphtory._config import jars, java, java_args
+from pyraphtory._config import jars, java, java_args, get_local_lib
 from pyraphtory import __version__
 import subprocess
 import sys
+
 
 def standalone():
     if java_args:
@@ -9,11 +10,13 @@ def standalone():
     else:
         subprocess.run([java, "-cp", jars, "com.raphtory.service.Standalone"])
 
+
 def clustermanager():
     if java_args:
         subprocess.run([java, java_args, "-cp", jars, "com.raphtory.service.ClusterManager"])
     else:
         subprocess.run([java, "-cp", jars, "com.raphtory.service.ClusterManager"])
+
 
 def ingestion():
     if java_args:
@@ -21,11 +24,13 @@ def ingestion():
     else:
         subprocess.run([java, "-cp", jars, "com.raphtory.service.Ingestion"])
 
+
 def partition():
     if java_args:
         subprocess.run([java, java_args, "-cp", jars, "com.raphtory.service.Partition"])
     else:
         subprocess.run([java, "-cp", jars, "com.raphtory.service.Partition"])
+
 
 def query():
     if java_args:
@@ -33,12 +38,17 @@ def query():
     else:
         subprocess.run([java, "-cp", jars, "com.raphtory.service.Query"])
 
+
 def classpath():
     sys.stdout.write(jars)
 
 
 def version():
     sys.stdout.write(__version__)
+
+
+def include():
+    sys.stdout.write(str(get_local_lib()))
 
 
 if __name__ == "__main__":
