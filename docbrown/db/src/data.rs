1fdcd3b2a0bb2675d4cbd30a1f58ea1cd9b55e13
RachelChan <25484244+rachchan@users.noreply.github.com>
2023-02-22 13:57:12 +0000

    first draft of data loader

--- docbrown/db/src/data.rs
+++ docbrown/db/src/data.rs
@@ -1,10 +1,14 @@
+use crate::{graphdb::GraphDB, loaders::csv::CsvLoader};
+use csv::StringRecord;
+use docbrown_core::utils;
+use docbrown_core::{Direction, Prop};
 use fetch_data::{fetch, FetchDataError};
+use serde::Deserialize;
 use std::path::PathBuf;
-use std::env;
-
+use std::{env, path::Path, time::Instant};
 // In order to add new files to this module, obtain the hash using bin/hash_for_url.rs
 
-pub fn lotr() -> Result<PathBuf, FetchDataError> {
+pub fn lotr_file() -> Result<PathBuf, FetchDataError> {
     fetch_file(
         "lotr.csv",
         "https://raw.githubusercontent.com/Raphtory/Data/main/lotr.csv",
@@ -12,7 +16,7 @@ pub fn lotr() -> Result<PathBuf, FetchDataError> {
     )
 }
 
-pub fn twitter() -> Result<PathBuf, FetchDataError> {
+pub fn twitter_file() -> Result<PathBuf, FetchDataError> {
     fetch_file(
         "twitter.csv",
         "https://raw.githubusercontent.com/Raphtory/Data/main/snap-twitter.csv",
@@ -25,4 +29,91 @@ fn fetch_file(name: &str, url: &str, hash: &str) -> Result<PathBuf, FetchDataErr
     let file = tmp_dir.join(name);
     fetch(url, hash, &file)?;
     Ok(file)
+}
+
+fn parse_record(rec: &StringRecord) -> Option<(String, String, i64)> {
+    let src = rec.get(0).and_then(|s| s.parse::<String>().ok())?;
+    let dst = rec.get(1).and_then(|s| s.parse::<String>().ok())?;
+    let t = rec.get(2).and_then(|s| s.parse::<i64>().ok())?;
+    Some((src, dst, t))
+}
+
+pub fn lotr_graph(shards: usize) -> GraphDB {
+    let g = GraphDB::new(shards);
+
+    let data_dir = lotr_file().expect("Failed to get lotr.csv file");
+
+    if let Ok(mut reader) = csv::Reader::from_path(data_dir) {
+        for rec_res in reader.records() {
+            if let Ok(rec) = rec_res {
+                if let Some((src, dst, t)) = parse_record(&rec) {
+                    let src_id = utils::calculate_hash(&src);
+                    let dst_id = utils::calculate_hash(&dst);
+
+                    g.add_vertex(
+                        src_id,
+                        t,
+                        &vec![("name".to_string(), Prop::Str("Character".to_string()))],
+                    );
+                    g.add_vertex(
+                        dst_id,
+                        t,
+                        &vec![("name".to_string(), Prop::Str("Character".to_string()))],
+                    );
+                    g.add_edge(
+                        src_id,
+                        dst_id,
+                        t,
+                        &vec![(
+                            "name".to_string(),
+                            Prop::Str("Character Co-occurrence".to_string()),
+                        )],
+                    );
+                }
+            }
+        }
+    }
+    g
+}
+
+pub fn twitter_graph(shards: usize) -> GraphDB {
+    let g = GraphDB::new(shards);
+
+    let data_dir = twitter_file().expect("Failed to get twitter.csv file");
+
+    fn parse_record(rec: &StringRecord) -> Option<(String, String, i64)> {
+        let src = rec.get(0).and_then(|s| s.parse::<String>().ok())?;
+        let dst = rec.get(1).and_then(|s| s.parse::<String>().ok())?;
+        let t = 1;
+        Some((src, dst, t))
+    }
+
+    if let Ok(mut reader) = csv::Reader::from_path(data_dir) {
+        for rec_res in reader.records() {
+            if let Ok(rec) = rec_res {
+                if let Some((src, dst, t)) = parse_record(&rec) {
+                    let src_id = utils::calculate_hash(&src);
+                    let dst_id = utils::calculate_hash(&dst);
+
+                    g.add_vertex(
+                        src_id,
+                        t,
+                        &vec![("name".to_string(), Prop::Str("User".to_string()))],
+                    );
+                    g.add_vertex(
+                        dst_id,
+                        t,
+                        &vec![("name".to_string(), Prop::Str("User".to_string()))],
+                    );
+                    g.add_edge(
+                        src_id,
+                        dst_id,
+                        t,
+                        &vec![("name".to_string(), Prop::Str("Tweet".to_string()))],
+                    );
+                }
+            }
+        }
+    }
+    g
 }
