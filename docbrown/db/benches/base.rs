f561296ae7be56eafc15a5216212c13c190241d4
Haaroon Yousaf <Haaroon@users.noreply.github.com>
2023-02-27 17:18:09 +0000

    uncomment

--- docbrown/db/benches/base.rs
+++ docbrown/db/benches/base.rs
@@ -6,14 +6,14 @@ use docbrown_db::data;
 mod common;
 
 pub fn base(c: &mut Criterion) {
-    // let mut ingestion_group = c.benchmark_group("ingestion");
-    // ingestion_group.throughput(Throughput::Elements(1));
-    // run_ingestion_benchmarks(&mut ingestion_group, || bootstrap_graph(4, 10_000), None);
-    // ingestion_group.finish();
-    //
-    // let mut analysis_group = c.benchmark_group("analysis");
-    // run_analysis_benchmarks(&mut analysis_group, || data::lotr_graph(4), None);
-    // analysis_group.finish();
+    let mut ingestion_group = c.benchmark_group("ingestion");
+    ingestion_group.throughput(Throughput::Elements(1));
+    run_ingestion_benchmarks(&mut ingestion_group, || bootstrap_graph(4, 10_000), None);
+    ingestion_group.finish();
+
+    let mut analysis_group = c.benchmark_group("analysis");
+    run_analysis_benchmarks(&mut analysis_group, || data::lotr_graph(4), None);
+    analysis_group.finish();
 
     let mut large_group = c.benchmark_group("large");
     run_large_ingestion_benchmarks(&mut large_group, || bootstrap_graph(4, 0));
