744b674a72d4224270bd08b3c237c794b04ecc5a
RachelChan <25484244+rachchan@users.noreply.github.com>
2023-03-10 12:14:03 +0000

    expose algos in python

--- raphtory/src/lib.rs
+++ raphtory/src/lib.rs
@@ -1,7 +1,5 @@
-pub mod wrappers;
-pub mod graph;
-pub mod graph_window;
 pub mod algorithms;
+pub mod graph;
 pub mod graph_gen;
 pub mod graph_loader;
 
@@ -9,11 +7,25 @@ use pyo3::prelude::*;
 
 use crate::wrappers::{Direction, Perspective};
 use crate::graph::Graph;
+pub mod graph_window;
+pub mod wrappers;
+
+use pyo3::prelude::*;
+
+use crate::algorithms::avg_degree;
+use crate::algorithms::clustering_coefficient;
+use crate::algorithms::graph_density;
+use crate::algorithms::max_indegree;
+use crate::algorithms::max_outdegree;
+use crate::algorithms::min_indegree;
+use crate::algorithms::min_outdegree;
 use crate::algorithms::triangle_count;
-use crate::graph_gen::random_attachment;
+
 use crate::graph_gen::ba_preferential_attachment;
 use crate::graph_loader::lotr_graph;
 use crate::graph_loader::twitter_graph;
+use crate::graph_gen::random_attachment;
+
 
 #[pymodule]
 fn raphtory(py: Python<'_>, m: &PyModule) -> PyResult<()> {
@@ -23,6 +35,13 @@ fn raphtory(py: Python<'_>, m: &PyModule) -> PyResult<()> {
 
     let algorithm_module = PyModule::new(py, "algorithms")?;
     algorithm_module.add_function(wrap_pyfunction!(triangle_count, algorithm_module)?)?;
+    algorithm_module.add_function(wrap_pyfunction!(clustering_coefficient, algorithm_module)?)?;
+    algorithm_module.add_function(wrap_pyfunction!(avg_degree, algorithm_module)?)?;
+    algorithm_module.add_function(wrap_pyfunction!(graph_density, algorithm_module)?)?;
+    algorithm_module.add_function(wrap_pyfunction!(max_outdegree, algorithm_module)?)?;
+    algorithm_module.add_function(wrap_pyfunction!(max_indegree, algorithm_module)?)?;
+    algorithm_module.add_function(wrap_pyfunction!(min_outdegree, algorithm_module)?)?;
+    algorithm_module.add_function(wrap_pyfunction!(min_indegree, algorithm_module)?)?;
     m.add_submodule(algorithm_module)?;
 
     let graph_loader_module = PyModule::new(py, "graph_loader")?;
@@ -32,7 +51,10 @@ fn raphtory(py: Python<'_>, m: &PyModule) -> PyResult<()> {
 
     let graph_gen_module = PyModule::new(py, "graph_gen")?;
     graph_gen_module.add_function(wrap_pyfunction!(random_attachment, graph_gen_module)?)?;
-    graph_gen_module.add_function(wrap_pyfunction!(ba_preferential_attachment, graph_gen_module)?)?;
+    graph_gen_module.add_function(wrap_pyfunction!(
+        ba_preferential_attachment,
+        graph_gen_module
+    )?)?;
     m.add_submodule(graph_gen_module)?;
 
     Ok(())
