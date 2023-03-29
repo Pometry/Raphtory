c4309f0b2249dfb94e1dac99f65ffe1f428dadf8
Haaroon Y <Haaroon@users.noreply.github.com>
2023-03-29 15:30:42 +0100

    fix tests (#121)

--- python/src/graph_loader.rs
+++ python/src/graph_loader.rs
@@ -8,14 +8,6 @@ pub(crate) fn lotr_graph(shards: usize) -> Graph {
 }
 
 #[pyfunction]
-#[pyo3(signature = (shards=1))]
-pub(crate) fn twitter_graph(shards: usize) -> Graph {
-    Graph::from_db_graph(docbrown_db::graph_loader::twitter_graph::twitter_graph(
-        shards,
-    ))
-}
-
-#[pyfunction]
 #[pyo3(signature = (shards=1,timeout_seconds=600))]
 pub(crate) fn reddit_hyperlink_graph(shards: usize, timeout_seconds: u64) -> Graph {
     Graph::from_db_graph(docbrown_db::graph_loader::reddit_hyperlinks::reddit_graph(
