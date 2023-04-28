424bcccd4799c854db86fef062c136239353208c
github-action-benchmark <github@users.noreply.github.com>
2023-04-28 16:27:22 +0000

    add Rust Benchmark (cargo) benchmark result for f375af6eed09c4968752c2dc830634224f622404

---
+++ dev/bench/data.js
@@ -0,0 +1,40 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1682699242470,
+  "repoUrl": "https://github.com/Pometry/Raphtory",
+  "entries": {
+    "Rust Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "name": "Pometry",
+            "username": "Pometry"
+          },
+          "committer": {
+            "name": "Pometry",
+            "username": "Pometry"
+          },
+          "id": "f375af6eed09c4968752c2dc830634224f622404",
+          "message": "maybe new benchmark?",
+          "timestamp": "2023-04-26T21:03:50Z",
+          "url": "https://github.com/Pometry/Raphtory/pull/850/commits/f375af6eed09c4968752c2dc830634224f622404"
+        },
+        "date": 1682699241927,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "large/1k fixed edge updates with varying time",
+            "value": 1036182,
+            "range": "± 24881",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "large/1k random edge additions",
+            "value": 1059916,
+            "range": "± 113090",
+            "unit": "ns/iter"
+          }
+        ]
+      }
+    ]
+  }
+}
