7b4d5fa2023be3b342ee49d14840330c24e6d343
Shivam Kapoor <4599890+iamsmkr@users.noreply.github.com>
2023-05-02 16:15:34 +0100

    simplify stable coin impl

--- examples/rust/src/bin/crypto/main.rs
+++ examples/rust/src/bin/crypto/main.rs
@@ -1,15 +1,8 @@
 use raphtory::algorithms::generic_taint::generic_taint;
-use raphtory::core::Prop;
-use raphtory::core::{state, utils};
-use raphtory::db::graph::Graph;
-use raphtory::db::program::{GlobalEvalState, Program};
 use raphtory::db::view_api::*;
-use raphtory::graph_loader::source::csv_loader::CsvLoader;
-use serde::Deserialize;
-use std::path::PathBuf;
-use std::{env, path::Path, time::Instant};
-use raphtory::db::view_api::internal::GraphViewInternalOps;
 use raphtory::graph_loader::example::stable_coins::stable_coin_graph;
+use serde::Deserialize;
+use std::time::Instant;
 
 #[derive(Deserialize, std::fmt::Debug)]
 pub struct StableCoin {
@@ -23,13 +16,19 @@ pub struct StableCoin {
 }
 
 fn main() {
-    let g = stable_coin_graph(Some("/tmp/stablecoin/mydir".to_string()), 1, 600);
+    let g = stable_coin_graph(None, 1);
 
     assert_eq!(g.num_vertices(), 1523333);
     assert_eq!(g.num_edges(), 2814155);
 
     let now = Instant::now();
-    let r = generic_taint(&g, 20, 1651105815, vec!["0xd30b438df65f4f788563b2b3611bd6059bff4ad9"], vec![]);
+    let r = generic_taint(
+        &g,
+        20,
+        1651105815,
+        vec!["0xd30b438df65f4f788563b2b3611bd6059bff4ad9"],
+        vec![],
+    );
     println!("Time taken: {}", now.elapsed().as_secs());
     println!("{:?}", r);
 }
