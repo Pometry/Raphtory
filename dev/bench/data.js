fa32cc699fc164b5d8f5bd6bd9ece6908e44f248
github-action-benchmark <github@users.noreply.github.com>
2023-05-09 09:22:17 +0000

    add Rust Benchmark (cargo) benchmark result for 5e580fd81e12a894ed85c3e1ab37ea6b3a5fa9b3

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1682699242470,
+  "lastUpdate": 1683624137441,
   "repoUrl": "https://github.com/Pometry/Raphtory",
   "entries": {
     "Rust Benchmark": [
@@ -31,6 +31,42 @@ window.BENCHMARK_DATA = {
             "name": "large/1k random edge additions",
             "value": 1059916,
             "range": "± 113090",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "Haaroon@users.noreply.github.com",
+            "name": "Haaroon Y",
+            "username": "Haaroon"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "5e580fd81e12a894ed85c3e1ab37ea6b3a5fa9b3",
+          "message": "Save new PRs into Master as Benchmark into a stats page (#850)\n\n* maybe new benchmark?\r\n\r\n* remove one bit\r\n\r\n* git ignored blocked me :<\r\n\r\n* add comment, add storage only on non master comit, add link to readme\r\n\r\n* bug\r\n\r\n* dont try save\r\n\r\n* dont try save",
+          "timestamp": "2023-05-09T10:16:54+01:00",
+          "tree_id": "2de8fdb80dcfbe52003fb20bcefa7d2dfaff1654",
+          "url": "https://github.com/Pometry/Raphtory/commit/5e580fd81e12a894ed85c3e1ab37ea6b3a5fa9b3"
+        },
+        "date": 1683624135903,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "large/1k fixed edge updates with varying time",
+            "value": 321649,
+            "range": "± 21160",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "large/1k random edge additions",
+            "value": 797919,
+            "range": "± 168428",
             "unit": "ns/iter"
           }
         ]
