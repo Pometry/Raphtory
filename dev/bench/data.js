c3f061065acba8979bb0155675130a7a7cf6be3d
github-action-benchmark <github@users.noreply.github.com>
2023-05-09 10:19:40 +0000

    add Rust Benchmark (cargo) benchmark result for b25e762960f0eef88b4604520a7a5bdc97398803

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1683624137441,
+  "lastUpdate": 1683627580615,
   "repoUrl": "https://github.com/Pometry/Raphtory",
   "entries": {
     "Rust Benchmark": [
@@ -67,6 +67,42 @@ window.BENCHMARK_DATA = {
             "name": "large/1k random edge additions",
             "value": 797919,
             "range": "± 168428",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "Haaroon@users.noreply.github.com",
+            "name": "Haaroon Y",
+            "username": "Haaroon"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "b25e762960f0eef88b4604520a7a5bdc97398803",
+          "message": "More Code Coverage (#847)\n\n* dont use pointer in loader\r\n\r\n* some extra code coverages\r\n\r\n* fix missing module, display tests\r\n\r\n* fix python graph loader\r\n\r\n* my family needs me\r\n\r\n* Update README.md\r\n\r\n* fix test, remove python from default, clean up warnings\r\n\r\n* fix url in pyproject\r\n\r\n* add retry to benchmark step\r\n\r\n* Update README.md\r\n\r\n* fix test issues",
+          "timestamp": "2023-05-09T11:15:13+01:00",
+          "tree_id": "83ea074e7cd2224a7d1ae36e137218240c3feacd",
+          "url": "https://github.com/Pometry/Raphtory/commit/b25e762960f0eef88b4604520a7a5bdc97398803"
+        },
+        "date": 1683627579928,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "large/1k fixed edge updates with varying time",
+            "value": 331068,
+            "range": "± 1691",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "large/1k random edge additions",
+            "value": 424743,
+            "range": "± 26817",
             "unit": "ns/iter"
           }
         ]
