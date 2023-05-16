dc8c387a64d2598ad06921f09a9e3568593f189a
Fabian Murariu <2404621+fabianmurariu@users.noreply.github.com>
2023-05-16 22:37:54 +0100

    Moved Fabians flux capacitor work over to the main repo (#896)
    * Moved Fabians flux capacitor work over to the main repo
    
    * fixes post rebase
    
    ---------
    
    Co-authored-by: miratepuffin <b.a.steer@qmul.ac.uk>

---
+++ flux-capacitor/src/data.rs
@@ -0,0 +1,72 @@
+use std::sync::Arc;
+
+use raphtory::{
+    core::Prop,
+    db::{graph::Graph, view_api::GraphViewOps},
+};
+use raphtory_io::graph_loader::source::csv_loader::CsvLoader;
+use serde::Deserialize;
+
+pub(crate) type TGraph = Graph;
+
+pub(crate) struct Metadata<G: GraphViewOps> {
+    parent_graph: Arc<G>,
+}
+#[derive(Deserialize, std::fmt::Debug)]
+pub struct Lotr {
+    src_id: String,
+    dst_id: String,
+    time: i64,
+}
+
+impl Metadata<TGraph> {
+    pub(crate) fn graph(&self) -> &TGraph {
+        &self.parent_graph
+    }
+
+    pub(crate) fn lotr() -> Self {
+        let graph = TGraph::new(4);
+
+        CsvLoader::new("/tmp/lotr.csv")
+            .load_into_graph(&graph, |lotr: Lotr, g: &Graph| {
+                g.add_vertex(
+                    lotr.time,
+                    lotr.src_id.clone(),
+                    &vec![("type".to_string(), Prop::Str("Character".to_string()))],
+                )
+                .expect("Failed to add vertex");
+
+                g.add_vertex(
+                    lotr.time,
+                    lotr.dst_id.clone(),
+                    &vec![("type".to_string(), Prop::Str("Character".to_string()))],
+                )
+                .expect("Failed to add vertex");
+
+                g.add_edge(
+                    lotr.time,
+                    lotr.src_id.clone(),
+                    lotr.dst_id.clone(),
+                    &vec![(
+                        "type".to_string(),
+                        Prop::Str("Character Co-occurrence".to_string()),
+                    )],
+                    None,
+                )
+                .expect("Failed to add edge");
+            })
+            .expect("Failed to load csv into graph");
+
+        Self {
+            parent_graph: Arc::new(graph),
+        }
+    }
+}
+
+impl Default for Metadata<TGraph> {
+    fn default() -> Self {
+        Self {
+            parent_graph: Arc::new(TGraph::new(4)),
+        }
+    }
+}
