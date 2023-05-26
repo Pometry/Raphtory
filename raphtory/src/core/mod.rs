fa538349143ebf0a44623c86957cca4a033d03f4
ljeub-pometry <97447091+ljeub-pometry@users.noreply.github.com>
2023-05-26 14:01:26 +0200

    Freature/property access improvements (#979)
    * add conversion methods for different property types
    
    * easy unwrapping of property values in rust

--- raphtory/src/core/mod.rs
+++ raphtory/src/core/mod.rs
@@ -79,6 +79,182 @@ pub enum Prop {
     Graph(Graph),
 }
 
+pub trait PropUnwrap: Sized {
+    fn into_str(self) -> Option<String>;
+    fn unwrap_str(self) -> String {
+        self.into_str().unwrap()
+    }
+
+    fn into_i32(self) -> Option<i32>;
+    fn unwrap_i32(self) -> i32 {
+        self.into_i32().unwrap()
+    }
+
+    fn into_i64(self) -> Option<i64>;
+    fn unwrap_i64(self) -> i64 {
+        self.into_i64().unwrap()
+    }
+
+    fn into_u32(self) -> Option<u32>;
+    fn unwrap_u32(self) -> u32 {
+        self.into_u32().unwrap()
+    }
+
+    fn into_u64(self) -> Option<u64>;
+    fn unwrap_u64(self) -> u64 {
+        self.into_u64().unwrap()
+    }
+
+    fn into_f32(self) -> Option<f32>;
+    fn unwrap_f32(self) -> f32 {
+        self.into_f32().unwrap()
+    }
+
+    fn into_f64(self) -> Option<f64>;
+    fn unwrap_f64(self) -> f64 {
+        self.into_f64().unwrap()
+    }
+
+    fn into_bool(self) -> Option<bool>;
+    fn unwrap_bool(self) -> bool {
+        self.into_bool().unwrap()
+    }
+
+    fn into_dtime(self) -> Option<NaiveDateTime>;
+    fn unwrap_dtime(self) -> NaiveDateTime {
+        self.into_dtime().unwrap()
+    }
+
+    fn into_graph(self) -> Option<Graph>;
+    fn unwrap_graph(self) -> Graph {
+        self.into_graph().unwrap()
+    }
+}
+
+impl<P: PropUnwrap> PropUnwrap for Option<P> {
+    fn into_str(self) -> Option<String> {
+        self.and_then(|p| p.into_str())
+    }
+
+    fn into_i32(self) -> Option<i32> {
+        self.and_then(|p| p.into_i32())
+    }
+
+    fn into_i64(self) -> Option<i64> {
+        self.and_then(|p| p.into_i64())
+    }
+
+    fn into_u32(self) -> Option<u32> {
+        self.and_then(|p| p.into_u32())
+    }
+
+    fn into_u64(self) -> Option<u64> {
+        self.and_then(|p| p.into_u64())
+    }
+
+    fn into_f32(self) -> Option<f32> {
+        self.and_then(|p| p.into_f32())
+    }
+
+    fn into_f64(self) -> Option<f64> {
+        self.and_then(|p| p.into_f64())
+    }
+
+    fn into_bool(self) -> Option<bool> {
+        self.and_then(|p| p.into_bool())
+    }
+
+    fn into_dtime(self) -> Option<NaiveDateTime> {
+        self.and_then(|p| p.into_dtime())
+    }
+
+    fn into_graph(self) -> Option<Graph> {
+        self.and_then(|p| p.into_graph())
+    }
+}
+
+impl PropUnwrap for Prop {
+    fn into_str(self) -> Option<String> {
+        if let Prop::Str(s) = self {
+            Some(s)
+        } else {
+            None
+        }
+    }
+
+    fn into_i32(self) -> Option<i32> {
+        if let Prop::I32(v) = self {
+            Some(v)
+        } else {
+            None
+        }
+    }
+
+    fn into_i64(self) -> Option<i64> {
+        if let Prop::I64(v) = self {
+            Some(v)
+        } else {
+            None
+        }
+    }
+
+    fn into_u32(self) -> Option<u32> {
+        if let Prop::U32(v) = self {
+            Some(v)
+        } else {
+            None
+        }
+    }
+
+    fn into_u64(self) -> Option<u64> {
+        if let Prop::U64(v) = self {
+            Some(v)
+        } else {
+            None
+        }
+    }
+
+    fn into_f32(self) -> Option<f32> {
+        if let Prop::F32(v) = self {
+            Some(v)
+        } else {
+            None
+        }
+    }
+
+    fn into_f64(self) -> Option<f64> {
+        if let Prop::F64(v) = self {
+            Some(v)
+        } else {
+            None
+        }
+    }
+
+    fn into_bool(self) -> Option<bool> {
+        if let Prop::Bool(v) = self {
+            Some(v)
+        } else {
+            None
+        }
+    }
+
+    fn into_dtime(self) -> Option<NaiveDateTime> {
+        if let Prop::DTime(v) = self {
+            Some(v)
+        } else {
+            None
+        }
+    }
+
+    fn into_graph(self) -> Option<Graph> {
+        if let Prop::Graph(g) = self {
+            Some(g)
+        } else {
+            None
+        }
+    }
+}
+
 impl fmt::Display for Prop {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match self {
