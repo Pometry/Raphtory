d7dfc32d91fb1fce14ffe12eef344b3e7a5f837c
Haaroon Y <Haaroon@users.noreply.github.com>
2023-05-30 15:40:04 +0100

    Benchmark Suite v1 (#977)
    * memmy benchy
    
    * graph_Tool
    
    * networkx
    
    * cozo start, annoying
    
    * checkin
    
    * raphtory bench
    
    * kuzu bench
    
    * neo4j bench
    
    * fix out neighbours no ids
    
    * return all results
    
    * memgraph bench
    
    * add driver
    
    * add driver
    
    * use complex rel file
    
    * set shards to cpu count
    
    * fix n x bench
    
    * fix n x bench
    
    * fix n x bench
    
    * add cozo bench
    
    * add names
    
    * add pp result when complete
    
    * allow running all benchmarks
    
    * add download and unzip data files
    
    * fix graph tool and point to correct file for all benches
    
    * add close() fn to all benches to release graph resources and delete graph object
    
    * add arg selection to driver, add docker option but no implementation, fix cozo bench dir issue
    
    * initial docker test but its fialing on some xcode issue
    
    * benchmark with cozo and docker complete
    
    * better docker bench
    
    * networkx with docker
    
    * raphtory docker
    
    * i dont know why my neo bench gets stuck
    
    * add kuzu docker
    
    * graph tool potentially done?
    
    * graph tool done
    
    * memgraph done, use internal csv loader instead
    
    * fix memgraph version
    
    * fix neo benchmark
    
    * benchmark complete
    
    * enable docker by default, add start of readme
    
    * everything works except memgraph :<
    
    * memgraph now works,
    
    * benchmark complete
    
    * move folder
    
    * add better template
    
    * cleanup soft
    
    * fix outside name
    
    * dont fail if not locally installed
    
    * fix cozo import too quick
    
    * run all
    
    * fix networkx hang
    
    * remove ugly not found, maybe fix memgraph?
    
    * use z mode for volume
    
    * remove tqdm in raphtory
    
    * Fixed call to pagerank
    
    ---------
    
    Co-authored-by: Ben Steer <b.a.steer@qmul.ac.uk>

---
+++ benchmark/benchmark_driver.py
@@ -0,0 +1,190 @@
+import argparse
+import pandas as pd
+from benchmark_imports import *
+import time
+import shutil
+import gzip
+import requests
+import os
+from io import StringIO
+
+fns = ['setup', 'degree', 'out_neighbours', 'page_rank', 'connected_components']
+
+
+def process_arguments():
+    parser = argparse.ArgumentParser(description='benchmark args')
+    parser.add_argument('--docker', action=argparse.BooleanOptionalAction,
+                        help='Launch with docker containers, --no-docker to run locally', default=True)
+    parser.add_argument('-b', '--bench', type=str, help="""
+    Run specific benchmark,
+    default: Goes to Menu (if docker runs all),
+    all: Run All
+    download: Download Data
+    r: Run Raphtory Benchmark
+    gt: Run GraphTool Benchmark
+    k: Run Kuzu Benchmark
+    nx: Run NetworkX Benchmark
+    neo: Run Neo4j Benchmark
+    mem: Run Memgraph Benchmark
+    cozo: Run CozoDB Benchmark
+    exit: Exit
+    """, default='menu')
+    return parser.parse_args()
+
+
+# Display menu and get user's choice
+def display_menu():
+    print("Benchmark Options:")
+    print("all: Run All")
+    print("download: Download Data")
+    print("r: Run Raphtory Benchmark")
+    print("gt: Run GraphTool Benchmark")
+    print("k: Run Kuzu Benchmark")
+    print("nx: Run NetworkX Benchmark")
+    print("neo: Run Neo4j Benchmark")
+    print("mem: Run Memgraph Benchmark")
+    print("cozo: Run CozoDB Benchmark")
+    print("exit: Exit")
+    choice = int(input("Enter your choice: "))
+    return choice
+
+
+def setup():
+    return {
+        'all': 'ALL',
+        'download': 'DOWNLOAD',
+        'r': RaphtoryBench,
+        'gt': GraphToolBench,
+        'k': KuzuBench,
+        'nx': NetworkXBench,
+        'neo': Neo4jBench,
+        'mem': MemgraphBench,
+        'cozo': CozoDBBench
+    }
+
+
+def run_benchmark(choice, docker=False):
+    benchmarks_to_run = []
+    if choice.lower() == 'all':
+        for key in setup().keys():
+            if key == 'menu' or key == 'all' or key == 'download':
+                continue
+            benchmarks_to_run.append(key)
+    elif choice == 'download' or choice == 'menu':
+        return
+    elif choice in setup().keys():
+        benchmarks_to_run.append(choice)
+
+    results = {}
+    print("Running benchmarks: " + str(benchmarks_to_run))
+    for key in benchmarks_to_run:
+        driver = setup()[key]
+        if docker:
+            print("** Running dockerized benchmark " + str(key) + "...")
+            print("Starting docker container...")
+            exit_code, logs = driver.start_docker()
+            print("Docker container exited with code " + str(exit_code))
+            print("Logs: " + logs)
+            results[driver.name()] = logs
+        else:
+            print("** Running for " + driver.name() + "...")
+            times = ''
+            fn_header = ''
+            for fn in fns:
+                print("** Running " + fn + "...")
+                start_time = time.time()
+                getattr(driver, fn)()
+                end_time = time.time()
+                print(fn + " time: " + str(end_time - start_time))
+                fn_header += fn + ','
+                times += str(end_time - start_time) + ','
+            fn_header = fn_header[:-1]
+            times = times[:-1]
+            results[driver.name()] = fn_header + '\n' + times
+            pd.DataFrame([times.split(',')], columns=fns).to_csv('/tmp/bench-'+driver.name()+'-'+str(time.time())+'.csv')
+    return results
+
+
+def print_table(data):
+    if len(data) == 0:
+        return
+    _data = {}
+    for key, value in data.items():
+        _data[key] = pd.read_csv(StringIO(value))
+    merged_df = pd.concat([df.assign(key=key) for key, df in _data.items()])
+    col = merged_df.pop("key")
+    if 'Unnamed: 0' in merged_df.columns:
+        merged_df.drop('Unnamed: 0', axis=1, inplace=True)
+    merged_df.insert(0, 'System', col)
+    print(merged_df.to_string(index=False, justify='left'))
+
+
+def dl_file(url, path):
+    # Download the first file
+    print("Downloading " + url + "...")
+    r = requests.get(url, stream=True)
+    if r.status_code == 200:
+        with open(path, 'wb') as f:
+            f.write(r.raw.read())
+    else:
+        print("Error downloading data")
+        return
+
+
+def create_directory(directory_path):
+    if not os.path.exists(directory_path):
+        os.makedirs(directory_path)
+        print(f"Directory '{directory_path}' created successfully.")
+    else:
+        print(f"Directory '{directory_path}' already exists.")
+
+
+def download_data():
+    # Download the data
+    print("Downloading data...")
+    urls = {
+        'simple-profiles.csv.gz': 'https://raw.githubusercontent.com/Raphtory/Data/main/simple-profiles.csv.gz',
+        'simple-relationships.csv.gz': 'https://media.githubusercontent.com/media/Raphtory/Data/main/simple-relationships.csv.gz'
+    }
+
+    # make the data directory
+    create_directory('data')
+
+    for name, url in urls.items():
+        dl_file(url, 'data/' + name)
+
+    # Unzip the files
+    for name in urls.keys():
+        print("Unzipping " + name + "...")
+        with gzip.open('data/' + name, 'rb') as f_in:
+            with open('data/' + name[:-3], 'wb') as f_out:
+                shutil.copyfileobj(f_in, f_out)
+        print("Done unzipping " + name + "...")
+        os.remove('data/' + name)
+
+    # return the file paths
+    return 'data/simple-profiles.csv', 'data/simple-relationships.csv'
+
+
+def main(docker, choice):
+    print("Welcome to the Raphtory Benchmarking Tool")
+    results = {}
+    try:
+        if choice == 'menu':
+            choice = display_menu()
+        if choice == 'download' or choice == 1:
+            download_data()
+        elif choice == 'exit' or choice not in setup():
+            print(str(choice) + ". Exiting...")
+        else:
+            results = run_benchmark(choice, docker)
+    except Exception as ex:
+        print("Error: " + str(ex))
+        raise ex
+    finally:
+        print_table(results)
+
+
+if __name__ == "__main__":
+    args = process_arguments()
+    main(args.docker, args.bench)
