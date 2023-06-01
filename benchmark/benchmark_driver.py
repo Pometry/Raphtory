04136477015c7633952dfd9d7f521529948ae45a
Haaroon Yousaf <Haaroon@users.noreply.github.com>
2023-06-01 19:45:52 +0100

    nearly there but its mad slow

--- benchmark/benchmark_driver.py
+++ benchmark/benchmark_driver.py
@@ -99,7 +99,7 @@ def run_benchmark(choice, docker=False):
                 fn_header += fn + ','
                 if driver.name() == 'Neo4j' and fn == 'setup':
                     # take away 15 seconds for the sleep time when restarting the database
-                    end_time = end_time - 15
+                    end_time = end_time - 30
                 times += str(end_time - start_time) + ','
             fn_header = fn_header[:-1]
             times = times[:-1]
