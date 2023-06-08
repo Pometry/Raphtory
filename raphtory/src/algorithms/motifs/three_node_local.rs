a83763242de55dd82a3d0fadf4c9f83449d014d6
narnolddd <n.a.arnold@qmul.ac.uk>
2023-06-08 15:53:20 +0100

    motifs algorithm works!!!!

--- raphtory/src/algorithms/motifs/three_node_local.rs
+++ raphtory/src/algorithms/motifs/three_node_local.rs
@@ -62,7 +62,6 @@ pub fn triangle_motif_count<G:GraphViewOps>(graph:&G, delta:i64) -> HashMap<u64,
     for u in graph.vertices() {
         let uid = u.id();
         for v in u.neighbours().iter().filter(|x| x.id() > uid) {
-            let mut nb_ct=0;
             for nb in u.neighbours().iter().filter(|x| x.id() > v.id()) {
                 let mut tri_edges : Vec<TriangleEdge> = Vec::new();
                 let out = graph.edge(v.id(), nb.id(), None);
@@ -71,20 +70,20 @@ pub fn triangle_motif_count<G:GraphViewOps>(graph:&G, delta:i64) -> HashMap<u64,
                 match (out, inc) {
                     (Some(o),Some(i)) => {
                         tri_edges.append(&mut o.explode()
-                        .map(|e| new_triangle_edge(false, 1, nb_ct, 1, e.time().unwrap()))
+                        .map(|e| new_triangle_edge(false, 1, 0, 1, e.time().unwrap()))
                         .collect::<Vec<TriangleEdge>>());
                         tri_edges.append(&mut i.explode()
-                        .map(|e| new_triangle_edge(false, 1, nb_ct, 0, e.time().unwrap()))
+                        .map(|e| new_triangle_edge(false, 1, 0, 0, e.time().unwrap()))
                         .collect::<Vec<TriangleEdge>>());
                     }
                     (Some(o), None) => {
                         tri_edges.append(&mut o.explode()
-                        .map(|e| new_triangle_edge(false, 1, nb_ct, 1, e.time().unwrap()))
+                        .map(|e| new_triangle_edge(false, 1, 0, 1, e.time().unwrap()))
                         .collect::<Vec<TriangleEdge>>());
                     }
                     (None, Some(i)) => {
                         tri_edges.append(&mut i.explode()
-                        .map(|e| new_triangle_edge(false, 1, nb_ct, 0, e.time().unwrap()))
+                        .map(|e| new_triangle_edge(false, 1, 0, 0, e.time().unwrap()))
                         .collect::<Vec<TriangleEdge>>());
                     }
                     (None, None) => {continue;}
@@ -95,32 +94,31 @@ pub fn triangle_motif_count<G:GraphViewOps>(graph:&G, delta:i64) -> HashMap<u64,
                     match (uout, uin) {
                         (Some(o), Some(i)) => {
                         tri_edges.append(&mut o.explode()
-                        .map(|e| new_triangle_edge(false, 0, nb_ct, 1, e.time().unwrap()))
+                        .map(|e| new_triangle_edge(false, 0, 0, 1, e.time().unwrap()))
                         .collect::<Vec<TriangleEdge>>());
                         tri_edges.append(&mut i.explode()
-                        .map(|e| new_triangle_edge(false, 0, nb_ct, 0, e.time().unwrap()))
+                        .map(|e| new_triangle_edge(false, 0, 0, 0, e.time().unwrap()))
                         .collect::<Vec<TriangleEdge>>());
                         }
                         (Some(o), None) => {
                             tri_edges.append(&mut o.explode()
-                            .map(|e| new_triangle_edge(false, 0, nb_ct, 1, e.time().unwrap()))
+                            .map(|e| new_triangle_edge(false, 0, 0, 1, e.time().unwrap()))
                             .collect::<Vec<TriangleEdge>>());
                         }
                         (None, Some(i)) => {
                             tri_edges.append(&mut i.explode()
-                            .map(|e| new_triangle_edge(false, 0, nb_ct, 0, e.time().unwrap()))
+                            .map(|e| new_triangle_edge(false, 0, 0, 0, e.time().unwrap()))
                             .collect::<Vec<TriangleEdge>>());
                         }
                         (None, None) => {continue;}
                     }
-                    nb_ct+=1;
                     // found triangle at this point!!
-                    let mut u_to_v = match graph.edge(uid,v.id(),None)  {
+                    let u_to_v = match graph.edge(uid,v.id(),None)  {
                         Some (edge) => { let r = edge.explode().map(|e| new_triangle_edge(true, 1, 0, 1, e.time().unwrap())).collect::<Vec<TriangleEdge>>();
                         r.into_iter()},
                         None => vec![].into_iter()
                     }; 
-                    let mut v_to_u = match graph.edge(v.id(),uid,None)  {
+                    let v_to_u = match graph.edge(v.id(),uid,None)  {
                         Some (edge) => { let r = edge.explode().map(|e| new_triangle_edge(true, 0, 0, 0, e.time().unwrap())).collect::<Vec<TriangleEdge>>();
                         r.into_iter()},
                         None => vec![].into_iter()
@@ -129,7 +127,7 @@ pub fn triangle_motif_count<G:GraphViewOps>(graph:&G, delta:i64) -> HashMap<u64,
                     tri_edges.append(&mut v_to_u.collect::<Vec<TriangleEdge>>());
                     tri_edges.sort_by_key(|e| e.time);
 
-                    let mut tri_count = init_tri_count(nb_ct);
+                    let mut tri_count = init_tri_count(1);
                     tri_count.execute(&tri_edges, delta);
                     let tmp_counts = tri_count.return_counts().iter();
                     for id in [uid, v.id(), nb.id()] {
@@ -151,9 +149,9 @@ pub fn all_motifs_count<G:GraphViewOps>(graph:&G, delta:i64) -> HashMap<u64,Vec<
     let mut counts = triangle_motif_count(graph, delta);
     for v in graph.vertices() {
         let vid = v.id();
-        let mut two_nodes = twonode_motif_count(graph, vid, delta).to_vec();
-        let mut tmp_stars = star_motif_count(graph, vid, delta);
-        let mut stars : Vec<usize> = tmp_stars
+        let two_nodes = twonode_motif_count(graph, vid, delta).to_vec();
+        let tmp_stars = star_motif_count(graph, vid, delta);
+        let stars : Vec<usize> = tmp_stars
         .iter()
         .zip(two_nodes.iter().cycle().take(24))
         .map(|(&x1,&x2)| x1 - x2)
@@ -170,7 +168,7 @@ pub fn all_motifs_count<G:GraphViewOps>(graph:&G, delta:i64) -> HashMap<u64,Vec<
 pub fn global_motifs_count<G:GraphViewOps>(graph:&G, delta:i64) -> Vec<usize> {
     let counts = all_motifs_count(graph, delta);
     let mut tmp_counts = counts.values().fold(vec![0;40], |acc, x| acc.iter().zip(x.iter()).map(|(x1,x2)| x1 + x2).collect());
-    for ind in 31..40 {
+    for ind in 32..40 {
         tmp_counts[ind] = tmp_counts[ind]/3;
     }
     tmp_counts
