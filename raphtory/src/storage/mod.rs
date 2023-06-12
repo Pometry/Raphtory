9e795110a654d14c6259db95cf30b53762798425
Fabian Murariu <murariu.fabian@gmail.com>
2023-06-12 11:02:16 +0100

    added iterator that uses Rc to read all the values

--- raphtory/src/storage/mod.rs
+++ raphtory/src/storage/mod.rs
@@ -1,35 +1,32 @@
 use std::{
+    fmt::Debug,
     ops::Deref,
-    sync::{
-        atomic::{AtomicUsize, Ordering},
-        Arc,
-    },
+    rc::Rc,
+    sync::atomic::{AtomicUsize, Ordering},
 };
 
-// use dashmap::DashMap;
-use lock_api::{ArcRwLockReadGuard, RawRwLock, RwLock};
+use lock_api::{RawRwLock, RwLock};
 
-#[derive(Clone, Debug)]
-pub(crate) struct LockVec<T, L: RawRwLock> {
-    data: Arc<RwLock<L, Vec<T>>>,
+#[derive(Debug)]
+pub struct LockVec<T, L: RawRwLock> {
+    data: RwLock<L, Vec<T>>,
 }
 
-// new for LockVec
 impl<T, L: RawRwLock> LockVec<T, L> {
-    pub(crate) fn new() -> Self {
+    pub fn new() -> Self {
         Self {
-            data: Arc::new(RwLock::new(Vec::new())),
+            data: RwLock::new(Vec::new()),
         }
     }
 }
 
-pub(crate) struct RawStorage<T, L: RawRwLock, const N: usize> {
-    data: [LockVec<T, L>; N],
+pub struct RawStorage<T, L: RawRwLock, const N: usize> {
+    data: Box<[LockVec<T, L>; N]>,
     len: AtomicUsize,
 }
 
 impl<T: std::fmt::Debug + Default, L: RawRwLock, const N: usize> RawStorage<T, L, N> {
-    pub(crate) fn new() -> Self {
+    pub fn new() -> Self {
         let mut data = Vec::with_capacity(N);
         for _ in 0..N {
             data.push(LockVec::new());
@@ -51,7 +48,7 @@ impl<T: std::fmt::Debug + Default, L: RawRwLock, const N: usize> RawStorage<T, L
         (bucket, offset)
     }
 
-    pub(crate) fn push(&self, value: T) {
+    pub fn push(&self, value: T) {
         let index = self.len.fetch_add(1, Ordering::SeqCst);
         let (bucket, offset) = self.resolve(index);
         let mut vec = self.data[bucket].data.write();
@@ -59,44 +56,42 @@ impl<T: std::fmt::Debug + Default, L: RawRwLock, const N: usize> RawStorage<T, L
         vec.insert(offset, value);
     }
 
-    pub(crate) fn entry(&self, index: usize) -> Entry<'_, T, L> {
+    pub fn entry(&self, index: usize) -> Entry<'_, T, L> {
         let (bucket, offset) = self.resolve(index);
         let guard: lock_api::RwLockReadGuard<'_, L, Vec<T>> = self.data[bucket].data.read();
         Entry { i: offset, guard }
     }
 
-    pub(crate) fn len(&self) -> usize {
+    pub fn len(&self) -> usize {
         self.len.load(Ordering::SeqCst)
     }
 
-    pub(crate) fn items<'a>(&'a self) -> IterableGuards<'a, T, L>{
+    pub fn items<'a>(&'a self) -> IterableGuards<'a, T, L> {
         let guards = self.data.iter().map(|vec| vec.data.read()).collect();
         IterableGuards { guards }
     }
-}
 
-pub(crate) struct IterableGuard<'a, T: 'static, L: RawRwLock> {
-    guard: lock_api::RwLockReadGuard<'a ,L, Vec<T>>,
-}
-
-impl<'a, T: 'static, L: RawRwLock> IterableGuard<'a, T, L> {
-    fn iter(&'a self) -> std::slice::Iter<'a, T> {
-        self.guard.iter()
+    pub fn iter2<'a>(&'a self) -> Iter<'a, T, L, N> {
+        Iter {
+            raw: self,
+            i: 0,
+            current: None,
+        }
     }
 }
 
-pub(crate) struct IterableGuards<'a, T: 'static, L: RawRwLock> {
+pub struct IterableGuards<'a, T: 'static, L: RawRwLock> {
     guards: Vec<lock_api::RwLockReadGuard<'a, L, Vec<T>>>,
 }
 
 impl<'a, T: 'static, L: RawRwLock> IterableGuards<'a, T, L> {
-    fn iter(&'a self) -> Box<dyn Iterator<Item = &'a T> + 'a> {
+    pub fn iter(&'a self) -> Box<dyn Iterator<Item = &'a T> + 'a> {
         let iter = self.guards.iter().flat_map(|guard| guard.iter());
         Box::new(iter)
     }
 }
 
-pub(crate) struct Entry<'a, T: 'static, L: RawRwLock> {
+pub struct Entry<'a, T: 'static, L: RawRwLock> {
     i: usize,
     guard: lock_api::RwLockReadGuard<'a, L, Vec<T>>,
 }
@@ -109,10 +104,79 @@ impl<'a, T, L: lock_api::RawRwLock> Deref for Entry<'a, T, L> {
     }
 }
 
+type GuardIter<'a, T, L> = (
+    Rc<lock_api::RwLockReadGuard<'a, L, Vec<T>>>,
+    std::slice::Iter<'a, T>,
+);
+
+pub struct Iter<'a, T, L: lock_api::RawRwLock, const N: usize> {
+    raw: &'a RawStorage<T, L, N>,
+    i: usize,
+    current: Option<GuardIter<'a, T, L>>,
+}
+
+pub struct RefT<'a, T, L: lock_api::RawRwLock, const N: usize> {
+    _guard: Rc<lock_api::RwLockReadGuard<'a, L, Vec<T>>>,
+    t: &'a T,
+}
+
+impl<'a, T, L: lock_api::RawRwLock, const N: usize> Deref for RefT<'a, T, L, N> {
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
+        self.t
+    }
+}
+
+// simple impl for RefT that returns &T in the value function
+impl<'a, T, L: lock_api::RawRwLock, const N: usize> RefT<'a, T, L, N> {
+    pub fn value(&self) -> &T {
+        self.t
+    }
+}
+
+/// # Safety
+///
+/// Requires that you ensure the reference does not become invalid.
+/// The object has to outlive the reference.
+pub unsafe fn change_lifetime_const<'a, 'b, T>(x: &'a T) -> &'b T {
+    &*(x as *const T)
+}
+
+impl<'a, T: Debug + Default, L: lock_api::RawRwLock, const N: usize> Iterator
+    for Iter<'a, T, L, N>
+{
+    type Item = RefT<'a, T, L, N>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        loop {
+            if let Some(current) = self.current.as_mut() {
+                if let Some(t) = current.1.next() {
+                    let guard = current.0.clone();
+                    return Some(RefT { _guard: guard, t });
+                }
+            }
+
+            if self.i >= N {
+                return None;
+            }
+
+            let guard = self.raw.data[self.i].data.read();
+
+            let raw = unsafe { change_lifetime_const(&*guard) };
+
+            let iter = raw.iter();
+
+            self.current = Some((Rc::new(guard), iter));
+
+            self.i += 1;
+        }
+    }
+}
+
 #[cfg(test)]
 mod test {
-    use dashmap::DashMap;
-    use lock_api::{RawRwLock, RwLock};
+    use lock_api::RawRwLock;
 
     use super::RawStorage;
 
@@ -160,6 +224,11 @@ mod test {
         let actual = items.iter().map(|s| s.to_owned()).collect::<Vec<_>>();
 
         assert_eq!(actual, vec!["0", "2", "4", "1", "3"]);
-    }
 
+        let items_iter = storage.iter2();
+
+        let actual = items_iter.map(|s| s.to_owned()).collect::<Vec<_>>();
+
+        assert_eq!(actual, vec!["0", "2", "4", "1", "3"]);
+    }
 }
