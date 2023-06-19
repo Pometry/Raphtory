1450a4193345ef05a736099cad61f804493b6979
Louis Chan <louisch@fastmail.com>
2023-06-19 15:08:46 +0800

    Add new filter for filtering properties in GraphQL

--- raphtory-graphql/src/model/filters/nodefilter.rs
+++ raphtory-graphql/src/model/filters/nodefilter.rs
@@ -3,6 +3,8 @@ use raphtory::db::vertex::VertexView;
 use raphtory::db::view_api::VertexViewOps;
 use crate::model::graph::graph::DynamicGraph;
 use crate::model::graph::node::Node;
+use crate::model::filters::primitives::{NumberFilter, StringFilter};
+use crate::model::filters::property::PropertyHasFilter;
 use dynamic_graphql::{InputObject};
 use dynamic_graphql::internal::{FromValue, InputTypeName, InputValueResult, Register, TypeName};
 use raphtory::core::Prop;
@@ -14,52 +16,21 @@ pub struct NodeFilter {
     node_type:Option<StringFilter>,
     in_degree: Option<NumberFilter>,
     out_degree: Option<NumberFilter>,
-}
-
-#[derive(InputObject)]
-pub struct StringFilter {
-    eq: Option<String>,
-    ne: Option<String>,
-}
-
-#[derive(InputObject)]
-pub struct NumberFilter {
-    gt: Option<usize>,
-    lt: Option<usize>,
-    eq: Option<usize>,
-    ne: Option<usize>,
-    gte: Option<usize>,
-    lte: Option<usize>,
+    property_has: Option<PropertyHasFilter>,
 }
 
 impl NodeFilter {
     pub(crate) fn matches(&self, node: &Node) -> bool {
         if let Some(name_filter) = &self.name {
-            if let Some(eq) = &name_filter.eq {
-                if node.vv.name() != *eq {
-                    return false;
-                }
-            }
-
-            if let Some(ne) = &name_filter.ne {
-                if node.vv.name() == *ne {
-                    return false;
-                }
+            if !name_filter.matches(&node.vv.name()) {
+                return false;
             }
         }
 
         if let Some(type_filter) = &self.node_type {
             let node_type= node.vv.property("type".to_string(),true).unwrap_or(Prop::Str("NONE".to_string())).to_string();
-            if let Some(eq) = &type_filter.eq {
-                if node_type != *eq {
-                    return false;
-                }
-            }
-
-            if let Some(ne) = &type_filter.ne {
-                if node_type == *ne {
-                    return false;
-                }
+            if !type_filter.matches(&node_type) {
+                return false;
             }
         }
 
@@ -75,44 +46,8 @@ impl NodeFilter {
             }
         }
 
-        true
-    }
-}
-
-impl NumberFilter {
-    fn matches(&self, value: usize) -> bool {
-        if let Some(gt) = self.gt {
-            if value <= gt {
-                return false;
-            }
-        }
-
-        if let Some(lt) = self.lt {
-            if value >= lt {
-                return false;
-            }
-        }
-
-        if let Some(eq) = self.eq {
-            if value != eq {
-                return false;
-            }
-        }
-
-        if let Some(ne) = self.ne {
-            if value == ne {
-                return false;
-            }
-        }
-
-        if let Some(gte) = self.gte {
-            if value < gte {
-                return false;
-            }
-        }
-
-        if let Some(lte) = self.lte {
-            if value > lte {
+        if let Some(property_has_filter) = &self.property_has {
+            if !property_has_filter.matches(&node) {
                 return false;
             }
         }
