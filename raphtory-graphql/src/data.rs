f3ed1d6fb997990fbbb4d373500831573fdd8008
Shivam Kapoor <4599890+iamsmkr@users.noreply.github.com>
2023-06-19 17:27:21 +0100

    panic when graph already found

--- raphtory-graphql/src/data.rs
+++ raphtory-graphql/src/data.rs
@@ -22,22 +22,31 @@ impl Data {
             }
         }
 
+        let mut graphs_loaded: Vec<String> = vec![];
+        let mut is_graph_already_loaded = |graph_name: String| {
+            if graphs_loaded.contains(&graph_name) {
+                panic!("Graph by name {} is already loaded", graph_name);
+            } else {
+                graphs_loaded.push(graph_name);
+            }
+        };
+
         let graphs: HashMap<String, Graph> = valid_paths
             .into_iter()
             .map(|path| {
                 let graph = Graph::load_from_file(&path).expect("Unable to load from graph");
                 let maybe_graph_name = graph.static_prop("name");
+
                 return match maybe_graph_name {
-                    None => (
-                        Path::new(&path)
-                            .file_name()
-                            .unwrap()
-                            .to_str()
-                            .unwrap()
-                            .to_string(),
-                        graph,
-                    ),
-                    Some(graph_name) => (graph_name.to_string(), graph),
+                    None => {
+                        let graph_name = Path::new(&path).file_name().unwrap().to_str().unwrap();
+                        is_graph_already_loaded(graph_name.to_string());
+                        (graph_name.to_string(), graph)
+                    }
+                    Some(graph_name) => {
+                        is_graph_already_loaded(graph_name.to_string());
+                        (graph_name.to_string(), graph)
+                    }
                 };
             })
             .collect();
