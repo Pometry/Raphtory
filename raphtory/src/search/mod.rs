655f38ed6f27aa8eebf5a31c29bbb980ccb7a4d8
Fabian Murariu <murariu.fabian@gmail.com>
2023-07-06 14:39:51 +0100

    ignore the jira graph loader

--- raphtory/src/search/mod.rs
+++ raphtory/src/search/mod.rs
@@ -718,7 +718,7 @@ mod test {
     }
 
     #[test]
-    // #[ignore = "this test is for experiments with the jira graph"]
+    #[ignore = "this test is for experiments with the jira graph"]
     fn load_jira_graph() -> Result<(), GraphError> {
         let graph = Graph::load_from_file("/tmp/graphs/jira").expect("failed to load graph");
         assert!(graph.num_vertices() > 0);
