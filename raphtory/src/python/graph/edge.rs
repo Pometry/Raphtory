03d68d2a66c3910df685c38174a931c80ed012f1
Haaroon Y <Haaroon@users.noreply.github.com>
2023-07-06 09:38:43 +0100

    Clean docs (#1088)
    * - [x] Added a straight redirect if anyone clicks on the rust docs link it goes into cargo right away
    - [x] Add a screenshot of the crate page into the rust
    - [x] Removed the roadmap section and add an issue so we can add it back in
    - [x] Removed the user guide section
    - [x] Removed intro to raphtory and tutorials
    - [x] Used the summary from the paper in the first page of the docs and linked to the paper
    - [x] reduced font size of tutoiral questions
        - [x] Expand description in visualising the graph
    - [x] Moved license to a link
    - [x] fixed getting started typos
    - [x] Slack link does now works
    
    * - [x] Rewrite the text to be more developer friendly
    - [x] Use the overview from the paper into the getting started questions and its a summary of raphtory is used for, with he overview + projects that use raphtory and the 3 functionalities
    - [x] Package overview is a summary of raphtory
    - [x] In overview replace the text with the sumarry from the paper
    - [x] Move project overview into getting started
    - [x] 		Link the getting started tutorials in tutorials
    - [x] Moved far to separate section and reduced header sizes
    - [x] Fixed the broken text copied in source
    
    * add autosummary toc to raphtory python docs
    
    * update sphinx version, add missing rst files
    
    * deps?
    
    * filled in all the missing docs for all the missing public functions
    
    * fix environment for rtd
    
    * update python version
    
    * bad abi
    
    * bad ncurses
    
    * all more than
    
    * shivam did not like the color, so we added a darker one

--- raphtory/src/python/graph/edge.rs
+++ raphtory/src/python/graph/edge.rs
@@ -84,6 +84,7 @@ impl PyEdge {
         s.finish()
     }
 
+    /// The id of the edge.
     pub fn id(&self) -> (u64, u64) {
         self.edge.id()
     }
@@ -467,11 +468,16 @@ impl PyEdges {
         self.iter().count()
     }
 
+    /// Returns all source vertices of the Edges as an iterable.
+    ///
+    /// Returns:
+    ///   The source vertices of the Edges as an iterable.
     fn src(&self) -> PyVertexIterable {
         let builder = self.builder.clone();
         (move || builder().src()).into()
     }
 
+    /// Returns all destination vertices as an iterable
     fn dst(&self) -> PyVertexIterable {
         let builder = self.builder.clone();
         (move || builder().dst()).into()
@@ -521,6 +527,16 @@ impl PyEdges {
         (move || edges().latest_time()).into()
     }
 
+    /// Returns the value of the properties with the given name.
+    /// If the property is not found, None is returned.
+    /// If the property is found, the value of the property is returned.
+    ///
+    /// Arguments:
+    ///    name (str): The name of the property to retrieve.
+    ///    include_static (bool): Whether to include static properties in the result.
+    ///
+    /// Returns:
+    ///   The values of the property with the given name as an iterable.
     fn property(&self, name: String, include_static: Option<bool>) -> OptionPropIterable {
         let edges: Arc<
             dyn Fn() -> Box<dyn Iterator<Item = EdgeView<DynamicGraph>> + Send> + Send + Sync,
@@ -536,6 +552,7 @@ impl PyEdges {
         (move || edges().static_properties()).into()
     }
 
+    /// Returns all ids of the edges.
     fn id(&self) -> PyGenericIterable {
         let edges = self.builder.clone();
         (move || edges().id()).into()
