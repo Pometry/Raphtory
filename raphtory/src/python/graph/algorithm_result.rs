50a51afc13c403df18dec646be4a9a48f1b6c39d
Haaroon Yousaf <Haaroon@users.noreply.github.com>
2023-07-18 23:58:49 +0100

    add docstrings

--- raphtory/src/python/graph/algorithm_result.rs
+++ raphtory/src/python/graph/algorithm_result.rs
@@ -19,14 +19,25 @@ macro_rules! py_algorithm_result_base {
     ($name:ident, $rustKey:ty, $rustValue:ty) => {
         #[pymethods]
         impl $name {
+            /// Returns a reference to the entire `result` hashmap.
             fn get_all(&self) -> std::collections::HashMap<$rustKey, $rustValue> {
                 self.0.get_all().clone()
             }
 
+            /// Returns the value corresponding to the provided key in the `result` hashmap.
+            ///
+            /// # Arguments
+            ///
+            /// * `key`: The key of type `H` for which the value is to be retrieved.
             fn get(&self, key: $rustKey) -> Option<$rustValue> {
                 self.0.get(&key).cloned()
             }
 
+            /// Creates a dataframe from the result
+            ///
+            /// # Returns
+            ///
+            /// A `pandas.DataFrame` containing the result
             #[pyo3(signature = ())]
             pub fn to_df(
                 &self,
@@ -56,16 +67,49 @@ macro_rules! py_algorithm_result_partial_ord {
     ($name:ident, $rustKey:ty, $rustValue:ty) => {
         #[pymethods]
         impl $name {
+
+            /// Sorts the `AlgorithmResult` by its values in ascending or descending order.
+            ///
+            /// # Arguments
+            ///
+            /// * `reverse`: If `true`, sorts the result in descending order; otherwise, sorts in ascending order.
+            ///
+            /// # Returns
+            ///
+            /// A sorted vector of tuples containing keys of type `H` and values of type `Y`.
             #[pyo3(signature = (reverse=true))]
             fn sort_by_value(&self, reverse: bool) -> Vec<($rustKey, $rustValue)> {
                 self.0.sort_by_value(reverse)
             }
 
+            /// Sorts the `AlgorithmResult` by its keys in ascending or descending order.
+            ///
+            /// # Arguments
+            ///
+            /// * `reverse`: If `true`, sorts the result in descending order; otherwise, sorts in ascending order.
+            ///
+            /// # Returns
+            ///
+            /// A sorted vector of tuples containing keys of type `H` and values of type `Y`.
             #[pyo3(signature = (reverse=true))]
             fn sort_by_key(&self, reverse: bool) -> Vec<($rustKey, $rustValue)> {
                 self.0.sort_by_key(reverse)
             }
 
+            /// Retrieves the top-k elements from the `AlgorithmResult` based on its values.
+            ///
+            /// # Arguments
+            ///
+            /// * `k`: The number of elements to retrieve.
+            /// * `percentage`: If `true`, the `k` parameter is treated as a percentage of total elements.
+            /// * `reverse`: If `true`, retrieves the elements in descending order; otherwise, in ascending order.
+            ///
+            /// # Returns
+            ///
+            /// An `Option` containing a vector of tuples with keys of type `H` and values of type `Y`.
+            /// If `percentage` is `true`, the returned vector contains the top `k` percentage of elements.
+            /// If `percentage` is `false`, the returned vector contains the top `k` elements.
+            /// Returns `None` if the result is empty or if `k` is 0.
             #[pyo3(signature = (k, percentage=false, reverse=true))]
             fn top_k(&self, k: usize, percentage: bool, reverse: bool) -> Option<Vec<($rustKey, $rustValue)>> {
                 self.0.top_k(k, percentage, reverse)
@@ -79,6 +123,12 @@ macro_rules! py_algorithm_result_ord_hash_eq {
     ($name:ident, $rustKey:ty, $rustValue:ty) => {
         #[pymethods]
         impl $name {
+            /// Groups the `AlgorithmResult` by its values.
+            ///
+            /// # Returns
+            ///
+            /// A `HashMap` where keys are unique values from the `AlgorithmResult` and values are vectors
+            /// containing keys of type `H` that share the same value.
             fn group_by(&self) -> std::collections::HashMap<$rustValue, Vec<$rustKey>> {
                 self.0.group_by()
             }
@@ -103,6 +153,7 @@ py_algorithm_result!(AlgorithmResultStrF64, String, OrderedFloat<f64>);
 
 #[pymethods]
 impl AlgorithmResultStrF64 {
+    /// Returns all results as a dict
     #[pyo3(signature = ())]
     fn get_all(&self) -> std::collections::HashMap<String, f64> {
         self.0.get_all().into_iter()
@@ -110,11 +161,24 @@ impl AlgorithmResultStrF64 {
             .collect()
     }
 
-
+    /// Returns the value corresponding to the provided key
+    ///
+    /// # Arguments
+    ///
+    /// * `key`: The key of type `H` for which the value is to be retrieved.
     fn get(&self, key: String) -> Option<f64> {
         Some(self.0.get(&key).unwrap().0)
     }
 
+    /// Sorts the `AlgorithmResult` by its values in ascending or descending order.
+    ///
+    /// # Arguments
+    ///
+    /// * `reverse`: If `true`, sorts the result in descending order; otherwise, sorts in ascending order.
+    ///
+    /// # Returns
+    ///
+    /// A sorted vector of tuples containing keys of type `H` and values of type `Y`.
     #[pyo3(signature = (reverse=true))]
     fn sort_by_value(&self, reverse: bool) -> Vec<(String, f64)> {
         self.0.sort_by_value(reverse)
@@ -123,6 +187,15 @@ impl AlgorithmResultStrF64 {
             .collect()
     }
 
+    /// Sorts the `AlgorithmResult` by its keys in ascending or descending order.
+    ///
+    /// # Arguments
+    ///
+    /// * `reverse`: If `true`, sorts the result in descending order; otherwise, sorts in ascending order.
+    ///
+    /// # Returns
+    ///
+    /// A sorted vector of tuples containing keys of type `H` and values of type `Y`.
     #[pyo3(signature = (reverse=true))]
     fn sort_by_key(&self, reverse: bool) -> Vec<(String, f64)> {
         self.0.sort_by_key(reverse).into_iter()
@@ -130,6 +203,20 @@ impl AlgorithmResultStrF64 {
             .collect()
     }
 
+    /// Retrieves the top-k elements from the `AlgorithmResult` based on its values.
+    ///
+    /// # Arguments
+    ///
+    /// * `k`: The number of elements to retrieve.
+    /// * `percentage`: If `true`, the `k` parameter is treated as a percentage of total elements.
+    /// * `reverse`: If `true`, retrieves the elements in descending order; otherwise, in ascending order.
+    ///
+    /// # Returns
+    ///
+    /// An `Option` containing a vector of tuples with keys of type `H` and values of type `Y`.
+    /// If `percentage` is `true`, the returned vector contains the top `k` percentage of elements.
+    /// If `percentage` is `false`, the returned vector contains the top `k` elements.
+    /// Returns `None` if the result is empty or if `k` is 0.
     #[pyo3(signature = (k, percentage=false, reverse=true))]
     fn top_k(&self, k: usize, percentage: bool, reverse: bool) -> Option<Vec<(String, f64)>> {
         self.0.top_k(k, percentage, reverse)
@@ -140,6 +227,11 @@ impl AlgorithmResultStrF64 {
             })
     }
 
+    /// Creates a dataframe from the result
+    ///
+    /// # Returns
+    ///
+    /// A `pandas.DataFrame` containing the result
     #[pyo3(signature = ())]
     fn to_df(&self) -> PyResult<PyObject> {
         let hashmap: std::collections::HashMap<String, f64> = self.0.get_all().clone().into_iter()
@@ -161,6 +253,12 @@ impl AlgorithmResultStrF64 {
         })
     }
 
+    /// Groups the `AlgorithmResult` by its values.
+    ///
+    /// # Returns
+    ///
+    /// A `HashMap` where keys are unique values from the `AlgorithmResult` and values are vectors
+    /// containing keys of type `H` that share the same value.
     #[pyo3(signature = ())]
     fn group_by(&self) -> std::collections::HashMap<String, Vec<String>> {
         let ordered_map = self.0.group_by();
