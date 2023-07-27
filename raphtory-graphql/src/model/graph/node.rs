210ce6ea2d441af2338d82d06949f69843dd5c36
Haaroon Yousaf <Haaroon@users.noreply.github.com>
2023-07-27 16:53:16 +0100

    add exploded in, out, all, edges to graphql

--- raphtory-graphql/src/model/graph/node.rs
+++ raphtory-graphql/src/model/graph/node.rs
@@ -225,8 +225,16 @@ impl Node {
         return res;
     }
 
-    async fn exploded_edges(&self, layer: Option<String>) -> Vec<Edge> {
+    async fn exploded_in_edges(&self) -> Vec<Edge> {
+        self.vv.in_edges().explode().map(|ee| ee.into()).collect()
+    }
+
+    async fn exploded_out_edges(&self) -> Vec<Edge> {
         self.vv.out_edges().explode().map(|ee| ee.into()).collect()
+    }
+
+    async fn exploded_edges(&self, layer: Option<String>) -> Vec<Edge> {
+        self.vv.edges().explode().map(|ee| ee.into()).collect()
     }
 
     async fn start_date(&self) -> Option<i64> {
