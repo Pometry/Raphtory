73df9cfcebedb38c673418bae14b6824ee37a0d6
Abdullah Hasan <dulla2go@gmail.com>
2023-08-29 16:47:50 +0200

    feat(graphql): Extend property filtering to edges in graphql (#1203)
    * feat(graphql): Extend property filtering to edges in graphql
    
    * Fix importing
    
    ---------
    
    Co-authored-by: Abdullah Hasan <abdullah.hasan@payaut.com>

--- raphtory-graphql/src/model/filters/property_filter.rs
+++ raphtory-graphql/src/model/filters/property_filter.rs
@@ -1,8 +1,11 @@
-use crate::model::{filters::primitive_filter::NumberFilter, graph::node::Node};
+use crate::model::{
+    filters::primitive_filter::NumberFilter,
+    graph::{edge::Edge, node::Node},
+};
 use dynamic_graphql::InputObject;
-use raphtory::{core::Prop, db::api::view::VertexViewOps};
+use raphtory::{core::Prop, db::api::view::VertexViewOps, prelude::EdgeViewOps};
 
-#[derive(InputObject)]
+#[derive(InputObject, Clone)]
 pub(crate) struct PropertyHasFilter {
     key: Option<String>,
     value_str: Option<String>,
@@ -10,7 +13,7 @@ pub(crate) struct PropertyHasFilter {
 }
 
 impl PropertyHasFilter {
-    pub(crate) fn matches(&self, node: &Node) -> bool {
+    pub(crate) fn matches_node_properties(&self, node: &Node) -> bool {
         let valid_prop = |prop| valid_prop(prop, &self.value_str, &self.value_num);
 
         return match &self.key {
@@ -22,6 +25,21 @@ impl PropertyHasFilter {
                 }
             }
             None => node.vv.properties().values().any(valid_prop),
+        };
+    }
+
+    pub(crate) fn matches_edge_properties(&self, edge: &Edge) -> bool {
+        let valid_prop = |prop| valid_prop(prop, &self.value_str, &self.value_num);
+
+        return match &self.key {
+            Some(key) => {
+                if let Some(prop) = EdgeViewOps::properties(&edge.ee).get(key) {
+                    valid_prop(prop)
+                } else {
+                    false
+                }
+            }
+            None => EdgeViewOps::properties(&edge.ee).values().any(valid_prop),
         };
     }
 }
