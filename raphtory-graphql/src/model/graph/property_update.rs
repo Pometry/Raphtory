669ec558337e3b9d6a6186d3a78f6a662a62a3c1
Abdullah Hasan <dulla2go@gmail.com>
2023-08-30 10:43:14 +0200

    feat(graphql): allow searching for multiple property histories in one… (#1205)
    * feat(graphql): allow searching for multiple property histories in one query
    
    * Rework the way properties histories are fetched.
    
    ---------
    
    Co-authored-by: Abdullah Hasan <abdullah.hasan@payaut.com>
    Co-authored-by: Ben Steer <b.a.steer@qmul.ac.uk>

--- raphtory-graphql/src/model/graph/property_update.rs
+++ raphtory-graphql/src/model/graph/property_update.rs
@@ -1,13 +1,30 @@
 use dynamic_graphql::SimpleObject;
 
+/// A single property at a given `time` with a given `value`
 #[derive(SimpleObject)]
 pub(crate) struct PropertyUpdate {
     pub(crate) time: i64,
     pub(crate) value: String,
 }
 
+// A collection of `PropertyUpdate`s under their `propertyName`
+#[derive(SimpleObject)]
+pub(crate) struct PropertyUpdateGroup {
+    pub(crate) property_name: String,
+    pub(crate) property_updates: Vec<PropertyUpdate>,
+}
+
 impl PropertyUpdate {
     pub fn new(time: i64, value: String) -> Self {
         Self { time, value }
+    }
+}
+
+impl PropertyUpdateGroup {
+    pub fn new(property_name: String, property_updates: Vec<PropertyUpdate>) -> Self {
+        Self {
+            property_name,
+            property_updates,
+        }
     }
 }
