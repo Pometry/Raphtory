67b01c40ed8bd680ebc6cf8f6a7313b3e4b6cb4e
Shivam Kapoor <4599890+iamsmkr@users.noreply.github.com>
2023-09-12 19:39:36 +0100

    Graph state (#1243)
    * change gql dynamicgraph to gql for mutability
    
    * change dynamic graphs to graphs python
    
    * add update ui properties gql api
    
    * ren graphs to subgraphs
    
    * impl get subgraph by name gql api
    
    * persist ui props
    
    * fix tests

--- raphtory-graphql/src/model/graph/graph.rs
+++ raphtory-graphql/src/model/graph/graph.rs
@@ -19,7 +19,7 @@ use raphtory::{
         },
         graph::edge::EdgeView,
     },
-    prelude::EdgeViewOps,
+    prelude::{EdgeViewOps, PropertyAdditionOps},
     search::IndexedGraph,
 };
 
@@ -64,10 +64,10 @@ pub(crate) struct GqlGraph {
     graph: IndexedGraph<DynamicGraph>,
 }
 
-impl<G: GraphViewOps + IntoDynamic> From<G> for GqlGraph {
-    fn from(value: G) -> Self {
+impl<G: GraphViewOps + IntoDynamic> From<IndexedGraph<G>> for GqlGraph {
+    fn from(value: IndexedGraph<G>) -> Self {
         Self {
-            graph: value.into_dynamic().into(),
+            graph: value.into_dynamic_indexed(),
         }
     }
 }
@@ -83,7 +83,7 @@ impl GqlGraph {
     /// Return a graph containing only the activity between `start` and `end` measured as milliseconds from epoch
     async fn window(&self, start: i64, end: i64) -> GqlGraph {
         let w = self.graph.window(start, end);
-        w.into()
+        w.into_dynamic_indexed().into()
     }
 
     async fn layer_names(&self) -> Vec<String> {
