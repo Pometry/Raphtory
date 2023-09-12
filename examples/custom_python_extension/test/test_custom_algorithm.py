7c7be2bdaf3dc17a538835a0690ad2f8e335e168
Lucas Jeub <lucas.jeub@pometry.com>
2023-09-12 16:29:08 +0200

    add test to check that errors are sensible

--- examples/custom_python_extension/test/test_custom_algorithm.py
+++ examples/custom_python_extension/test/test_custom_algorithm.py
@@ -1,5 +1,6 @@
 from raphtory import Graph
 from custom_python_extension import custom_algorithm
+from pytest import raises
 
 
 def test_custom_algorithm():
@@ -7,3 +8,9 @@ def test_custom_algorithm():
     for v in range(10):
         g.add_vertex(0, v)
     assert custom_algorithm(g) == 10
+
+
+def test_error_for_wrong_type():
+    """ calling with the wrong type should still raise a type error (unless it defines a bincode method)"""
+    with raises(TypeError):
+        custom_algorithm(1)
