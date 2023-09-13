0b2e5df5836525ab3d983ad3aa44fa49004686d7
miratepuffin <b.a.steer@qmul.ac.uk>
2023-09-13 22:53:05 +0100

    Found an issue with expanding

--- raphtory/src/db/api/view/time.rs
+++ raphtory/src/db/api/view/time.rs
@@ -153,7 +153,10 @@ impl<T: TimeOps + Clone> Iterator for WindowSet<T> {
     fn next(&mut self) -> Option<Self::Item> {
         if self.cursor < self.end + self.step {
             let window_end = self.cursor;
-            let window_start = self.window.map(|w| window_end - w).unwrap_or(self.view.start().unwrap_or(window_end));
+            let window_start = self
+                .window
+                .map(|w| window_end - w)
+                .unwrap_or(self.view.start().unwrap_or(window_end));
             let window = self.view.window(window_start, window_end);
             self.cursor = self.cursor + self.step;
             Some(window)
