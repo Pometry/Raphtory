f1669d99f18777081d090bd7f24cfa55f93080f5
Fabian Murariu <murariu.fabian@gmail.com>
2023-09-20 17:02:23 +0100

    fix the out of order edge addition

--- raphtory-storage/src/arrow/mod.rs
+++ raphtory-storage/src/arrow/mod.rs
@@ -1,59 +1,8 @@
-use polars_core::utils::arrow::{array::{MutablePrimitiveArray, MutableStructArray}, trusted_len::TrustedLen};
-use raphtory::core::entities::{edges::edge_ref::EdgeRef, EID, VID};
+use polars_core::utils::arrow::array::{MutablePrimitiveArray, MutableStructArray};
 
-pub(crate) mod static_graph;
+pub(crate) mod columnar_graph;
 
 type MPArr<T> = MutablePrimitiveArray<T>;
-
-enum IngestEdgeType {
-    Outbound(EdgeRef, u64),
-    Inbound(EdgeRef, u64),
-}
-
-impl IngestEdgeType {
-    fn pid(&self) -> EID {
-        match self {
-            IngestEdgeType::Outbound(e_ref, _) => e_ref.pid(),
-            IngestEdgeType::Inbound(e_ref, _) => e_ref.pid(),
-        }
-    }
-
-    fn pid_u64(&self) -> u64 {
-        let e: usize = self.pid().into();
-        e as u64
-    }
-
-    fn local(&self) -> VID {
-        match self {
-            IngestEdgeType::Outbound(e_ref, _) => e_ref.local(),
-            IngestEdgeType::Inbound(e_ref, _) => e_ref.local(),
-        }
-    }
-
-    fn vertex_u64(&self) -> u64 {
-        let vid: usize = self.local().into();
-        vid as u64
-    }
-
-    fn gid(&self) -> u64 {
-        match self {
-            IngestEdgeType::Outbound(_, gid) => *gid,
-            IngestEdgeType::Inbound(_, gid) => *gid,
-        }
-    }
-
-    fn remote(&self) -> VID {
-        match self {
-            IngestEdgeType::Outbound(e_ref, _) => e_ref.remote(),
-            IngestEdgeType::Inbound(e_ref, _) => e_ref.remote(),
-        }
-    }
-
-    fn remote_u64(&self) -> u64 {
-        let vid: usize = self.remote().into();
-        vid as u64
-    }
-}
 
 #[repr(transparent)]
 struct MutEdgePair<'a>(&'a mut MutableStructArray);
@@ -67,19 +16,6 @@ impl<'a> MutEdgePair<'a> {
 
     fn add_pair_usize(&mut self, v_id: usize, e_id: usize) {
         self.add_pair(v_id as u64, e_id as u64);
-    }
-
-    fn add_vals<I: TrustedLen<Item = u64>>(&mut self, vs: I, es: I) {
-        self.0
-            .value::<MPArr<u64>>(0)
-            .unwrap()
-            .extend_trusted_len_values(vs);
-        self.0
-            .value::<MPArr<u64>>(1)
-            .unwrap()
-            .extend_trusted_len_values(es);
-
-        self.0.push(true);
     }
 }
 
