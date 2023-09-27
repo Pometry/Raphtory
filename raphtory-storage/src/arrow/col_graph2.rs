36127328fd23e36356235779710f6ed2ba90eaa6
Lucas Jeub <lucas.jeub@pometry.com>
2023-09-27 12:34:37 +0200

    refactor edge frame builder

--- raphtory-storage/src/arrow/col_graph2.rs
+++ raphtory-storage/src/arrow/col_graph2.rs
@@ -1,20 +1,18 @@
-use std::{
-    collections::HashMap,
-    path::{Path, PathBuf},
-    sync::Arc,
-};
+use std::{path::Path, sync::Arc};
 
-use crate::arrow::mmap::{mmap_batches, write_batches};
+use crate::arrow::{
+    edge_frame_builder::EdgeFrameBuilder, vertex_frame_builder::VertexFrameBuilder, Error,
+    E_ADDITIONS_COLUMN, GID_COLUMN, INBOUND_COLUMN, OUTBOUND_COLUMN,
+};
 use arrow2::error::Result as ArrowResult;
-use itertools::{Chunk, Itertools};
+use itertools::Itertools;
 use polars_core::{
     error::ArrowError,
-    frame::ArrowChunk,
     prelude::*,
     utils::arrow::{
         array::{
             Array, ListArray, MutableArray, MutableListArray, MutablePrimitiveArray,
-            MutableStructArray, PrimitiveArray, StructArray,
+            PrimitiveArray, StructArray,
         },
         offset::Offsets,
     },
@@ -111,41 +109,14 @@ struct TempColGraph {
     fragments: Arc<[TempColGraphFragment]>,
 }
 
-#[derive(thiserror::Error, Debug)]
-enum Error {
-    #[error("Polars error: {0}")]
-    Polars(#[from] PolarsError),
-    #[error("Arrow error: {0}")]
-    Arrow(#[from] ArrowError),
-}
-
-const FRAGMENT_ROW_COUNT: usize = 100_000;
-
-const OUTBOUND_COLUMN: &str = "outbound";
-const INBOUND_COLUMN: &str = "inbound";
-
-const V_ADDITIONS_COLUMN: &str = "additions";
-const E_ADDITIONS_COLUMN: &str = "additions";
-const E_DELETIONS_COLUMN: &str = "deletions";
-
-const NAME_COLUMN: &str = "name";
-const TEMPORAL_PROPS_COLUMN: &str = "t_props";
-
-const GID_COLUMN: &str = "global_vertex_id";
-const SRC_COLUMN: &str = "src";
-const DST_COLUMN: &str = "dst";
-
-const V_COLUMN: &str = "v";
-const E_COLUMN: &str = "e";
-
-type Time = i64;
+pub type Time = i64;
 
 #[derive(Debug)]
 pub struct SparseTable {
+    chunk_size: usize,
     sorted_gids: Vec<u64>,
     adj_out_chunks: Vec<Box<dyn Array>>,
-    edge_time_offsets: Vec<i64>,
-    time_col: Series,
+    edge_chunks: Vec<Box<dyn Array>>,
 }
 
 impl SparseTable {
@@ -204,7 +175,8 @@ impl TempColGraph {
         times: &ChunkedArray<Int64Type>,
         chunk_size: usize,
     ) -> ArrowResult<SparseTable> {
-        let mut vf_builder = VertexFrameBuilder::new(chunk_size, base_dir);
+        let mut vf_builder = VertexFrameBuilder::new(chunk_size, &base_dir);
+        let mut edge_builder = EdgeFrameBuilder::new(chunk_size, &base_dir);
 
         // initialise vertex global id table to preserve order
         for chunk in srcs.chunks() {
@@ -216,8 +188,6 @@ impl TempColGraph {
                 vf_builder.push_source(*v)
             }
         }
-        let mut edge_time_offsets: Vec<i64> = vec![0];
-
         // g_id, [{v_id1, e_id1}, {v_id2, e_id2}, ...]
         for (event_id, ((src, dst), time)) in srcs
             .into_iter()
@@ -226,186 +196,17 @@ impl TempColGraph {
             .zip(times.into_iter().flatten())
             .enumerate()
         {
-            if let Some((prev_src, prev_dst)) = vf_builder.last_edge {
-                if prev_src != src || prev_dst != dst {
-                    edge_time_offsets.push(event_id as i64);
-                }
-            }
-            vf_builder.push_update(src, dst)?;
+            let (src_id, dst_id) = vf_builder.push_update(src, dst)?;
+            edge_builder.push_update(time, src_id, dst_id)?;
         }
         vf_builder.finalise_empty_chunks()?;
-
-        if vf_builder.last_edge.is_some() {
-            edge_time_offsets.push(times.len() as i64);
-        }
+        edge_builder.finalise()?;
         Ok(SparseTable {
             sorted_gids: vf_builder.sorted_gids,
             adj_out_chunks: vf_builder.adj_out_chunks,
-            edge_time_offsets,
-            time_col: times.clone().into_series(),
+            edge_chunks: edge_builder.edge_chunks,
         })
     }
-}
-
-struct VertexFrameBuilder {
-    adj_out_chunks: Vec<Box<dyn Array>>, // chunks for the adjacency list, these are ListArrays with a struct {eid, vid}
-    sorted_gids: Vec<u64>,               // the sorted global ids of the vertices
-
-    adj_out_dst: Vec<u64>, // the dst of the adjacency list for the current chunk
-    adj_out_eid: Vec<u64>, // the eid of the adjacency list for the current chunk
-    adj_out_offsets: Vec<i64>, // the offsets of the adjacency list for the current chunk
-
-    chunk_size: usize,
-    chunk_adj_out_offset: i64,
-    last_edge: Option<(u64, u64)>,
-    vertex_count: usize,
-    e_id: u64,
-    location_path: PathBuf,
-}
-
-impl VertexFrameBuilder {
-    fn new<P: AsRef<Path>>(chunk_size: usize, path: P) -> Self {
-        Self {
-            adj_out_chunks: vec![],
-            sorted_gids: vec![],
-            adj_out_dst: vec![],
-            adj_out_eid: vec![],
-            adj_out_offsets: vec![0],
-            chunk_size,
-            chunk_adj_out_offset: 0,
-            last_edge: None,
-            vertex_count: 0,
-            e_id: 0,
-            location_path: path.as_ref().to_path_buf(),
-        }
-    }
-
-    fn push_chunk(&mut self, fill_size: usize) -> ArrowResult<()> {
-        let mut adj_out_eid_prev = Vec::with_capacity(self.adj_out_eid.len());
-        let mut adj_out_dst_prev = Vec::with_capacity(self.adj_out_dst.len());
-        let mut adj_out_offsets_prev = Vec::with_capacity(self.adj_out_offsets.len());
-
-        std::mem::swap(&mut adj_out_eid_prev, &mut self.adj_out_eid);
-        std::mem::swap(&mut adj_out_dst_prev, &mut self.adj_out_dst);
-        std::mem::swap(&mut adj_out_offsets_prev, &mut self.adj_out_offsets);
-
-        // fill chunk with empty adjacency lists
-        adj_out_offsets_prev.resize(fill_size + 1, self.chunk_adj_out_offset);
-
-        let col =
-            new_arrow_adj_list_chunk(adj_out_dst_prev, adj_out_eid_prev, adj_out_offsets_prev);
-
-        self.persist_and_mmap_adj_chunk(col)?;
-        self.chunk_adj_out_offset = 0i64;
-        Ok(())
-    }
-
-    fn persist_and_mmap_adj_chunk(&mut self, col: Box<dyn Array>) -> ArrowResult<()> {
-        let dtype = col.data_type().clone();
-        let schema = ArrowSchema::from(vec![ArrowField::new("adj_out", dtype, false)]);
-        let file_path = self
-            .location_path
-            .join(format!("adj_out_chunk_{}.ipc", self.adj_out_chunks.len()));
-        let chunk = [ArrowChunk::try_new(vec![col])?];
-        write_batches(file_path.as_path(), schema, &chunk)?;
-        let mmapped_chunk = unsafe { mmap_batches(file_path.as_path(), 0)? };
-        let mmapped_adj = mmapped_chunk[0].clone();
-        self.adj_out_chunks.push(mmapped_adj);
-        Ok(())
-    }
-
-    fn push_source(&mut self, src: u64) {
-        if !(self.sorted_gids.last() == Some(&src)) {
-            self.sorted_gids.push(src);
-        }
-    }
-
-    fn find_or_push_vertex(&mut self, vertex: u64) -> usize {
-        if let Ok(idx) = self.sorted_gids.binary_search(&vertex) {
-            idx
-        } else {
-            self.sorted_gids.push(vertex);
-            self.sorted_gids.len() - 1
-        }
-    }
-
-    fn push_update(&mut self, src: u64, dst: u64) -> ArrowResult<()> {
-        if self
-            .last_edge
-            .filter(|(prev_src, _)| prev_src != &src)
-            .is_some()
-            && (self.vertex_count + 1) % self.chunk_size == 0
-        {
-            println!("chunk cut off at {:?} {src} {dst}", self.last_edge);
-            self.push_chunk(self.chunk_size)?;
-        }
-        if Some((src, dst)) != self.last_edge {
-            self.adj_out_eid.push(self.e_id);
-            let dst_idx = self.find_or_push_vertex(dst);
-            self.adj_out_dst.push(dst_idx as u64);
-
-            if let Some((prev_src, prev_dst)) = self.last_edge {
-                if prev_src != src {
-                    self.adj_out_offsets.push(self.chunk_adj_out_offset);
-                    self.vertex_count += 1;
-                }
-            }
-
-            self.e_id += 1;
-            self.chunk_adj_out_offset += 1;
-        }
-        self.last_edge = Some((src, dst));
-        Ok(())
-    }
-
-    fn finalise_empty_chunks(&mut self) -> ArrowResult<()> {
-        if self.last_edge.is_some() {
-            // deal with the last chunk
-            let remaining_slots_in_chunk = self.chunk_size - self.adj_out_offsets.len();
-            let remaining_vertices = self.sorted_gids.len() - self.vertex_count - 1;
-            let fill_chunk_remaining = remaining_slots_in_chunk.min(remaining_vertices);
-            self.push_chunk(self.adj_out_offsets.len() + fill_chunk_remaining)?;
-
-            // deal with the rest of the vertices
-            let remaining_vertices = remaining_vertices - fill_chunk_remaining;
-            let remaining_chunks = remaining_vertices / self.chunk_size;
-            let size_of_last_chunk = remaining_vertices % self.chunk_size;
-
-            for _ in 0..remaining_chunks {
-                self.push_chunk(self.chunk_size)?;
-            }
-            if size_of_last_chunk > 0 {
-                self.push_chunk(size_of_last_chunk)?;
-            }
-        }
-        Ok(())
-    }
-}
-
-fn new_arrow_adj_list_chunk(
-    adj_out_dst: Vec<u64>,
-    adj_out_eid: Vec<u64>,
-    adj_out_offsets: Vec<i64>,
-) -> Box<dyn Array> {
-    let fields = vec![
-        ArrowField::new(V_COLUMN, ArrowDataType::UInt64, false),
-        ArrowField::new(E_COLUMN, ArrowDataType::UInt64, false),
-    ];
-    let schema = ArrowDataType::Struct(fields);
-
-    let dst_col = Box::new(MutablePrimitiveArray::<u64>::from_vec(adj_out_dst));
-    let eid_col = Box::new(MutablePrimitiveArray::<u64>::from_vec(adj_out_eid));
-
-    let values = MutableStructArray::new(schema.clone(), vec![dst_col, eid_col]);
-
-    let outbound2 = MutableListArray::new_from_mutable(
-        values,
-        Offsets::try_from(adj_out_offsets).unwrap(),
-        None,
-    );
-
-    let outbound: ListArray<i64> = outbound2.into();
-    Box::new(outbound)
 }
 
 #[cfg(test)]
