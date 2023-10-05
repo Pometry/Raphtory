037daebf33e9d9850203ad2bbe4457a098be58cd
Ben Steer <b.a.steer@qmul.ac.uk>
2023-10-05 19:01:41 +0100

    Pandas error handling (#1320)
    * testing new version of tqdm
    
    * fmt
    
    * Added check for missing cols pandas
    
    * half broken
    
    * refactored pandas
    
    * fmt
    
    * dropping cols which are not requests by a load
    
    * property checks now working
    
    * fixed tests
    
    * Fixed pandas test

--- python/tests/test_algorithms.py
+++ python/tests/test_algorithms.py
@@ -1,19 +1,26 @@
 def test_degree_centrality():
     from raphtory import Graph
     from raphtory.algorithms import degree_centrality
+
     g = Graph()
     g.add_edge(0, 0, 1, {})
     g.add_edge(0, 0, 2, {})
     g.add_edge(0, 0, 3, {})
     g.add_edge(0, 1, 2, {})
     g.add_edge(0, 1, 3, {})
-    assert degree_centrality(g).get_all() == {'0': 1.0, '1': 1.0, '2': 2 / 3, '3': 2 / 3}
+    assert degree_centrality(g).get_all() == {
+        "0": 1.0,
+        "1": 1.0,
+        "2": 2 / 3,
+        "3": 2 / 3,
+    }
 
 
 def test_max_min_degree():
     from raphtory import Graph
     from raphtory.algorithms import max_degree
     from raphtory.algorithms import min_degree
+
     g = Graph()
     g.add_edge(0, 0, 1, {})
     g.add_edge(0, 0, 2, {})
@@ -23,9 +30,11 @@ def test_max_min_degree():
     assert max_degree(g) == 3
     assert min_degree(g) == 2
 
+
 def test_single_source_shortest_path():
     from raphtory import Graph
     from raphtory.algorithms import single_source_shortest_path
+
     g = Graph()
     g.add_edge(0, 1, 2, {})
     g.add_edge(0, 2, 3, {})
@@ -34,6 +43,11 @@ def test_single_source_shortest_path():
     g.add_edge(0, 2, 4, {})
     res_one = single_source_shortest_path(g, 1, 1)
     res_two = single_source_shortest_path(g, 1, 2)
-    assert res_one.get_all() == {'1': ['1'], '2': ['1', '2'], '4': ['1', '4']}
-    assert (res_two.get_all() == {'1': ['1'], '2': ['1', '2'], '3': ['1', '2', '3'], '4': ['1', '4']}) or \
-           (res_two.get_all() == {'1': ['1'], '3': ['1', '4', '3'], '2': ['1', '2'], '4': ['1', '4']})
+    assert res_one.get_all() == {"1": ["1"], "2": ["1", "2"], "4": ["1", "4"]}
+    assert (
+        res_two.get_all()
+        == {"1": ["1"], "2": ["1", "2"], "3": ["1", "2", "3"], "4": ["1", "4"]}
+    ) or (
+        res_two.get_all()
+        == {"1": ["1"], "3": ["1", "4", "3"], "2": ["1", "2"], "4": ["1", "4"]}
+    )
