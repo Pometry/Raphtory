dea1d03811612fe12eac902fd9be0b43e9a08227
Fabian Murariu <murariu.fabian@gmail.com>
2023-10-06 16:04:21 +0100

    add example with hop hop forward

--- raphtory-storage/examples/hop_hop_forward.rs
+++ raphtory-storage/examples/hop_hop_forward.rs
@@ -1,9 +1,35 @@
-use raphtory::arrow::col_graph2::TempColGraphFragment;
+use std::time::Instant;
+
+use raphtory::{arrow::col_graph2::TempColGraphFragment, core::Direction};
 
 fn main() {
     let graph_dir = std::env::args()
         .nth(1)
         .expect("please supply a graph directory");
 
-    TempColGraphFragment::new(graph_dir).unwrap();
+    let graph = TempColGraphFragment::new(graph_dir).unwrap();
+    let now = Instant::now();
+    let mut count = 0usize;
+
+    for (e1, n1) in graph.edges(0.into(), Direction::OUT){
+        let edge1 = graph.edge(e1);
+        let e1_ts = edge1.timestamps();
+        // TODO: add filter on edge property of bytes
+
+        for (e2, _) in graph.edges(n1, Direction::OUT) {
+            let edge2 = graph.edge(e2);
+            let e2_ts = edge2.timestamps();
+
+            match (e2_ts.last(), e1_ts.first()) {
+                (Some(e2_ts), Some(e1_ts)) => {
+                    if e2_ts > e1_ts {
+                        count+=1;
+                    }
+                }
+                _ => {}
+            }
+        }
+    }
+
+    println!("Time taken: {:?}, counted {count}", now.elapsed());
 }
