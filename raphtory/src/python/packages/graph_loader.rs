123847df4c199fa7014b57f98be1861d700bf9c2
Haaroon Yousaf <Haaroon@users.noreply.github.com>
2023-10-09 19:04:51 +0100

    fixes issue with python?

--- raphtory/src/python/packages/graph_loader.rs
+++ raphtory/src/python/packages/graph_loader.rs
@@ -110,7 +110,6 @@ pub fn neo4j_movie_graph(
 ///     Zachary. The ZACHE matrix represents the presence or absence of ties among the members of the
 ///     club; the ZACHC matrix indicates the relative strength of the associations (number of
 ///     situations in and outside the club in which interactions occurred).
-///
 ///     Zachary (1977) used these data and an information flow model of network conflict resolution
 ///     to explain the split-up of this group following disputes among the members.
 ///
