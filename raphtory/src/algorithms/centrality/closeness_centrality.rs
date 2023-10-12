52dfe6b6c392ac1c7de1d955d8061e53b97a57b3
miratepuffin <b.a.steer@qmul.ac.uk>
2023-10-12 13:41:23 +0100

    in the middle of fixing

--- raphtory/src/algorithms/centrality/closeness_centrality.rs
+++ raphtory/src/algorithms/centrality/closeness_centrality.rs
@@ -1,3 +1,5 @@
 use crate::{core::entities::vertices::input_vertex::InputVertex, prelude::GraphViewOps};
 
-pub fn closeness_centrality<G: GraphViewOps, T: InputVertex>(graph: &G, weight: String) {}
+pub fn closeness_centrality<G: GraphViewOps, T: InputVertex>(graph: &G, weight: String) {
+
+}
