0d4d3753eb562bafa5f4e03c23fc26bbc835d54c
Pedro Rico Pinazo <ricopinazo@gmail.com>
2023-10-19 18:09:39 +0200

    skip split if text is already smaller than max size

--- raphtory/src/vectors/document_source.rs
+++ raphtory/src/vectors/document_source.rs
@@ -44,29 +44,30 @@ impl<G: GraphViewOps> DocumentSource for EdgeView<G> {
 /// Splits the input text in chunks of no more than max_size trying to use line breaks
 /// as much as possible
 fn split_text_by_line_breaks(text: String, max_size: usize) -> Vec<String> {
-    println!("splitting: {text}");
-    // TODO: maybe use async_stream crate instead
-    let mut substrings = text.split("\n");
-    let first_substring = substrings.next().unwrap().to_owned();
-    let mut chunks = vec![first_substring];
+    if text.len() <= max_size {
+        vec![text]
+    } else {
+        // TODO: maybe use async_stream crate instead
+        let mut substrings = text.split("\n");
+        let first_substring = substrings.next().unwrap().to_owned();
+        let mut chunks = vec![first_substring];
 
-    // TODO: add shortcut, it text is smaller than max size return vec![text]
-
-    for substring in substrings {
-        let last_chunk = chunks.last_mut().unwrap(); // at least one element
-        if substring.len() > max_size {
-            for subsubstring in split_text_with_constant_size(substring, max_size).into_iter() {
-                chunks.push(subsubstring.to_owned());
+        for substring in substrings {
+            let last_chunk = chunks.last_mut().unwrap(); // at least one element
+            if substring.len() > max_size {
+                for subsubstring in split_text_with_constant_size(substring, max_size).into_iter() {
+                    chunks.push(subsubstring.to_owned());
+                }
+            } else if last_chunk.len() + substring.len() <= max_size {
+                last_chunk.push_str("\n"); // add back line break removed by split
+                last_chunk.push_str(substring);
+            } else {
+                chunks.push(substring.to_owned());
             }
-        } else if last_chunk.len() + substring.len() <= max_size {
-            last_chunk.push_str("\n"); // add back line break removed by split
-            last_chunk.push_str(substring);
-        } else {
-            chunks.push(substring.to_owned());
         }
-    }
 
-    chunks
+        chunks
+    }
 }
 
 // TODO: test this function
