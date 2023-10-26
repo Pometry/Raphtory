e99e34a84f73a6a7a439740e6904cfca306b8dd7
Shivam Kapoor <4599890+iamsmkr@users.noreply.github.com>
2023-10-26 14:48:37 +0100

    fmt

--- raphtory/src/algorithms/community_detection/scc.rs
+++ raphtory/src/algorithms/community_detection/scc.rs
@@ -182,8 +182,10 @@ where
 
 #[cfg(test)]
 mod strongly_connected_components_tests {
-    use crate::algorithms::community_detection::scc::strongly_connected_components;
-    use crate::prelude::{AdditionOps, Graph, NO_PROPS};
+    use crate::{
+        algorithms::community_detection::scc::strongly_connected_components,
+        prelude::{AdditionOps, Graph, NO_PROPS},
+    };
 
     #[test]
     fn scc_test() {
