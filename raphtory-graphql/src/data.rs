a30fc345852b916c10c481cd1b5db81091be3c96
Shivam Kapoor <4599890+iamsmkr@users.noreply.github.com>
2023-11-06 13:40:57 +0000

    fmt

--- raphtory-graphql/src/data.rs
+++ raphtory-graphql/src/data.rs
@@ -1,10 +1,10 @@
 use itertools::Itertools;
 use parking_lot::RwLock;
-use raphtory::db::api::properties::internal::ConstPropertiesOps;
 use raphtory::{
     core::Prop,
     db::{
-        api::view::internal::MaterializedGraph, graph::views::deletion_graph::GraphWithDeletions,
+        api::{properties::internal::ConstPropertiesOps, view::internal::MaterializedGraph},
+        graph::views::deletion_graph::GraphWithDeletions,
     },
     prelude::{Graph, GraphViewOps, PropertyAdditionOps},
     search::IndexedGraph,
