772816255019a6aaeb118167bcf1e889080b4278
Lucas Jeub <lucas.jeub@pometry.com>
2023-11-14 18:35:02 +0100

    fix the documentation for time ops

--- raphtory/src/python/types/macros/timeops.rs
+++ raphtory/src/python/types/macros/timeops.rs
@@ -8,7 +8,7 @@ macro_rules! impl_timeops {
     ($obj:ty, $field:ident, $base_type:ty, $name:literal) => {
         #[pyo3::pymethods]
         impl $obj {
-            #[doc = concat!(r" Gets the earliest time that this ", $name, r" is valid")]
+            #[doc = concat!(r" Gets the start time for rolling and expanding windows for this ", $name)]
             ///
             /// Returns:
             #[doc = concat!(r"    The earliest time that this ", $name, r" is valid or None if the ", $name, r" is valid for all times.")]
@@ -17,69 +17,64 @@ macro_rules! impl_timeops {
                 self.$field.start()
             }
 
-            /// Gets the earliest datetime that this vertex is valid
+            #[doc = concat!(r" Gets the earliest datetime that this ", $name, r" is valid")]
             ///
             /// Returns:
-            ///     The earliest datetime that this vertex is valid or None if the vertex is valid for all times.
+            #[doc = concat!(r"     The earliest datetime that this ", $name, r" is valid or None if the ", $name, r" is valid for all times.")]
             #[getter]
             pub fn start_date_time(&self) -> Option<NaiveDateTime> {
                 let start_time = self.$field.start()?;
                 NaiveDateTime::from_timestamp_millis(start_time)
             }
 
-            /// Gets the latest time that this vertex is valid.
+            #[doc = concat!(r" Gets the latest time that this ", $name, r" is valid.")]
             ///
             /// Returns:
-            ///   The latest time that this vertex is valid or None if the vertex is valid for all times.
+            #[doc = concat!("   The latest time that this ", $name, r" is valid or None if the ", $name, r" is valid for all times.")]
             #[getter]
             pub fn end(&self) -> Option<i64> {
                 self.$field.end()
             }
 
-            /// Gets the latest datetime that this vertex is valid
+            #[doc = concat!(r" Gets the latest datetime that this ", $name, r" is valid")]
             ///
             /// Returns:
-            ///     The latest datetime that this vertex is valid or None if the vertex is valid for all times.
+            #[doc = concat!(r"     The latest datetime that this ", $name, r" is valid or None if the ", $name, r" is valid for all times.")]
             #[getter]
             pub fn end_date_time(&self) -> Option<NaiveDateTime> {
                 let end_time = self.$field.end()?;
                 NaiveDateTime::from_timestamp_millis(end_time)
             }
 
+            #[doc = concat!(r" Get the window size (difference between start and end) for this ", $name)]
             #[getter]
             pub fn window_size(&self) -> Option<u64> {
                 self.$field.window_size()
             }
 
-            /// Creates a `PyVertexWindowSet` with the given `step` size and optional `start` and `end` times,
-            /// using an expanding window.
+            /// Creates a `WindowSet` with the given `step` size using an expanding window.
             ///
             /// An expanding window is a window that grows by `step` size at each iteration.
-            /// This will tell you whether a vertex exists at different points in the window and what
-            /// its properties are at those points.
             ///
             /// Arguments:
             ///     step (int): The step size of the window.
             ///
             /// Returns:
-            ///     A `PyVertexWindowSet` object.
+            ///     A `WindowSet` object.
             fn expanding(&self, step: PyInterval) -> Result<WindowSet<$base_type>, ParseTimeError> {
                 self.$field.expanding(step)
             }
 
-            /// Creates a `PyVertexWindowSet` with the given `window` size and optional `step`, `start` and `end` times,
-            /// using a rolling window.
+            /// Creates a `WindowSet` with the given `window` size and optional `step` using a rolling window.
             ///
             /// A rolling window is a window that moves forward by `step` size at each iteration.
-            /// This will tell you whether a vertex exists at different points in the window and what
-            /// its properties are at those points.
             ///
             /// Arguments:
             ///     window: The size of the window.
             ///     step: The step size of the window. Defaults to the window size.
             ///
             /// Returns:
-            ///     A `PyVertexWindowSet` object.
+            ///     A `WindowSet` object.
             fn rolling(
                 &self,
                 window: PyInterval,
@@ -88,14 +83,14 @@ macro_rules! impl_timeops {
                 self.$field.rolling(window, step)
             }
 
-            // #[doc =r" Create a view of the `{}` including all events between `start` (inclusive) and `end` (exclusive)", stringify!($obj))]
+            #[doc = concat!(r" Create a view of the ", $name, r" including all events between `start` (inclusive) and `end` (exclusive)")]
             ///
             /// Arguments:
-            ///     start (int, str or datetime(utc)): The start time of the window. Defaults to the start time of the vertex.
-            ///     end (int, str or datetime(utc)): The end time of the window. Defaults to the end time of the vertex.
+            #[doc = concat!(r"     start: The start time of the window. Defaults to the start time of the ", $name, r".")]
+            #[doc = concat!(r"     end: The end time of the window. Defaults to the end time of the ", $name, r".")]
             ///
             /// Returns:
-            ///    A `PyVertex` object.
+            #[doc = concat!("r    A ", $name, " object.")]
             #[pyo3(signature = (start = None, end = None))]
             pub fn window(
                 &self,
@@ -106,22 +101,35 @@ macro_rules! impl_timeops {
                     .window(start.unwrap_or(PyTime::MIN), end.unwrap_or(PyTime::MAX))
             }
 
-            /// Create a view of the vertex including all events at `t`.
+            #[doc = concat!(r" Create a view of the ", $name, r" including all events at `time`.")]
             ///
             /// Arguments:
-            ///     end (int, str or datetime(utc)): The time of the window.
+            ///     time: The time of the window.
             ///
             /// Returns:
-            ///     A `PyVertex` object.
-            #[pyo3(signature = (end))]
-            pub fn at(&self, end: PyTime) -> <$base_type as TimeOps>::WindowedViewType {
-                self.$field.at(end)
+            #[doc = concat!(r"     A ", $name, r" object.")]
+            pub fn at(&self, time: PyTime) -> <$base_type as TimeOps>::WindowedViewType {
+                self.$field.at(time)
             }
 
+            #[doc = concat!(r" Create a view of the ", $name, r" including all events before `end` (exclusive).")]
+            ///
+            /// Arguments:
+            ///     end: The end time of the window.
+            ///
+            /// Returns:
+            #[doc = concat!(r"     A ", $name, r" object.")]
             pub fn before(&self, end: PyTime) -> <$base_type as TimeOps>::WindowedViewType {
                 self.$field.before(end)
             }
 
+            #[doc = concat!(r" Create a view of the ", $name, r" including all events after `start` (exclusive).")]
+            ///
+            /// Arguments:
+            ///     start: The start time of the window.
+            ///
+            /// Returns:
+            #[doc = concat!(r"     A ", $name, r" object.")]
             pub fn after(&self, start: PyTime) -> <$base_type as TimeOps>::WindowedViewType {
                 self.$field.after(start)
             }
