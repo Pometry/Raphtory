de0aa211161133dee3a6f9b2d3ca4dd5666bc6fc
Lucas Jeub <lucas.jeub@pometry.com>
2023-11-29 14:05:23 +0100

    fix netflow test

--- examples/netflow/test/test_custom_algorithm.py
+++ examples/netflow/test/test_custom_algorithm.py
@@ -7,13 +7,13 @@ def test_one_path():
     graph = Graph()
     graph.add_edge(0, 1, 2, layer="Events2v4624")
     graph.add_edge(1, 2, 2, layer="Events1v4688")
-    (graph.add_edge(2,2,3, {"dstBytes": 100_000_005},"Netflow")
+    graph.add_edge(2, 2, 3, {"dstBytes": 100_000_005}, "Netflow")
 
-    actual = netflow_one_path_vertex(graph, True);
+    actual = netflow_one_path_vertex(graph, True)
     assert actual == 1
 
 
 def test_error_for_wrong_type():
     """ calling with the wrong type should still raise a type error (unless it defines a bincode method)"""
     with raises(TypeError):
-        custom_algorithm(1)
+        netflow_one_path_vertex(1, True)
