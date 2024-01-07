69a42a8f4fe2ca746d0ca8cf68594be3d3b5e29a
miratepuffin <b.a.steer@qmul.ac.uk>
2024-01-07 22:13:03 +0000

    Added timezone support in python

--- raphtory/src/python/utils/mod.rs
+++ raphtory/src/python/utils/mod.rs
@@ -10,10 +10,10 @@ use crate::{
     db::api::view::*,
     python::graph::node::PyNode,
 };
-use chrono::NaiveDateTime;
+use chrono::{FixedOffset, NaiveDate, NaiveDateTime, NaiveTime, TimeZone};
 use pyo3::{exceptions::PyTypeError, prelude::*};
 use std::{future::Future, thread};
-use pyo3::types::PyDateTime;
+use pyo3::types::{PyDateAccess, PyDateTime, PyDelta, PyDeltaAccess, PyTimeAccess, PyTzInfoAccess};
 
 pub mod errors;
 
@@ -72,15 +72,36 @@ impl<'source> FromPyObject<'source> for PyTime {
         if let Ok(parsed_datetime) = time.extract::<NaiveDateTime>() {
             return Ok(PyTime::new(parsed_datetime.try_into_time()?));
         }
-        if let Ok(parsed_datetime) = time.extract::<PyDateTime>() {
-            
+        if let Ok(py_datetime) = time.extract::<&PyDateTime>() {
+            let naive_date = NaiveDate::from_ymd_opt(py_datetime.get_year(),
+                                                 py_datetime.get_month() as u32,
+                                                 py_datetime.get_day() as u32);
+            let naive_time = NaiveTime::from_hms_micro_opt(py_datetime.get_hour() as u32,
+                                                       py_datetime.get_minute() as u32,
+                                                       py_datetime.get_second() as u32,
+                                                       py_datetime.get_microsecond());
+
+            if let (Some(naive_date), Some(naive_time))= (naive_date, naive_time) {
+                let naive_datetime= NaiveDateTime::new(naive_date, naive_time);
+                // Handle timezone
+                let tz_offset = if let Some(tz) = py_datetime.get_tzinfo() {
+                    let offset = tz.call_method1("utcoffset", (py_datetime,))?.extract::<&PyDelta>()?;
+                    let offset_seconds = offset.call_method0("total_seconds")?.extract::<f64>()? as i32;
+                    FixedOffset::east_opt(offset_seconds).ok_or(PyTypeError::new_err("Could not pass timezone"))
+                } else {
+                    FixedOffset::east_opt(0).ok_or(PyTypeError::new_err("Could not pass timezone")) // No timezone information, assume UTC
+                }?;
+
+                let utc_datetime = tz_offset.from_local_datetime(&naive_datetime).unwrap();
+                let timestamp = utc_datetime.timestamp()*1000;
+                return Ok(PyTime::new(timestamp.try_into_time()?));
+            }
         }
 
         let message = format!("time '{time}' must be a str, datetime or an integer");
         Err(PyTypeError::new_err(message))
     }
 }
-
 impl PyTime {
     fn new(parsing_result: i64) -> Self {
         Self { parsing_result }
