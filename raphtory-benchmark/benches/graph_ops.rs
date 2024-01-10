87d7fa9fe2b0809b8dc6585bed5630aa7f2a9e2c
Lucas Jeub <lucas.jeub@pometry.com>
2024-01-10 15:48:14 +0100

    fix the benchmarks

--- raphtory-benchmark/benches/graph_ops.rs
+++ raphtory-benchmark/benches/graph_ops.rs
@@ -18,8 +18,8 @@ pub fn graph(c: &mut Criterion) {
     );
     graph_window_group_100.finish();
     let mut graph_window_group_10 = c.benchmark_group("analysis_graph_window_10");
-    let latest = graph.end().expect("non-empty graph");
-    let earliest = graph.start().expect("non-empty graph");
+    let latest = graph.latest_time().expect("non-empty graph");
+    let earliest = graph.earliest_time().expect("non-empty graph");
     let start = latest - (latest - earliest) / 10;
     graph_window_group_10.sample_size(10);
     run_analysis_benchmarks(
