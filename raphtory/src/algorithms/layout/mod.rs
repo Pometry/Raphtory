b0474f7680d5055800c5ec92e792ec437588bbad
Pedro Rico Pinazo <ricopinazo@gmail.com>
2024-01-10 18:15:30 +0100

    make cohesive reingold public

--- raphtory/src/algorithms/layout/mod.rs
+++ raphtory/src/algorithms/layout/mod.rs
@@ -1,7 +1,7 @@
 use glam::Vec2;
 use std::collections::HashMap;
 
-mod cohesive_fruchterman_reingold;
+pub mod cohesive_fruchterman_reingold;
 pub mod fruchterman_reingold;
 pub mod fruchterman_reingold_unbounded;
 
