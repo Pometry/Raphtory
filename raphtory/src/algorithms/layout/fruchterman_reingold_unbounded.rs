e621bcce444c792e72ae78eb02d932bed3443776
Pedro Rico Pinazo <ricopinazo@gmail.com>
2024-01-11 11:22:13 +0100

    add random initial positions

--- raphtory/src/algorithms/layout/fruchterman_reingold_unbounded.rs
+++ raphtory/src/algorithms/layout/fruchterman_reingold_unbounded.rs
@@ -3,17 +3,19 @@ use crate::{
     prelude::{GraphViewOps, NodeViewOps},
 };
 use glam::Vec2;
+use quad_rand::RandomRange;
 
 pub fn fruchterman_reingold_unbounded<'graph, G: GraphViewOps<'graph>>(
     graph: &'graph G,
     iterations: u64,
     scale: f32,
+    node_start_size: f32,
     cooloff_factor: f32,
     dt: f32,
 ) -> NodeVectors {
     println!("fruchterman_reingold_unbounded");
-    let mut positions = init_node_vectors(graph);
-    let mut velocities = init_node_vectors(graph);
+    let mut positions = init_positions(graph, node_start_size);
+    let mut velocities = init_velocities(graph);
 
     println!("{positions:?}");
 
@@ -108,10 +110,25 @@ fn unit_vector(a: Vec2, b: Vec2) -> Vec2 {
     (b - a).normalize_or_zero()
 }
 
-fn init_node_vectors<'graph, G: GraphViewOps<'graph>>(graph: &G) -> NodeVectors {
+fn init_velocities<'graph, G: GraphViewOps<'graph>>(graph: &G) -> NodeVectors {
     graph
         .nodes()
         .iter()
         .map(|node| (node.id(), Vec2::ZERO))
+        .collect()
+}
+
+fn init_positions<'graph, G: GraphViewOps<'graph>>(graph: &G, node_start_size: f32) -> NodeVectors {
+    let half_node_start_width = node_start_size / 2.0;
+    graph
+        .nodes()
+        .iter()
+        .map(|node| {
+            let position = Vec2::new(
+                RandomRange::gen_range(-half_node_start_width, half_node_start_width),
+                RandomRange::gen_range(-half_node_start_width, half_node_start_width),
+            );
+            (node.id(), position)
+        })
         .collect()
 }
