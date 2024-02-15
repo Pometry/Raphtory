ce671314eae1629030aea2236749a00339ad52fd
Lucas Jeub <lucas.jeub@pometry.com>
2024-02-15 15:36:50 +0100

    disable multithreading when reading props from file

--- raphtory/src/arrow/col_graph2.rs
+++ raphtory/src/arrow/col_graph2.rs
@@ -434,8 +434,8 @@ impl TempColGraphFragment {
 
         let offset_iter = ParquetOffsetIter::new(props.iter(), t_props_chunk_size);
         let edge_chunks = offset_iter.collect_vec();
-        let edge_props_values = edge_props_builder
-            .load_t_edges_from_par_structs(num_threads, edge_chunks.into_par_iter())?;
+        let edge_props_values =
+            edge_props_builder.load_t_edges_from_par_structs(1, edge_chunks.into_par_iter())?;
 
         let edges = Edges::new(
             src_chunks,
