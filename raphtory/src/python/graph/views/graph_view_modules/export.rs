5d4559587279f3388223d37d4d40f2f5d85aaf44
Haaroon Yousaf <Haaroon@users.noreply.github.com>
2024-02-16 18:27:00 +0000

    fmt

--- raphtory/src/python/graph/views/graph_view_modules/export.rs
+++ raphtory/src/python/graph/views/graph_view_modules/export.rs
@@ -241,13 +241,14 @@ impl PyGraphView {
                         let prop_hist = e.properties().temporal().histories();
                         let mut prop_hist_map: HashMap<ArcStr, Vec<(i64, Prop)>> = HashMap::new();
                         for (key, value) in prop_hist {
-                            prop_hist_map.entry(key).or_insert_with(Vec::new).push(value);
+                            prop_hist_map
+                                .entry(key)
+                                .or_insert_with(Vec::new)
+                                .push(value);
                         }
-                        let output: Vec<(ArcStr, Vec<(i64, Prop)>)> = prop_hist_map.into_iter().collect();
-                        properties.set_item(
-                            "temporal",
-                            PyList::new(py, output),
-                        )?;
+                        let output: Vec<(ArcStr, Vec<(i64, Prop)>)> =
+                            prop_hist_map.into_iter().collect();
+                        properties.set_item("temporal", PyList::new(py, output))?;
                     } else {
                         for (key, value) in e.properties().as_map() {
                             properties.set_item(key, value.into_py(py))?;
