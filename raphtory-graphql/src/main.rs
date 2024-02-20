5e393a1a39558b046055b96fa7e2d11889e30895
Shivam Kapoor <4599890+iamsmkr@users.noreply.github.com>
2024-02-20 11:56:26 +0000

    impl log levels as config

--- raphtory-graphql/src/main.rs
+++ raphtory-graphql/src/main.rs
@@ -1,5 +1,6 @@
 use crate::server::RaphtoryServer;
 use std::env;
+use tracing::Level;
 
 mod data;
 mod model;
@@ -10,8 +11,10 @@ mod server;
 #[tokio::main]
 async fn main() {
     let graph_directory = env::var("GRAPH_DIRECTORY").unwrap_or("/tmp/graphs".to_string());
+    let config_path = "config.toml";
+
     RaphtoryServer::from_directory(&graph_directory)
-        .run()
+        .run(config_path)
         .await
         .unwrap()
 }
