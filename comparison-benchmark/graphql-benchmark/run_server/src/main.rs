45b91d8783ee16dca1162e09094ebfa234ba6d04
Haaroon Yousaf <Haaroon@users.noreply.github.com>
2024-02-27 16:34:53 +0000

    with rust one

---
+++ comparison-benchmark/graphql-benchmark/run_server/src/main.rs
@@ -0,0 +1,10 @@
+use raphtory_graphql::RaphtoryServer;
+
+#[tokio::main]
+async fn main() {
+    let graph_directory = "/Users/haaroony/Documents/dev/Raphtory/comparison-benchmark/graphql-benchmark/graphs";
+    RaphtoryServer::from_directory(&graph_directory)
+        .run( "INFO")
+        .await
+        .unwrap()
+}
