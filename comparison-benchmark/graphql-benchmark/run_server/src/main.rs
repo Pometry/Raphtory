57d744d697c7d4d7e38e8fecd2f626240b88b3ef
Haaroon Y <Haaroon@users.noreply.github.com>
2024-02-29 16:43:30 +0000

    Feature/even more benchmarks (#1525)
    * extra benches
    
    * extra benches
    
    * extra benches
    
    * loop
    
    * extra benches
    
    * more power
    
    * maybe now it uses all cores?????
    
    * undo undo
    
    * cleanup
    
    * cleanup
    
    * cleanup
    
    * cleanup
    
    * fmt

--- comparison-benchmark/graphql-benchmark/run_server/src/main.rs
+++ comparison-benchmark/graphql-benchmark/run_server/src/main.rs
@@ -1,6 +1,6 @@
 use raphtory_graphql::RaphtoryServer;
 
-#[tokio::main]
+#[tokio::main(flavor = "multi_thread", worker_threads = 63)]
 async fn main() {
     let graph_directory =
         "/Users/haaroony/Documents/dev/Raphtory/comparison-benchmark/graphql-benchmark/graphs";
