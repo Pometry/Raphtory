89be27016d1aa4873105cefdfa69f45d9d947875
Shivam Kapoor <4599890+iamsmkr@users.noreply.github.com>
2024-03-04 18:00:26 +0000

    impl scripts

---
+++ examples/lanl/python/main.py
@@ -0,0 +1,121 @@
+from raphtory import ArrowGraph
+from raphtory.lanl import lanl_query1, lanl_query2, lanl_query3, lanl_query3b, lanl_query3c, lanl_query4, exfilteration_query1, exfilteration_count_query_total, exfiltration_list_query_count
+from raphtory import algorithms
+import time
+from typing import TypeVar, Callable
+import os
+import sys
+
+
+B = TypeVar('B')
+
+def measure(name: str, f: Callable[..., B], *args, print_result: bool = True) -> B:
+    start_time = time.time()
+    result = f(*args)
+    elapsed_time = time.time() - start_time
+    
+    time_unit = "s"
+    elapsed_time_display = elapsed_time
+    if elapsed_time < 1:
+        time_unit = "ms"
+        elapsed_time_display *= 1000
+
+    if print_result:
+        print(f"Running {name}: time: {elapsed_time_display:.3f}{time_unit}, result: {result}")
+    else:
+        print(f"Running {name}: time: {elapsed_time_display:.3f}{time_unit}")
+
+    return result
+
+
+def main():
+    if len(sys.argv) != 3:
+        print("Usage: python3 main.py <path_to_resources_directory> <path_to_target_directory>")
+        sys.exit(1)
+
+    resources_dir = sys.argv[1]
+    target_dir = sys.argv[2]
+
+    graph_dir = target_dir
+    
+    layer_parquet_cols = [
+        {
+            "parquet_dir": os.path.join(resources_dir, "netflowsorted/nft_sorted"),
+            "layer": "netflow",
+            "src_col": "src",
+            "src_hash_col": "src_hash",
+            "dst_col": "dst",
+            "dst_hash_col": "dst_hash",
+            "time_col": "epoch_time",
+        },
+        {
+            "parquet_dir": os.path.join(resources_dir, "netflowsorted/v1_sorted"),
+            "layer": "events_1v",
+            "src_col": "src",
+            "src_hash_col": "src_hash",
+            "dst_col": "dst",
+            "dst_hash_col": "dst_hash",
+            "time_col": "epoch_time",
+        },
+        {
+            "parquet_dir": os.path.join(resources_dir, "netflowsorted/v2_sorted"),
+            "layer": "events_2v",
+            "src_col": "src",
+            "src_hash_col": "src_hash",
+            "dst_col": "dst",
+            "dst_hash_col": "dst_hash",
+            "time_col": "epoch_time",
+        }
+    ]
+
+    # # Read the Parquet file
+    # table = pq.read_table(parquet_dir + '/part-00000-8b31eaa4-2bd9-4f07-b61c-a353aed2af22-c000.snappy.parquet')
+    # print(table.schema)
+
+    print()
+    print("Loading the graph")
+    try:
+        print("Attempting to load the graph from the directory")
+        g = ArrowGraph.load_from_dir(graph_dir)
+    except Exception as e:
+        print("Failed to load the graph from the directory. Attempting to load from parquet files ", e)
+        
+        chunk_size = 268_435_456
+        num_threads = 4
+        t_props_chunk_size = int(chunk_size / 8)
+        read_chunk_size = 4_000_000
+        concurrent_files = 1
+        
+        g = ArrowGraph.load_from_parquets(
+            graph_dir,
+            layer_parquet_cols,
+            chunk_size, 
+            t_props_chunk_size,
+            read_chunk_size,
+            concurrent_files,
+            num_threads,
+        )
+
+    print("Nodes count =", g.count_nodes())
+    print("Edges count =", g.count_edges())
+    print("Earliest time =", g.earliest_time)
+    print("Latest time =", g.latest_time)
+
+    measure("Query 1", lanl_query1, g)
+    measure("Query 2", lanl_query2, g)
+    measure("Query 3", lanl_query3, g)
+    measure("Query 3b", lanl_query3b, g)
+    # assert(measure("Query 3c", lanl_query3c, g) == 0)
+    measure("Query 4", lanl_query4, g)
+
+    measure("CC", algorithms.connected_components, g, print_result=False)
+    measure("Weakly CC  Layer", algorithms.weakly_connected_components, g.layer("netflow"), 20, print_result=False)
+    measure("Weakly CC", algorithms.weakly_connected_components, g, 20, print_result=False)
+    measure("Page Rank", algorithms.pagerank, g, 100, print_result=False)
+    
+    measure("Exfilteration Query 1", exfilteration_query1, g)
+    measure("Exfilteration Count Query Total", exfilteration_count_query_total, g, 30)
+    measure("Exfilteration List Query Count", exfiltration_list_query_count, g, 30)
+
+if __name__ == "__main__":
+    main()
--- examples/lanl/src/main.rs
+++ examples/lanl/src/main.rs
@@ -1,15 +1,37 @@
 use raphtory::{
+    algorithms::{
+        centrality::pagerank::unweighted_page_rank, components::weakly_connected_components,
+    },
+    arrow::algorithms::connected_components,
     arrow::graph_impl::{ArrowGraph, ParquetLayerCols},
     prelude::GraphViewOps,
 };
+use raphtory_storage::lanl::exfiltration;
 use raphtory_storage::lanl::*;
-use std::num::NonZeroUsize;
+use std::{env, num::NonZeroUsize, vec};
 
 fn main() {
-    let graph_dir: &str = "target";
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() != 3 {
+        eprintln!("Usage: {} <path_to_resources_directory> <path_to_target_directory>", args[0]);
+        std::process::exit(1);
+    }
+
+    let resources_dir = &args[1];
+    let target_dir = &args[2];
+
+    let graph_dir: &str = target_dir;
+
+    let parquet_dirs = vec![
+        format!("{}/netflowsorted/nft_sorted", resources_dir),
+        format!("{}/netflowsorted/v1_sorted", resources_dir),
+        format!("{}/netflowsorted/v2_sorted", resources_dir)
+    ];
+
     let layer_parquet_cols: Vec<ParquetLayerCols> = vec![
         ParquetLayerCols {
-            parquet_dir: "data/netflowsorted/nft_sorted",
+            parquet_dir: &parquet_dirs[0],
             layer: "netflow",
             src_col: "src",
             src_hash_col: "src_hash",
@@ -18,7 +40,7 @@ fn main() {
             time_col: "epoch_time",
         },
         ParquetLayerCols {
-            parquet_dir: "data/netflowsorted/v1_sorted",
+            parquet_dir: &parquet_dirs[1],
             layer: "events_1v",
             src_col: "src",
             src_hash_col: "src_hash",
@@ -27,7 +49,7 @@ fn main() {
             time_col: "epoch_time",
         },
         ParquetLayerCols {
-            parquet_dir: "data/netflowsorted/v2_sorted",
+            parquet_dir: &parquet_dirs[1],
             layer: "events_2v",
             src_col: "src",
             src_hash_col: "src_hash",
@@ -63,15 +85,47 @@ fn main() {
         }
     };
 
-    println!("Node count {}", graph.count_nodes());
-    println!("Edge count {}", graph.count_edges());
-    println!("Earliest time {}", graph.earliest());
-    println!("Latest time {}", graph.latest());
+    println!("Node count = {}", graph.count_nodes());
+    println!("Edge count = {}", graph.count_edges());
+    println!("Earliest time = {}", graph.earliest());
+    println!("Latest time = {}", graph.latest());
 
-    measure("Query 1", || query1::run(&graph).unwrap(), false);
-    measure("Query 2", || query2::run(&graph).unwrap(), false);
-    measure("Query 3", || query3::run(&graph).unwrap(), false);
-    measure("Query 3b", || query3b::run(&graph).unwrap(), false);
-    // # measure("Query 3c", || query3c::run(&graph).unwrap(), false);
-    measure("Query 4", || query4::run2(&graph).unwrap(), false);
+    measure("Query 1", || query1::run(&graph).unwrap(), true);
+    measure("Query 2", || query2::run(&graph).unwrap(), true);
+    measure("Query 3", || query3::run(&graph).unwrap(), true);
+    measure("Query 3b", || query3b::run(&graph).unwrap(), true);
+    // # measure("Query 3c", || query3c::run(&graph).unwrap(), true);
+    measure("Query 4", || query4::run2(&graph).unwrap(), true);
+
+    measure(
+        "CC",
+        || connected_components::connected_components(&graph.layer(0)),
+        false,
+    );
+    measure(
+        "Weakly CC",
+        || weakly_connected_components(&graph, 20, None),
+        false,
+    );
+    measure(
+        "Page Rank",
+        || unweighted_page_rank(&graph, 100, Some(1), None, true),
+        false,
+    );
+
+    measure(
+        "Exfilteration Query 1",
+        || exfiltration::query1::run(&graph),
+        true,
+    );
+    measure(
+        "Exfilteration Count Query Total",
+        || exfiltration::count::query_total(&graph, 30),
+        true,
+    );
+    measure(
+        "Exfilteration List Query Count",
+        || exfiltration::list::query_count(&graph, 30),
+        true,
+    );
 }
