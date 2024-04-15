5681f1e4d398a7afce27012201369d018d39fe0e
Fabian Murariu <murariu.fabian@gmail.com>
2024-04-15 11:16:14 +0100

    cypher print will print everything

--- raphtory-cypher/examples/raphtory_cypher.rs
+++ raphtory-cypher/examples/raphtory_cypher.rs
@@ -2,7 +2,7 @@ use arrow::util::pretty::print_batches;
 use clap::Parser;
 use futures::{stream, StreamExt};
 use raphtory::arrow::graph_impl::ArrowGraph;
-use raphtory_cypher::run_cypher_to_streams;
+use raphtory_cypher::{run_cypher, run_cypher_to_streams};
 
 /// Simple program to greet a person
 #[derive(Parser, Debug)]
@@ -25,28 +25,22 @@ struct Args {
 async fn main() {
     let args = Args::parse();
 
-    // let graph_dir = std::env::args().nth(1).expect("No graph file provided");
-    // let cypher_query = std::env::args().nth(2).expect("No cypher query provided");
-
     let graph = ArrowGraph::load_from_dir(&args.graph_dir).expect("Failed to load graph");
 
     let now = std::time::Instant::now();
-    let streams = run_cypher_to_streams(&args.query, &graph).await.unwrap();
 
-    let num_rows = stream::iter(streams)
-        .flatten()
-        .enumerate()
-        .filter_map(|(i, batch)| async move { batch.ok().map(|b| (i, b)) })
-        .map(|(i, batch)| {
-            let len = batch.num_rows();
-            if i == 0 && args.print {
-                print_batches(&[batch]).expect("Failed to print batches");
-            }
-            len
-        })
-        .fold(0, |acc, x| async move { acc + x })
-        .await;
-
-    println!("Query execution: {:?}, num_rows: {num_rows}", now.elapsed());
-
+    if args.print {
+        let df = run_cypher(&args.query, &graph).await.unwrap();
+        let batches = df.collect().await.unwrap();
+        print_batches(&batches).expect("Failed to print batches");
+    } else {
+        let streams = run_cypher_to_streams(&args.query, &graph).await.unwrap();
+        let num_rows = stream::iter(streams)
+            .flatten()
+            .filter_map(|batch| async move { batch.ok() })
+            .map(|batch| batch.num_rows())
+            .fold(0, |acc, x| async move { acc + x })
+            .await;
+        println!("Query execution: {:?}, num_rows: {num_rows}", now.elapsed());
+    }
 }
