42a83b368863259ff6fe10cb9b9138cf22dd27ba
miratepuffin <b.a.steer@qmul.ac.uk>
2024-05-15 12:14:44 +0100

    fmt

--- comparison-benchmark/graphql-benchmark/run_server/src/main.rs
+++ comparison-benchmark/graphql-benchmark/run_server/src/main.rs
@@ -5,7 +5,7 @@ async fn main() {
     let graph_directory =
         "/Users/haaroony/Documents/dev/Raphtory/comparison-benchmark/graphql-benchmark/graphs";
     RaphtoryServer::from_directory(&graph_directory)
-        .run("INFO",false)
+        .run("INFO", false)
         .await
         .unwrap()
 }
