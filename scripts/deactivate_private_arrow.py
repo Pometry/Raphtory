181e3f01d286e6cd12d36c1149e2551d6794c13f
Lucas Jeub <lucas.jeub@pometry.com>
2024-05-27 16:13:16 +0200

    make private arrow crate a submodule and tweak all the scripts accordingly

---
+++ scripts/deactivate_private_arrow.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+import subprocess
+from pathlib import Path
+import re
+
+directory = "./raphtory-arrow"
+root_dir = Path(__file__).parent.parent
+toml_file = root_dir / "Cargo.toml"
+
+with open(toml_file, "r") as f:
+    lines = f.readlines()
+
+for i, line in enumerate(lines[:-1]):
+    if "#[public-arrow]" in line:
+        next_line = lines[i + 1]
+        if next_line.strip().startswith("#") and "raphtory-arrow" in next_line:
+            lines[i + 1] = re.sub(r"#\s*", "", next_line, 1)
+    if "#[private-arrow]" in line:
+        next_line = lines[i + 1]
+        if next_line.strip().startswith("raphtory-arrow"):
+            lines[i + 1] = next_line.replace("raphtory-arrow", "# raphtory-arrow", 1)
+
+with open(toml_file, "w") as f:
+    f.writelines(lines)
+
+subprocess.run(
+    [
+        "git",
+        "submodule",
+        "deinit",
+        "raphtory-arrow-private"
+    ]
+)
