1d8492566bb4a40ced46f06b324405ad9d6c6623
narnolddd <n.a.arnold@qmul.ac.uk>
2024-05-28 15:37:24 +0100

    remove single threaded version to avoid confusion

--- raphtory/src/algorithms/motifs/mod.rs
+++ raphtory/src/algorithms/motifs/mod.rs
@@ -1,7 +1,6 @@
 pub mod global_temporal_three_node_motifs;
 pub mod local_temporal_three_node_motifs;
 pub mod local_triangle_count;
-pub mod three_node_local_single_thread;
 pub mod three_node_motifs;
 pub mod triangle_count;
 pub mod triplet_count;
