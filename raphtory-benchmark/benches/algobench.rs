36b10b8d4e6a3d13412a0ef7ca7dfe5552d7561e
Lucas Jeub <lucas.jeub@pometry.com>
2024-06-05 11:45:35 +0200

    more benchmark fixes

--- raphtory-benchmark/benches/algobench.rs
+++ raphtory-benchmark/benches/algobench.rs
@@ -27,7 +27,7 @@ pub fn local_triangle_count_analysis(c: &mut Criterion) {
         let windowed_graph = g.window(i64::MIN, i64::MAX);
 
         b.iter(|| {
-            let node_ids = windowed_graph.nodes().id().collect();
+            let node_ids = windowed_graph.nodes().id().collect::<Vec<_>>();
 
             node_ids.into_par_iter().for_each(|v| {
                 local_triangle_count(&windowed_graph, v).unwrap();
