69ffe8982a758f7beac375dfffcb431cad94185c
Shivam Kapoor <4599890+iamsmkr@users.noreply.github.com>
2024-06-20 10:19:08 +0100

    add tests for props

--- python/tests/test_load_from_parquet.py
+++ python/tests/test_load_from_parquet.py
@@ -1,13 +1,42 @@
 import os
 
+import pandas as pd
 import pyarrow as pa
 import pyarrow.parquet as pq
+import pytest
+
 from raphtory import Graph, PersistentGraph
+
+@pytest.mark.skip(reason="Prepares data for debugging purposes")
+def test_prepare_data():
+    data = {
+        "id": [1, 2, 3, 4, 5, 6],
+        "name": ["Alice", "Bob", "Carol", "Dave", "Eve", "Frank"],
+        "time": [1, 2, 3, 4, 5, 6],
+        "type": ["Person 1", "Person 2", "Person 3", "Person 4", "Person 5", "Person 6"],
+        "node_type": ["p", "p", "p", "p", "p", "p"],
+    }
+
+    table = pa.table(data)
+    pq.write_table(table, '/tmp/parquet/nodes.parquet')
+
+    data = {
+        "src": [1, 2, 3, 4, 5],
+        "dst": [2, 3, 4, 5, 6],
+        "time": [1, 2, 3, 4, 5],
+        "weight": [1.0, 2.0, 3.0, 4.0, 5.0],
+        "marbles": ["red", "blue", "green", "yellow", "purple"],
+        "marbles_const": ["red", "blue", "green", "yellow", "purple"],
+        "layers": ["layer 1", "layer 2", "layer 3", "layer 4", "layer 5"],
+    }
+
+    table = pa.table(data)
+    pq.write_table(table, '/tmp/parquet/edges.parquet')
 
 
 def test_load_from_parquet():
-    nodes_parquet_file_path = os.path.join(os.path.dirname(__file__), 'data', 'parquet', 'nodes.parquet')
     edges_parquet_file_path = os.path.join(os.path.dirname(__file__), 'data', 'parquet', 'edges.parquet')
+    nodes_parquet_file_path = os.path.join(os.path.dirname(__file__), 'data', 'parquet', 'nodes.parquet')
 
     expected_node_ids = [1, 2, 3, 4, 5, 6]
     expected_nodes = [
@@ -41,11 +70,6 @@ def test_load_from_parquet():
             edges.append((e.src.id, e.dst.id, weight, marbles))
         assert edges == expected_edges
 
-    g = Graph()
-    g.load_nodes_from_parquet(nodes_parquet_file_path, "id", "time", "node_type", properties=["name"])
-    g.load_edges_from_parquet(edges_parquet_file_path, "src", "dst", "time", ["weight", "marbles"])
-    assertions(g)
-
     g = Graph.load_from_parquet(
         edge_parquet_file_path=edges_parquet_file_path,
         edge_src="src",
@@ -60,35 +84,9 @@ def test_load_from_parquet():
     )
     assertions(g)
 
-#     # g = PersistentGraph.load_from_parquet(
-#     #     'test_data.parquet', "src", "dst", "time", ["weight", "marbles"]
-#     # )
-#     # assertions(g)
-
-def test_load_edges_from_parquet():
-    file_path = os.path.join(os.path.dirname(__file__), 'data', 'parquet', 'edges.parquet')
-    expected_edges = [
-        (1, 2, 1.0, "red"),
-        (2, 3, 2.0, "blue"),
-        (3, 4, 3.0, "green"),
-        (4, 5, 4.0, "yellow"),
-        (5, 6, 5.0, "purple"),
-    ]
-
-    g = Graph()
-    g.load_edges_from_parquet(file_path, "src", "dst", "time", ["weight", "marbles"])
-
-    edges = []
-    for e in g.edges:
-        weight = e["weight"]
-        marbles = e["marbles"]
-        edges.append((e.src.id, e.dst.id, weight, marbles))
-
-    assert edges == expected_edges
-
 
 def test_load_nodes_from_parquet():
-    file_path = os.path.join(os.path.dirname(__file__), 'data', 'parquet', 'nodes.parquet')
+    nodes_parquet_file_path = os.path.join(os.path.dirname(__file__), 'data', 'parquet', 'nodes.parquet')
     expected_node_ids = [1, 2, 3, 4, 5, 6]
     expected_nodes = [
         (1, "Alice"),
@@ -100,7 +98,7 @@ def test_load_nodes_from_parquet():
     ]
 
     g = Graph()
-    g.load_nodes_from_parquet(file_path, "id", "time", "node_type", properties=["name"])
+    g.load_nodes_from_parquet(nodes_parquet_file_path, "id", "time", "node_type", properties=["name"])
 
     nodes = []
     for v in g.nodes:
@@ -109,3 +107,124 @@ def test_load_nodes_from_parquet():
 
     assert g.nodes.id.collect() == expected_node_ids
     assert nodes == expected_nodes
+
+
+def test_load_node_props_from_parquet():
+    nodes_parquet_file_path = os.path.join(os.path.dirname(__file__), 'data', 'parquet', 'nodes.parquet')
+    expected_node_ids = [1, 2, 3, 4, 5, 6]
+    expected_nodes = [
+        (1, "Alice"),
+        (2, "Bob"),
+        (3, "Carol"),
+        (4, "Dave"),
+        (5, "Eve"),
+        (6, "Frank"),
+    ]
+
+    g = Graph()
+    g.load_nodes_from_parquet(
+        nodes_parquet_file_path,
+        "id",
+        "time",
+        "node_type",
+        properties=["name"]
+    )
+
+    nodes = []
+    for v in g.nodes:
+        name = v["name"]
+        nodes.append((v.id, name))
+
+    assert g.nodes.id.collect() == expected_node_ids
+    assert nodes == expected_nodes
+
+    g.load_node_props_from_parquet(
+        nodes_parquet_file_path,
+        "id",
+        const_properties=["type"],
+        shared_const_properties={"tag": "test_tag"},
+    )
+
+    assert g.nodes.properties.constant.get("type").collect() == [
+        "Person 1",
+        "Person 2",
+        "Person 3",
+        "Person 4",
+        "Person 5",
+        "Person 6",
+    ]
+    assert g.nodes.properties.constant.get("tag").collect() == [
+        "test_tag",
+        "test_tag",
+        "test_tag",
+        "test_tag",
+        "test_tag",
+        "test_tag",
+    ]
+
+
+def test_load_edges_from_parquet():
+    edges_parquet_file_path = os.path.join(os.path.dirname(__file__), 'data', 'parquet', 'edges.parquet')
+    expected_edges = [
+        (1, 2, 1.0, "red"),
+        (2, 3, 2.0, "blue"),
+        (3, 4, 3.0, "green"),
+        (4, 5, 4.0, "yellow"),
+        (5, 6, 5.0, "purple"),
+    ]
+
+    g = Graph()
+    g.load_edges_from_parquet(edges_parquet_file_path, "src", "dst", "time", ["weight", "marbles"])
+
+    edges = []
+    for e in g.edges:
+        weight = e["weight"]
+        marbles = e["marbles"]
+        edges.append((e.src.id, e.dst.id, weight, marbles))
+
+    assert edges == expected_edges
+
+
+def test_load_edge_props_from_parquet():
+    edges_parquet_file_path = os.path.join(os.path.dirname(__file__), 'data', 'parquet', 'edges.parquet')
+    expected_edges = [
+        (1, 2, 1.0, "red"),
+        (2, 3, 2.0, "blue"),
+        (3, 4, 3.0, "green"),
+        (4, 5, 4.0, "yellow"),
+        (5, 6, 5.0, "purple"),
+    ]
+
+    g = Graph()
+    g.load_edges_from_parquet(edges_parquet_file_path, "src", "dst", "time", ["weight", "marbles"], layer="layers")
+
+    edges = []
+    for e in g.edges:
+        weight = e["weight"]
+        marbles = e["marbles"]
+        edges.append((e.src.id, e.dst.id, weight, marbles))
+
+    assert edges == expected_edges
+
+    g.load_edge_props_from_parquet(
+        edges_parquet_file_path,
+        "src",
+        "dst",
+        const_properties=["marbles_const"],
+        shared_const_properties={"tag": "test_tag"},
+        layer="layers",
+    )
+    assert g.layers(
+        ["layer 1", "layer 2", "layer 3"]
+    ).edges.properties.constant.get("marbles_const").collect() == [
+       {"layer 1": "red"},
+       {"layer 2": "blue"},
+       {"layer 3": "green"},
+    ]
+    assert g.edges.properties.constant.get("tag").collect() == [
+        {"layer 1": "test_tag"},
+        {"layer 2": "test_tag"},
+        {"layer 3": "test_tag"},
+        {"layer 4": "test_tag"},
+        {"layer 5": "test_tag"},
+    ]
