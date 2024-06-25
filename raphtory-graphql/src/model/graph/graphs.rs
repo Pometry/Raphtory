4521346448f147c7947fd14cc556bd3c27bbe152
Shivam Kapoor <4599890+iamsmkr@users.noreply.github.com>
2024-06-25 11:16:36 +0100

    properties, unique layers to gqlgraphs

--- raphtory-graphql/src/model/graph/graphs.rs
+++ raphtory-graphql/src/model/graph/graphs.rs
@@ -1,8 +1,11 @@
 use dynamic_graphql::{ResolvedObject, ResolvedObjectFields};
+use itertools::Itertools;
+use raphtory::db::api::properties::dyn_props::DynProperties;
 use raphtory::db::api::view::DynamicGraph;
 use raphtory::prelude::GraphViewOps;
 use raphtory::search::IndexedGraph;
 use raphtory::search::into_indexed::DynamicIndexedGraph;
+use crate::model::graph::property::GqlProperties;
 
 #[derive(ResolvedObject)]
 pub(crate) struct GqlGraphs {
@@ -21,6 +24,20 @@ impl GqlGraphs {
         self.graphs
             .iter()
             .map(|g| g.properties().constant().get("name").unwrap().to_string())
+            .collect()
+    }
+
+    async fn properties(&self) -> Vec<GqlProperties> {
+        self.graphs
+            .iter()
+            .map(|g| Into::<DynProperties>::into(g.properties()).into())
+            .collect()
+    }
+
+    async fn unique_layers(&self) -> Vec<Vec<String>> {
+        self.graphs
+            .iter()
+            .map(|g| g.unique_layers().map_into().collect())
             .collect()
     }
 }
