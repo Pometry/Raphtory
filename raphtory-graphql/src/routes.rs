c8ac55e49c11af5c4251a17787023cb275ccc292
Pedro Rico Pinazo <ricopinazo@gmail.com>
2024-10-15 10:52:58 +0100

    GraphQL UI (#1816)
    * add ui route at /
    
    * add meta tag to allow traggic to cdns
    
    * add index content
    
    * remove meta tag for csp
    
    * remove cdn dependencies from graphql playground
    
    ---------
    
    Co-authored-by: Ben Steer <bensteer@Bens-MBP.communityfibre.co.uk>

--- raphtory-graphql/src/routes.rs
+++ raphtory-graphql/src/routes.rs
@@ -20,9 +20,10 @@ pub(crate) async fn health() -> impl IntoResponse {
 
 #[handler]
 pub(crate) async fn graphql_playground() -> impl IntoResponse {
-    Html(playground_source(
-        GraphQLPlaygroundConfig::new("/")
-            .subscription_endpoint("/ws")
-            .with_setting("request.credentials", "include"),
-    ))
+    Html(include_str!("../resources/playground.html"))
+}
+
+#[handler]
+pub(crate) async fn ui() -> impl IntoResponse {
+    Html(include_str!("../resources/index.html"))
 }
