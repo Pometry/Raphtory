c72a26452ccf02d0eaf52593aecbdca42a646b84
Shivam Kapoor <4599890+iamsmkr@users.noreply.github.com>
2024-11-05 16:35:50 +0000

    fmt

--- raphtory-graphql/src/main.rs
+++ raphtory-graphql/src/main.rs
@@ -1,4 +1,4 @@
-use raphtory_graphql::{config::app_config::AppConfigBuilder, GraphServer, server::DEFAULT_PORT};
+use raphtory_graphql::{config::app_config::AppConfigBuilder, server::DEFAULT_PORT, GraphServer};
 use std::{
     env,
     path::{Path, PathBuf},
@@ -8,11 +8,15 @@ use tokio::io::Result as IoResult;
 #[tokio::main]
 async fn main() -> IoResult<()> {
     let default_path = Path::new(".");
-    let port: u16 = env::var("PORT").map(|port| port.parse().unwrap()).unwrap_or(DEFAULT_PORT);
+    let port: u16 = env::var("PORT")
+        .map(|port| port.parse().unwrap())
+        .unwrap_or(DEFAULT_PORT);
     let work_dir = env::var("GRAPH_DIRECTORY").unwrap_or(default_path.display().to_string());
     let work_dir = PathBuf::from(&work_dir);
     let app_config = Some(AppConfigBuilder::new().with_tracing(true).build());
-    GraphServer::new(work_dir, app_config, None)?.run_with_port(port).await?;
+    GraphServer::new(work_dir, app_config, None)?
+        .run_with_port(port)
+        .await?;
 
     Ok(())
 }
