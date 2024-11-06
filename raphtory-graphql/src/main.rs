82607f45cd7dc0563ed97f61e12fb3d848991f1a
Pedro Rico Pinazo <ricopinazo@gmail.com>
2024-11-06 13:17:35 +0000

    Fix existing rust Dockerfile (#1844)
    * fix existing rust Dockerfile
    
    * fmt
    
    ---------
    
    Co-authored-by: Shivam Kapoor <4599890+iamsmkr@users.noreply.github.com>
    Co-authored-by: Shivam <4599890+shivam-880@users.noreply.github.com>

--- raphtory-graphql/src/main.rs
+++ raphtory-graphql/src/main.rs
@@ -1,4 +1,4 @@
-use raphtory_graphql::{config::app_config::AppConfigBuilder, GraphServer};
+use raphtory_graphql::{config::app_config::AppConfigBuilder, server::DEFAULT_PORT, GraphServer};
 use std::{
     env,
     path::{Path, PathBuf},
@@ -7,11 +7,16 @@ use tokio::io::Result as IoResult;
 
 #[tokio::main]
 async fn main() -> IoResult<()> {
-    let default_path = Path::new("/tmp/graphs");
+    let default_path = Path::new(".");
+    let port: u16 = env::var("PORT")
+        .map(|port| port.parse().unwrap())
+        .unwrap_or(DEFAULT_PORT);
     let work_dir = env::var("GRAPH_DIRECTORY").unwrap_or(default_path.display().to_string());
     let work_dir = PathBuf::from(&work_dir);
     let app_config = Some(AppConfigBuilder::new().with_tracing(true).build());
-    GraphServer::new(work_dir, app_config, None)?.run().await?;
+    GraphServer::new(work_dir, app_config, None)?
+        .run_with_port(port)
+        .await?;
 
     Ok(())
 }
