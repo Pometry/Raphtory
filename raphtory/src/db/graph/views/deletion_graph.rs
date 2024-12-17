e1b9efbfe6ad2700ff3cd3138345a4499ee6e729
Lucas Jeub <lucas.jeub@pometry.com>
2024-12-17 13:37:48 +0100

    PersistentGraph materialize window is broken

--- raphtory/src/db/graph/views/deletion_graph.rs
+++ raphtory/src/db/graph/views/deletion_graph.rs
@@ -890,6 +890,16 @@ mod test_deletions {
     }
 
     #[test]
+    fn test_materialize_window_node_props() {
+        let g = PersistentGraph::new();
+        g.add_node(0, 1, [("test", "test")], None).unwrap();
+
+        let wg = g.window(3, 5);
+        let mg = wg.materialize().unwrap();
+        assert_graph_equal(&wg, &mg);
+    }
+
+    #[test]
     fn test_exploded_latest_time() {
         let g = PersistentGraph::new();
         let e = g.add_edge(0, 1, 2, NO_PROPS, None).unwrap();
