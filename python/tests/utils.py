ca1e68bec0288d11a53f6f0ec8229a22d29d7fe7
Lucas Jeub <lucas.jeub@pometry.com>
2025-01-14 13:03:07 +0100

    add node sorting tests

--- python/tests/utils.py
+++ python/tests/utils.py
@@ -1,6 +1,13 @@
+import json
+import re
+import tempfile
 import time
 from typing import TypeVar, Callable
 
+import pytest
+
+from graphql.test_edge_sorting import create_test_graph, PORT
+from raphtory.graphql import GraphServer
 
 B = TypeVar("B")
 
@@ -24,3 +31,36 @@ def measure(name: str, f: Callable[..., B], *args, print_result: bool = True) ->
         print(f"Running {name}: time: {elapsed_time_display:.3f}{time_unit}")
 
     return result
+
+
+def run_graphql_test(query, expected_output, graph):
+    create_test_graph(graph)
+    tmp_work_dir = tempfile.mkdtemp()
+    with GraphServer(tmp_work_dir).start(PORT) as server:
+        client = server.get_client()
+        client.send_graph(path="g", graph=graph)
+
+        response = client.query(query)
+
+        # Convert response to a dictionary if needed and compare
+        response_dict = json.loads(response) if isinstance(response, str) else response
+        assert response_dict == expected_output
+
+
+def run_graphql_error_test(query, expected_error_message, graph):
+    create_test_graph(graph)
+    tmp_work_dir = tempfile.mkdtemp()
+    with GraphServer(tmp_work_dir).start(PORT) as server:
+        client = server.get_client()
+        client.send_graph(path="g", graph=graph)
+
+        with pytest.raises(Exception) as excinfo:
+            client.query(query)
+
+        full_error_message = str(excinfo.value)
+        match = re.search(r'"message":"(.*?)"', full_error_message)
+        error_message = match.group(1) if match else ""
+
+        assert (
+            error_message == expected_error_message
+        ), f"Expected '{expected_error_message}', but got '{error_message}'"
