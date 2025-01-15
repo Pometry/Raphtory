fd2b136cdb00507d9a6a55d9ddcbc41268dd0b6a
Lucas Jeub <lucas.jeub@pometry.com>
2025-01-15 10:45:23 +0100

    tidy up pyvis export

--- raphtory/src/python/graph/views/graph_view_modules/export.rs
+++ raphtory/src/python/graph/views/graph_view_modules/export.rs
@@ -17,42 +17,47 @@ impl PyGraphView {
     /// Pyvis is a required dependency. If you intend to use this function make sure that you install Pyvis
     /// with ``pip install pyvis``
     ///
-    ///     Args:
-    ///         graph (graph): A Raphtory graph.
-    ///         explode_edges (bool): A boolean that is set to True if you want to explode the edges in the graph. Defaults to False.
-    ///         edge_color (str): A string defining the colour of the edges in the graph. Defaults to "#000000".
-    ///         shape (str): An optional string defining what the node looks like. Defaults to "dot".
+    /// Args:
+    ///     explode_edges (bool): A boolean that is set to True if you want to explode the edges in the graph. Defaults to False.
+    ///     edge_color (str): A string defining the colour of the edges in the graph. Defaults to "#000000".
+    ///     shape (str): A string defining what the node looks like. Defaults to "dot".
     ///             There are two types of nodes. One type has the label inside of it and the other type has the label underneath it.
     ///             The types with the label inside of it are: ellipse, circle, database, box, text.
     ///             The ones with the label outside of it are: image, circularImage, diamond, dot, star, triangle, triangleDown, square and icon.
-    ///         node_image (str, optional): An optional string defining the url of a custom node image.
-    ///         edge_weight (str, optional): An optional string defining the name of the property where edge weight is set on your Raphtory graph.
-    ///             If not provided, the edge weight is set to `1.0` for all edges.
-    ///         edge_label (str): An optional string defining the name of the property where edge label is set on your Raphtory graph. By default, an empty string as the label is set.
-    ///         notebook (bool): A boolean that is set to True if using jupyter notebook. Defaults to False
-    ///         kwargs: Additional keyword arguments that are passed to the pyvis Network class.
+    ///     node_image (str, optional): An optional node property used as the url of a custom node image. Use together with `shape="image"`.
+    ///     edge_weight (str, optional): An optional string defining the name of the property where edge weight is set on your Raphtory graph.
+    ///         If provided, the default weight for edges that are missing the property is 1.0.
+    ///     edge_label (str, optional): An optional string defining the name of the property where edge label is set on your Raphtory graph. By default, the edge layer is used as the label.
+    ///     colour_nodes_by_type (bool): If True, nodes with different types have different colours. Defaults to False.
+    ///     directed (bool): Visualise the graph as directed. Defaults to True.
+    ///     notebook (bool): A boolean that is set to True if using jupyter notebook. Defaults to False.
+    ///     kwargs: Additional keyword arguments that are passed to the pyvis Network class.
     ///
-    ///     Returns:
+    /// Returns:
     ///         A pyvis network
-    #[pyo3(signature = (explode_edges=false, edge_color="#000000", shape=None, node_image=None, edge_weight=None, edge_label=None, colour_nodes_by_type=false, notebook=false, **kwargs))]
+    #[pyo3(signature = (explode_edges=false, edge_color="#000000", shape="dot", node_image=None, edge_weight=None, edge_label=None, colour_nodes_by_type=false, directed=true, notebook=false, **kwargs))]
     pub fn to_pyvis<'py>(
         &self,
-        explode_edges: Option<bool>,
+        explode_edges: bool,
         edge_color: Option<&str>,
-        shape: Option<&str>,
+        shape: &str,
         node_image: Option<&str>,
         edge_weight: Option<&str>,
         edge_label: Option<&str>,
-        colour_nodes_by_type: Option<bool>,
-        notebook: Option<bool>,
-        kwargs: Option<&Bound<'py, PyDict>>,
+        colour_nodes_by_type: bool,
+        directed: bool,
+        notebook: bool,
+        kwargs: Option<Bound<'py, PyDict>>,
         py: Python<'py>,
     ) -> PyResult<Bound<'py, PyAny>> {
         let pyvis = PyModule::import(py, "pyvis.network")?;
         let network = pyvis.getattr("Network")?;
-        let vis_graph = network.call(("notebook", notebook.unwrap_or(true)), kwargs)?;
+        let kwargs = kwargs.unwrap_or_else(|| PyDict::new(py));
+        kwargs.set_item("notebook", notebook)?;
+        kwargs.set_item("directed", directed)?;
+        let vis_graph = network.call((), Some(&kwargs))?;
         let mut groups = HashMap::new();
-        if colour_nodes_by_type.unwrap_or(false) {
+        if colour_nodes_by_type {
             let mut index = 1;
             for node in self.graph.nodes() {
                 let value = node.node_type().unwrap_or(ArcStr::from("_default"));
@@ -66,18 +71,20 @@ impl PyGraphView {
         colours.insert(ArcStr::from("_default"), 0);
 
         for v in self.graph.nodes() {
-            let image = match node_image {
-                Some(image) => v.properties().get(image).unwrap_or(Prop::from(
-                    "https://cdn-icons-png.flaticon.com/512/7584/7584620.png",
-                )),
-                None => Prop::from("https://cdn-icons-png.flaticon.com/512/7584/7584620.png"),
-            };
-            let shape = shape.unwrap_or("dot");
             let kwargs_node = PyDict::new(py);
+            if let Some(image_prop) = node_image {
+                match v.properties().get(image_prop).into_str() {
+                    Some(image) => kwargs_node.set_item("image", image)?,
+                    None => kwargs_node.set_item(
+                        "image",
+                        "https://cdn-icons-png.flaticon.com/512/7584/7584620.png",
+                    )?,
+                }
+            }
+
             kwargs_node.set_item("label", v.name())?;
             kwargs_node.set_item("shape", shape)?;
-            kwargs_node.set_item("image", image)?;
-            if colour_nodes_by_type.unwrap_or(false) {
+            if colour_nodes_by_type {
                 let node_type = v.node_type().unwrap_or(ArcStr::from("_default"));
                 let group = match colours.get(&node_type) {
                     None => {
@@ -94,31 +101,36 @@ impl PyGraphView {
                 vis_graph.call_method("add_node", (v.id(),), Some(&kwargs_node))?;
             }
         }
-        let edges = if explode_edges.unwrap_or(false) {
+        let edges = if explode_edges {
             self.graph.edges().explode()
         } else {
             self.graph.edges().explode_layers()
         };
         for edge in edges {
-            let weight = match edge_weight {
-                Some(weight) => {
-                    let w = edge.properties().get(weight).unwrap_or(Prop::from(0.0f64));
-                    w.unwrap_f64()
-                }
-                None => 0.0f64,
-            };
-            let label = match edge_label {
-                Some(label) => {
-                    let l = edge.properties().get(label).unwrap_or(Prop::from(""));
-                    l.unwrap_str()
-                }
-                None => ArcStr::from(""),
-            };
             let kwargs = PyDict::new(py);
-            kwargs.set_item("value", weight)?;
+            if let Some(weight) = edge_weight {
+                kwargs.set_item(
+                    "value",
+                    edge.properties().get(weight).as_f64().unwrap_or(1.0),
+                )?;
+            };
+            match edge_label {
+                Some(label) => {
+                    if let Some(label) = edge.properties().get(label) {
+                        kwargs.set_item("title", label.to_string())?;
+                    }
+                }
+                None => {
+                    let layer = edge
+                        .layer_name()
+                        .expect("edge is always exploded at least by layer");
+                    if layer != "_default" {
+                        kwargs.set_item("title", layer)?;
+                    }
+                }
+            };
             let edge_col = edge_color.unwrap_or("#000000");
             kwargs.set_item("color", edge_col)?;
-            kwargs.set_item("title", label)?;
             kwargs.set_item("arrowStrikethrough", false)?;
             vis_graph.call_method(
                 "add_edge",
@@ -136,14 +148,14 @@ impl PyGraphView {
     ///     you install Network X with ``pip install networkx``
     ///
     ///     Args:
-    ///         explode_edges (bool): A boolean that is set to True if you want to explode the edges in the graph. By default this is set to False.
-    ///         include_node_properties (bool): A boolean that is set to True if you want to include the node properties in the graph. By default this is set to True.
-    ///         include_edge_properties (bool): A boolean that is set to True if you want to include the edge properties in the graph. By default this is set to True.
-    ///         include_update_history (bool): A boolean that is set to True if you want to include the update histories in the graph. By default this is set to True.
-    ///         include_property_history (bool): A boolean that is set to True if you want to include the histories in the graph. By default this is set to True.
+    ///         explode_edges (bool): A boolean that is set to True if you want to explode the edges in the graph. Defaults to False.
+    ///         include_node_properties (bool): A boolean that is set to True if you want to include the node properties in the graph. Defaults to True.
+    ///         include_edge_properties (bool): A boolean that is set to True if you want to include the edge properties in the graph. Defaults to True.
+    ///         include_update_history (bool): A boolean that is set to True if you want to include the update histories in the graph. Defaults to True.
+    ///         include_property_history (bool): A boolean that is set to True if you want to include the histories in the graph. Defaults to True.
     ///
     ///     Returns:
-    ///         A Networkx MultiDiGraph.
+    ///         MultiDiGraph: A Networkx MultiDiGraph.
     #[pyo3(signature = (explode_edges=false, include_node_properties=true, include_edge_properties=true,include_update_history=true,include_property_history=true))]
     pub fn to_networkx<'py>(
         &self,
