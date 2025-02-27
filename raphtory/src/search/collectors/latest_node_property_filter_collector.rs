abc134213cca83e575d165f06419bac1902f537d
Shivam Kapoor <4599890+iamsmkr@users.noreply.github.com>
2025-02-27 16:34:30 +0000

    add secondary time index to latest node filter collector

--- raphtory/src/search/collectors/latest_node_property_filter_collector.rs
+++ raphtory/src/search/collectors/latest_node_property_filter_collector.rs
@@ -46,11 +46,15 @@ where
     ) -> tantivy::Result<Self::Child> {
         let column_opt_time = segment_reader.fast_fields().column_opt(fields::TIME)?;
         let column_opt_entity_id = segment_reader.fast_fields().column_opt(&self.field)?;
+        let column_opt_secondary_time: Option<Column<u64>> = segment_reader
+            .fast_fields()
+            .column_opt(fields::SECONDARY_TIME)?;
 
         Ok(LatestNodePropertyFilterSegmentCollector {
             prop_id: self.prop_id,
             column_opt_time,
             column_opt_entity_id,
+            column_opt_secondary_time,
             segment_ord: segment_local_id,
             unique_entity_ids: HashSet::new(),
             graph: self.graph.clone(),
@@ -77,6 +81,7 @@ pub struct LatestNodePropertyFilterSegmentCollector<G> {
     prop_id: usize,
     column_opt_time: Option<Column<i64>>,
     column_opt_entity_id: Option<Column<u64>>,
+    column_opt_secondary_time: Option<Column<u64>>,
     segment_ord: u32,
     unique_entity_ids: HashSet<u64>,
     graph: G,
@@ -98,6 +103,10 @@ where
             .column_opt_entity_id
             .as_ref()
             .and_then(|col| col.values_for_doc(doc_id).next());
+        let opt_secondary_time = self
+            .column_opt_secondary_time
+            .as_ref()
+            .and_then(|col| col.values_for_doc(doc_id).next());
 
         // let searcher = self.reader.searcher();
         // let schema = searcher.schema();
@@ -106,14 +115,16 @@ where
         //     .unwrap();
         // println!("doc = {:?}", doc.to_json(schema));
 
-        if let (Some(time), Some(entity_id)) = (opt_time, opt_entity_id) {
+        if let (Some(time), Some(entity_id), Some(secondary_time)) =
+            (opt_time, opt_entity_id, opt_secondary_time)
+        {
             // If is_node_prop_update_latest check is true for a doc, we can ignore validating all other docs
             // against expensive is_node_prop_update_latest check for a given node id.
             if !self.unique_entity_ids.contains(&entity_id) {
                 if self.graph.is_node_prop_update_latest(
                     self.prop_id,
                     VID(entity_id as usize),
-                    TimeIndexEntry::end(time),
+                    TimeIndexEntry::new(time, secondary_time as usize),
                 ) {
                     self.unique_entity_ids.insert(entity_id);
                 }
