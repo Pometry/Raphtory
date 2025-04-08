f1bf8e17048a9a2f55bfe61588c3f07854a537ec
Pedro Rico Pinazo <ricopinazo@gmail.com>
2025-04-08 11:09:16 +0100

    simplify build.rs

--- raphtory-graphql/build.rs
+++ raphtory-graphql/build.rs
@@ -1,9 +1,6 @@
 fn main() {
     let ui_index_path =
         std::env::var("RAPHTORY_UI_INDEX_PATH").unwrap_or("../resources/index.html".to_owned());
-    println!(
-        "{}",
-        format!("cargo:rustc-env=RAPHTORY_UI_INDEX_PATH={ui_index_path}")
-    );
+    println!("cargo:rustc-env=RAPHTORY_UI_INDEX_PATH={ui_index_path}");
     println!("cargo:rerun-if-env-changed=RAPHTORY_UI_INDEX_PATH")
 }
