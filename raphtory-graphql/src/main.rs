7d9f4ef640e4df9e4b41e0bbe9b357987d3b5903
Pedro Rico Pinazo <ricopinazo@gmail.com>
2025-04-16 13:01:17 +0100

    add option to output graphql schema

--- raphtory-graphql/src/main.rs
+++ raphtory-graphql/src/main.rs
@@ -10,6 +10,7 @@ use raphtory_graphql::{
             DEFAULT_TRACING_ENABLED,
         },
     },
+    model::App,
     server::DEFAULT_PORT,
     GraphServer,
 };
@@ -52,29 +53,36 @@ struct Args {
 
     #[arg(long, default_value_t = DEFAULT_AUTH_ENABLED_FOR_READS)]
     auth_enabled_for_reads: bool,
+
+    #[arg(long, default_value = None)]
+    output_schema: Option<String>,
 }
 
 #[tokio::main]
 async fn main() -> IoResult<()> {
     let args = Args::parse();
+    if let Some(output_schema) = args.output_schema {
+        let schema = App::create_schema().finish().unwrap();
+        std::fs::write(output_schema, schema.sdl()).unwrap();
+    } else {
+        let app_config = Some(
+            AppConfigBuilder::new()
+                .with_cache_capacity(args.cache_capacity)
+                .with_cache_tti_seconds(args.cache_tti_seconds)
+                .with_log_level(args.log_level)
+                .with_tracing(args.tracing)
+                .with_otlp_agent_host(args.otlp_agent_host)
+                .with_otlp_agent_port(args.otlp_agent_port)
+                .with_otlp_tracing_service_name(args.otlp_tracing_service_name)
+                .with_auth_public_key(args.auth_public_key)
+                .expect(PUBLIC_KEY_DECODING_ERR_MSG)
+                .with_auth_enabled_for_reads(args.auth_enabled_for_reads)
+                .build(),
+        );
 
-    let app_config = Some(
-        AppConfigBuilder::new()
-            .with_cache_capacity(args.cache_capacity)
-            .with_cache_tti_seconds(args.cache_tti_seconds)
-            .with_log_level(args.log_level)
-            .with_tracing(args.tracing)
-            .with_otlp_agent_host(args.otlp_agent_host)
-            .with_otlp_agent_port(args.otlp_agent_port)
-            .with_otlp_tracing_service_name(args.otlp_tracing_service_name)
-            .with_auth_public_key(args.auth_public_key)
-            .expect(PUBLIC_KEY_DECODING_ERR_MSG)
-            .with_auth_enabled_for_reads(args.auth_enabled_for_reads)
-            .build(),
-    );
-
-    GraphServer::new(args.working_dir, app_config, None)?
-        .run_with_port(args.port)
-        .await?;
+        GraphServer::new(args.working_dir, app_config, None)?
+            .run_with_port(args.port)
+            .await?;
+    }
     Ok(())
 }
