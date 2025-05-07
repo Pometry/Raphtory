0a538a12a094d1cc4deb05afac420a02a3c7a9f1
arienandalibi <arienandalibi2@gmail.com>
2025-05-07 02:54:41 -0400

    Updated HistoryImplemented to have merge() and compose() functions. They return MergedHistory and CompositeHistory wrapped in HistoryImplemented objects respectively. MergedHistory holds two InternalHistoryOps objects while CompositeHistory stores a vector of arbitrarily many. MergedHistory is slightly more efficient but if nested with itself, can lose efficiency.

--- examples/rust/src/bin/node_history/main.rs
+++ examples/rust/src/bin/node_history/main.rs
@@ -1,20 +1,15 @@
 #![allow(unused_imports)]
 #![allow(dead_code)]
 
-use std::iter::Peekable;
-use std::slice::Iter;
 use chrono::{DateTime, Utc};
 use itertools::Itertools;
-use raphtory::core::entities::LayerIds;
-use raphtory::core::entities::nodes::node_ref::AsNodeRef;
-use raphtory::prelude::*;
-use raphtory::core::storage::timeindex::{AsTime, TimeIndex, TimeIndexEntry};
-use raphtory::core::utils::iter;
-use raphtory::db::api::properties::internal::{TemporalPropertiesOps, TemporalPropertyViewOps};
-use raphtory::db::api::view::{Base, BoxedLIter, IntoDynBoxed};
+use raphtory::core::storage::timeindex::{AsTime, TimeIndexEntry};
+use raphtory::db::api::properties::internal::TemporalPropertiesOps;
 use raphtory::db::api::view::internal::{InternalLayerOps, TimeSemantics};
+use raphtory::db::api::view::{BoxedLIter, IntoDynBoxed};
 use raphtory::db::graph::edge::EdgeView;
 use raphtory::db::graph::node::NodeView;
+use raphtory::prelude::*;
 
 pub struct RaphtoryTime {
     time_index: TimeIndexEntry
@@ -22,11 +17,9 @@ pub struct RaphtoryTime {
 
 impl RaphtoryTime {
     fn apply(time_index: TimeIndexEntry) -> Self { Self{time_index} }
-    
     pub fn dt(&self) -> Option<DateTime<Utc>> {
         self.time_index.dt()
     }
-    
     pub fn epoch(&self) -> i64 { 
         self.time_index.t()
     }
@@ -37,118 +30,103 @@ pub trait InternalHistoryOps {
     fn iter_rev(&self) -> BoxedLIter<TimeIndexEntry>;
 }
 
-pub trait HistoryObject {
-    fn iter(&self) -> BoxedLIter<TimeIndexEntry>;
-    fn iter_rev(&self) -> BoxedLIter<TimeIndexEntry>;
-    fn earliest_time(&self) -> Option<RaphtoryTime>;
-    fn latest_time(&self) -> Option<RaphtoryTime>;
-    fn print(&self, prelude: &str);
-}
-
-// TODO: Add iterators for deletions of edges, add nodes later
+// FIXME: Doesn't support deletions of edges yet
 pub struct HistoryImplemented<T>(T);
 
 impl<T: InternalHistoryOps> HistoryImplemented<T> {
     pub fn new(item: T) -> Self {
         Self(item)
     }
-    
+
     // see if theres a way to get secondary temporal information (two entries for the same time value)
     
     // Currently wont include any deletions which are available on edges but are not available on nodes
     // We will have node deletions soon, and we'll get 4 iterators in here, for insertions and deletions
     // TODO: Implement tests (filters, combinations of filters, layer filter on edges, windowing filter)
+    // find some non-trivial data (like AML example) and run some tests. 
+    // try to do some operations using the things above and make sure the desired effect is observed on the history object.
 }
 
-impl<T: InternalHistoryOps> HistoryObject for HistoryImplemented<T> {
-    fn iter(&self) -> BoxedLIter<TimeIndexEntry> {
+impl<T: InternalHistoryOps> HistoryImplemented<T> {
+    pub fn iter(&self) -> BoxedLIter<TimeIndexEntry> {
         self.0.iter()
     }
 
-    fn iter_rev(&self) -> BoxedLIter<TimeIndexEntry> {
+    pub fn iter_rev(&self) -> BoxedLIter<TimeIndexEntry> {
         self.0.iter_rev()
     }
+
+    pub fn merge<R: InternalHistoryOps>(self, right: HistoryImplemented<R>) -> HistoryImplemented<MergedHistory<T, R>> {
+        HistoryImplemented::new(MergedHistory::new(self.0, right.0))
+    }
     
-    fn earliest_time(&self) -> Option<RaphtoryTime> {
+    // FIXME: Ask if we want this vec to contain InternalHistoryOps objects or HistoryImplemented objects (like above). Also ask about 'static.
+    pub fn compose(self, mut others: Vec<Box<dyn InternalHistoryOps>>) -> HistoryImplemented<CompositeHistory>
+    where 
+        T: 'static 
+    {
+        others.push(Box::new(self.0));
+        HistoryImplemented::new(CompositeHistory::new(others))
+    }
+
+    pub fn earliest_time(&self) -> Option<RaphtoryTime> {
         self.0.iter().next().map(|t| RaphtoryTime::apply(t.clone()))
     }
 
-    fn latest_time(&self) -> Option<RaphtoryTime> {
+    pub fn latest_time(&self) -> Option<RaphtoryTime> {
         self.0.iter_rev().next().map(|t| RaphtoryTime::apply(t.clone()))
     }
 
-    fn print(&self, prelude: &str) {
+    pub fn print(&self, prelude: &str) {
         println!("{}{:?}", prelude, self.0.iter().collect::<Vec<_>>());
     }
 }
 
-// might be a good idea to not store different HistoryObjects and instead store Vec<Box<dyn InternalHistoryOps>>
+/// Separate from CompositeHistory in that it can only hold two items. They can be nested.
+/// Slightly more efficient because we are calling iter.merge() instead of iter.kmerge(). Efficiency benefits are lost if we nest these objects too much
+pub struct MergedHistory<L, R> {
+    left: L,
+    right: R
+}
+
+impl<L: InternalHistoryOps, R: InternalHistoryOps> MergedHistory<L, R> {
+    pub fn new(left: L, right: R) -> Self {
+        Self { left, right }
+    }
+}
+
+impl<L: InternalHistoryOps, R: InternalHistoryOps> InternalHistoryOps for MergedHistory<L, R> {
+    fn iter(&self) -> BoxedLIter<TimeIndexEntry> {
+        self.left.iter().merge(self.right.iter()).into_dyn_boxed()
+    }
+    
+    fn iter_rev(&self) -> BoxedLIter<TimeIndexEntry> {
+        self.left.iter_rev().merge_by(self.right.iter_rev(), |a, b| a >= b).into_dyn_boxed()
+    }
+}
+
+/// Holds a vector of multiple objects implementing InternalHistoryOps
 pub struct CompositeHistory {
-    history_objects: Vec<Box<dyn HistoryObject>>
+    history_objects: Vec<Box<dyn InternalHistoryOps>>
 }
 
 impl CompositeHistory {
-    fn new(history_objects: Vec<Box<dyn HistoryObject>>) -> Self {
+    pub fn new(history_objects: Vec<Box<dyn InternalHistoryOps>>) -> Self {
         Self { history_objects }
-    }
-    
-    fn create_empty() -> Self {
-        Self { history_objects: Vec::new() }
-    }
-
-    fn add_boxed_history_object(&mut self, history_object: Box<dyn HistoryObject>) {
-        self.history_objects.push(history_object);
-    }
-    
-    fn add_history_object<T: InternalHistoryOps + 'static>(&mut self, history_object: HistoryImplemented<T>) {
-        self.history_objects.push(Box::new(history_object));
-    }
-
-    // FIXME: unable to get this working, can't add items directly to the composite history object
-    // fn add_item(&mut self, item: Box<dyn InternalHistoryOps>) {
-    //     self.history_objects.push(HistoryObject::new_boxed(item));
-    // }
-
-    // Does this implementation improve performance? It avoids calling each HistoryObject's earliest_time or latest_time implementations
-    // It avoids a TimeIndexEntry clone operation and a RaphtoryTime object creation for each HistoryObject
-    fn earliest_time(&self) -> Option<RaphtoryTime> {
-        let mut earliest_time: Option<TimeIndexEntry> = None;
-        for history_object in &self.history_objects {
-            let mut tmp_iter = history_object.iter();
-            if let Some(tmp_time) = tmp_iter.next() {
-                if earliest_time.is_none() || tmp_time.t() < earliest_time?.t() {
-                    earliest_time = Some(tmp_time);
-                }
-            }
-        }
-        earliest_time.map(|t| RaphtoryTime::apply(t.clone()))
-    }
-
-    fn latest_time(&self) -> Option<RaphtoryTime> {
-        let mut latest_time: Option<TimeIndexEntry> = None;
-        for history_object in &self.history_objects {
-            let mut tmp_iter = history_object.iter_rev();
-            if let Some(tmp_time) = tmp_iter.next() {
-                if latest_time.is_none() || tmp_time.t() > latest_time?.t() {
-                    latest_time = Some(tmp_time);
-                }
-            }
-        }
-        latest_time.map(|t| RaphtoryTime::apply(t.clone()))
-    }
-    
-    fn print(&self, prelude: &str) {
-        let mut count = 0;
-        println!("{}", prelude);
-        for history_object in &self.history_objects {
-            print!("\tHistory Object #{}: ", count);
-            history_object.print("");
-            count = count + 1;
-        }
     }
 }
 
-impl<G: TimeSemantics + InternalLayerOps> InternalHistoryOps for NodeView<G, G> {
+impl InternalHistoryOps for CompositeHistory {
+    fn iter(&self) -> BoxedLIter<TimeIndexEntry> {
+        self.history_objects.iter().map(|object| object.iter()).kmerge().into_dyn_boxed()
+    }
+
+    fn iter_rev(&self) -> BoxedLIter<TimeIndexEntry> {
+        self.history_objects.iter().map(|object| object.iter_rev()).kmerge_by(|a, b| a >= b).into_dyn_boxed()
+    }
+}
+
+impl<G: TimeSemantics> InternalHistoryOps for NodeView<G> {
     fn iter(&self) -> BoxedLIter<TimeIndexEntry> {
         self.graph.node_history(self.node)
     }
@@ -161,7 +139,7 @@ impl<G: TimeSemantics + InternalLayerOps> InternalHistoryOps for NodeView<G, G> 
     }
 }
 
-impl<G: TimeSemantics + InternalLayerOps> InternalHistoryOps for EdgeView<G, G> {
+impl<G: TimeSemantics + InternalLayerOps> InternalHistoryOps for EdgeView<G> {
     fn iter(&self) -> BoxedLIter<TimeIndexEntry> {
         self.graph.edge_history(self.edge, self.graph.layer_ids())
     }
@@ -205,21 +183,20 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let meeting_prop_id = edge.get_temporal_prop_id("meeting");
 
     // create gandalf node history object
-    let gandalf_node_history_object = HistoryImplemented::new(gandalf_node);
+    let gandalf_node_history_object = HistoryImplemented::new(gandalf_node.clone());
     gandalf_node_history_object.print("Gandalf Node history: ");
 
     // create Harry node history object
-    let harry_node_history_object = HistoryImplemented::new(harry_node);
+    let harry_node_history_object = HistoryImplemented::new(harry_node.clone());
     harry_node_history_object.print("Harry Node history: ");
 
     // create edge history object
-    let edge_history_object = HistoryImplemented::new(edge);
+    let edge_history_object = HistoryImplemented::new(edge.clone());
     edge_history_object.print("Gandalf-Harry Edge history: ");
     
     // create Composite History Object
-    let tmp_vector: Vec<Box<dyn HistoryObject>> = vec![Box::new(gandalf_node_history_object), Box::new(harry_node_history_object)];
-    let mut composite_history_object = CompositeHistory::new(tmp_vector);
-    composite_history_object.add_history_object(edge_history_object);
+    let tmp_vector: Vec<Box<dyn InternalHistoryOps>> = vec![Box::new(harry_node), Box::new(edge)];
+    let composite_history_object = gandalf_node_history_object.compose(tmp_vector);
     composite_history_object.print("Composite history: ");
     
     println!("Composite history's earliest time: {}", composite_history_object.earliest_time().unwrap().epoch());
