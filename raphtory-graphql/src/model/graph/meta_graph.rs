8900d5f0402e29dec447f041d603a20b87cf3c3c
Ben Steer <ben.steer@pometry.com>
2025-05-13 22:53:13 +0100

    par metagraph

--- raphtory-graphql/src/model/graph/meta_graph.rs
+++ raphtory-graphql/src/model/graph/meta_graph.rs
@@ -1,8 +1,9 @@
 use crate::{model::graph::property::GqlProperty, paths::ExistingGraphFolder};
 use dynamic_graphql::{ResolvedObject, ResolvedObjectFields, SimpleObject};
 use raphtory::{core::utils::errors::GraphError, serialise::metadata::GraphMetadata};
+use tokio::task::spawn_blocking;
 
-#[derive(ResolvedObject)]
+#[derive(ResolvedObject, Clone)]
 pub(crate) struct MetaGraph {
     folder: ExistingGraphFolder,
 }
@@ -16,23 +17,43 @@ impl MetaGraph {
 #[ResolvedObjectFields]
 impl MetaGraph {
     async fn name(&self) -> Option<String> {
-        self.folder.get_graph_name().ok()
+        let self_clone = self.clone();
+        spawn_blocking(move || self_clone.folder.get_graph_name().ok())
+            .await
+            .unwrap()
     }
     async fn path(&self) -> String {
-        self.folder.get_original_path_str().to_owned()
+        let self_clone = self.clone();
+        spawn_blocking(move || self_clone.folder.get_original_path_str().to_owned())
+            .await
+            .unwrap()
     }
     async fn created(&self) -> Result<i64, GraphError> {
-        self.folder.created()
+        let self_clone = self.clone();
+        spawn_blocking(move || self_clone.folder.created())
+            .await
+            .unwrap()
     }
     async fn last_opened(&self) -> Result<i64, GraphError> {
-        self.folder.last_opened()
+        let self_clone = self.clone();
+        spawn_blocking(move || self_clone.folder.last_opened())
+            .await
+            .unwrap()
     }
     async fn last_updated(&self) -> Result<i64, GraphError> {
-        self.folder.last_updated()
+        let self_clone = self.clone();
+        spawn_blocking(move || self_clone.folder.last_updated())
+            .await
+            .unwrap()
     }
     async fn metadata(&self) -> Result<GqlGraphMetadata, GraphError> {
-        let metadata = self.folder.read_metadata()?;
-        Ok(GqlGraphMetadata::from(metadata))
+        let self_clone = self.clone();
+        spawn_blocking(move || {
+            let metadata = self_clone.folder.read_metadata()?;
+            Ok(GqlGraphMetadata::from(metadata))
+        })
+        .await
+        .unwrap()
     }
 }
 
