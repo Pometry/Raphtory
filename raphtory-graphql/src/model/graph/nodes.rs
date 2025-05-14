0889d4a1c6e79d05c5c990633c286e87b971e60c
Ben Steer <ben.steer@pometry.com>
2025-05-14 23:56:36 +0100

    par nodes

--- raphtory-graphql/src/model/graph/nodes.rs
+++ raphtory-graphql/src/model/graph/nodes.rs
@@ -21,8 +21,9 @@ use raphtory::{
 };
 use raphtory_api::core::entities::VID;
 use std::cmp::Ordering;
+use tokio::{spawn, task::spawn_blocking};
 
-#[derive(ResolvedObject)]
+#[derive(ResolvedObject, Clone)]
 #[graphql(name = "Nodes")]
 pub(crate) struct GqlNodes {
     pub(crate) nn: Nodes<'static, DynamicGraph>,
@@ -51,33 +52,49 @@ impl GqlNodes {
     // LAYERS AND WINDOWS //
     ////////////////////////
     async fn default_layer(&self) -> Self {
-        self.update(self.nn.default_layer())
+        let self_clone = self.clone();
+        spawn_blocking(move || self_clone.update(self_clone.nn.default_layer()))
+            .await
+            .unwrap()
     }
 
     async fn layers(&self, names: Vec<String>) -> Self {
-        self.update(self.nn.valid_layers(names))
+        let self_clone = self.clone();
+        spawn_blocking(move || self_clone.update(self_clone.nn.valid_layers(names)))
+            .await
+            .unwrap()
     }
 
     async fn exclude_layers(&self, names: Vec<String>) -> Self {
-        self.update(self.nn.exclude_valid_layers(names))
+        let self_clone = self.clone();
+        spawn_blocking(move || self_clone.update(self_clone.nn.exclude_valid_layers(names)))
+            .await
+            .unwrap()
     }
 
     async fn layer(&self, name: String) -> Self {
-        self.update(self.nn.valid_layers(name))
+        let self_clone = self.clone();
+        spawn_blocking(move || self_clone.update(self_clone.nn.valid_layers(name)))
+            .await
+            .unwrap()
     }
 
     async fn exclude_layer(&self, name: String) -> Self {
-        self.update(self.nn.exclude_valid_layers(name))
+        let self_clone = self.clone();
+        spawn_blocking(move || self_clone.update(self_clone.nn.exclude_valid_layers(name)))
+            .await
+            .unwrap()
     }
 
-    fn rolling(
+    async fn rolling(
         &self,
         window_str: Option<String>,
         window_int: Option<i64>,
         step_str: Option<String>,
         step_int: Option<i64>,
     ) -> Result<GqlNodesWindowSet, GraphError> {
-        match (window_str, window_int) {
+        let self_clone = self.clone();
+        spawn_blocking(move || match (window_str, window_int) {
             (Some(_), Some(_)) => Err(WrongNumOfArgs(
                 "window_str".to_string(),
                 "window_int".to_string(),
@@ -87,7 +104,7 @@ impl GqlNodes {
                     return Err(MismatchedIntervalTypes);
                 }
                 Ok(GqlNodesWindowSet::new(
-                    self.nn.rolling(window_int, step_int)?,
+                    self_clone.nn.rolling(window_int, step_int)?,
                 ))
             }
             (Some(window_str), None) => {
@@ -95,27 +112,36 @@ impl GqlNodes {
                     return Err(MismatchedIntervalTypes);
                 }
                 Ok(GqlNodesWindowSet::new(
-                    self.nn.rolling(window_str, step_str)?,
+                    self_clone.nn.rolling(window_str, step_str)?,
                 ))
             }
             (None, None) => return Err(NoIntervalProvided),
-        }
+        })
+        .await
+        .unwrap()
     }
 
-    fn expanding(
+    async fn expanding(
         &self,
         step_str: Option<String>,
         step_int: Option<i64>,
     ) -> Result<GqlNodesWindowSet, GraphError> {
-        match (step_str, step_int) {
+        let self_clone = self.clone();
+        spawn_blocking(move || match (step_str, step_int) {
             (Some(_), Some(_)) => Err(WrongNumOfArgs(
                 "step_str".to_string(),
                 "step_int".to_string(),
             )),
-            (None, Some(step_int)) => Ok(GqlNodesWindowSet::new(self.nn.expanding(step_int)?)),
-            (Some(step_str), None) => Ok(GqlNodesWindowSet::new(self.nn.expanding(step_str)?)),
+            (None, Some(step_int)) => {
+                Ok(GqlNodesWindowSet::new(self_clone.nn.expanding(step_int)?))
+            }
+            (Some(step_str), None) => {
+                Ok(GqlNodesWindowSet::new(self_clone.nn.expanding(step_str)?))
+            }
             (None, None) => return Err(NoIntervalProvided),
-        }
+        })
+        .await
+        .unwrap()
     }
 
     async fn window(&self, start: i64, end: i64) -> Self {
@@ -127,7 +153,10 @@ impl GqlNodes {
     }
 
     async fn latest(&self) -> Self {
-        self.update(self.nn.latest())
+        let self_clone = self.clone();
+        spawn_blocking(move || self_clone.update(self_clone.nn.latest()))
+            .await
+            .unwrap()
     }
 
     async fn snapshot_at(&self, time: i64) -> Self {
@@ -135,7 +164,10 @@ impl GqlNodes {
     }
 
     async fn snapshot_latest(&self) -> Self {
-        self.update(self.nn.snapshot_latest())
+        let self_clone = self.clone();
+        spawn_blocking(move || self_clone.update(self_clone.nn.snapshot_latest()))
+            .await
+            .unwrap()
     }
 
     async fn before(&self, time: i64) -> Self {
@@ -159,96 +191,107 @@ impl GqlNodes {
     }
 
     async fn type_filter(&self, node_types: Vec<String>) -> Self {
-        self.update(self.nn.type_filter(&node_types))
+        let self_clone = self.clone();
+        spawn_blocking(move || self_clone.update(self_clone.nn.type_filter(&node_types)))
+            .await
+            .unwrap()
     }
 
     async fn node_filter(&self, filter: NodeFilter) -> Result<Self, GraphError> {
-        filter.validate()?;
-        let filter: CompositeNodeFilter = filter.try_into()?;
-        let filtered_nodes = self.nn.filter_nodes(filter)?;
-        Ok(self.update(filtered_nodes.into_dyn()))
+        let self_clone = self.clone();
+        spawn_blocking(move || {
+            filter.validate()?;
+            let filter: CompositeNodeFilter = filter.try_into()?;
+            let filtered_nodes = self_clone.nn.filter_nodes(filter)?;
+            Ok(self_clone.update(filtered_nodes.into_dyn()))
+        })
+        .await
+        .unwrap()
     }
 
     async fn apply_views(&self, views: Vec<NodesViewCollection>) -> Result<GqlNodes, GraphError> {
         let mut return_view: GqlNodes = GqlNodes::new(self.nn.clone());
+        spawn(async move {
+            for view in views {
+                let mut count = 0;
+                if let Some(_) = view.default_layer {
+                    count += 1;
+                    return_view = return_view.default_layer().await;
+                }
+                if let Some(layers) = view.layers {
+                    count += 1;
+                    return_view = return_view.layers(layers).await;
+                }
+                if let Some(layers) = view.exclude_layers {
+                    count += 1;
+                    return_view = return_view.exclude_layers(layers).await;
+                }
+                if let Some(layer) = view.layer {
+                    count += 1;
+                    return_view = return_view.layer(layer).await;
+                }
+                if let Some(layer) = view.exclude_layer {
+                    count += 1;
+                    return_view = return_view.exclude_layer(layer).await;
+                }
+                if let Some(window) = view.window {
+                    count += 1;
+                    return_view = return_view.window(window.start, window.end).await;
+                }
+                if let Some(time) = view.at {
+                    count += 1;
+                    return_view = return_view.at(time).await;
+                }
+                if let Some(_) = view.latest {
+                    count += 1;
+                    return_view = return_view.latest().await;
+                }
+                if let Some(time) = view.snapshot_at {
+                    count += 1;
+                    return_view = return_view.snapshot_at(time).await;
+                }
+                if let Some(_) = view.snapshot_latest {
+                    count += 1;
+                    return_view = return_view.snapshot_latest().await;
+                }
+                if let Some(time) = view.before {
+                    count += 1;
+                    return_view = return_view.before(time).await;
+                }
+                if let Some(time) = view.after {
+                    count += 1;
+                    return_view = return_view.after(time).await;
+                }
+                if let Some(window) = view.shrink_window {
+                    count += 1;
+                    return_view = return_view.shrink_window(window.start, window.end).await;
+                }
+                if let Some(time) = view.shrink_start {
+                    count += 1;
+                    return_view = return_view.shrink_start(time).await;
+                }
+                if let Some(time) = view.shrink_end {
+                    count += 1;
+                    return_view = return_view.shrink_end(time).await;
+                }
+                if let Some(types) = view.type_filter {
+                    count += 1;
+                    return_view = return_view.type_filter(types).await;
+                }
+                if let Some(node_filter) = view.node_filter {
+                    count += 1;
+                    return_view = return_view.node_filter(node_filter).await?;
+                }
 
-        for view in views {
-            let mut count = 0;
-            if let Some(_) = view.default_layer {
-                count += 1;
-                return_view = return_view.default_layer().await;
-            }
-            if let Some(layers) = view.layers {
-                count += 1;
-                return_view = return_view.layers(layers).await;
-            }
-            if let Some(layers) = view.exclude_layers {
-                count += 1;
-                return_view = return_view.exclude_layers(layers).await;
-            }
-            if let Some(layer) = view.layer {
-                count += 1;
-                return_view = return_view.layer(layer).await;
-            }
-            if let Some(layer) = view.exclude_layer {
-                count += 1;
-                return_view = return_view.exclude_layer(layer).await;
-            }
-            if let Some(window) = view.window {
-                count += 1;
-                return_view = return_view.window(window.start, window.end).await;
-            }
-            if let Some(time) = view.at {
-                count += 1;
-                return_view = return_view.at(time).await;
-            }
-            if let Some(_) = view.latest {
-                count += 1;
-                return_view = return_view.latest().await;
-            }
-            if let Some(time) = view.snapshot_at {
-                count += 1;
-                return_view = return_view.snapshot_at(time).await;
-            }
-            if let Some(_) = view.snapshot_latest {
-                count += 1;
-                return_view = return_view.snapshot_latest().await;
-            }
-            if let Some(time) = view.before {
-                count += 1;
-                return_view = return_view.before(time).await;
-            }
-            if let Some(time) = view.after {
-                count += 1;
-                return_view = return_view.after(time).await;
-            }
-            if let Some(window) = view.shrink_window {
-                count += 1;
-                return_view = return_view.shrink_window(window.start, window.end).await;
-            }
-            if let Some(time) = view.shrink_start {
-                count += 1;
-                return_view = return_view.shrink_start(time).await;
-            }
-            if let Some(time) = view.shrink_end {
-                count += 1;
-                return_view = return_view.shrink_end(time).await;
-            }
-            if let Some(types) = view.type_filter {
-                count += 1;
-                return_view = return_view.type_filter(types).await;
-            }
-            if let Some(node_filter) = view.node_filter {
-                count += 1;
-                return_view = return_view.node_filter(node_filter).await?;
+                if count > 1 {
+                    return Err(GraphError::TooManyViewsSet);
+                }
             }
 
-            if count > 1 {
-                return Err(GraphError::TooManyViewsSet);
-            }
-        }
-
-        Ok(return_view)
+            Ok(return_view)
+        })
+        .await
+        .unwrap()
     }
 
     /////////////////
@@ -256,50 +299,56 @@ impl GqlNodes {
     /////////////////
 
     async fn sorted(&self, sort_bys: Vec<NodeSortBy>) -> Self {
-        let sorted: Index<VID> = self
-            .nn
-            .iter()
-            .sorted_by(|first_node, second_node| {
-                sort_bys
-                    .iter()
-                    .fold(Ordering::Equal, |current_ordering, sort_by| {
-                        current_ordering.then_with(|| {
-                            let ordering = if sort_by.id == Some(true) {
-                                first_node.id().partial_cmp(&second_node.id())
-                            } else if let Some(sort_by_time) = sort_by.time.as_ref() {
-                                let (first_time, second_time) = match sort_by_time {
-                                    SortByTime::Latest => {
-                                        (first_node.latest_time(), second_node.latest_time())
-                                    }
-                                    SortByTime::Earliest => {
-                                        (first_node.earliest_time(), second_node.earliest_time())
-                                    }
-                                };
-                                first_time.partial_cmp(&second_time)
-                            } else if let Some(sort_by_property) = sort_by.property.as_ref() {
-                                let first_prop_maybe =
-                                    first_node.properties().get(sort_by_property);
-                                let second_prop_maybe =
-                                    second_node.properties().get(sort_by_property);
-                                first_prop_maybe.partial_cmp(&second_prop_maybe)
-                            } else {
-                                None
-                            };
-                            if let Some(ordering) = ordering {
-                                if sort_by.reverse == Some(true) {
-                                    ordering.reverse()
+        let self_clone = self.clone();
+        spawn_blocking(move || {
+            let sorted: Index<VID> = self_clone
+                .nn
+                .iter()
+                .sorted_by(|first_node, second_node| {
+                    sort_bys
+                        .iter()
+                        .fold(Ordering::Equal, |current_ordering, sort_by| {
+                            current_ordering.then_with(|| {
+                                let ordering = if sort_by.id == Some(true) {
+                                    first_node.id().partial_cmp(&second_node.id())
+                                } else if let Some(sort_by_time) = sort_by.time.as_ref() {
+                                    let (first_time, second_time) = match sort_by_time {
+                                        SortByTime::Latest => {
+                                            (first_node.latest_time(), second_node.latest_time())
+                                        }
+                                        SortByTime::Earliest => (
+                                            first_node.earliest_time(),
+                                            second_node.earliest_time(),
+                                        ),
+                                    };
+                                    first_time.partial_cmp(&second_time)
+                                } else if let Some(sort_by_property) = sort_by.property.as_ref() {
+                                    let first_prop_maybe =
+                                        first_node.properties().get(sort_by_property);
+                                    let second_prop_maybe =
+                                        second_node.properties().get(sort_by_property);
+                                    first_prop_maybe.partial_cmp(&second_prop_maybe)
                                 } else {
-                                    ordering
+                                    None
+                                };
+                                if let Some(ordering) = ordering {
+                                    if sort_by.reverse == Some(true) {
+                                        ordering.reverse()
+                                    } else {
+                                        ordering
+                                    }
+                                } else {
+                                    Ordering::Equal
                                 }
-                            } else {
-                                Ordering::Equal
-                            }
+                            })
                         })
-                    })
-            })
-            .map(|node_view| node_view.node)
-            .collect();
-        GqlNodes::new(self.nn.indexed(sorted))
+                })
+                .map(|node_view| node_view.node)
+                .collect();
+            GqlNodes::new(self_clone.nn.indexed(sorted))
+        })
+        .await
+        .unwrap()
     }
 
     ////////////////////////
@@ -319,19 +368,33 @@ impl GqlNodes {
     /////////////////
 
     async fn count(&self) -> usize {
-        self.iter().count()
+        let self_clone = self.clone();
+        spawn_blocking(move || self_clone.iter().count())
+            .await
+            .unwrap()
     }
 
     async fn page(&self, limit: usize, offset: usize) -> Vec<GqlNode> {
-        let start = offset * limit;
-        self.iter().skip(start).take(limit).collect()
+        let self_clone = self.clone();
+        spawn_blocking(move || {
+            let start = offset * limit;
+            self_clone.iter().skip(start).take(limit).collect()
+        })
+        .await
+        .unwrap()
     }
 
     async fn list(&self) -> Vec<GqlNode> {
-        self.iter().collect()
+        let self_clone = self.clone();
+        spawn_blocking(move || self_clone.iter().collect())
+            .await
+            .unwrap()
     }
 
     async fn ids(&self) -> Vec<String> {
-        self.nn.name().collect()
+        let self_clone = self.clone();
+        spawn_blocking(move || self_clone.nn.name().collect())
+            .await
+            .unwrap()
     }
 }
