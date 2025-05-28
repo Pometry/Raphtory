2c173ebcbd8e6c4ae2ed461f8c96c7a58e81d7bf
Lucas Jeub <lucas.jeub@pometry.com>
2025-05-28 12:10:24 +0200

    minor activation script cleanup

--- scripts/deactivate_private_storage.py
+++ scripts/deactivate_private_storage.py
@@ -1,9 +1,7 @@
 #!/usr/bin/env python3
-import subprocess
 from pathlib import Path
 import re
 
-directory = "./pometry-storage"
 root_dir = Path(__file__).parent.parent
 toml_file = root_dir / "Cargo.toml"
 
@@ -14,7 +12,7 @@ for i, line in enumerate(lines[:-1]):
     if "#[public-storage]" in line:
         next_line = lines[i + 1]
         if next_line.strip().startswith("#"):
-            lines[i + 1] = re.sub(r"#\s*", "", next_line, 1)
+            lines[i + 1] = re.sub(r"#\s?", "", next_line, 1)
     if "#[private-storage]" in line:
         next_line = lines[i + 1]
         if not next_line.strip().startswith("#"):
