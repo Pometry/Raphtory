9e82061013a76395a23b40acf8ccaabcbcf82947
Shivam <4599890+shivamka1@users.noreply.github.com>
2025-06-13 14:22:13 +0100

    impl gql path filter, add tests (#2117)
    * impl gql path filter, add tests
    
    * fix test
    
    * fix test
    
    * fix test
    
    * fix tests
    
    * rid time
    
    ---------
    
    Co-authored-by: Shivam Kapoor <4599890+iamsmkr@users.noreply.github.com>

--- raphtory-graphql/src/model/graph/namespace.rs
+++ raphtory-graphql/src/model/graph/namespace.rs
@@ -10,7 +10,7 @@ use std::path::PathBuf;
 use tokio::task::spawn_blocking;
 use walkdir::WalkDir;
 
-#[derive(ResolvedObject, Clone)]
+#[derive(ResolvedObject, Clone, Ord, Eq, PartialEq, PartialOrd)]
 pub(crate) struct Namespace {
     base_dir: PathBuf,
     current_dir: PathBuf,
@@ -52,6 +52,7 @@ impl Namespace {
                     None
                 }
             })
+            .sorted()
             .collect()
     }
 }
@@ -129,6 +130,7 @@ impl Namespace {
                             None
                         }
                     })
+                    .sorted()
                     .collect(),
             )
         })
