9a1dec59f248a9ace5bee07eab282584032394e9
James Baross <james.baross@pometry.com>
2025-07-01 16:24:13 +0100

    new index

--- docs/scripts/gen_docs_pages.py
+++ docs/scripts/gen_docs_pages.py
@@ -1,7 +1,6 @@
 """Generate the code reference pages."""
 
 from pathlib import Path
-from shutil import copy
 
 import mkdocs_gen_files
 
@@ -35,14 +34,22 @@ def gen_class(name: str, cls: griffe.Class) -> Path:
     mkdocs_gen_files.set_edit_path(doc_path, cls.module.filepath.relative_to(root))
     return doc_path
 
-
+# TODO
+# - rename path as raphtory_path
+# - create modules_path from raphtory_path by using the stem modules and suffix .md and insert in nav
+# - make modules entry in index a link
 def gen_module(name: str, module: griffe.Module) -> Path:
     path = module.filepath.relative_to(src).with_suffix("")
     parts = tuple(path.parts)
+    #modules_path = None
     if path.stem == "__init__":
         parts = parts[:-1]
-        path = path.with_stem("index")
+        modules_path = path.with_stem("modules")
+        path = path.with_stem("overview")
     nav[parts] = path.with_suffix(".md").as_posix()
+    print('TESTING:',nav)
+    #if modules_path is not None:
+    #    nav[parts] = modules_path.with_suffix(".md").as_posix()
     doc_path = doc_root / path.with_suffix(".md")
 
     with mkdocs_gen_files.open(doc_path, "w") as fd:
@@ -50,14 +57,20 @@ def gen_module(name: str, module: griffe.Module) -> Path:
         print(f"    options:", file=fd)
         print(f"      members: false", file=fd)
 
+        print(f'docpath = {doc_path} \n', file=fd)
+        print(f'foo = {parts}', file=fd)
+
         public_modules = _public_items(module.modules)
         if public_modules:
             print("## Modules", file=fd)
+            print(f'docpath = {doc_path} \n', file=fd)
+            print(f'corepath = {module.filepath.relative_to(src).with_suffix("")}', file=fd)
             for member_name, sub_module in public_modules:
                 sub_path = gen_module(member_name, sub_module)
                 link_path = sub_path.relative_to(doc_path.parent)
                 print(f"### [`{member_name}`]({link_path})", file=fd)
                 print(f"{_docstr_desc(sub_module)}\n", file=fd)
+                print(f'sub_path = {sub_path}', file=fd)
 
         public_classes = _public_items(module.classes)
         if public_classes:
