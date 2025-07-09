09000dbc635a9ee5af6b919d7813a42ff30d97ba
Lucas Jeub <lucas.jeub@pometry.com>
2025-07-09 09:25:01 +0200

    simplify the relabelling

--- raphtory/src/algorithms/components/connected_components.rs
+++ raphtory/src/algorithms/components/connected_components.rs
@@ -69,8 +69,9 @@ impl<'graph, G: GraphView + 'graph> ComponentState<'graph, G> {
         }
         // only increment this if we still have a potentially valid node to avoid getting too many chunks
         let chunk_id = self.next_chunk_label();
-        self.chunk_labels[chunk_id].fetch_min(chunk_id, Ordering::Release);
+        self.chunk_labels[chunk_id].fetch_min(chunk_id, Ordering::Relaxed);
         loop {
+            //
             if self.node_labels[next_start]
                 .compare_exchange(usize::MAX, chunk_id, Ordering::Relaxed, Ordering::Relaxed)
                 .is_ok()
@@ -87,27 +88,7 @@ impl<'graph, G: GraphView + 'graph> ComponentState<'graph, G> {
     }
 
     fn next_chunk_label(&self) -> usize {
-        self.next_chunk.fetch_add(1, Ordering::Release)
-    }
-
-    /// Link two chunks `chunk_id_1` and `chunk_id_2` such that they will be part of the same
-    /// component in the final result.
-    ///
-    /// The components always link from larger id to smaller id. If while linking, we find that
-    /// the component was already linked, we rewire that link as well (the implementation is effectively
-    /// the same as calling this function recursively)
-    fn link_chunks(&self, chunk_id_1: usize, chunk_id_2: usize) {
-        let mut src = chunk_id_1.max(chunk_id_2);
-        let mut dst = chunk_id_1.min(chunk_id_2);
-        while src != dst {
-            let old_label = self.chunk_labels[src].fetch_min(dst, Ordering::Acquire);
-            if old_label > dst {
-                src = old_label
-            } else {
-                src = dst;
-                dst = old_label
-            }
-        }
+        self.next_chunk.fetch_add(1, Ordering::Release) // synchronises with `Acquire` in `run` to ensure all updates from the chunk task are visible
     }
 
     /// Find a new starting point and run breadth-first search
@@ -124,13 +105,23 @@ impl<'graph, G: GraphView + 'graph> ComponentState<'graph, G> {
             for node_id in frontier.drain(..) {
                 for neighbour in NodeView::new_internal(self.graph, node_id).neighbours() {
                     let node_id = neighbour.node;
-                    let old_label =
-                        self.node_labels[node_id.index()].fetch_min(chunk_id, Ordering::Relaxed);
-                    if old_label != usize::MAX {
-                        self.link_chunks(chunk_id, old_label);
-                    } else {
-                        // node not visited previously
-                        new_frontier.push(node_id);
+                    match self.node_labels[node_id.index()].compare_exchange(
+                        usize::MAX,
+                        chunk_id,
+                        Ordering::Relaxed,
+                        Ordering::Relaxed,
+                    ) {
+                        Ok(_) => {
+                            // node not visited previously
+                            new_frontier.push(node_id);
+                        }
+                        Err(old_label) => {
+                            if old_label > chunk_id {
+                                self.chunk_labels[old_label].fetch_min(chunk_id, Ordering::Relaxed);
+                            } else {
+                                self.chunk_labels[chunk_id].fetch_min(old_label, Ordering::Relaxed);
+                            }
+                        }
                     }
                 }
             }
@@ -138,13 +129,24 @@ impl<'graph, G: GraphView + 'graph> ComponentState<'graph, G> {
         Some(())
     }
 
+    /// Runs breadth-first search in parallel from different starting points until the graph is covered
     ///
+    /// During the first phase, the `node_labels` contain the id of the first task that visited that node
+    /// and the `chunk_labels` maintain a mapping from task to the minimum over other tasks that also
+    /// reached a node first discovered by this task.
+    ///
+    /// In the second phase, starting from each task, we follow the relabelling map until we hit a root task (i.e., one where the label is mapped to itself)
+    /// and simplify the chunk_labels
+    ///
+    /// Finally, the simplified chunk_labels are used to update the `node_labels` for the final output.
     fn run(self) -> Vec<usize> {
         let num_tasks = rayon::current_num_threads();
         (0..num_tasks)
             .into_par_iter()
             .for_each(|_| while self.run_chunk().is_some() {});
-        let num_chunks = self.next_chunk.load(Ordering::Acquire);
+        let num_chunks = self.next_chunk.load(Ordering::Acquire); // synchronises with `Release` in `next_chunk_label` to ensure all updates from the chunk task are visible
+        dbg!(&self);
+        dbg!(num_chunks);
         self.chunk_labels[0..num_chunks]
             .par_iter()
             .enumerate()
@@ -157,6 +159,7 @@ impl<'graph, G: GraphView + 'graph> ComponentState<'graph, G> {
                 }
                 if component_id != chunk_id {
                     self.chunk_labels[chunk_id].fetch_min(component_id, Ordering::Release);
+                    // synchronise with `Acquire` below such that we are guaranteed to have the final label
                 }
             });
         self.node_labels
@@ -164,7 +167,7 @@ impl<'graph, G: GraphView + 'graph> ComponentState<'graph, G> {
             .map(|l| {
                 let l = l.into_inner();
                 if l != usize::MAX {
-                    self.chunk_labels[l].load(Ordering::Acquire)
+                    self.chunk_labels[l].load(Ordering::Acquire) // synchronise with `Release` above such that we are guaranteed to have the final label
                 } else {
                     l
                 }
