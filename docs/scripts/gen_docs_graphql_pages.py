ba34928bf25a66e4f930523b3ba3b1253373b447
James Baross <james.baross@pometry.com>
2025-07-18 15:45:16 +0100

    pathlib for working dir

--- docs/scripts/gen_docs_graphql_pages.py
+++ docs/scripts/gen_docs_graphql_pages.py
@@ -1,4 +1,10 @@
 import subprocess
+from pathlib import Path
+
+root = Path(__file__).parent.parent.parent
+doc_root = Path("reference")
+graphql_dir = doc_root / "reference" / "graphql"
+schema_dir = root / "raphtory-graphql"
 
 import os
 print(os.getcwd())
@@ -7,8 +13,8 @@ result = subprocess.run(
     [
         "npx", "graphql-markdown",
         "--no-toc",
-        "--update-file", "reference/graphql/graphql_API.md",
-        "../raphtory-graphql/schema.graphql"
+        "--update-file", (graphql_dir / "graphql_API.md").as_posix(),
+        (schema_dir / "schema.graphql").as_posix()
     ],
     capture_output=True,
     text=True
