f785f6d21f4ce1ddee806bd5ecc3d44128b9f23b
James Baross <james.baross@pometry.com>
2025-07-18 14:07:48 +0100

    paths relative to docs root

--- docs/scripts/gen_docs_graphql_pages.py
+++ docs/scripts/gen_docs_graphql_pages.py
@@ -7,8 +7,8 @@ result = subprocess.run(
     [
         "npx", "graphql-markdown",
         "--no-toc",
-        "--update-file", "../reference/graphql/graphql_API.md",
-        "../../raphtory-graphql/schema.graphql"
+        "--update-file", "reference/graphql/graphql_API.md",
+        "../raphtory-graphql/schema.graphql"
     ],
     capture_output=True,
     text=True
