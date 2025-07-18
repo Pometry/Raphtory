f9438af1ae1a51da3674afa6dba44fe45ca70f32
James Baross <james.baross@pometry.com>
2025-07-18 11:14:21 +0100

    test chained script

---
+++ docs/scripts/gen_docs_graphql_pages.py
@@ -0,0 +1,13 @@
+import subprocess
+
+result = subprocess.run(
+    [
+        "npx", "graphql-markdown",
+        "--no-toc",
+        "--update-file", "../reference/graphql/graphql_API.md",
+        "../../raphtory-graphql/schema.graphql"
+    ],
+    capture_output=True,
+    text=True
+)
+print(result.stdout)
