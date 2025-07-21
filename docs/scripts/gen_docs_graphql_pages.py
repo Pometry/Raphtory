634117784d14479c001a77e8d26988d7da35ed34
James Baross <james.baross@pometry.com>
2025-07-21 12:04:31 +0100

    use mkdocs_gen_files

--- docs/scripts/gen_docs_graphql_pages.py
+++ docs/scripts/gen_docs_graphql_pages.py
@@ -1,19 +1,31 @@
 import subprocess
 from pathlib import Path
+import mkdocs_gen_files
 
 root = Path(__file__).parent.parent.parent
-
 graphql_dir = root /"docs" / "reference" / "graphql"
 schema_dir = root / "raphtory-graphql"
+
+frontmatter = """---
+hide:
+  - navigation
+---
+"""
 
 result = subprocess.run(
     [
         "npx", "graphql-markdown",
         "--no-toc",
-        "--update-file", (graphql_dir / "graphql_API.md").as_posix(),
         (schema_dir / "schema.graphql").as_posix()
     ],
     capture_output=True,
     text=True
 )
-print(result)
+
+output_str = result.stdout
+
+doc_path = graphql_dir / "graphql_API.md"
+
+with mkdocs_gen_files.open(doc_path, "w") as fd:
+    print(f"{frontmatter}", file=fd)
+    print(f"{output_str}", file=fd)
