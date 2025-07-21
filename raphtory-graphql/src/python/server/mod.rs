f1bf5a3b67586993cfee3318339cd16bb4561d13
Ben Steer <ben.steer@pometry.com>
2025-07-21 15:57:05 +0100

    fix warnings

--- raphtory-graphql/src/python/server/mod.rs
+++ raphtory-graphql/src/python/server/mod.rs
@@ -1,12 +1,11 @@
 use crate::{
-    model::App,
     python::{
         server::{running_server::ServerHandler, server::PyGraphServer},
         RUNNING_SERVER_CONSUMED_MSG,
     },
     GraphServer,
 };
-use pyo3::{exceptions::PyException, PyRefMut, PyResult, Python};
+use pyo3::{exceptions::PyException, PyRefMut, PyResult};
 use raphtory_api::python::error::adapt_err_value;
 
 pub mod running_server;
