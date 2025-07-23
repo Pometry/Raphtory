7f7b931c694b0f9a458805543430050a3ed1eecd
Lucas Jeub <lucas.jeub@pometry.com>
2025-07-23 18:20:56 +0200

    rename the field

--- raphtory-graphql/src/model/graph/meta_graph.rs
+++ raphtory-graphql/src/model/graph/meta_graph.rs
@@ -47,7 +47,7 @@ impl MetaGraph {
 pub(crate) struct GqlGraphMetadata {
     pub(crate) node_count: usize,
     pub(crate) edge_count: usize,
-    pub(crate) properties: Vec<GqlProperty>,
+    pub(crate) metadata: Vec<GqlProperty>,
 }
 
 impl From<GraphMetadata> for GqlGraphMetadata {
@@ -55,7 +55,7 @@ impl From<GraphMetadata> for GqlGraphMetadata {
         GqlGraphMetadata {
             node_count: metadata.node_count,
             edge_count: metadata.edge_count,
-            properties: metadata
+            metadata: metadata
                 .metadata
                 .into_iter()
                 .map(|(key, prop)| GqlProperty::new(key.to_string(), prop))
