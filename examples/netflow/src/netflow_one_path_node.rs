e4332f775e7841b3a82639cd63ba4f4e354db27e
shivamka1 <4599890+shivamka1@users.noreply.github.com>
2025-07-23 11:25:55 +0100

    fix example

--- examples/netflow/src/netflow_one_path_node.rs
+++ examples/netflow/src/netflow_one_path_node.rs
@@ -29,13 +29,8 @@ fn get_one_hop_counts<'graph, G: GraphViewOps<'graph>>(
         .sum::<usize>()
 }
 
-fn one_path_algorithm<
-    'graph,
-    G: GraphViewOps<'graph>,
-    GH: GraphViewOps<'graph>,
-    CS: ComputeState,
->(
-    nf_e_edge_expl: EvalEdgeView<'graph, '_, G, GH, CS, ()>,
+fn one_path_algorithm<'graph, G: GraphViewOps<'graph>, CS: ComputeState>(
+    nf_e_edge_expl: EvalEdgeView<'graph, '_, G, CS, ()>,
     no_time: bool,
 ) -> usize {
     //     MATCH
