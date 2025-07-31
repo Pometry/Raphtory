db26c67322f5457856f2e90746f90d0973dfd3fb
James Baross <james.baross@pometry.com>
2025-07-31 10:50:16 +0100

    better test attempt

--- python/tests/test_base_install/test_graphql/test_gen_schema.py
+++ python/tests/test_base_install/test_graphql/test_gen_schema.py
@@ -1,17 +1,9 @@
-import subprocess
+from raphtory.graphql import GraphServer, schema
 from pathlib import Path
 
 root = Path(__file__).parent
 #graphql_dir = root /"docs" / "reference" / "graphql"
 graphql_file_path = (root / "schema.graphql").as_posix()
 
-result = subprocess.run(
-    [
-        "raphtory", "schema"
-    ],
-    capture_output=True,
-    text=True
-)
-
 with open(graphql_file_path, 'w', encoding='utf8') as f:
-    f.write(result)
+    f.write(schema())
