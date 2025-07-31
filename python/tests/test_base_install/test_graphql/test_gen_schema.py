ba148e6d7f1dbc67dfae84f46db7da6b7ce9cae2
James Baross <james.baross@pometry.com>
2025-07-31 11:28:08 +0100

    correct path

--- python/tests/test_base_install/test_graphql/test_gen_schema.py
+++ python/tests/test_base_install/test_graphql/test_gen_schema.py
@@ -1,11 +1,11 @@
 from raphtory.graphql import GraphServer, schema
 from pathlib import Path
 
-root = Path(__file__).parent
-#graphql_dir = root /"docs" / "reference" / "graphql"
-graphql_file_path = (root / "schema.graphql").as_posix()
+root = Path(__file__).parent.parent.parent.parent.parent
+schema_dir = root / "raphtory-graphql"
+graphql_file_path = (schema_dir / "schema.graphql").as_posix()
 
-print(f"Creating {graphql_file_path} graphql.schema file")
+print(f"Creating graphql.schema file at {graphql_file_path}")
 
 with open(graphql_file_path, 'w', encoding='utf8') as f:
     f.write(schema())
