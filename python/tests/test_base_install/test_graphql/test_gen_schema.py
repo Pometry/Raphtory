67decd93512d4e1e825292e9db9d06d1b51ec987
James Baross <james.baross@pometry.com>
2025-07-31 11:21:34 +0100

    log visibility

--- python/tests/test_base_install/test_graphql/test_gen_schema.py
+++ python/tests/test_base_install/test_graphql/test_gen_schema.py
@@ -5,5 +5,8 @@ root = Path(__file__).parent
 #graphql_dir = root /"docs" / "reference" / "graphql"
 graphql_file_path = (root / "schema.graphql").as_posix()
 
+print(f"Creating {graphql_file_path} graphql.schema file")
+
 with open(graphql_file_path, 'w', encoding='utf8') as f:
     f.write(schema())
+
