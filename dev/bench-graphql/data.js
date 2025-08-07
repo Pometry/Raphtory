fba59b9eaf449882a76c8a98e1c555113b96562b
github-action-benchmark <github@users.noreply.github.com>
2025-08-07 13:47:18 +0000

    add GraphQL Benchmark (customBiggerIsBetter) benchmark result for 4094cc61895003543b0bac892b81b361c737a07f

---
+++ dev/bench-graphql/data.js
@@ -0,0 +1,33 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1754574438439,
+  "repoUrl": "https://github.com/Pometry/Raphtory",
+  "entries": {
+    "GraphQL Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "name": "Pometry",
+            "username": "Pometry"
+          },
+          "committer": {
+            "name": "Pometry",
+            "username": "Pometry"
+          },
+          "id": "4094cc61895003543b0bac892b81b361c737a07f",
+          "message": "graphql bench on CI and vector bench",
+          "timestamp": "2025-08-07T13:11:29Z",
+          "url": "https://github.com/Pometry/Raphtory/pull/2198/commits/4094cc61895003543b0bac892b81b361c737a07f"
+        },
+        "date": 1754574437577,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "randomNodePage",
+            "value": null,
+            "unit": "req/s"
+          }
+        ]
+      }
+    ]
+  }
+}
