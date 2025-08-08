8e8a8177f6ba652d3abb1cd638d8d31519947c95
Pedro Rico Pinazo <ricopinazo@gmail.com>
2025-08-08 14:39:15 +0100

    cleanup PR

--- graphql-bench/tsup.config.ts
+++ graphql-bench/tsup.config.ts
@@ -1,13 +1,7 @@
 import { defineConfig } from 'tsup';
 
 export default defineConfig({
-    entry: [
-        'src/index.ts',
-        'src/test.ts',
-        'src/breakpoint.ts',
-        'src/query-breakpoint.ts',
-        'src/setup-server.ts',
-    ],
+    entry: ['src/bench.ts'],
     format: ['esm'], // or ['cjs', 'esm']
     external: ['k6', 'k6/http', 'k6/metrics'],
 });
