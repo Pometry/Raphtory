196274d1dd0e4d88b165f981ef0eae36696e5055
github-action-benchmark <github@users.noreply.github.com>
2025-08-14 13:05:39 +0000

    add GraphQL Benchmark (customBiggerIsBetter) benchmark result for ceafe286febccb4e5a7c763ae6b5c026b4ec2726

---
+++ dev/bench-graphql/data.js
@@ -0,0 +1,62 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1755176739505,
+  "repoUrl": "https://github.com/Pometry/Raphtory",
+  "entries": {
+    "GraphQL Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "james.baross@pometry.com",
+            "name": "James Baross",
+            "username": "jbaross-pometry"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "ceafe286febccb4e5a7c763ae6b5c026b4ec2726",
+          "message": "James/graphql docstrings (#2210)\n\n* init\n\n* docstrings\n\n* docstrings for edges\n\n* docstrings for edges\n\n* regen schema and docs\n\n* run formatting\n\n* chore: apply tidy-public auto-fixes\n\n* more docstrings\n\n* backticks are not used by docs parser so remove\n\n* more docstrings\n\n* chore: apply tidy-public auto-fixes\n\n* more docstrings\n\n* chore: apply tidy-public auto-fixes\n\n* more docstrings\n\n* more docstrings\n\n* update schema and format\n\n* chore: apply tidy-public auto-fixes\n\n* more docstrings\n\n* chore: apply tidy-public auto-fixes\n\n* more docstrings\n\n* chore: apply tidy-public auto-fixes\n\n* more docstrings\n\n* chore: apply tidy-public auto-fixes\n\n* more docstrings\n\n* more docstrings\n\n* cleanup\n\n* more docstrings\n\n* chore: apply tidy-public auto-fixes\n\n* testcase for inputs\n\n* cleanup\n\n* chore: apply tidy-public auto-fixes\n\n* more docstrings\n\n* cleanup\n\n* chore: apply tidy-public auto-fixes\n\n* more docstrings\n\n* cleanup\n\n* chore: apply tidy-public auto-fixes\n\n* fix page  docstrings\n\n* cleanup\n\n* remove latin\n\n* chore: apply tidy-public auto-fixes\n\n* initial fixes\n\n* fmt\n\n* chore: apply tidy-public auto-fixes\n\n---------\n\nCo-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>\nCo-authored-by: Ben Steer <ben.steer@pometry.com>",
+          "timestamp": "2025-08-14T13:30:32+01:00",
+          "tree_id": "e5bb1e8da7e6748e41304ab0f3bc68b18c91e8b8",
+          "url": "https://github.com/Pometry/Raphtory/commit/ceafe286febccb4e5a7c763ae6b5c026b4ec2726"
+        },
+        "date": 1755176737038,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "addNode",
+            "value": null,
+            "unit": "req/s"
+          },
+          {
+            "name": "randomNodePage",
+            "value": 294,
+            "unit": "req/s"
+          },
+          {
+            "name": "randomEdgePage",
+            "value": 209,
+            "unit": "req/s"
+          },
+          {
+            "name": "nodePropsByName",
+            "value": 1206,
+            "unit": "req/s"
+          },
+          {
+            "name": "nodeNeighboursByName",
+            "value": 1070,
+            "unit": "req/s"
+          },
+          {
+            "name": "readAndWriteNodeProperties",
+            "value": 1629,
+            "unit": "req/s"
+          }
+        ]
+      }
+    ]
+  }
+}
