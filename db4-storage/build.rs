90f608c0789ea7c8f4ed9a0edf631d696f63cc2a
Fabian Murariu <murariu.fabian@gmail.com>
2025-09-08 22:44:03 +0100

    working debug symbols bypass

--- db4-storage/build.rs
+++ db4-storage/build.rs
@@ -2,11 +2,8 @@ use std::io::Result;
 
 fn main() -> Result<()> {
     println!("cargo::rustc-check-cfg=cfg(has_debug_symbols)");
-
-    if let Ok(profile) = std::env::var("PROFILE") {
-        if profile.contains("debug") {
-            println!("cargo::rustc-cfg=has_debug_symbols");
-        }
+    if let Ok("true") = std::env::var("DEBUG").as_deref() {
+        println!("cargo::rustc-cfg=has_debug_symbols");
     }
     Ok(())
 }
--- raphtory/build.rs
+++ raphtory/build.rs
@@ -3,11 +3,10 @@ use std::io::Result;
 fn main() -> Result<()> {
     prost_build::compile_protos(&["src/serialise/graph.proto"], &["src/serialise"])?;
     println!("cargo::rerun-if-changed=src/serialise/graph.proto");
-    println!("cargo::rustc-check-cfg=has_debug_symbols");
-    if let Ok(profile) = std::env::var("PROFILE") {
-        if profile.contains("debug") {
-            println!("cargo::rustc-cfg=has_debug_symbols");
-        }
+
+    println!("cargo::rustc-check-cfg=cfg(has_debug_symbols)");
+    if let Ok("true") = std::env::var("DEBUG").as_deref() {
+        println!("cargo::rustc-cfg=has_debug_symbols");
     }
     Ok(())
 }
@@ -15,10 +14,8 @@ fn main() -> Result<()> {
 #[cfg(not(feature = "proto"))]
 fn main() -> Result<()> {
     println!("cargo::rustc-check-cfg=cfg(has_debug_symbols)");
-    if let Ok(profile) = std::env::var("PROFILE") {
-        if profile.contains("debug") {
-            println!("cargo::rustc-cfg=has_debug_symbols");
-        }
+    if let Ok("true") = std::env::var("DEBUG").as_deref() {
+        println!("cargo::rustc-cfg=has_debug_symbols");
     }
     Ok(())
 }
