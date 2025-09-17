d900dd1d1078622fe61aa650d95ba6ea32e4f30f
Fadhil Abubaker <fadhil.abubaker@gmail.com>
2025-09-17 12:39:26 -0400

    Use encode/decode in url_encode/url_decode

--- raphtory-graphql/src/url_encode.rs
+++ raphtory-graphql/src/url_encode.rs
@@ -2,10 +2,8 @@ use base64::{prelude::BASE64_URL_SAFE, DecodeError, Engine};
 use raphtory::{
     db::api::view::MaterializedGraph,
     errors::GraphError,
-    prelude::{ParquetDecoder, ParquetEncoder},
+    prelude::{StableDecode, StableEncode},
 };
-use std::io::copy;
-use zip::write::SimpleFileOptions;
 
 #[derive(thiserror::Error, Debug)]
 pub enum UrlDecodeError {
@@ -23,54 +21,15 @@ pub enum UrlDecodeError {
 
 pub fn url_encode_graph<G: Into<MaterializedGraph>>(graph: G) -> Result<String, GraphError> {
     let g: MaterializedGraph = graph.into();
-    let temp_dir = tempfile::tempdir()?;
-    g.encode_parquet(temp_dir.path())?;
-    // now zip the entire directory, don't bother with compression
-    let mut bytes = Vec::new();
-    let mut zip = zip::ZipWriter::new(std::io::Cursor::new(&mut bytes));
+    let bytes = g.encode_to_bytes();
 
-    let mut paths = vec![temp_dir.path().to_path_buf()];
-    while let Some(path) = paths.pop() {
-        for entry in std::fs::read_dir(&path)? {
-            let entry = entry?;
-            let path = entry.path();
-            if path.is_dir() {
-                // If it's a directory, we should add it to the list to process later
-                paths.push(path);
-            } else {
-                // If it's a file, we should write it directly
-                let options =
-                    SimpleFileOptions::default().compression_method(zip::CompressionMethod::Stored);
-                let rel_path = path.strip_prefix(temp_dir.path()).unwrap();
-                zip.start_file_from_path(rel_path, options)?;
-                let mut file = std::fs::File::open(path)?;
-                copy(&mut file, &mut zip)?;
-            }
-        }
-    }
-    zip.finish()?;
     Ok(BASE64_URL_SAFE.encode(bytes))
 }
 
 pub fn url_decode_graph<T: AsRef<[u8]>>(graph: T) -> Result<MaterializedGraph, GraphError> {
-    let zip_bytes = BASE64_URL_SAFE.decode(graph.as_ref()).unwrap();
+    let bytes = BASE64_URL_SAFE.decode(graph.as_ref()).unwrap();
 
-    let temp_dir = tempfile::tempdir()?;
-    let mut zip = zip::ZipArchive::new(std::io::Cursor::new(zip_bytes))?;
-
-    for i in 0..zip.len() {
-        let mut file = zip.by_index(i)?;
-        let out_path = temp_dir.path().join(file.enclosed_name().unwrap());
-        dbg!(&out_path);
-
-        if let Some(path) = out_path.parent() {
-            std::fs::create_dir_all(path)?;
-        }
-        let mut out_file = std::fs::File::create(&out_path)?;
-        std::io::copy(&mut file, &mut out_file)?;
-    }
-
-    MaterializedGraph::decode_parquet(temp_dir.path())
+    MaterializedGraph::decode_from_bytes(&bytes)
 }
 
 #[cfg(test)]
