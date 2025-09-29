b76939db51c1182835e63723a020b1bb51aa9365
Lucas Jeub <lucas.jeub@pometry.com>
2025-09-29 11:47:36 +0200

    make a write pool

--- raphtory-graphql/src/rayon.rs
+++ raphtory-graphql/src/rayon.rs
@@ -1,4 +1,12 @@
+use rayon::{ThreadPool, ThreadPoolBuilder};
+use std::sync::LazyLock;
 use tokio::sync::oneshot;
+
+static WRITE_POOL: LazyLock<ThreadPool> = LazyLock::new(|| {
+    ThreadPoolBuilder::new()
+        .build()
+        .expect("failed to build threadpool")
+});
 
 /// Use the rayon threadpool to execute a task
 ///
@@ -11,5 +19,14 @@ pub async fn blocking_compute<R: Send + 'static, F: FnOnce() -> R + Send + 'stat
         let _ = send.send(closure()); // this only errors if no-one is listening anymore
     });
 
+    recv.await.expect("Function panicked in rayon::spawn")
+}
+
+/// Use a separate rayon threadpool to execute write tasks to avoid potential deadlocks
+pub async fn blocking_write<R: Send + 'static, F: FnOnce() -> R + Send + 'static>(closure: F) -> R {
+    let (send, recv) = oneshot::channel();
+    WRITE_POOL.spawn(move || {
+        let _ = send.send(closure()); // this only errors if no-one is listening anymore
+    });
     recv.await.expect("Function panicked in rayon::spawn")
 }
