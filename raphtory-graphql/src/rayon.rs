86039536e8c778f0a387cf9bfbf56efb9021a017
Lucas Jeub <lucas.jeub@pometry.com>
2025-09-29 18:14:48 +0200

    fix the recursive read deadlock

--- raphtory-graphql/src/rayon.rs
+++ raphtory-graphql/src/rayon.rs
@@ -2,12 +2,6 @@ use rayon::{ThreadPool, ThreadPoolBuilder};
 use std::sync::LazyLock;
 use tokio::sync::oneshot;
 
-static WRITE_POOL: LazyLock<ThreadPool> = LazyLock::new(|| {
-    ThreadPoolBuilder::new()
-        .build()
-        .expect("failed to build threadpool")
-});
-
 /// Use the rayon threadpool to execute a task
 ///
 /// Use this for long-running, compute-heavy work
@@ -25,7 +19,7 @@ pub async fn blocking_compute<R: Send + 'static, F: FnOnce() -> R + Send + 'stat
 /// Use a separate rayon threadpool to execute write tasks to avoid potential deadlocks
 pub async fn blocking_write<R: Send + 'static, F: FnOnce() -> R + Send + 'static>(closure: F) -> R {
     let (send, recv) = oneshot::channel();
-    WRITE_POOL.spawn(move || {
+    rayon::spawn(move || {
         let _ = send.send(closure()); // this only errors if no-one is listening anymore
     });
     recv.await.expect("Function panicked in rayon::spawn")
