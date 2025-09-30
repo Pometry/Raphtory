a55587c056713db4e289403255d949208cc2ced4
David Moran <david.moran@pometry.com>
2025-09-30 07:29:31 -0600

    New node state for tracing algo

--- raphtory/src/python/graph/node_state/node_state.rs
+++ raphtory/src/python/graph/node_state/node_state.rs
@@ -660,3 +660,9 @@ impl_node_state!(
     "NodeStateF64StringI64",
     "Tuple[float, str, int]"
 );
+
+impl_node_state!(
+    NodeStateF64F64StringI64<(f64, f64, String, i64)>,
+    "NodeStateF64F64StringI64",
+    "Tuple[float, float, str, int]"
+);
