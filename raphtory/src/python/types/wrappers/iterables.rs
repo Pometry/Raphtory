ea42008fe08f6a59e679d8c4c4ef3b86a4cec7ea
arienandalibi <arienandalibi2@gmail.com>
2025-10-08 01:57:45 -0400

    Moved impl blocks for EventTime iterables to a separate file, so custom function definitions aren't alongside macro uses.

--- raphtory/src/python/types/wrappers/iterables.rs
+++ raphtory/src/python/types/wrappers/iterables.rs
@@ -1,23 +1,10 @@
-use crate::{
-    db::api::view::BoxedIter,
-    prelude::Prop,
-    python::types::{
-        repr::Repr,
-        result_iterable::{
-            NestedResultOptionUtcDateTimeIterable, NestedResultUtcDateTimeIterable,
-            ResultOptionUtcDateTimeIterable, ResultUtcDateTimeIterable,
-        },
-    },
-};
+use crate::{db::api::view::BoxedIter, prelude::Prop, python::types::repr::Repr};
 use chrono::{DateTime, Utc};
 use num::cast::AsPrimitive;
 use pyo3::prelude::*;
 use raphtory_api::core::{
     entities::GID,
-    storage::{
-        arc_str::ArcStr,
-        timeindex::{AsTime, EventTime},
-    },
+    storage::{arc_str::ArcStr, timeindex::EventTime},
 };
 use std::iter::Sum;
 
@@ -109,127 +96,18 @@ py_iterable_comp!(
 
 py_ordered_iterable!(EventTimeIterable, EventTime);
 py_iterable_comp!(EventTimeIterable, EventTime, EventTimeIterableCmp);
-// Custom EventTime operations on iterables as well
-#[pymethods]
-impl EventTimeIterable {
-    /// Change this Iterable of EventTime into an Iterable of corresponding Unix timestamps in milliseconds.
-    ///
-    /// Returns:
-    ///     I64Iterable: Iterable of millisecond timestamps since the Unix epoch for each EventTime.
-    #[getter]
-    fn t(&self) -> I64Iterable {
-        let builder = self.builder.clone();
-        (move || builder().map(|t| t.t())).into()
-    }
-
-    /// Change this Iterable of EventTime into an Iterable of corresponding UTC DateTimes.
-    ///
-    /// Returns:
-    ///     ResultUtcDateTimeIterable: Iterable of UTC datetimes for each EventTime.
-    ///
-    /// Raises:
-    ///     TimeError: Returns TimeError on timestamp conversion errors (e.g. out-of-range timestamp).
-    #[getter]
-    fn dt(&self) -> ResultUtcDateTimeIterable {
-        let builder = self.builder.clone();
-        (move || builder().map(|t| t.dt())).into()
-    }
-
-    /// Change this Iterable of EventTime into an Iterable of their associated event ids.
-    ///
-    /// Returns:
-    ///     UsizeIterable: Iterable of event ids associated to each EventTime.
-    #[getter]
-    fn event_id(&self) -> UsizeIterable {
-        let builder = self.builder.clone();
-        (move || builder().map(|t| t.i())).into()
-    }
-}
 py_nested_ordered_iterable!(NestedEventTimeIterable, EventTime, OptionEventTimeIterable);
 py_iterable_comp!(
     NestedEventTimeIterable,
     EventTimeIterableCmp,
     NestedEventTimeIterableCmp
 );
-// Custom EventTime operations on nested iterables as well
-#[pymethods]
-impl NestedEventTimeIterable {
-    /// Change this nested Iterable of EventTime into a nested Iterable of corresponding Unix timestamps in milliseconds.
-    ///
-    /// Returns:
-    ///     NestedI64Iterable: Nested iterable of millisecond timestamps since the Unix epoch for each EventTime.
-    #[getter]
-    fn t(&self) -> NestedI64Iterable {
-        let builder = self.builder.clone();
-        (move || builder().map(|t_iter| t_iter.map(|t| t.t()))).into()
-    }
-
-    /// Change this nested Iterable of EventTime into a nested Iterable of corresponding UTC DateTimes.
-    ///
-    /// Returns:
-    ///     NestedResultUtcDateTimeIterable: Nested iterable of UTC datetimes for each EventTime.
-    ///
-    /// Raises:
-    ///     TimeError: Returns TimeError on timestamp conversion errors (e.g. out-of-range timestamp).
-    #[getter]
-    fn dt(&self) -> NestedResultUtcDateTimeIterable {
-        let builder = self.builder.clone();
-        (move || builder().map(|t_iter| t_iter.map(|t| t.dt()))).into()
-    }
-
-    /// Change this nested Iterable of EventTime into a nested Iterable of their associated event ids.
-    ///
-    /// Returns:
-    ///     NestedUsizeIterable: Nested iterable of event ids associated to each EventTime.
-    #[getter]
-    fn event_id(&self) -> NestedUsizeIterable {
-        let builder = self.builder.clone();
-        (move || builder().map(|t_iter| t_iter.map(|t| t.i()))).into()
-    }
-}
-
 py_ordered_iterable!(OptionEventTimeIterable, Option<EventTime>);
 py_iterable_comp!(
     OptionEventTimeIterable,
     Option<EventTime>,
     OptionEventTimeIterableCmp
 );
-// Custom EventTime operations on iterables of Option<EventTime> as well
-#[pymethods]
-impl OptionEventTimeIterable {
-    /// Change this Iterable of Optional[EventTime] into an Iterable of corresponding Unix timestamps in milliseconds.
-    ///
-    /// Returns:
-    ///     OptionI64Iterable: Iterable of millisecond timestamps since the Unix epoch for each EventTime, if available.
-    #[getter]
-    fn t(&self) -> OptionI64Iterable {
-        let builder = self.builder.clone();
-        (move || builder().map(|t_opt| t_opt.map(|t| t.t()))).into()
-    }
-
-    /// Change this Iterable of Optional[EventTime] into an Iterable of corresponding UTC DateTimes.
-    ///
-    /// Returns:
-    ///     ResultOptionUtcDateTimeIterable: Iterable of UTC datetimes for each EventTime, if available.
-    ///
-    /// Raises:
-    ///     TimeError: Returns TimeError on timestamp conversion errors (e.g. out-of-range timestamp).
-    #[getter]
-    fn dt(&self) -> ResultOptionUtcDateTimeIterable {
-        let builder = self.builder.clone();
-        (move || builder().map(|t_opt| t_opt.map(|t| t.dt()).transpose())).into()
-    }
-
-    /// Change this Iterable of Optional[EventTime] into an Iterable of their associated event ids.
-    ///
-    /// Returns:
-    ///     OptionUsizeIterable: Iterable of event ids associated to each EventTime, if available.
-    #[getter]
-    fn event_id(&self) -> OptionUsizeIterable {
-        let builder = self.builder.clone();
-        (move || builder().map(|t_opt| t_opt.map(|t| t.i()))).into()
-    }
-}
 py_ordered_iterable!(OptionOptionEventTimeIterable, Option<Option<EventTime>>);
 py_iterable_comp!(
     OptionOptionEventTimeIterable,
@@ -246,44 +124,6 @@ py_iterable_comp!(
     OptionEventTimeIterableCmp,
     NestedOptionEventTimeIterableCmp
 );
-// Custom EventTime operations on nested iterables of Option<EventTime> as well
-#[pymethods]
-impl NestedOptionEventTimeIterable {
-    /// Change this nested Iterable of Optional[EventTime] into a nested Iterable of corresponding Unix timestamps in milliseconds.
-    ///
-    /// Returns:
-    ///     NestedOptionI64Iterable: Nested iterable of millisecond timestamps since the Unix epoch for each EventTime, if available.
-    #[getter]
-    fn t(&self) -> NestedOptionI64Iterable {
-        let builder = self.builder.clone();
-        (move || builder().map(|t_iter| t_iter.map(|t_opt| t_opt.map(|t| t.t())))).into()
-    }
-
-    /// Change this nested Iterable of Optional[EventTime] into a nested Iterable of corresponding UTC DateTimes.
-    ///
-    /// Returns:
-    ///     NestedResultOptionUtcDateTimeIterable: Nested iterable of UTC datetimes for each EventTime, if available.
-    ///
-    /// Raises:
-    ///     TimeError: Returns TimeError on timestamp conversion errors (e.g. out-of-range timestamp).
-    #[getter]
-    fn dt(&self) -> NestedResultOptionUtcDateTimeIterable {
-        let builder = self.builder.clone();
-        (move || builder().map(|t_iter| t_iter.map(|t_opt| t_opt.map(|t| t.dt()).transpose())))
-            .into()
-    }
-
-    /// Change this nested Iterable of Optional[EventTime] into a nested Iterable of their associated event ids.
-    ///
-    /// Returns:
-    ///     NestedOptionUsizeIterable: Nested iterable of event ids associated to each EventTime, if available.
-    #[getter]
-    fn event_id(&self) -> NestedOptionUsizeIterable {
-        let builder = self.builder.clone();
-        (move || builder().map(|t_iter| t_iter.map(|t_opt| t_opt.map(|t| t.i())))).into()
-    }
-}
-
 py_numeric_iterable!(UsizeIterable, usize);
 py_iterable_comp!(UsizeIterable, usize, UsizeIterableCmp);
 
