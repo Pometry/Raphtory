37ad4d60df8990feee654a5ec6fb11c598d17ee4
Lucas Jeub <lucas.jeub@pometry.com>
2025-10-16 12:08:02 +0200

    tidy up warnings

--- db4-storage/src/pages/mod.rs
+++ db4-storage/src/pages/mod.rs
@@ -419,7 +419,6 @@ mod test {
         },
     };
     use chrono::{DateTime, NaiveDateTime, Utc};
-    use core::panic;
     use proptest::prelude::*;
     use raphtory_api::core::entities::properties::prop::Prop;
     use raphtory_core::{entities::VID, storage::timeindex::TimeIndexOps};
