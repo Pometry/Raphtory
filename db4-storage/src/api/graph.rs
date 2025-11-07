693c1cfb4d152d9046bd6bb6258e7f5a89a0e871
Fadhil Abubaker <fadhil.abubaker@gmail.com>
2025-11-06 21:29:47 -0500

    Add head() method for GraphSegmentView

--- db4-storage/src/api/graph.rs
+++ db4-storage/src/api/graph.rs
@@ -3,7 +3,7 @@ use std::path::Path;
 use crate::error::StorageError;
 use raphtory_core::entities::properties::tprop::TPropCell;
 use raphtory_api::core::entities::properties::prop::Prop;
-use parking_lot::RwLockWriteGuard;
+use parking_lot::{RwLockReadGuard, RwLockWriteGuard};
 use crate::segments::graph::segment::MemGraphSegment;
 
 pub trait GraphSegmentOps: Send + Sync + Debug + 'static
@@ -16,9 +16,11 @@ where
 
     fn load(path: impl AsRef<Path>) -> Result<Self, StorageError>;
 
-    fn entry(&self) -> Self::Entry<'_>;
+    fn head(&self) -> RwLockReadGuard<'_, MemGraphSegment>;
 
     fn head_mut(&self) -> RwLockWriteGuard<'_, MemGraphSegment>;
+
+    fn entry(&self) -> Self::Entry<'_>;
 }
 
 /// Methods for reading graph properties and metadata from storage.
