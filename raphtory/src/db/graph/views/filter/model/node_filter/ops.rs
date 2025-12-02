b592d0796e1df97ce6476010021a48ee045c1295
shivamka1 <4599890+shivamka1@users.noreply.github.com>
2025-12-02 15:32:34 +0000

    rearrange

--- raphtory/src/db/graph/views/filter/model/node_filter/ops.rs
+++ raphtory/src/db/graph/views/filter/model/node_filter/ops.rs
@@ -7,64 +7,6 @@ use crate::db::graph::views::filter::model::{
 };
 use raphtory_api::core::entities::GID;
 
-pub trait NodeFilterOps: InternalNodeFilterBuilder {
-    fn eq(&self, value: impl Into<String>) -> Self::Wrapped<Self::FilterType> {
-        let filter = Filter::eq(self.field_name(), value);
-        self.wrap(filter.into())
-    }
-
-    fn ne(&self, value: impl Into<String>) -> Self::Wrapped<Self::FilterType> {
-        let filter = Filter::ne(self.field_name(), value);
-        self.wrap(filter.into())
-    }
-
-    fn is_in(&self, values: impl IntoIterator<Item = String>) -> Self::Wrapped<Self::FilterType> {
-        let filter = Filter::is_in(self.field_name(), values);
-        self.wrap(filter.into())
-    }
-
-    fn is_not_in(
-        &self,
-        values: impl IntoIterator<Item = String>,
-    ) -> Self::Wrapped<Self::FilterType> {
-        let filter = Filter::is_not_in(self.field_name(), values);
-        self.wrap(filter.into())
-    }
-
-    fn starts_with(&self, value: impl Into<String>) -> Self::Wrapped<Self::FilterType> {
-        let filter = Filter::starts_with(self.field_name(), value);
-        self.wrap(filter.into())
-    }
-
-    fn ends_with(&self, value: impl Into<String>) -> Self::Wrapped<Self::FilterType> {
-        let filter = Filter::ends_with(self.field_name(), value);
-        self.wrap(filter.into())
-    }
-
-    fn contains(&self, value: impl Into<String>) -> Self::Wrapped<Self::FilterType> {
-        let filter = Filter::contains(self.field_name(), value);
-        self.wrap(filter.into())
-    }
-
-    fn not_contains(&self, value: impl Into<String>) -> Self::Wrapped<Self::FilterType> {
-        let filter = Filter::not_contains(self.field_name(), value.into());
-        self.wrap(filter.into())
-    }
-
-    fn fuzzy_search(
-        &self,
-        value: impl Into<String>,
-        levenshtein_distance: usize,
-        prefix_match: bool,
-    ) -> Self::Wrapped<Self::FilterType> {
-        let filter =
-            Filter::fuzzy_search(self.field_name(), value, levenshtein_distance, prefix_match);
-        self.wrap(filter.into())
-    }
-}
-
-impl<T: InternalNodeFilterBuilder + ?Sized> NodeFilterOps for T {}
-
 pub trait NodeIdFilterOps: InternalNodeIdFilterBuilder {
     fn eq<T: Into<GID>>(&self, value: T) -> Self::Wrapped<NodeIdFilter> {
         let filter = Filter::eq_id(self.field_name(), value);
@@ -147,3 +89,61 @@ pub trait NodeIdFilterOps: InternalNodeIdFilterBuilder {
 }
 
 impl<T: InternalNodeIdFilterBuilder + ?Sized> NodeIdFilterOps for T {}
+
+pub trait NodeFilterOps: InternalNodeFilterBuilder {
+    fn eq(&self, value: impl Into<String>) -> Self::Wrapped<Self::FilterType> {
+        let filter = Filter::eq(self.field_name(), value);
+        self.wrap(filter.into())
+    }
+
+    fn ne(&self, value: impl Into<String>) -> Self::Wrapped<Self::FilterType> {
+        let filter = Filter::ne(self.field_name(), value);
+        self.wrap(filter.into())
+    }
+
+    fn is_in(&self, values: impl IntoIterator<Item = String>) -> Self::Wrapped<Self::FilterType> {
+        let filter = Filter::is_in(self.field_name(), values);
+        self.wrap(filter.into())
+    }
+
+    fn is_not_in(
+        &self,
+        values: impl IntoIterator<Item = String>,
+    ) -> Self::Wrapped<Self::FilterType> {
+        let filter = Filter::is_not_in(self.field_name(), values);
+        self.wrap(filter.into())
+    }
+
+    fn starts_with(&self, value: impl Into<String>) -> Self::Wrapped<Self::FilterType> {
+        let filter = Filter::starts_with(self.field_name(), value);
+        self.wrap(filter.into())
+    }
+
+    fn ends_with(&self, value: impl Into<String>) -> Self::Wrapped<Self::FilterType> {
+        let filter = Filter::ends_with(self.field_name(), value);
+        self.wrap(filter.into())
+    }
+
+    fn contains(&self, value: impl Into<String>) -> Self::Wrapped<Self::FilterType> {
+        let filter = Filter::contains(self.field_name(), value);
+        self.wrap(filter.into())
+    }
+
+    fn not_contains(&self, value: impl Into<String>) -> Self::Wrapped<Self::FilterType> {
+        let filter = Filter::not_contains(self.field_name(), value.into());
+        self.wrap(filter.into())
+    }
+
+    fn fuzzy_search(
+        &self,
+        value: impl Into<String>,
+        levenshtein_distance: usize,
+        prefix_match: bool,
+    ) -> Self::Wrapped<Self::FilterType> {
+        let filter =
+            Filter::fuzzy_search(self.field_name(), value, levenshtein_distance, prefix_match);
+        self.wrap(filter.into())
+    }
+}
+
+impl<T: InternalNodeFilterBuilder + ?Sized> NodeFilterOps for T {}
