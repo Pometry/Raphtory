43fb586d81c0c4d908adcf76e1b146d6b3447b62
Pedro Rico Pinazo <ricopinazo@gmail.com>
2026-02-04 10:32:58 +0100

    fix compilation errors

--- raphtory-graphql/src/main.rs
+++ raphtory-graphql/src/main.rs
@@ -1,6 +1,21 @@
+use std::io::Result as IoResult;
+
+use crate::server::GraphServer;
+mod auth;
 mod cli;
+mod config;
+mod data;
+mod embeddings;
+mod graph;
+mod model;
+mod observability;
+mod paths;
+mod rayon;
+mod routes;
+mod server;
+mod url_encode;
 
 #[tokio::main]
 async fn main() -> IoResult<()> {
-    cli::cli()
+    cli::cli().await
 }
