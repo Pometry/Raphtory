8a68f574eb8749c7364ba008053a5476f8679efd
miratepuffin <b.a.steer@qmul.ac.uk>
2026-02-12 11:26:43 +0000

    allocator issues

--- python/src/lib.rs
+++ python/src/lib.rs
@@ -1,3 +1,8 @@
+#[cfg(not(target_env = "msvc"))]
+use tikv_jemallocator::Jemalloc;
+#[cfg(not(target_env = "msvc"))]
+#[global_allocator]
+static GLOBAL: Jemalloc = Jemalloc;
 use clam_core::python::py_gql::base_gql_module;
 use pyo3::prelude::*;
 use raphtory::python::{
