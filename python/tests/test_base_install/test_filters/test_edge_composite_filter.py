1b27135b13ee58743302c0b594b1cc81eff01b35
Fadhil Abubaker <fadhil.abubaker@gmail.com>
2026-05-07 15:08:24 -0400

    Re-enable string prop filter tests

--- python/tests/test_base_install/test_filters/test_edge_composite_filter.py
+++ python/tests/test_base_install/test_filters/test_edge_composite_filter.py
@@ -27,12 +27,11 @@ def test_edge_composite_filter():
         )
         assert result_ids == expected_ids
 
-        # TODO: Enable this test once string property is fixed for disk_storage_graph
-        #     filter_expr1 = filter.Edge.property("p2") < 9
-        #     filter_expr2 = filter.Edge.property("p1") == "shivam_kapoor"
-        #     result_ids = sorted(graph.filter(filter_expr1 & filter_expr2).edges.id)
-        #     expected_ids = [("1", "2")]
-        #     assert result_ids == expected_ids
+        filter_expr1 = filter.Edge.property("p2") < 9
+        filter_expr2 = filter.Edge.property("p1") == "shivam_kapoor"
+        result_ids = sorted(graph.filter(filter_expr1 & filter_expr2).edges.id)
+        expected_ids = [("1", "2")]
+        assert result_ids == expected_ids
 
         filter_expr1 = filter.Edge.property("p2") < 9
         filter_expr2 = filter.Edge.property("p3") < 9
@@ -46,12 +45,11 @@ def test_edge_composite_filter():
         ]
         assert result_ids == expected_ids
 
-        # TODO: Enable this test once string property is fixed for disk_storage_graph
-        #     filter_expr1 = filter.Edge.src().name() == "1"
-        #     filter_expr2 = filter.Edge.property("p1") == "shivam_kapoor"
-        #     result_ids = sorted(graph.filter(filter_expr1 & filter_expr2).edges.id)
-        #     expected_ids = [("1", "2")]
-        #     assert result_ids == expected_ids
+        filter_expr1 = filter.Edge.src().name() == "1"
+        filter_expr2 = filter.Edge.property("p1") == "shivam_kapoor"
+        result_ids = sorted(graph.filter(filter_expr1 & filter_expr2).edges.id)
+        expected_ids = [("1", "2")]
+        assert result_ids == expected_ids
 
         filter_expr1 = filter.Edge.dst().name() == "1"
         filter_expr2 = filter.Edge.property("p2") <= 6
@@ -59,13 +57,12 @@ def test_edge_composite_filter():
         expected_ids = sorted([("2", "1"), ("3", "1")])
         assert result_ids == expected_ids
 
-    # TODO: Enable this test once string property is fixed for disk_storage_graph
-    #     filter_expr1 = filter.Edge.src().name() == "1"
-    #     filter_expr2 = filter.Edge.property("p1") == "shivam_kapoor"
-    #     filter_expr3 = filter.Edge.property("p3") == 5
-    #     result_ids = sorted(graph.filter((filter_expr1 & filter_expr2) | filter_expr3).edges.id)
-    #     expected_ids = [("1", "2")]
-    #     assert result_ids == expected_ids
+        filter_expr1 = filter.Edge.src().name() == "1"
+        filter_expr2 = filter.Edge.property("p1") == "shivam_kapoor"
+        filter_expr3 = filter.Edge.property("p3") == 5
+        result_ids = sorted(graph.filter((filter_expr1 & filter_expr2) | filter_expr3).edges.id)
+        expected_ids = [("1", "2")]
+        assert result_ids == expected_ids
 
     return check
 
