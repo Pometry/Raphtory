274d04f46b78dea828b4ea53a4a3cf526b553ae4
shivamka1 <4599890+shivamka1@users.noreply.github.com>
2026-06-26 10:42:24 +0100

    fix isactivenode

--- raphtory/src/db/graph/views/filter/model/is_active_node_filter.rs
+++ raphtory/src/db/graph/views/filter/model/is_active_node_filter.rs
@@ -3,17 +3,21 @@ use crate::{
         api::state::ops::{GraphView, HistoryOp, Map, NodeOp},
         graph::views::filter::{
             model::{
-                edge_filter::CompositeEdgeFilter, ComposableFilter, CompositeExplodedEdgeFilter,
-                CompositeNodeFilter, CreateView, TryAsCompositeFilter,
+                edge_expr::EdgeOp,
+                edge_filter::CompositeEdgeFilter,
+                node_expr::{CreateOp, EntityExpr},
+                ComposableFilter, CompositeExplodedEdgeFilter, CompositeNodeFilter, CreateView,
+                TryAsCompositeFilter,
             },
             node_filtered_graph::NodeFilteredGraph,
             CreateFilter,
         },
     },
     errors::GraphError,
-    prelude::GraphViewOps,
+    prelude::{GraphViewOps, NodeFilter},
 };
-use std::fmt;
+use raphtory_api::core::entities::properties::prop::Prop;
+use std::{fmt, sync::Arc};
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub struct IsActiveNode<E> {
@@ -23,6 +27,25 @@ pub struct IsActiveNode<E> {
 impl<E> fmt::Display for IsActiveNode<E> {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         write!(f, "IS_ACTIVE_NODE")
+    }
+}
+
+impl<E: Clone + Send + Sync + 'static> EntityExpr for IsActiveNode<E> {
+    type Marker = NodeFilter;
+
+    fn entity(&self) -> Self::Marker {
+        NodeFilter
+    }
+}
+
+impl<E: CreateView + Clone> CreateOp for IsActiveNode<E> {
+    fn create_node_op<'g, G: GraphView + 'g>(
+        &self,
+        graph: G,
+    ) -> Result<Arc<dyn NodeOp<Output = Option<Prop>> + 'g>, GraphError> {
+        Ok(Arc::new(
+            HistoryOp::new(self.view_expr.create_view(graph)?).map(|h| !h.is_empty()),
+        ))
     }
 }
 
