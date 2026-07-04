7777081687f980cce0450169aed5b16ffef51f43
Fabian Murariu <murariu.fabian@gmail.com>
2026-07-04 12:39:02 +0100

    remove vectors from some example modules

--- examples/netflow/src/lib.rs
+++ examples/netflow/src/lib.rs
@@ -6,7 +6,7 @@ use raphtory::{
     db::api::view::DynamicGraph,
     python::packages::base_modules::{
         add_raphtory_classes, base_algorithm_module, base_graph_gen_module,
-        base_graph_loader_module, base_vectors_module,
+        base_graph_loader_module,
     },
 };
 use raphtory_graphql::python::pymodule::base_graphql_module;
@@ -25,12 +25,10 @@ fn raphtory_netflow(py: Python<'_>, m: &Bound<PyModule>) -> PyResult<()> {
     let algorithm_module = base_algorithm_module(py)?;
     let graph_loader_module = base_graph_loader_module(py)?;
     let graph_gen_module = base_graph_gen_module(py)?;
-    let vectors_module = base_vectors_module(py)?;
     m.add_submodule(&graphql_module)?;
     m.add_submodule(&algorithm_module)?;
     m.add_submodule(&graph_loader_module)?;
     m.add_submodule(&graph_gen_module)?;
-    m.add_submodule(&vectors_module)?;
 
     //new content
     algorithm_module.add_function(wrap_pyfunction!(py_netflow_one_path_node, m)?)?;
