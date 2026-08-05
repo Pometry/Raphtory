c64fe82c405be856ad5b0f568fcfac7ce6d10ac6
Daniel Lacina <daniellacina8@gmail.com>
2026-08-05 17:08:39 -0500

    fixed PyArgs parsing so it actually contains the passed in BaseConfig attributes

--- raphtory/src/python/config.rs
+++ raphtory/src/python/config.rs
@@ -10,6 +10,13 @@ impl<'a, 'py> FromPyObject<'a, 'py> for PyArgs {
     fn extract(obj: Borrowed<'a, 'py, PyAny>) -> Result<Self, Self::Error> {
         let args: Args = depythonize(&obj.into_bound())?;
         let mut args_from_env = Args::from_env();
+        // sync args_with_env base config with args since args_from_env.update does not do it
+        if let Some(max_node_page_len) = args.max_node_page_len() {
+            args_from_env = args_from_env.with_max_node_page_len(max_node_page_len);
+        } 
+        if let Some(max_edge_page_len) = args.max_edge_page_len() {
+            args_from_env = args_from_env.with_max_edge_page_len(max_edge_page_len); 
+        }
 
         // Read values from env first, then apply args on top.
         args_from_env.update(args);
