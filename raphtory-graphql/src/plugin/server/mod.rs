d7504fb48b252b4420d8cc9820ab3038d33e650a
Lucas Jeub <lucas.jeub@pometry.com>
2026-08-13 14:28:28 +0200

    compile-time plugin registration

--- raphtory-graphql/src/plugin/server/mod.rs
+++ raphtory-graphql/src/plugin/server/mod.rs
@@ -5,13 +5,55 @@
 //!
 //!
 
-use crate::plugin::server::internal::ServerPluginImpl;
-use indexmap::IndexMap;
+use crate::{
+    plugin::server::{internal::ServerPluginImpl, plugin::PluginRegistration},
+    server::ServerError,
+};
+use indexmap::{map::Entry, IndexMap};
 use once_cell::sync::Lazy;
-use std::sync::Mutex;
+use std::{
+    iter::{IntoIterator, Iterator},
+    ops::Deref,
+    sync::Mutex,
+};
+use thiserror::Error;
 
-static EXTENSIONS: Lazy<Mutex<IndexMap<String, Box<dyn ServerPluginImpl>>>> =
-    Lazy::new(|| Mutex::new(IndexMap::new()));
+#[derive(Error, Debug, Clone)]
+pub enum PluginRegistrationError {
+    #[error("Multiple cli plugins registered with name '{0}'")]
+    Multiple(String),
+    #[error("No registered plugin with name '{0}'")]
+    Unknown(String),
+}
+
+static EXTENSIONS: Lazy<IndexMap<String, Box<dyn ServerPluginImpl>>> = Lazy::new(|| {
+    let mut map = IndexMap::new();
+    for registration in inventory::iter::<PluginRegistration> {
+        let plugin = registration.0();
+        let name = plugin.name();
+        match map.entry(name) {
+            Entry::Occupied(entry) => {
+                // unrecoverable
+                panic!("Multiple plugins registered with name {}", entry.key());
+            }
+            Entry::Vacant(entry) => {
+                entry.insert(plugin);
+            }
+        }
+    }
+    map
+});
+
+fn get_plugin(name: &str) -> Result<&dyn ServerPluginImpl, PluginRegistrationError> {
+    EXTENSIONS
+        .get(name)
+        .map(|plugin| plugin.as_ref())
+        .ok_or_else(|| PluginRegistrationError::Unknown(name.to_string()))
+}
+
+fn get_plugins() -> impl Iterator<Item = &'static dyn ServerPluginImpl> {
+    EXTENSIONS.values().map(|ext| ext.as_ref())
+}
 
 pub mod extension;
 pub mod plugin;
--- raphtory-graphql/src/plugin/server/plugin.rs
+++ raphtory-graphql/src/plugin/server/plugin.rs
@@ -1,8 +1,13 @@
-use crate::plugin::server::{extension::ServerExtension, EXTENSIONS};
+use crate::plugin::server::{extension::ServerExtension, internal::ServerPluginImpl};
+use inventory::collect;
+
+/// re-export for use in macro
+#[doc(hidden)]
+pub use inventory::submit;
 
 /// Interface for defining a command-line plugin. This only defines the constructor for the actual
 /// plugin, the actual parsing is defined by `Self::Extension`.
-pub trait ServerPlugin: Send + Sync + 'static {
+pub trait ServerPlugin: Clone + Send + Sync + 'static {
     type Extension: ServerExtension + clap::Args + Clone;
 
     /// Create a new plugin instance
@@ -10,11 +15,22 @@ pub trait ServerPlugin: Send + Sync + 'static {
     fn new(&self) -> Self::Extension;
 }
 
-/// Register a command-line plugin. This injects the command line arguments defined by the plugin
-/// and enables reading of the plugin configuration from the config file
-pub fn register_cli_plugin(plugin: impl ServerPlugin) {
-    EXTENSIONS
-        .lock()
-        .expect("plugin lock poisoned")
-        .insert(plugin.new().name().to_string(), Box::new(plugin));
+/// Type used for plugin registration. Use `register_cli_plugin` instead of constructing this type
+#[doc(hidden)]
+pub struct PluginRegistration(pub fn() -> Box<dyn ServerPluginImpl>);
+
+collect!(PluginRegistration);
+
+#[macro_export]
+/// Register a command line interface plugin at compile time
+///
+/// The input for this plugin is a type that implements the `ServerPlugin` trait. This macro uses the
+/// inventory and does
+/// not run any code. It should be placed outside any function.
+macro_rules! register_cli_plugin {
+    ($plugin:tt) => {
+        $crate::plugin::server::plugin::submit! {
+            $crate::plugin::server::plugin::PluginRegistration(|| Box::new($plugin))
+        }
+    };
 }
