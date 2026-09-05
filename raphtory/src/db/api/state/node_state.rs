718af6484700824aafab13272196929f5cb345f8
miratepuffin <b.a.steer@qmul.ac.uk>
2026-09-05 18:34:21 +0100

    Remove duplicate nodestate issue

--- raphtory/src/db/api/state/node_state.rs
+++ raphtory/src/db/api/state/node_state.rs
@@ -52,12 +52,6 @@ impl<K> From<IndexSet<K, RandomState>> for Index<K> {
     }
 }
 
-impl<K> From<IndexSet<K, ahash::RandomState>> for Index<K> {
-    fn from(value: IndexSet<K, ahash::RandomState>) -> Self {
-        Self::Partial(Arc::new(value))
-    }
-}
-
 impl<K> Default for Index<K> {
     fn default() -> Self {
         Self::Partial(Arc::new(IndexSet::default()))
