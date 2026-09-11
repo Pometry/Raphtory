cb4d1b38a2e1e130800b20e0f32be46364412c37
arienandalibi <arienandalibi2@gmail.com>
2026-09-11 05:38:51 -0400

    Fix bug where a Claim was turned into a Var in lower_case_value

--- raphtory-graphql/src/model/graph/namespace_filtering.rs
+++ raphtory-graphql/src/model/graph/namespace_filtering.rs
@@ -198,7 +198,7 @@ fn lower_case_value(value: &Value) -> Value {
         Value::NDTime(v) => Value::NDTime(v.clone()),
         Value::Decimal(v) => Value::Decimal(v.clone()),
         Value::Var(v) => Value::Var(v.clone()),
-        Value::Claim(v) => Value::Var(v.clone()),
+        Value::Claim(v) => Value::Claim(v.clone()),
     }
 }
 
