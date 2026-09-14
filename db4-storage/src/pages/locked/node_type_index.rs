c559b7c8421af569d8edfba4313d0b4020422d34
Fadhil Abubaker <fadhil.abubaker@gmail.com>
2026-09-14 14:17:02 -0400

    Add WriteLockedNodeTypeIndex

---
+++ db4-storage/src/pages/locked/node_type_index.rs
@@ -0,0 +1,18 @@
+use parking_lot::{ArcRwLockWriteGuard, RawRwLock};
+use crate::{api::node_type_index::NodeTypeIndexOps, error::StorageError, segments::node_type_index::MemNodeTypeIndex};
+use std::sync::Arc;
+
+pub struct WriteLockedNodeTypeIndex<NTI> {
+    head: ArcRwLockWriteGuard<RawRwLock, MemNodeTypeIndex>,
+    index: Arc<NTI>,
+}
+
+impl<NTI: NodeTypeIndexOps> WriteLockedNodeTypeIndex<NTI> {
+    pub fn new(head: ArcRwLockWriteGuard<RawRwLock, MemNodeTypeIndex>, index: Arc<NTI>) -> Self {
+        Self { head, index }
+    }
+
+    pub fn flush(&mut self) -> Result<(), StorageError> {
+        Ok(())
+    }
+}
